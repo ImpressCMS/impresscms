6a68126b791fab2a8f80c9bb6d22c217213a300b
Marc-André Lanciault <marcan@impresscms.org>
2007-12-11 03:23:00 +0000

    Documenting the debug functions

--- htdocs/include/debug_functions.php
+++ htdocs/include/debug_functions.php
@@ -1,30 +1,19 @@
 <?php
-// $Id: common.php,v 1.2 2007/06/26 18:51:34 marcan Exp $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
+/**
+ * Debugging functions
+ *
+ * @license GNU
+ * @author marcan <marcan@smartfactory.ca>
+ * @version $Id: smartdbupdater.php,v 1.2 2007/10/26 11:50:47 marcan Exp $
+ * @link http://impresscms.org ImpressCMS
+ */
 
+/**
+ * Output a line of debug
+ *
+ * @param string $msg text to be outputed as a debug line
+ * @param bool $exit if TRUE the script will end
+ */
 function im_debug($msg, $exit=false)
 {
 	echo "<div style='padding: 5px; color: red; font-weight: bold'>debug :: $msg</div>";
@@ -33,15 +22,20 @@ function im_debug($msg, $exit=false)
 	}
 }
 
-function im_debug_vardump($text)
+/**
+ * Output a dump of a variable
+ *
+ * @param string $var variable which will be dumped
+ */
+function im_debug_vardump($var)
 {
 	if (class_exists('MyTextSanitizer')) {
 		$myts = MyTextSanitizer::getInstance();
-		im_debug($myts->displayTarea(var_export($text, true)));
+		im_debug($myts->displayTarea(var_export($var, true)));
 	} else {
-		$text = var_export($text, true);
-		$text = preg_replace("/(\015\012)|(\015)|(\012)/","<br />",$text);
-		im_debug($text);
+		$var = var_export($var, true);
+		$var = preg_replace("/(\015\012)|(\015)|(\012)/","<br />",$var);
+		im_debug($var);
 	}
 }
 ?>
