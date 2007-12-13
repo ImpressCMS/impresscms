44519c164d4090ba80478e75369d4e79035ab5f0
Marc-André Lanciault <marcan@impresscms.org>
2007-12-13 01:18:18 +0000

    changing file header

--- htdocs/include/common.php
+++ htdocs/include/common.php
@@ -1,33 +1,38 @@
 <?php
-// $Id: common.php 1135 2007-11-01 04:11:06Z dugris $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
+/** ---------------------------------------------------------------------------
+ *  $Id: mainfile.dist.php 506 2006-05-26 23:10:37Z skalpa $
+ *  ---------------------------------------------------------------------------
+ *
+ *  Project: ImpressCMS <http://www.impresscms.org/>
+ *
+ *  ImpressCMS is derived from XOOPS 2.0.17.1  <http://www.xoops.org/> Copyright (c) 2000-2007 XOOPS.org
+ *  Subsequent changes and additions: Copyright (c) 2007 ImpressCMS
+ *
+ *  ---------------------------------------------------------------------------
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  You may not change or alter any portion of this comment or credits
+ *  of supporting developers from this source code or any supporting
+ *  source code which is considered copyrighted (c) material of the
+ *  original comment or credit authors.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *  ---------------------------------------------------------------------------
+ */
 
 defined("XOOPS_MAINFILE_INCLUDED") or die();
 
-	set_magic_quotes_runtime(0);
+set_magic_quotes_runtime(0);
 
 /**
  * Extremely reduced kernel class
