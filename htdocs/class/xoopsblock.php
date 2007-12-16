b420fe31b89163f4342dcc024e6a28b18a5ed9b0
young-pee <young-pee>
2007-12-16 02:45:20 +0000

    Removed a debuging line.

--- htdocs/class/xoopsblock.php
+++ htdocs/class/xoopsblock.php
@@ -408,7 +408,6 @@ class XoopsBlock extends XoopsObject
             $where_query .= " AND visible='".intval($visible)."'";
         }
         $where_query .= " ORDER BY $orderby";
-        echo $where_query."<br />";
         switch ($rettype) {
         case "object":
             $sql = "SELECT * FROM ".$db->prefix("newblocks")."".$where_query;
