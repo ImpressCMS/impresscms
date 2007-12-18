f9c16f561b0b624dcd423e103d9681fc2f202ef4
xoops-magazine <xoops-magazine>
2007-12-18 19:22:40 +0000

    Line break away in the privacy policy.

--- htdocs/install/language/english/install2.php
+++ htdocs/install/language/english/install2.php
@@ -9,39 +9,18 @@ define("_INSTALL_ANONUSERSD","Anonymous Users Group");
 
 define("_INSTALL_ANON","Anonymous");
 
-define("_INSTALL_DISCLMR", "While the administrators and moderators of this site will attempt to remove
-or edit any generally objectionable material as quickly as possible, it is
-impossible to review every message. Therefore you acknowledge that all posts
-made to this site express the views and opinions of the author and not the
-administrators, moderators or webmaster (except for posts by these people)
-and hence will not be held liable. 
+define("_INSTALL_DISCLMR", "While the administrators and moderators of this site will attempt to remove or edit any generally objectionable material as quickly as possible, it is impossible to review every message. Therefore you acknowledge that all posts made to this site express the views and opinions of the author and not the administrators, moderators or webmaster (except for posts by these people) and hence will not be held liable. 
 
-You agree not to post any abusive, obscene, vulgar, slanderous, hateful,
-threatening, sexually-orientated or any other material that may violate any
-applicable laws. Doing so may lead to you being immediately and permanently
-banned (and your service provider being informed). The IP address of all
-posts is recorded to aid in enforcing these conditions. Creating multiple
-accounts for a single user is not allowed. You agree that the webmaster,
-administrator and moderators of this site have the right to remove, edit,
-move or close any topic at any time should they see fit. As a user you agree
-to any information you have entered above being stored in a database. While
-this information will not be disclosed to any third party without your
-consent the webmaster, administrator and moderators cannot be held
-responsible for any hacking attempt that may lead to the data being
-compromised. 
+You agree not to post any abusive, obscene, vulgar, slanderous, hateful, threatening, sexually-orientated or any other material that may violate any applicable laws. Doing so may lead to you being immediately and permanently banned (and your service provider being informed). The IP address of all posts is recorded to aid in enforcing these conditions. Creating multiple accounts for a single user is not allowed. You agree that the webmaster, administrator and moderators of this site have the right to remove, edit, move or close any topic at any time should they see fit. As a user you agree to any information you have entered above being stored in a database. While this information will not be disclosed to any third party without your consent the webmaster, administrator and moderators cannot be held responsible for any hacking attempt that may lead to the data being compromised. 
 
-This site system uses cookies to store information on your local computer.
-These cookies do not contain any of the information you have entered above,
-they serve only to improve your viewing pleasure. The email address is used
-only for confirming your registration details and password (and for sending
-new passwords should you forget your current one). 
+This site system uses cookies to store information on your local computer. These cookies do not contain any of the information you have entered above, they serve only to improve your viewing pleasure. The email address is used only for confirming your registration details and password (and for sending new passwords should you forget your current one). 
 
 By clicking Register below you agree to be bound by these conditions.");
 
 define("_INSTALL_PRIVPOLICY", "<p>
-This privacy policy sets out how “[business name]” uses and protects any information that you give “[business name]” when you use this website.<br />
-“[website name]” is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.<br /><br />
-“[website name]” may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.<br /><br />
+This privacy policy sets out how ï¿½[business name]ï¿½ uses and protects any information that you give ï¿½[business name]ï¿½ when you use this website.<br />
+ï¿½[website name]ï¿½ is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.<br /><br />
+ï¿½[website name]ï¿½ may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.<br /><br />
 This policy is effective from [date].<br /><br />
 
 <b>What we collect</b><br /><br />
