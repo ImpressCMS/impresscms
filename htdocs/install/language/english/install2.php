e031e69731299138ad4a26fd94612971a80f4f5b
xoops-magazine <xoops-magazine>
2007-12-19 09:20:42 +0000

    change the HTML Entries on install2.php

--- htdocs/install/language/english/install2.php
+++ htdocs/install/language/english/install2.php
@@ -18,9 +18,9 @@ This site system uses cookies to store information on your local computer. These
 By clicking Register below you agree to be bound by these conditions.");
 
 define("_INSTALL_PRIVPOLICY", "<p>
-This privacy policy sets out how �[business name]� uses and protects any information that you give �[business name]� when you use this website.<br />
-�[website name]� is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.<br /><br />
-�[website name]� may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.<br /><br />
+This privacy policy sets out how '[business name]' uses and protects any information that you give '[business name]' when you use this website.<br />
+'[website name]' is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.<br /><br />
+'[website name]' may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.<br /><br />
 This policy is effective from [date].<br /><br />
 
 <b>What we collect</b><br /><br />
