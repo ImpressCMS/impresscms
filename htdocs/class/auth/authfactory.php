b8d0a2c1f6ad32c05cd38af787adc5531ec43f7b
young-pee <young-pee>
2007-12-20 05:52:30 +0000

    Improved auth class folder organization.

--- htdocs/class/auth/authfactory.php
+++ htdocs/class/auth/authfactory.php
@@ -32,7 +32,7 @@
  * @author	    Pierre-Eric MENUET	<pemphp@free.fr>
  * @copyright	copyright (c) 2000-2005 XOOPS.org
  */
-class AuthFactory{
+class XoopsAuthFactory{
 
 	/**
 	 * Get a reference to the only instance of authentication class
