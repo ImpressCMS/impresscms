2cd2d3cb90b4d52bb4e4ac41a5942454bb79bc5e
kurak_bu <kurak.bu@gmail.com>
2007-12-22 22:54:08 +0000

    small modifications after some considering with David

--- htdocs/install/language/english/finish.php
+++ htdocs/install/language/english/finish.php
@@ -4,6 +4,10 @@ $content .=
 "<h3>Your site</h3>
 <p>You can now access the <a href='../index.php'>home page of your site</a>.</p>
 <h3>Support</h3>
-<p>Visit <a href='http://www.impresscms.org/' target='_blank'>ImpressCMS.org</a></p>
+<ul class=\"diags\">
+<li class=\"success\"><a href='http://www.impresscms.org/' target='_blank'>www.impresscms.org</a> - Project Homepage</p></li>" .
+"<li class=\"success\"><a href='http://www.community.impresscms.org/' target='_blank'>community.impresscms.org</a> - for Support</li>" .
+"<li class=\"success\"><a href='http://www.addons.impresscms.org/' target='_blank'>addons.impresscms.org</a> - for Modules & Themes</li>" .
+"<li class=\"success\"><a href='http://www.wiki.impresscms.org/' target='_blank'>wiki.impresscms.org</a> - for Community Documentation</li>
 ";
 ?>
