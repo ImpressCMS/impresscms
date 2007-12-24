81a2be91ee721e7aab698ae57cb303f710a0ce0e
Marc-André Lanciault <marcan@impresscms.org>
2007-12-24 17:19:57 +0000

    Cosmetic fix

--- htdocs/include/debug_functions.php
+++ htdocs/include/debug_functions.php
@@ -4,7 +4,7 @@
  *
  * @license GNU
  * @author marcan <marcan@smartfactory.ca>
- * @version $Id: smartdbupdater.php,v 1.2 2007/10/26 11:50:47 marcan Exp $
+ * @version		$Id: page_pathsettings.php 345 2007-12-24 16:17:22Z malanciault $
  * @link http://impresscms.org ImpressCMS
  */
 
