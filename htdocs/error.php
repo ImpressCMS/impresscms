d3e03b83635454c7001ae3724a53e76010ae0f30
Marc-André Lanciault <marcan@impresscms.org>
2008-01-02 15:07:57 +0000

    Improving the error.php feature

--- htdocs/error.php
+++ htdocs/error.php
@@ -1,26 +1,42 @@
 <?php
+/**
+* Page handling HTTP errors
+*
+* This page handles some HTTP errors that may occur on a site. The htaccess file needs to be
+* edited as well. An example of such htaccess can be found in htaccess.txt.
+*
+* @copyright	The ImpressCMS Project http://www.impresscms.org/
+* @license		http://www.fsf.org/copyleft/gpl.html GNU General Public License (GPL)
+* @package		core
+* @since		1.0
+* @author		young-pee <nekro@impresscms.org>
+* @author		malanciault <marcan@impresscms.org)
+* @version		$Id$
+*/
 
-	$xoopsOption['pagetype'] = 'error';
-	include_once ('mainfile.php');
+$xoopsOption['pagetype'] = 'error';
+include_once ('mainfile.php');
 
-	$errorn = isset( $_GET['errorn'] ) ? $_GET['errorn'] : 0;
+$e = isset( $_GET['e'] ) ? $_GET['e'] : 0;
 
-	// If there is not any error defined... it redirects to the home page.
- 	if( $errorn == 0 )
- 		header('Location: '.XOOPS_URL);
+// If there is not any error defined... it redirects to the home page.
+if( $e == 0 )
+	header('Location: '.XOOPS_URL);
 
-	$xoopsOption['template_main'] = 'system_error.html';
-	require_once XOOPS_ROOT_PATH.'/header.php';
+$xoopsOption['template_main'] = 'system_error.html';
+require_once XOOPS_ROOT_PATH.'/header.php';
 
-	$siteName = $xoopsConfig['sitename'];
-	$xoopsTpl->assign('lang_error_desc', sprintf(constant( '_ERR_'.$errorn.'_DESC' ), $siteName));
-	$xoopsTpl->assign('lang_error_title', constant( '_ERR_'.$errorn.'_TITLE' ));
-	$xoopsTpl->assign('xoops_pagetitle', constant( '_ERR_'.$errorn.'_PAGE_TITLE' ));
-	$xoopsTpl->assign('lang_found_contact', sprintf(_ERR_CONTACT, $xoopsConfig['adminmail']));
-	$xoopsTpl->assign('lang_search', _ERR_SEARCH);
-	$xoopsTpl->assign('lang_advanced_search', _ERR_ADVANCED_SEARCH);
-	$xoopsTpl->assign('lang_start_again', _ERR_START_AGAIN);
-	$xoopsTpl->assign('lang_search_our_site', _ERR_SEARCH_OUR_SITE);
+$siteName = $xoopsConfig['sitename'];
+$lang_error_no = sprintf(_ERR_NO, $e);
+$xoopsTpl->assign('lang_error_no', $lang_error_no);
+$xoopsTpl->assign('lang_error_desc', sprintf(constant( '_ERR_'.$e.'_DESC' ), $siteName));
+$xoopsTpl->assign('lang_error_title', $lang_error_no . ' ' . constant( '_ERR_'.$e.'_TITLE' ));
+$xoopsTpl->assign('xoops_pagetitle', $lang_error_no . ' ' . constant( '_ERR_'.$e.'_TITLE' ));
+$xoopsTpl->assign('lang_found_contact', sprintf(_ERR_CONTACT, $xoopsConfig['adminmail']));
+$xoopsTpl->assign('lang_search', _ERR_SEARCH);
+$xoopsTpl->assign('lang_advanced_search', _ERR_ADVANCED_SEARCH);
+$xoopsTpl->assign('lang_start_again', _ERR_START_AGAIN);
+$xoopsTpl->assign('lang_search_our_site', _ERR_SEARCH_OUR_SITE);
 
-	require_once XOOPS_ROOT_PATH.'/footer.php';
+require_once XOOPS_ROOT_PATH.'/footer.php';
 ?>
