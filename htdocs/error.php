89a55c2ad5461e2aa6886b3a5ac350ec0da92419
Marc-André Lanciault <marcan@impresscms.org>
2008-01-02 22:21:41 +0000

    Changing the link to the GPL text

--- htdocs/error.php
+++ htdocs/error.php
@@ -6,7 +6,7 @@
 * edited as well. An example of such htaccess can be found in htaccess.txt.
 *
 * @copyright	The ImpressCMS Project http://www.impresscms.org/
-* @license		http://www.fsf.org/copyleft/gpl.html GNU General Public License (GPL)
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @package		core
 * @since		1.0
 * @author		young-pee <nekro@impresscms.org>
