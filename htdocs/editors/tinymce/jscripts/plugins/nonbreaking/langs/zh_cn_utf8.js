dc9493809a032e461b05380771da54db3bb2a29b
young-pee <young-pee>
2008-02-22 04:07:56 +0000

    Merged the impresscms_145603-wysiwyg_editors task branch into the trunk.
    Task: #145603

---
+++ htdocs/editors/tinymce/jscripts/plugins/nonbreaking/langs/zh_cn_utf8.js
@@ -0,0 +1,6 @@
+// zh-cn lang variables
+// XOOPS::xoopseditor::tinymce
+
+tinyMCE.addToLang('nonbreaking',{
+desc : '插入空格符'
+});
---
+++ htdocs/editors/tinymce/jscripts/plugins/visualchars/langs/zh_cn.js
@@ -0,0 +1,6 @@
+// zh-cn lang variables
+// XOOPS::xoopseditor::tinymce
+
+tinyMCE.addToLang('visualchars',{
+desc : '控制符显示开关'
+});
