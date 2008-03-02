424c09fc1a6eb71af8d1bb03e17491bff3d5e021
Marc-André Lanciault <marcan@impresscms.org>
2008-03-02 17:01:30 +0000

    This is just to comment on my previous commit (click on the button to soon ;-)). So, revision 1086 was to add a new module in admion: ImpressCMS version checker.By clicking on this module on Control Panel, the system will check if he is running the latest release of ImpressCMS. If not, it will propose him a link to get more information. What is left to do: change the image of the module, syntax check of the language constants, and proper testing.

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -1,5 +1,7 @@
 <?php
 // $Id: version.php 1042 2007-09-09 16:55:33Z dugris $
 define("XOOPS_VERSION",'ImpressCMS 1.0 "Janus" RC 2');
+
+// Build number. This is used in the version checker
 define("ICMS_BUILD",4);
 ?>
