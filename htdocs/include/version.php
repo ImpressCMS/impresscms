c4c5c9fb54b649581fcb091b5f2e730ab6546b1d
Marc-André Lanciault <marcan@impresscms.org>
2008-03-26 14:35:16 +0000

    CHanging the version to ImpressCMS 1.1 Alpha

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
 * @version		$Id: version.php 897 2008-02-15 19:53:31Z malanciault $
 */
 
-define("ICMS_VERSION_NAME",'ImpressCMS 1.0 "Janus" Final');
+define("ICMS_VERSION_NAME",'ImpressCMS 1.1 Alpha');
 
 // For backward compatibility with XOOPS
 define("XOOPS_VERSION", ICMS_VERSION_NAME);
@@ -23,12 +23,12 @@ define("XOOPS_VERSION", ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define("ICMS_VERSION_STATUS", 10);
+define("ICMS_VERSION_STATUS", 1);
 
 /*
  * Build number
  * 
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-define("ICMS_VERSION_BUILD", 6);
+define("ICMS_VERSION_BUILD", 7);
 ?>
