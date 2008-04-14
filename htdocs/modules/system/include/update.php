d6942e70d50d8599738e9746a8bb07f78de18961
Sina Asghari <stranger@impresscms.org>
2008-04-14 07:49:23 +0000

    upgrading marcan's update script to insert configs of editors and ext_date_function .

--- htdocs/modules/system/include/update.php
+++ htdocs/modules/system/include/update.php
@@ -55,6 +55,12 @@ function xoops_module_update_system(&$module) {
 	    $icmsDatabaseUpdater->updateTable($table);	
 	    unset($table);	
 	    
+	    // create extended date function's config option
+	    $icmsDatabaseUpdater->insertConfig(XOOPS_CONF, 'use_ext_date', '_MD_AM_EXT_DATE', 0, '_MD_AM_EXT_DATEDSC', 'yesno', 'int', 8);
+	    // create editors config option
+	    $icmsDatabaseUpdater->insertConfig(XOOPS_CONF, 'editor_default', '_MD_AM_EDITOR_DEFAULT', 'default', '_MD_AM_EDITOR_DEFAULT_DESC', 'editor', 'text', 13);
+	    $icmsDatabaseUpdater->insertConfig(XOOPS_CONF, 'editor_enabled_list', '_MD_AM_EDITOR_ENABLED_LIST', ".addslashes(serialize(array('default'))).", '_MD_AM_EDITOR_ENABLED_LIST_DESC', 'editor_multi', 'array', 13);
+
 	    // create 2 new user config options
 	    $icmsDatabaseUpdater->insertConfig(XOOPS_CONF_USER, 'welcome_msg', '_MD_AM_WELCOMEMSG', 0, '_MD_AM_WELCOMEMSGDSC', 'yesno', 'int', 4);
 
