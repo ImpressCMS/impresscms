ce73c43680dd4412c394555e2be4c8cf389e8b8a
Marc-André Lanciault <marcan@impresscms.org>
2008-04-19 16:02:25 +0000

    Including a library for Protector for the module to load automatically without having to hack mainfile.php. Perhaps this should be put in a "preload" feature, which would mean we would have to separate libraries from preload features, which makes sense. More thoughts on the forum please. And please test this to be sure it,s working well, and then we'll decide if fe separate this in a new preload feature.

---
+++ htdocs/libraries/protector/icms.library.protector.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Initiating Protector module
+*
+* This file is responsible for initiating the Protector module so no hacks on mainfile are required
+*
+* @copyright	The ImpressCMS Project http://www.impresscms.org/
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package		libraries
+* @since		1.1
+* @author		marcan <marcan@impresscms.org>
+* @version		$Id$
+*/
+
+function icmsLibraryProtector_StartCoreBoot() {
+	$filename = ICMS_TRUST_PATH.'/modules/protector/include/precheck.inc.php';
+	if (file_exists($filename)) {
+		include $filename;
+	}
+}
+
+function icmsLibraryProtector_FinishCoreBoot() {
+	$filename = ICMS_TRUST_PATH.'/modules/protector/include/postcheck.inc.php';
+	if (file_exists($filename)) { 
+		include $filename;
+	}
+}
+
+?>
