af5ce7ba7c0745214136f616ccf1c59a30c242b5
Sina Asghari <stranger@impresscms.org>
2008-04-25 18:34:01 +0000

    removed ajaxfilemanager as requested here: http://community.impresscms.org/modules/newbb/viewtopic.php?post_id=15361#forumpost15361

--- htdocs/editors/tinymce/jscripts/plugins/ajaxfilemanager/inc/data.php
+++
@@ -1,8 +0,0 @@
-<pre>Array
-(
-    [currentFolderPath] => ../uploaded/Image/New/
-    [new_folder] => New Folder Really Long Name
-)
-</pre>
-
-08/Mar/2008 12:38:43
--- htdocs/editors/tinymce/jscripts/plugins/ajaxfilemanager/session/gc_log.ajax.php
+++
@@ -1,2 +0,0 @@
-<?php die(); ?>
-gc start at 08/Apr/2008 19:32:53
