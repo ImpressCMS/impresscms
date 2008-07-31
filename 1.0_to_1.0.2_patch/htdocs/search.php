e388cf1b554a9e9893f454318f802c20b019cebe
Marc-André Lanciault <marcan@impresscms.org>
2008-07-31 13:52:20 +0000

    New patch folder from 1.0 or 1.0.1 to 1.0.2

---
+++ 1.0_to_1.0.2_patch/htdocs/search.php
@@ -0,0 +1,275 @@
+<?php
+// $Id: search.php 506 2006-05-26 23:10:37Z skalpa $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+$xoopsOption['pagetype'] = "search";
+
+include 'mainfile.php';
+$config_handler =& xoops_gethandler('config');
+$xoopsConfigSearch =& $config_handler->getConfigsByCat(XOOPS_CONF_SEARCH);
+
+if ($xoopsConfigSearch['enable_search'] != 1) {
+    header('Location: '.XOOPS_URL.'/index.php');
+    exit();
+}
+$action = 'search';
+if(!empty($_GET['action'])) {$action = trim(StopXSS($_GET['action']));}
+elseif(!empty($_POST['action'])) {$action = trim(StopXSS($_POST['action']));}
+$query = '';
+if(!empty($_GET['query'])) {$query = StopXSS($_GET['query']);}
+elseif(!empty($_POST['query'])) {$query = StopXSS($_POST['query']);}
+$andor = 'AND';
+if(!empty($_GET['andor'])) {$andor = StopXSS($_GET['andor']);}
+elseif(!empty($_POST['andor'])) {$andor = StopXSS($_POST['andor']);}
+$mid = $uid = $start = 0;
+if(!empty($_GET['mid'])) {$mid = intval($_GET['mid']);}
+elseif(!empty($_POST['mid'])) {$mid = intval($_POST['mid']);}
+if(!empty($_GET['uid'])) {$uid = intval($_GET['uid']);}
+elseif(!empty($_POST['uid'])) {$uid = intval($_POST['uid']);}
+if(!empty($_GET['start'])) {$start = intval($_GET['start']);}
+elseif(!empty($_POST['start'])) {$start = intval($_POST['start']);}
+
+$queries = array();
+
+if ($action == "results") {
+    if ($query == "") {
+         redirect_header("search.php",1,_SR_PLZENTER);
+    }
+} elseif ($action == "showall") {
+    if ($query == "" || empty($mid)) {
+        redirect_header("search.php",1,_SR_PLZENTER);
+    }
+} elseif ($action == "showallbyuser") {
+    if (empty($mid) || empty($uid)) {
+        redirect_header("search.php",1,_SR_PLZENTER);
+    }
+}
+
+$groups = is_object($xoopsUser) ? $xoopsUser -> getGroups() : XOOPS_GROUP_ANONYMOUS;
+$gperm_handler = & xoops_gethandler( 'groupperm' );
+$available_modules = $gperm_handler->getItemIds('module_read', $groups);
+
+if ($action == 'search') {
+    include XOOPS_ROOT_PATH.'/header.php';
+    include 'include/searchform.php';
+    $search_form->display();
+    $xoopsTpl->assign('xoops_pagetitle', _SEARCH);
+    include XOOPS_ROOT_PATH.'/footer.php';
+    exit();
+}
+
+if ( $andor != "OR" && $andor != "exact" && $andor != "AND" ) {
+    $andor = "AND";
+}
+
+$myts =& MyTextSanitizer::getInstance();
+if ($action != 'showallbyuser') {
+    if ( $andor != "exact" ) {
+        $ignored_queries = array(); // holds kewords that are shorter than allowed minmum length
+        $temp_queries = preg_split('/[\s,]+/', $query);
+        foreach ($temp_queries as $q) {
+            $q = trim($q);
+            if (strlen($q) >= $xoopsConfigSearch['keyword_min']) {
+                $queries[] = $myts->addSlashes($q);
+            } else {
+                $ignored_queries[] = $myts->addSlashes($q);
+            }
+        }
+        if (count($queries) == 0) {
+            redirect_header('search.php', 2, sprintf(_SR_KEYTOOSHORT, $xoopsConfigSearch['keyword_min']));
+        }
+    } else {
+        $query = trim($query);
+        if (strlen($query) < $xoopsConfigSearch['keyword_min']) {
+            redirect_header('search.php', 2, sprintf(_SR_KEYTOOSHORT, $xoopsConfigSearch['keyword_min']));
+        }
+        $queries = array($myts->addSlashes($query));
+    }
+}
+switch ($action) {
+    case "results":
+    $module_handler =& xoops_gethandler('module');
+    $criteria = new CriteriaCompo(new Criteria('hassearch', 1));
+    $criteria->add(new Criteria('isactive', 1));
+    $criteria->add(new Criteria('mid', "(".implode(',', $available_modules).")", 'IN'));
+    $modules =& $module_handler->getObjects($criteria, true);
+    $mids = isset($_REQUEST['mids']) ? $_REQUEST['mids'] : array();
+    if (empty($mids) || !is_array($mids)) {
+        unset($mids);
+        $mids = array_keys($modules);
+    }
+    include XOOPS_ROOT_PATH."/header.php";
+    echo "<h3>"._SR_SEARCHRESULTS."</h3>\n";
+    echo _SR_KEYWORDS.':';
+    if ($andor != 'exact') {
+        foreach ($queries as $q) {
+            echo ' <b>'.htmlspecialchars(stripslashes($q)).'</b>';
+        }
+        if (!empty($ignored_queries)) {
+            echo '<br />';
+            printf(_SR_IGNOREDWORDS, $xoopsConfigSearch['keyword_min']);
+            foreach ($ignored_queries as $q) {
+                echo ' <b>'.htmlspecialchars(stripslashes($q)).'</b>';
+            }
+        }
+    } else {
+        echo ' "<b>'.htmlspecialchars(stripslashes($queries[0])).'</b>"';
+    }
+    echo '<br />';
+    foreach ($mids as $mid) {
+        $mid = intval($mid);
+        if ( in_array($mid, $available_modules) ) {
+            $module =& $modules[$mid];
+            $results =& $module->search($queries, $andor, 5, 0);
+            echo "<h4>".$myts->makeTboxData4Show($module->getVar('name'))."</h4>";
+            $count = count($results);
+            if (!is_array($results) || $count == 0) {
+                echo "<p>"._SR_NOMATCH."</p>";
+            } else {
+                for ($i = 0; $i < $count; $i++) {
+                    if (isset($results[$i]['image']) && $results[$i]['image'] != "") {
+                        echo "<img src='modules/".$module->getVar('dirname')."/".$results[$i]['image']."' alt='".$myts->makeTboxData4Show($module->getVar('name'))."' />&nbsp;";
+                    } else {
+                        echo "<img src='images/icons/posticon2.gif' alt='".$myts->makeTboxData4Show($module->getVar('name'))."' width='26' height='26' />&nbsp;";
+                    }
+                    if (!preg_match("/^http[s]*:\/\//i", $results[$i]['link'])) {
+                        $results[$i]['link'] = "modules/".$module->getVar('dirname')."/".$results[$i]['link'];
+                    }
+                    echo "<b><a href='".$results[$i]['link']."'>".$myts->makeTboxData4Show($results[$i]['title'])."</a></b><br />\n";
+                    echo "<small>";
+                    $results[$i]['uid'] = @intval($results[$i]['uid']);
+                    if ( !empty($results[$i]['uid']) ) {
+                        $uname = XoopsUser::getUnameFromId($results[$i]['uid']);
+                        echo "&nbsp;&nbsp;<a href='".XOOPS_URL."/userinfo.php?uid=".$results[$i]['uid']."'>".$uname."</a>\n";
+                    }
+                    echo !empty($results[$i]['time']) ? " (". formatTimestamp(intval($results[$i]['time'])).")" : "";
+                    echo "</small><br />\n";
+                }
+                if ( $count >= 5 ) {
+                    $search_url = XOOPS_URL.'/search.php?query='.urlencode(stripslashes(implode(' ', $queries)));
+                    $search_url .= "&mid=$mid&action=showall&andor=$andor";
+                    echo '<br /><a href="'.htmlspecialchars($search_url).'">'._SR_SHOWALLR.'</a></p>';
+                }
+            }
+        }
+        unset($results);
+        unset($module);
+    }
+    include "include/searchform.php";
+    $search_form->display();
+    $xoopsTpl->assign('xoops_pagetitle', _SR_SEARCHRESULTS); 
+    break;
+    case "showall":
+    case 'showallbyuser':
+    include XOOPS_ROOT_PATH."/header.php";
+    $module_handler =& xoops_gethandler('module');
+    $module =& $module_handler->get($mid);
+    $results =& $module->search($queries, $andor, 20, $start, $uid);
+    $count = count($results);
+    if (is_array($results) && $count > 0) {
+        $next_results =& $module->search($queries, $andor, 1, $start + 20, $uid);
+        $next_count = count($next_results);
+        $has_next = false;
+        if (is_array($next_results) && $next_count == 1) {
+            $has_next = true;
+        }
+        $xoopsTpl->assign('xoops_pagetitle', _SR_SEARCHRESULTS);
+        echo "<h4>"._SR_SEARCHRESULTS."</h4>\n";
+        if ($action == 'showall') {
+            echo _SR_KEYWORDS.':';
+            if ($andor != 'exact') {
+                foreach ($queries as $q) {
+                    echo ' <b>'.htmlspecialchars(stripslashes($q)).'</b>';
+                }
+            } else {
+                echo ' "<b>'.htmlspecialchars(stripslashes($queries[0])).'</b>"';
+            }
+            echo '<br />';
+        }
+        //    printf(_SR_FOUND,$count);
+        //    echo "<br />";
+        printf(_SR_SHOWING, $start+1, $start + $count);
+        echo "<h5>".$myts->makeTboxData4Show($module->getVar('name'))."</h5>";
+        for ($i = 0; $i < $count; $i++) {
+            if (isset($results[$i]['image']) && $results[$i]['image'] != '') {
+                echo "<img src='modules/".$module->getVar('dirname')."/".$results[$i]['image']."' alt='".$myts->makeTboxData4Show($module->getVar('name'))."' />&nbsp;";
+            } else {
+                echo "<img src='images/icons/posticon2.gif' alt='".$myts->makeTboxData4Show($module->name())."' width='26' height='26' />&nbsp;";
+            }
+            if (!preg_match("/^http[s]*:\/\//i", $results[$i]['link'])) {
+                $results[$i]['link'] = "modules/".$module->getVar('dirname')."/".$results[$i]['link'];
+            }
+            echo "<b><a href='".$results[$i]['link']."'>".$myts->makeTboxData4Show($results[$i]['title'])."</a></b><br />\n";
+            echo "<small>";
+            $results[$i]['uid'] = @intval($results[$i]['uid']);
+            if ( !empty($results[$i]['uid']) ) {
+                $uname = XoopsUser::getUnameFromId($results[$i]['uid']);
+                echo "&nbsp;&nbsp;<a href='".XOOPS_URL."/userinfo.php?uid=".$results[$i]['uid']."'>".$uname."</a>\n";
+            }
+            echo !empty($results[$i]['time']) ? " (". formatTimestamp(intval($results[$i]['time'])).")" : "";
+            echo "</small><br />\n";
+        }
+        echo '
+        <table>
+          <tr>
+        ';
+        $search_url = XOOPS_URL.'/search.php?query='.urlencode(stripslashes(implode(' ', $queries)));
+        $search_url .= "&mid=$mid&action=$action&andor=$andor";
+        if ($action=='showallbyuser') {
+            $search_url .= "&uid=$uid";
+        }
+        if ( $start > 0 ) {
+            $prev = $start - 20;
+            echo '<td align="left">
+            ';
+            $search_url_prev = $search_url."&start=$prev";
+            echo '<a href="'.htmlspecialchars($search_url_prev).'">'._SR_PREVIOUS.'</a></td>
+            ';
+        }
+        echo '<td>&nbsp;&nbsp;</td>
+        ';
+        if (false != $has_next) {
+            $next = $start + 20;
+            $search_url_next = $search_url."&start=$next";
+            echo '<td align="right"><a href="'.htmlspecialchars($search_url_next).'">'._SR_NEXT.'</a></td>
+            ';
+        }
+        echo '
+          </tr>
+        </table>
+        <p>
+        ';
+    } else {
+        echo '<p>'._SR_NOMATCH.'</p>';
+    }
+    include "include/searchform.php";
+    $search_form->display();
+    echo '</p>
+    ';
+    break;
+}
+include XOOPS_ROOT_PATH."/footer.php";
+?>
---
+++ 1.0_to_1.0.2_patch/htdocs/user.php
@@ -0,0 +1,205 @@
+<?php
+// $Id: user.php 1083 2007-10-16 16:42:51Z phppp $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+$xoopsOption['pagetype'] = 'user';
+include 'mainfile.php';
+
+$op = (isset($_GET['op']))?trim(StopXSS($_GET['op'])):((isset($_POST['op']))?trim(StopXSS($_POST['op'])):'main');
+
+if ($op == 'main') {
+    if ( !$xoopsUser ) {
+        $xoopsOption['template_main'] = 'system_userform.html';
+        include 'header.php';
+        $xoopsTpl->assign('lang_login', _LOGIN);
+        $xoopsTpl->assign('lang_username', _USERNAME);
+        if (isset($_COOKIE[$xoopsConfig['usercookie']])) {
+            $xoopsTpl->assign('usercookie', $_COOKIE[$xoopsConfig['usercookie']]);
+        }
+        if (isset($_GET['xoops_redirect'])) {
+	        $redirect = htmlspecialchars(trim($_GET['xoops_redirect']), ENT_QUOTES);
+	        $isExternal = false;
+	        if ($pos = strpos( $redirect, '://' )) {
+	            $xoopsLocation = substr( XOOPS_URL, strpos( XOOPS_URL, '://' ) + 3 );
+	             if ( substr($redirect, $pos + 3, strlen($xoopsLocation)) != $xoopsLocation)  {
+					$redirect = XOOPS_URL;
+		         }elseif(substr($redirect, $pos + 3, strlen($xoopsLocation)+1) == $xoopsLocation.'.') {
+		            $redirect = XOOPS_URL;
+		         }
+	        }
+        	$xoopsTpl->assign('redirect_page', $redirect);
+        }
+        $xoopsTpl->assign('lang_password', _PASSWORD);
+        $xoopsTpl->assign('lang_notregister', _US_NOTREGISTERED);
+        $xoopsTpl->assign('lang_lostpassword', _US_LOSTPASSWORD);
+        $xoopsTpl->assign('lang_noproblem', _US_NOPROBLEM);
+        $xoopsTpl->assign('lang_youremail', _US_YOUREMAIL);
+        $xoopsTpl->assign('lang_sendpassword', _US_SENDPASSWORD);
+        $xoopsTpl->assign('lang_rememberme', _US_REMEMBERME);
+        $xoopsTpl->assign('mailpasswd_token', $GLOBALS['xoopsSecurity']->createToken());
+        $config_handler =& xoops_gethandler('config');
+		$xoopsConfigUser =& $config_handler->getConfigsByCat(XOOPS_CONF_USER);
+
+		if ($xoopsConfigUser['allow_register'] == 1) {
+			$xoopsTpl->assign('allow_registration', $xoopsConfigUser['allow_register']);
+		}
+
+		if ($xoopsConfigUser['remember_me'] == 1) {
+			$xoopsTpl->assign('rememberme', $xoopsConfigUser['remember_me']);
+		}
+
+        $xoopsTpl->assign('xoops_pagetitle', _LOGIN);
+        include 'footer.php';
+    } elseif ( !empty($_GET['xoops_redirect']) ) {
+        $redirect = htmlspecialchars(trim($_GET['xoops_redirect']));
+        $isExternal = false;
+        if ($pos = strpos( $redirect, '://' )) {
+            $xoopsLocation = substr( XOOPS_URL, strpos( XOOPS_URL, '://' ) + 3 );
+             if ( substr($redirect, $pos + 3, strlen($xoopsLocation)) != $xoopsLocation)  {
+	              $redirect = XOOPS_URL;
+	         }elseif(substr($redirect, $pos + 3, strlen($xoopsLocation)+1) == $xoopsLocation.'.') {
+	              $redirect = XOOPS_URL;
+	         }
+        }
+        header('Location: ' . $redirect);
+		exit();
+    } else {
+        header('Location: '.XOOPS_URL.'/userinfo.php?uid='.intval($xoopsUser->getVar('uid')));
+		exit();
+    }
+    exit();
+}
+
+if ($op == 'login') {
+    include_once XOOPS_ROOT_PATH.'/include/checklogin.php';
+    exit();
+}
+
+if ($op == 'logout') {
+    $message = '';
+    // Regenrate a new session id and destroy old session
+    session_regenerate_id(true);
+    $_SESSION = array();
+    if ($xoopsConfig['use_mysession'] && $xoopsConfig['session_name'] != '') {
+        setcookie($xoopsConfig['session_name'], '', time()- 3600, '/',  '', 0);
+    }
+    // autologin hack GIJ (clear autologin cookies)
+    $xoops_cookie_path = defined('XOOPS_COOKIE_PATH') ? XOOPS_COOKIE_PATH : preg_replace( '?http://[^/]+(/.*)$?' , "$1" , XOOPS_URL ) ;
+    if( $xoops_cookie_path == XOOPS_URL ) $xoops_cookie_path = '/' ;
+    setcookie('autologin_uname', '', time() - 3600, $xoops_cookie_path, '', 0);
+    setcookie('autologin_pass', '', time() - 3600, $xoops_cookie_path, '', 0);
+    // end of autologin hack GIJ
+    // clear entry from online users table
+    if (is_object($xoopsUser)) {
+        $online_handler =& xoops_gethandler('online');
+        $online_handler->destroy($xoopsUser->getVar('uid'));
+    }
+    $message = _US_LOGGEDOUT.'<br />'._US_THANKYOUFORVISIT;
+    redirect_header('index.php', 1, $message);
+}
+
+if ($op == 'actv') {
+    $id = intval($_GET['id']);
+    $actkey = trim($_GET['actkey']);
+    if (empty($id)) {
+        redirect_header('index.php',1,'');
+        exit();
+    }
+    $member_handler =& xoops_gethandler('member');
+    $thisuser =& $member_handler->getUser($id);
+    if (!is_object($thisuser)) {
+        exit();
+    }
+    if ($thisuser->getVar('actkey') != $actkey) {
+        redirect_header('index.php',5,_US_ACTKEYNOT);
+    } else {
+        if ($thisuser->getVar('level') > 0 ) {
+            redirect_header( 'user.php', 5, _US_ACONTACT, false );
+        } else {
+            if (false != $member_handler->activateUser($thisuser)) {
+                $config_handler =& xoops_gethandler('config');
+                $xoopsConfigUser =& $config_handler->getConfigsByCat(XOOPS_CONF_USER);
+                if ($xoopsConfigUser['activation_type'] == 2) {
+                    $myts =& MyTextSanitizer::getInstance();
+                    $xoopsMailer =& getMailer();
+                    $xoopsMailer->useMail();
+                    $xoopsMailer->setTemplate('activated.tpl');
+                    $xoopsMailer->assign('SITENAME', $xoopsConfig['sitename']);
+                    $xoopsMailer->assign('ADMINMAIL', $xoopsConfig['adminmail']);
+                    $xoopsMailer->assign('SITEURL', XOOPS_URL."/");
+                    $xoopsMailer->setToUsers($thisuser);
+                    $xoopsMailer->setFromEmail($xoopsConfig['adminmail']);
+                    $xoopsMailer->setFromName($xoopsConfig['sitename']);
+                    $xoopsMailer->setSubject(sprintf(_US_YOURACCOUNT,$xoopsConfig['sitename']));
+                    include 'header.php';
+                    if ( !$xoopsMailer->send() ) {
+                        printf(_US_ACTVMAILNG, $thisuser->getVar('uname'));
+                    } else {
+                        printf(_US_ACTVMAILOK, $thisuser->getVar('uname'));
+                    }
+                    include 'footer.php';
+                } else {
+                    redirect_header( 'user.php', 5, _US_ACTLOGIN, false );
+                }
+            } else {
+                redirect_header('index.php',5,'Activation failed!');
+            }
+        }
+    }
+    exit();
+}
+
+if ($op == 'delete') {
+    $config_handler =& xoops_gethandler('config');
+    $xoopsConfigUser =& $config_handler->getConfigsByCat(XOOPS_CONF_USER);
+    if (!$xoopsUser || $xoopsConfigUser['self_delete'] != 1) {
+        redirect_header('index.php',5,_US_NOPERMISS);
+    } else {
+        $groups = $xoopsUser->getGroups();
+        if (in_array(XOOPS_GROUP_ADMIN, $groups)){
+            // users in the webmasters group may not be deleted
+            redirect_header('user.php', 5, _US_ADMINNO);
+        }
+        $ok = !isset($_POST['ok']) ? 0 : intval($_POST['ok']);
+        if ($ok != 1) {
+            include 'header.php';
+            xoops_confirm(array('op' => 'delete', 'ok' => 1), 'user.php', _US_SURETODEL.'<br/>'._US_REMOVEINFO);
+            include 'footer.php';
+        } else {
+            $del_uid = $xoopsUser->getVar("uid");
+            $member_handler =& xoops_gethandler('member');
+            if (false != $member_handler->deleteUser($xoopsUser)) {
+                $online_handler =& xoops_gethandler('online');
+                $online_handler->destroy($del_uid);
+                xoops_notification_deletebyuser($del_uid);
+                redirect_header('index.php', 5, _US_BEENDELED);
+            }
+            redirect_header('index.php',5,_US_NOPERMISS);
+        }
+        exit();
+    }
+}
+?>
