50c7b0e18702a9c5dea4f1d738efcb08f7318817
Sina Asghari <stranger@impresscms.org>
2008-08-02 03:42:42 +0000

    changed the version of admin version checker to 1.2 + updated the old internal javascript calendar library and moved it at once in the libraries folder and removed the old library!

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -118,7 +118,7 @@ switch($op)
 			echo '<br />';
 		}
 		$db = $GLOBALS['xoopsDB'];
-		if(getDbValue($db, 'modules', 'version', 'version="103"') == 0 AND getDbValue($db, 'modules', 'mid', 'mid="1"') == 1)
+		if(getDbValue($db, 'modules', 'version', 'version="120"') == 0 AND getDbValue($db, 'modules', 'mid', 'mid="1"') == 1)
 		{
 			xoops_error('<a href="'.ICMS_URL.'/modules/system/admin.php?fct=modulesadmin&op=update&module=system">'._WARNINGUPDATESYSTEM.'</a>');
 			echo '<br />';
--- htdocs/language/english/calendar.php
+++ htdocs/language/english/calendar.php
@@ -21,14 +21,63 @@ define("_CAL_OCTOBER", "October");
 define("_CAL_NOVEMBER", "November");
 define("_CAL_DECEMBER", "December");
 define("_CAL_TGL1STD", "Toggle first day of week");
-define("_CAL_PREVYR", "Prev. year (hold for menu)");
-define("_CAL_PREVMNTH", "Prev. month (hold for menu)");
+define("_CAL_PREVYR", "Prev. year");
+define("_CAL_PREVMNTH", "Prev. month");
 define("_CAL_GOTODAY", "Go Today");
-define("_CAL_NXTMNTH", "Next month (hold for menu)");
-define("_CAL_NEXTYR", "Next year (hold for menu)");
+define("_CAL_NXTMNTH", "Next month");
+define("_CAL_NEXTYR", "Next year");
 define("_CAL_SELDATE", "Select date");
 define("_CAL_DRAGMOVE", "Drag to move");
 define("_CAL_TODAY", "Today");
 define("_CAL_DISPM1ST", "Display Monday first");
 define("_CAL_DISPS1ST", "Display Sunday first");
+
+############# added since 1.2 #############
+define("_CAL_SUN", "Sun");
+define("_CAL_MON", "Mon");
+define("_CAL_TUE", "Tue");
+define("_CAL_WED", "Wed");
+define("_CAL_THU", "Thu");
+define("_CAL_FRI", "Fri");
+define("_CAL_SAT", "Sat");
+// First day of the week. "0" means display Sunday first, "1" means display Monday first, etc...
+define("_CAL_FIRSTDAY", "1");
+define("_CAL_JAN", "Jan");
+define("_CAL_FEB", "Feb");
+define("_CAL_MAR", "Mar");
+define("_CAL_APR", "Apr");
+define("_CAL_JUN", "Jun");
+define("_CAL_JUL", "Jul");
+define("_CAL_AUG", "Aug");
+define("_CAL_SEP", "Sept");
+define("_CAL_OCT", "Oct");
+define("_CAL_NOV", "Nov");
+define("_CAL_DEC", "Dec");
+// Direction of the calendar, ltr for left to right and rtl for roght to left (for Persian, Arabic, etc.)
+define("_CAL_DIRECTION", "ltr");
+define("_CAL_AM", "am");
+define("_CAL_AM_CAPS", "AM");
+define("_CAL_PM", "pm");
+define("_CAL_PM_CAPS", "PM");
+define("_CAL_TIME", "Time");
+define("_CAL_WK", "wk"); // shorten of week-end
+// This may be local-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+define("_CAL_WEEKEND", "0,6");
+define("_CAL_DSPFIRST", "Display %s first");
+//This constants are for the jalali calendar included in the library
+define('_CAL_Far','Farvardin');
+define('_CAL_Ord','Ordibehest');
+define('_CAL_Kho','Khordad');
+define('_CAL_Tir','Tir');
+define('_CAL_Mor','Mordad');
+define('_CAL_Sha','Shahrivar');
+define('_CAL_Meh','Mehr');
+define('_CAL_Aba','Aban');
+define('_CAL_Aza','Azar');
+define('_CAL_Dey','Day');
+define('_CAL_Bah','Bahman');
+define('_CAL_Esf','Esfand');
+define("_CAL_NUMS_ARRAY", "'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'"); // numeric values can differ in different languages
 ?>
