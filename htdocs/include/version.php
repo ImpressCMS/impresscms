1d3460a228ff07c64ef43bc23159d95a7d7d9314
Marc-André Lanciault <marcan@impresscms.org>
2008-11-04 16:45:39 +0000

    Preparing ImpressCMS 1.0.3 Final release

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
 * @version		$Id: version.php 897 2008-02-15 19:53:31Z malanciault $
 */
 
-define("ICMS_VERSION_NAME",'ImpressCMS 1.0.3 "Janus" RC');
+define("ICMS_VERSION_NAME",'ImpressCMS 1.0.3 "Janus" Final');
 
 // For backward compatibility with XOOPS
 define("XOOPS_VERSION", ICMS_VERSION_NAME);
@@ -23,7 +23,7 @@ define("XOOPS_VERSION", ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define("ICMS_VERSION_STATUS", 3);
+define("ICMS_VERSION_STATUS", 10);
 
 /*
  * Build number
