b7471a87741e869bfc3339b79df5bfedc5d94fbc
McDonald <pietjebell31@hotmail.com>
2008-11-16 20:37:32 +0000

    Adding extra features to icmsfeed (category, author, copyright, ttl)

--- htdocs/class/icmsfeed.php
+++ htdocs/class/icmsfeed.php
@@ -29,6 +29,12 @@ class IcmsFeed {
 		var $pubDate;
 		var $webMaster;
 		var $generator;
+		var $copyright;
+		var $lastbuild;
+		var $channelEditor;
+		var $width;
+		var $height;
+		var $ttl;
 		var $image = array ();
 
 	function IcmsFeed () {
@@ -39,8 +45,14 @@ class IcmsFeed {
 		$this->language = _LANGCODE;
 		$this->charset = _CHARSET;
 		$this->pubDate = date(_DATESTRING, time());
+		$this->lastbuild = formatTimestamp( time(), 'D, d M Y H:i:s' );
 		$this->webMaster = $xoopsConfig['adminmail'];
+		$this->channelEditor = $xoopsConfig['adminmail'];
 		$this->generator = XOOPS_VERSION;
+		$this-> copyright = 'Copyright ' . formatTimestamp( time(), 'Y' ) . ' ' . $xoopsConfig['sitename'];
+		$this->width  = 88;
+		$this->height = 31;
+		$this->ttl    = 60;
 		$this->image = array (
 			'title' => $this->title,
 			'url' => ICMS_URL.'/images/logo.gif',
@@ -56,10 +68,16 @@ class IcmsFeed {
 		$tpl->assign('channel_title', $this->title);
 		$tpl->assign('channel_link', $this->url);
 		$tpl->assign('channel_desc', $this->description);
-		$tpl->assign('channel_webmaster', $this->webMaster);
+		$tpl->assign('channel_webmaster', $this -> webMaster . ' (' . $this -> title . ')');
+		$tpl->assign('channel_editor', $this -> channelEditor . ' (' . $this -> title . ')');
 		$tpl->assign('channel_category', $this->category);
 		$tpl->assign('channel_generator', $this->generator);
 		$tpl->assign('channel_language', $this->language);
+		$tpl->assign('channel_lastbuild', $this -> lastbuild);
+		$tpl->assign('channel_copyright', $this -> copyright);
+		$tpl->assign('channel_width', $this -> width); 
+        $tpl->assign('channel_height', $this -> height);
+		$tpl->assign('channel_ttl', $this -> ttl);
 		$tpl->assign('image_url', $this->image['url']);
 		foreach ($this->feeds as $feed) {
 			$tpl->append('items', $feed);
