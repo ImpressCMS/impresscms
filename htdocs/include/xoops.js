d2fe031a883e0923c96a41e6b91a1eac62b0aaf9
Sina Asghari <stranger@impresscms.org>
2008-12-26 16:43:03 +0000

    some updates to xoops.js

--- htdocs/include/xoops.js
+++ htdocs/include/xoops.js
@@ -518,3 +518,42 @@ function icms_showDiv(type,id,classname){
 	changeDisplay(type+id);
 	document.anchors.item(type+id+'_anchor').scrollIntoView();
 }
+function appendSelectOption(selectMenuId, optionName, optionValue){
+	var selectMenu = xoopsGetElementById(selectMenuId);
+	var newoption = new Option(optionName, optionValue);
+	newoption.selected = true;
+	selectMenu.options[selectMenu.options.length] = newoption;
+}
+
+function imageResize(img, maxWidth){
+	if(img.width > maxWidth && maxWidth > 0) img.width = maxWidth;
+}
+
+/* Who is the original author of the scripts? let's find out later on */
+function CaricaFoto(img){
+	foto1= new Image();
+	foto1.src=(img);
+	Controlla(img);
+}
+
+function Controlla(img){
+	if((foto1.width!=0)&&(foto1.height!=0)){
+ 		viewFoto(img);
+	}else{
+		funzione="Controlla('\"+img+\"')";
+		intervallo=setTimeout(funzione,20);
+	}
+}
+
+function viewFoto(img){
+	largh=foto1.width;
+	altez=foto1.height;
+	stringa="width="+largh+",height="+altez;
+	finestra=window.open('','',stringa);
+	finestra.document.write ("<html><body leftmargin=0 topmargin=0>");
+	finestra.document.write ("<a href='javascript:this.close()'><img border=0 src=");
+	finestra.document.write (img);
+	finestra.document.write ("></a></body></html>");
+	finestra.document.close();
+	return false;
+}
