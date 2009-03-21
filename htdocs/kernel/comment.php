2887acca9cce5bca58edd7b538cde8a257bd877c
Steve Kenow <skenow@impresscms.org>
2009-03-21 15:21:16 +0000

    Adding code documentation to comment class, refs #478

--- htdocs/kernel/comment.php
+++ htdocs/kernel/comment.php
@@ -1,46 +1,21 @@
 <?php
-// $Id$
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
-// URL: http://www.xoops.org/ http://jp.xoops.org/  http://www.myweb.ne.jp/  //
-// Project: The XOOPS Project (http://www.xoops.org/)                        //
-// ------------------------------------------------------------------------- //
+/**
+ * Core class for managing comments
+ *
+ * @package     kernel
+ * @subpackage	comment
+ * @author	    Kazumi Ono	<onokazu@xoops.org>
+ * @copyright	http://www.xoops.org/ The XOOPS Project
+ * @copyright	XOOPS_copyrights.txt
+ * @copyright 	http://www.impresscms.org/ The ImpressCMS Project
+ * @license		LICENSE.txt
+ * @since		XOOPS
+ * @version		$Id$
+ */
 
 if (!defined('XOOPS_ROOT_PATH')) {
 	exit();
 }
-
-/**
- *
- *
- * @package     kernel
- *
- * @author	    Kazumi Ono	<onokazu@xoops.org>
- * @copyright	copyright (c) 2000-2003 XOOPS.org
- */
 
 /**
  * A Comment
@@ -114,6 +89,7 @@ class XoopsCommentHandler extends XoopsObjectHandler
 	 * @param	bool    $isNew  Flag the object as "new"?
 	 *
 	 * @return	object
+	 * @see htdocs/kernel/XoopsObjectHandler#create()
 	 */
 	function &create($isNew = true)
 	{
@@ -130,6 +106,7 @@ class XoopsCommentHandler extends XoopsObjectHandler
 	 * @param   int $id ID
 	 *
 	 * @return  object  {@link XoopsComment}, FALSE on fail
+	 * @see htdocs/kernel/XoopsObjectHandler#get($int_id)
 	 **/
 	function &get($id)
 	{
@@ -155,6 +132,7 @@ class XoopsCommentHandler extends XoopsObjectHandler
 	 * @param   object  &$comment
 	 *
 	 * @return  bool
+	 * @see htdocs/kernel/XoopsObjectHandler#insert($object)
 	 **/
 	function insert(&$comment)
 	{
@@ -197,6 +175,7 @@ class XoopsCommentHandler extends XoopsObjectHandler
 	 * @param   object  &$comment
 	 *
 	 * @return  bool
+	 *  @see htdocs/kernel/XoopsObjectHandler#delete($object)
 	 **/
 	function delete(&$comment)
 	{
