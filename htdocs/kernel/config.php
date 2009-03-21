1945f6205d62ffb4a92652148aa6b3691d6fe6a1
Steve Kenow <skenow@impresscms.org>
2009-03-21 15:28:48 +0000

    Adding and updating code documentation to config class, refs #478

--- htdocs/kernel/config.php
+++ htdocs/kernel/config.php
@@ -1,33 +1,18 @@
 <?php
-// $Id$
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
-// URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
-// ------------------------------------------------------------------------- //
+/**
+* Manage configuration items
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		LICENSE.txt
+* @package		kernel
+* @subpackage	config
+* @since		XOOPS
+* @author		Kazumi Ono (aka onokazo)
+* @author		http://www.xoops.org The XOOPS Project
+* @version		$Id$
+*/
 
 if (!defined('XOOPS_ROOT_PATH')) {
 	exit();
@@ -37,18 +22,12 @@ require_once XOOPS_ROOT_PATH.'/kernel/configoption.php';
 require_once XOOPS_ROOT_PATH.'/kernel/configitem.php';
 
 /**
- * @package     kernel
- *
- * @author	    Kazumi Ono	<onokazu@xoops.org>
- * @copyright	copyright (c) 2000-2003 XOOPS.org
- */
-
-/**
  * XOOPS configuration handling class.
  * This class acts as an interface for handling general configurations of XOOPS
  * and its modules.
  *
- *
+ * @package 	kernel
+ * @subpackage 	config
  * @author  Kazumi Ono <webmaster@myweb.ne.jp>
  * @todo    Tests that need to be made:
  *          - error handling
