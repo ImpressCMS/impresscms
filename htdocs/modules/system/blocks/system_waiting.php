8d94dc15f47b1047bad455eb4297a5af6d501145
Niels Drost <underdog@impresscms.org>
2009-04-04 13:24:50 +0000

    Documentation of modules/system/blocks/system_waiting.php refs #478 Added / changed file header for modules/system/blocks/system_waiting.php refs #478 Added svn keyword property 'Id' for file modules/system/blocks/system_waiting.php refs #503

--- htdocs/modules/system/blocks/system_waiting.php
+++ htdocs/modules/system/blocks/system_waiting.php
@@ -1,36 +1,24 @@
 <?php
 // $Id: waiting_waiting.php,v 1.8 2005/04/20 03:43:55 gij Exp $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
-// URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
-// ------------------------------------------------------------------------- //
+/**
+* All the blocks that are awaiting approval or admin intervention
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		LICENSE.txt
+* @package	Administration
+* @since		XOOPS
+* @author		http://www.xoops.org The XOOPS Project
+* @version		$Id$
+*/
 
-// EXTENSIBLE "waiting block" by plugins in both waiting and modules
-
+/**
+* EXTENSIBLE "waiting block" by plugins in both waiting and modules
+*
+* @param array $options The options for this block
+* @return mixed $block The generated waiting block or empty array
+*/
 function b_system_waiting_show($options)
 {
     global $xoopsUser, $xoopsConfig;
@@ -122,6 +110,13 @@ function b_system_waiting_show($options)
 	return $block ;
 }
 
+
+/**
+* The edit "waiting block" form
+*
+* @param array $options The options for this block
+* @return string $form The Edit waiting block form HTML string
+*/
 function b_system_waiting_edit($options){
 
 	$sql_cache_min = empty( $options[1] ) ? 0 : intval( $options[1] ) ;
@@ -139,6 +134,14 @@ function b_system_waiting_edit($options){
 
 	return $form;
 }
+
+/**
+* Gets the plugin information
+*
+* @param string $dirname The directory to get the plugin from
+* @param string $language The language for the plugin
+* @return array $ret The plugin information array or an empty array
+*/
 function system_get_plugin_info( $dirname , $language = 'english' )
 {
 	// get $mytrustdirname for D3 modules
