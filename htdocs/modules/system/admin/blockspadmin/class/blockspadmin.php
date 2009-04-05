7b35087e27caf95eec45951d16c0bbd3943ed946
Niels Drost <underdog@impresscms.org>
2009-04-05 11:02:46 +0000

    Added svn keyword property Id for file modules/system/admin/blockspadmin/class/blockspadmin.php refs #503 Cosmetic changes of modules/system/admin/blockspadmin/class/blockspadmin.php refs #492

--- htdocs/modules/system/admin/blockspadmin/class/blockspadmin.php
+++ htdocs/modules/system/admin/blockspadmin/class/blockspadmin.php
@@ -1,9 +1,20 @@
 <?php
+/**
+* Blocks position admin classes
+*
+* @copyright      http://www.impresscms.org/ The ImpressCMS Project
+* @license         LICENSE.txt
+* @package	(use an appropriate package name)
+* @since            1.2
+* @author		UnderDog <underdog@impresscms.org>
+* @version		$Id$
+*/
+
 
 require_once(ICMS_ROOT_PATH.'/kernel/blockposition.php');
 
 class SystemBlockspadmin extends IcmsBlockposition {
-	
+
 	/**
 	 * Constructor
 	 *
@@ -16,52 +27,57 @@ class SystemBlockspadmin extends IcmsBlockposition {
 		$this->hideFieldFromForm('block_default');
 		$this->hideFieldFromForm('block_type');
 	}
-	
+
+
 	public function getVar($key, $format = 's') {
 		if ( $format == 's' && in_array( $key, array ( 'titlec' ) ) ) {
 			return call_user_func(array ($this,	$key));
 		}
 		return parent :: getVar($key, $format);
 	}
-	
+
+
 	private function titlec(){
 		$rtn = defined($this->getVar('title')) ? constant($this->getVar('title')) : $this->getVar('title');
 		return $rtn;
 	}
-	
+
+
 	/**
-     * getDeleteItemLink
-     * 
-     * Overwrited Method
-     *
-     * @param string $onlyUrl
-     * @param boolean $withimage
-     * @param boolean $userSide
-     * @return string
-     */
-    public function getEditItemLink($onlyUrl=false, $withimage=true, $userSide=false){ 
-	  	if($this->getVar('block_default') == 1)
-	  		return "";
-    	return parent::getEditItemLink($onlyUrl, $withimage, $userSide);
-    }
-	
+   * getDeleteItemLink
+   * 
+   * Overwrited Method
+   *
+   * @param string $onlyUrl
+   * @param boolean $withimage
+   * @param boolean $userSide
+   * @return string
+   */
+  public function getEditItemLink($onlyUrl=false, $withimage=true, $userSide=false){ 
+ 	if($this->getVar('block_default') == 1)
+ 		return "";
+  	return parent::getEditItemLink($onlyUrl, $withimage, $userSide);
+  }
+
+
 	/**
-     * getDeleteItemLink
-     * 
-     * Overwrited Method
-     *
-     * @param string $onlyUrl
-     * @param boolean $withimage
-     * @param boolean $userSide
-     * @return string
-     */
-    public function getDeleteItemLink($onlyUrl=false, $withimage=true, $userSide=false){ 
-	  	if($this->getVar('block_default') == 1)
-	  		return "";
-    	return parent::getDeleteItemLink($onlyUrl, $withimage, $userSide);
-    }
-	
+   * getDeleteItemLink
+   * 
+   * Overwrited Method
+   *
+   * @param string $onlyUrl
+   * @param boolean $withimage
+   * @param boolean $userSide
+   * @return string
+   */
+  public function getDeleteItemLink($onlyUrl=false, $withimage=true, $userSide=false){ 
+ 	if($this->getVar('block_default') == 1)
+ 		return "";
+  	return parent::getDeleteItemLink($onlyUrl, $withimage, $userSide);
+  }
+
 }
+
 
 class SystemBlockspadminHandler extends IcmsBlockpositionHandler {
 	
@@ -75,4 +91,5 @@ class SystemBlockspadminHandler extends IcmsBlockpositionHandler {
 		$this->table = $this->db->prefix('block_positions');		
 	}
 }
+
 ?>
