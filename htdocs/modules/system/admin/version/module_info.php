176fd9cfd12a55b8891729f9288528035e17a4cd
Niels Drost <underdog@impresscms.org>
2009-04-11 09:54:47 +0000

    Added svn keyword property Id for file modules/system/admin/version/module_info.php refs #503 Changed file header for file modules/system/admin/version/module_info.php refs #478 Cosmetic changes of file modules/system/admin/version/module_info.php refs #492

--- htdocs/modules/system/admin/version/module_info.php
+++ htdocs/modules/system/admin/version/module_info.php
@@ -1,22 +1,32 @@
 <?php
+/**
+* Version checker, module_info file
+*
+* @copyright      http://www.impresscms.org/ The ImpressCMS Project
+* @license         LICENSE.txt
+* @package	(use an appropriate package name)
+* @since            1.0
+* @author		name of the author <email@domain.tld>
+* @version		$Id$
+*/
 
 if (!defined('XOOPS_ROOT_PATH')) {
 	die("ImpressCMS root path not defined");
 }
 
 if (intval($_GET['mid'])) {
-    $module_handler =& xoops_gethandler('module');
-    $versioninfo =& $module_handler->get(intval($_GET['mid']));
+	$module_handler =& xoops_gethandler('module');
+	$versioninfo =& $module_handler->get(intval($_GET['mid']));
 } else {
-    $mid = str_replace('..', '', trim($_GET['mid']));
-    if (file_exists(XOOPS_ROOT_PATH.'/modules/'.$mid.'/xoops_version.php') || file_exists(XOOPS_ROOT_PATH.'/modules/'.$mid.'/icms_version.php')) {
-        $module_handler =& xoops_gethandler('module');
-        $versioninfo =& $module_handler->create();
-        $versioninfo->loadInfo($mid);
-    }
+	$mid = str_replace('..', '', trim($_GET['mid']));
+	if (file_exists(XOOPS_ROOT_PATH.'/modules/'.$mid.'/xoops_version.php') || file_exists(XOOPS_ROOT_PATH.'/modules/'.$mid.'/icms_version.php')) {
+		$module_handler =& xoops_gethandler('module');
+		$versioninfo =& $module_handler->create();
+		$versioninfo->loadInfo($mid);
+	}
 }
 if (!isset($versioninfo) || !is_object($versioninfo)) {
-    exit();
+	exit();
 }
 
 //$css = getCss($theme);
@@ -51,11 +61,11 @@ echo "</head>\n";
 echo "<body onLoad=\"if(window.scroll)onWard()\" onmouseover=\"stop()\" onmouseout=\"if(window.scroll)onWard()\">\n";
 echo "<div><table width=\"100%\"><tr><td align=\"center\"><br /><br /><br /><br /><br />";
 if ($modimage = $versioninfo->getInfo('image')) {
-    $modimage_path = '/modules/'.$versioninfo->getInfo('dirname').'/'.$modimage;
-    $modimage_realpath = str_replace("\\", "/", realpath(XOOPS_ROOT_PATH.$modimage_path));
-    if (0 === strpos($modimage_realpath, XOOPS_ROOT_PATH) && is_file($modimage_realpath)) {
-        echo "<img src='".XOOPS_URL.$modimage_path."' border='0' /><br />";
-    }
+	$modimage_path = '/modules/'.$versioninfo->getInfo('dirname').'/'.$modimage;
+	$modimage_realpath = str_replace("\\", "/", realpath(XOOPS_ROOT_PATH.$modimage_path));
+	if (0 === strpos($modimage_realpath, XOOPS_ROOT_PATH) && is_file($modimage_realpath)) {
+		echo "<img src='".XOOPS_URL.$modimage_path."' border='0' /><br />";
+	}
 }
 if ($modname = $versioninfo->getInfo('name')) {
     echo "<big><b>".htmlspecialchars($modname)."</b></big>";
@@ -63,10 +73,10 @@ if ($modname = $versioninfo->getInfo('name')) {
 
 $modinfo = array('Version', 'Description', 'Author', 'Credits', 'License');
 foreach ($modinfo as $info) {
-    if ($info_output = $versioninfo->getInfo(strtolower($info))) {
-        echo "<br /><br /><u>$info</u><br />";
-        echo htmlspecialchars($info_output);
-    }
+	if ($info_output = $versioninfo->getInfo(strtolower($info))) {
+		echo "<br /><br /><u>$info</u><br />";
+		echo htmlspecialchars($info_output);
+	}
 }
 echo "<br /><br /><br /><br /><br />";
 echo "<br /><br /><br /><br /><br />";
@@ -74,4 +84,5 @@ echo "<a href=\"javascript:window.close();\">Close</a>";
 echo "<br /><br /><br /><br /><br /><br />";
 echo "</td></tr></table></div>";
 echo "</body></html>";
+
 ?>
