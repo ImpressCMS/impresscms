2343839d697436b381e0ef9587e953887449f94c
Niels Drost <underdog@impresscms.org>
2009-04-11 05:55:02 +0000

    Added svn keyword property Id for file modules/system/admin/preferences/main.php refs #503 Changed file header for file modules/system/admin/preferences/main.php refs #478 Cosmetic changes of file modules/system/admin/preferences/main.php refs #492

--- htdocs/modules/system/admin/preferences/main.php
+++ htdocs/modules/system/admin/preferences/main.php
@@ -1,33 +1,18 @@
 <?php
-// $Id: main.php 1029 2007-09-09 03:49:25Z phppp $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
-// URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
-// ------------------------------------------------------------------------- //
+// $Id$
+/**
+* Administration of preferences, main file
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license	LICENSE.txt
+* @package	Administration
+* @since	XOOPS
+* @author	http://www.xoops.org The XOOPS Project
+* @author	modified by UnderDog <underdog@impresscms.org>
+* @version	$Id$
+*/
 
 
 if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->isAdmin ( $xoopsModule->mid () )) {
@@ -39,10 +24,11 @@ if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->
 		}
 	}
 	$op = (isset ( $_GET ['op'] )) ? trim ( StopXSS ( $_GET ['op'] ) ) : ((isset ( $_POST ['op'] )) ? trim ( StopXSS ( $_POST ['op'] ) ) : 'list');
-	
+
 	if (isset ( $_GET ['confcat_id'] )) {
 		$confcat_id = intval ( $_GET ['confcat_id'] );
 	}
+
 	if ($op == 'list') {
 		/**
 		 * Allow easely change the order of Preferences.
@@ -76,7 +62,7 @@ if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->
 		xoops_cp_footer ();
 		exit ();
 	}
-	
+
 	if ($op == 'show') {
 		if (empty ( $confcat_id )) {
 			$confcat_id = 1;
@@ -351,7 +337,7 @@ if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->
 		xoops_cp_footer ();
 		exit ();
 	}
-	
+
 	if ($op == 'showmod') {
 		$config_handler = & xoops_gethandler ( 'config' );
 		$mod = isset ( $_GET ['mod'] ) ? intval ( $_GET ['mod'] ) : 0;
@@ -488,7 +474,7 @@ if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->
 		xoops_cp_footer ();
 		exit ();
 	}
-	
+
 	if ($op == 'save') {
 		if (! $GLOBALS ['xoopsSecurity']->check ()) {
 			redirect_header ( 'admin.php?fct=preferences', 3, implode ( '<br />', $GLOBALS ['xoopsSecurity']->getErrors () ) );
@@ -630,4 +616,5 @@ if (! is_object ( $xoopsUser ) || ! is_object ( $xoopsModule ) || ! $xoopsUser->
 		}
 	}
 }
+
 ?>
