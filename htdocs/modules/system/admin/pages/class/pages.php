aea617c42c19cf7b9762af2dc2267f43c87b8c8a
Niels Drost <underdog@impresscms.org>
2009-04-11 08:38:50 +0000

    Added svn keyword property Id for file modules/system/admin/pages/class/pages.php refs #503 Changed file header for file modules/system/admin/pages/class/pages.php refs #478 Cosmetic changes of file modules/system/admin/pages/class/pages.php refs #492

--- htdocs/modules/system/admin/pages/class/pages.php
+++ htdocs/modules/system/admin/pages/class/pages.php
@@ -1,4 +1,17 @@
 <?php
+/**
+* Administration of content pages, class file
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license	LICENSE.txt
+* @package	Administration
+* @since	XOOPS
+* @author	http://www.xoops.org The XOOPS Project
+* @author	modified by UnderDog <underdog@impresscms.org>
+* @version	$Id$
+*/
 
 require_once(ICMS_ROOT_PATH."/kernel/page.php");
 
@@ -14,14 +27,14 @@ class SystemPages extends IcmsPage {
 			'module' => 'system'
 		));
 	}
-	
+
 	public function getVar($key, $format = 's') {
 		if ($format == 's' && in_array($key, array ( 'page_status_custom', 'page_moduleid_custom'))) {
 			return call_user_func(array ($this,	$key));
 		}
 		return parent :: getVar($key, $format);
 	}
-	
+
 	private function page_status_custom(){
 		if($this->getVar('page_status') == 1)
 			$rtn = '<a href="'.ICMS_URL.'/modules/system/admin.php?fct=pages&op=status&page_id='.$this->getVar('page_id').'" title="'._VISIBLE.'" ><img src="'.ICMS_URL.'/images/crystal/actions/button_ok.png" alt="'._VISIBLE.'"/></a>';
@@ -29,18 +42,18 @@ class SystemPages extends IcmsPage {
 			$rtn = '<a href="'.ICMS_URL.'/modules/system/admin.php?fct=pages&op=status&page_id='.$this->getVar('page_id').'" title="'._VISIBLE.'" ><img src="'.ICMS_URL.'/images/crystal/actions/button_cancel.png" alt="'._VISIBLE.'"/></a>';
 		return $rtn;
 	}
-	
+
 	private function page_moduleid_custom(){
 		$modules = $this->handler->getModulesArray();
 		return $modules[$this->getVar('page_moduleid')];
 	}
-	
+
 	public function getAdminViewItemLink(){
 		$rtn = $this->getVar('page_title');
 		return $rtn;
 	}
 
-	
+
 	public function getViewItemLink() {
 		if (preg_match('/\*/',$this->getVar('page_url','e'))){
 			$ret = '';
@@ -48,44 +61,44 @@ class SystemPages extends IcmsPage {
 			$url = (substr($this->getVar('page_url','e'),0,7) == 'http://')?$this->getVar('page_url','e'):ICMS_URL.'/'.$this->getVar('page_url','e');
 			$ret = '<a href="'.$url.'" alt="'._PREVIEW.'" title="'._PREVIEW.'" targe="_blank"><img src="' . ICMS_URL . '/images/crystal/actions/viewmag.png" /></a>';
 		}
-		
+
 		return $ret;
 	}
 }
 
 
 class SystemPagesHandler extends IcmsPageHandler {
-	
+
 	private $modules_name;
-	
+
 	public function __construct( & $db ){
 		$this->IcmsPersistableObjectHandler($db, 'pages', 'page_id', 'page_title', '' , 'system');
 		$this->table = $db->prefix('icmspage');
 	}
-	
-	
+
 	public function getModulesArray($full = false){
-    	if( !count($this->modules_name) ){
+		if( !count($this->modules_name) ){
 			$icms_module_handler = xoops_gethandler('module');
 			$installed_modules =& $icms_module_handler->getObjects();
 			foreach( $installed_modules as $module ){
 				$this->modules_name[$module->getVar('mid')]['name'] = $module->getVar('name');
 				$this->modules_name[$module->getVar('mid')]['dirname'] = $module->getVar('dirname');
-			}	
+			}
 		}
 		$rtn = $this->modules_name;
+
 		if(!$full)
 			foreach($this->modules_name as $key => $module)
 				$rtn[$key] = $module['name'];
-    	return $rtn;
-    }
-	
+
+		return $rtn;
+	}
+
 	function changeStatus( $page_id ){
 		$page = $this->get( $page_id );
-        $page->setVar( 'page_status', !$page->getVar('page_status') );
-        return $this->insert($page, true);
-    }
-    
-    
+		$page->setVar( 'page_status', !$page->getVar('page_status') );
+		return $this->insert($page, true);
+	}
+
 }
 ?>
