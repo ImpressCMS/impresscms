be9eb657e161bc5eed5e1fbd5640752b8a19e2a9
Gustavo Alejandro Pilla <nekro@impresscms.org>
2009-04-13 05:58:31 +0000

    - Improved code to be more PHP5 like. - Improved documentation. - Renamed classes for backward compatibility and future development. - Improved code to be more PHP5 strict. Strict: Non-static method Database::getInstance() should not be called statically in file /include/functions.php line 587 Strict: Non-static method XoopsDatabaseFactory::getDatabaseConnection() should not be called statically in file /class/database/database.php line 137
    Refs #505

--- htdocs/class/database/database.php
+++ htdocs/class/database/database.php
@@ -1,121 +1,122 @@
-<?php
-// $Id$
-// database.php - defines abstract database wrapper class
+<?php 
 /**
-* Database Base Class
-*
-* @copyright	http://www.xoops.org/ The XOOPS Project
-* @copyright	XOOPS_copyrights.txt
-* @copyright	http://www.impresscms.org/ The ImpressCMS Project
-* @license	LICENSE.txt
-* @package	database
-* @since	XOOPS
-* @author	http://www.xoops.org The XOOPS Project
-* @author	modified by UnderDog <underdog@impresscms.org>
-* @version	$Id$
-*/
-
-/**
- * @package database
- * @subpackage  main
- * @since XOOPS
- * @version $Id$
+ * Database Base Class
  *
+ * Defines abstract database wrapper class
+ * 
+ * @copyright	The XOOPS Project <http://www.xoops.org/>
+ * @copyright	XOOPS_copyrights.txt
+ * @copyright	The ImpressCMS Project <http://www.impresscms.org/>
+ * @license		LICENSE.txt
+ * @package		database
+ * @since		XOOPS
+ * @version		$Id$
+ * @author		The XOOPS Project Community <http://www.xoops.org>
  * @author      Kazumi Ono  <onokazu@xoops.org>
- * @copyright   copyright (c) 2000-2003 XOOPS.org
- * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @author		modified by UnderDog <underdog@impresscms.org>
+ * @author		Gustavo Pilla (aka nekro) <nekro@impresscms.org>
  */
 
 /**
- * make sure this is only included once!
+ * Make sure this is only included once!
  */
 if ( !defined("XOOPS_C_DATABASE_INCLUDED") ) {
 	define("XOOPS_C_DATABASE_INCLUDED",1);
 
-  /**
-   * Abstract base class for Database access classes
-   *
-   * @abstract
-   *
-   * @package database
-   * @subpackage  main
-   * @since XOOPS
-   *
-   * @author      Kazumi Ono  <onokazu@xoops.org>
-   * @copyright   copyright (c) 2000-2003 XOOPS.org
-   * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
-   */
-  class XoopsDatabase
-  {
-    /**
-     * Prefix for tables in the database
-     * @var string
-     */
-    var $prefix = '';
-    /**
-     * reference to a {@link XoopsLogger} object
-           * @see XoopsLogger
-     * @var object XoopsLogger
-     */
-    var $logger;
-    
-    	/**
-    	 * If statements that modify the database are selected
-    	 * @var boolean
-    	 */
-  	var $allowWebChanges = false;
-    
-    /**
-     * constructor
-       *
-       * will always fail, because this is an abstract class!
-     */
-    function XoopsDatabase()
-    {
-    	// exit("Cannot instantiate this class directly");
-    }
-  
-    /**
-     * assign a {@link XoopsLogger} object to the database
-     *
-     * @see XoopsLogger
-     * @param object $logger reference to a {@link XoopsLogger} object
-     */
-    function setLogger(&$logger)
-    {
-    	$this->logger =& $logger;
-    }
-  
-    /**
-     * set the prefix for tables in the database
-     *
-     * @param string $value table prefix
-     */
-    function setPrefix($value)
-    {
-    	$this->prefix = $value;
-    }
-
+	/**
+	 * Abstract base class for Database access classes
+	 *
+	 * @abstract
+	 *
+	 * @package database
+	 * @subpackage  main
+	 *
+	 * @author      Gustavo Pilla  (aka nekro) <nekro@impresscms.org>
+	 * @copyright   copyright (c) 2000-2003 XOOPS.org
+	 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+	 */
+  	abstract class IcmsDatabase {
+		
+		/**
+		 * Prefix for tables in the database
+		 * @var string
+		 */
+		public $prefix = '';
+		
+		/**
+		 * reference to a {@link XoopsLogger} object
+		 * @see XoopsLogger
+		 * @var object XoopsLogger
+		 */
+		public $logger;
+		
+		/**
+		 * If statements that modify the database are selected
+		 * @var boolean
+		 */
+	 	public $allowWebChanges = false;
+	   
+	    /**
+	     * Constructor
+	     *
+	     * Will always fail, because this is an abstract class!
+	     */
+		public function __construct() { /* exit("Cannot instantiate this class directly"); */ }
+		
+		/**
+		 * assign a {@link XoopsLogger} object to the database
+		 *
+		 * @see XoopsLogger
+		 * @param object $logger reference to a {@link XoopsLogger} object
+		 */
+		public function setLogger(&$logger) {
+			$this->logger =& $logger;
+		}
+	 
+		/**
+		 * set the prefix for tables in the database
+		 *
+		 * @param string $value table prefix
+		 */
+	   	public function setPrefix($value) {
+	   		$this->prefix = $value;
+	   	}
+	
 		/**
 		 * attach the prefix.'_' to a given tablename
-     *
-     * if tablename is empty, only prefix will be returned
+	     *
+	     * if tablename is empty, only prefix will be returned
 		 *
-     * @param string $tablename tablename
-     * @return string prefixed tablename, just prefix if tablename is empty
+	     * @param string $tablename tablename
+	     * @return string prefixed tablename, just prefix if tablename is empty
 		 */
-		function prefix($tablename='')
-		{
+		public function prefix($tablename='') {
 			if ( $tablename != '' ) {
 				return $this->prefix .'_'. $tablename;
 			} else {
 				return $this->prefix;
 			}
 		}
-	} // DataBase class
-
-
-} // If database_included constant
+	} 
+	
+	/**
+	 * Abstract base class for Database access classes
+	 *
+	 * @abstract
+	 *
+	 * @package database
+	 * @subpackage  main
+	 * @since XOOPS
+	 *
+	 * @author      Kazumi Ono  <onokazu@xoops.org>
+	 * @copyright   copyright (c) 2000-2003 XOOPS.org
+	 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+	 * 
+	 * @deprecated
+	 */
+	abstract class XoopsDatabase extends IcmsDatabase { /* For Backwards compatibility */ }
+	
+}
 
 
 /**
@@ -131,11 +132,9 @@ if ( !defined("XOOPS_C_DATABASE_INCLUDED") ) {
  *
  * @deprecated
  */
-class Database
-{
-	function &getInstance() {
-		$inst =& XoopsDatabaseFactory::getDatabaseConnection();
-		return $inst;
+class Database {
+	static public function &getInstance() {
+		return XoopsDatabaseFactory::getDatabaseConnection();
 	}
 }
 
--- htdocs/class/database/databasefactory.php
+++ htdocs/class/database/databasefactory.php
@@ -5,44 +5,43 @@
  * @since XOOPS
  * @version $Id$
  *
- * @author ?
+ * @author		The XOOPS Project Community <http://www.xoops.org>
+ * @author      Gustavo Pilla  (aka nekro) <nekro@impresscms.org>
  * @copyright   copyright (c) 2000-2003 XOOPS.org
+ * @copyright   The ImpressCMS Project <http://www.impresscms.org>
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  */
 
 /**
- * XoopsDatabseFactory Class
+ * ImpressCMS Database Factory Class
  *
  * @package database
  * @subpackage  main
- * @since XOOPS
- * @version $Id$
  *
- * @author ?
- * @copyright   copyright (c) 2000-2003 XOOPS.org
+ * @author      Gustavo Pilla  (aka nekro) <nekro@impresscms.org>
+ * @copyright   The ImpressCMS Project <http://www.impresscms.org>
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  */
-class XoopsDatabaseFactory{
+class IcmsDatabaseFactory{
 
 	/**
 	 * Constructor
 	 *
 	 * Makes nothing.
 	 */
-	function XoopsDatabaseFactory(){}
+	protected function __construct(){ /* Empty! */ }
 
 	/**
 	 * Get a reference to the only instance of database class and connects to DB
-   *
-   * if the class has not been instantiated yet, this will also take
-   * care of that
 	 *
-   * @static
-   * @staticvar   object  The only instance of database class
-   * @return      object  Reference to the only instance of database class
+	 * if the class has not been instantiated yet, this will also take
+	 * care of that
+	 *
+	 * @static
+	 * @staticvar   object  The only instance of database class
+	 * @return      object  Reference to the only instance of database class
 	 */
-	function &getDatabaseConnection()
-	{
+	static public function &getDatabaseConnection(){
 		static $instance;
 		if (!isset($instance)) {
 			$file = XOOPS_ROOT_PATH.'/class/database/drivers/'.XOOPS_DB_TYPE.'/database.php';
@@ -55,7 +54,7 @@ class XoopsDatabaseFactory{
 			} else {
 				$class = 'Xoops'.ucfirst(XOOPS_DB_TYPE).'DatabaseProxy';
 			}
-			$instance =& new $class();
+			$instance = new $class();
 			$instance->setLogger(XoopsLogger::instance());
 			$instance->setPrefix(XOOPS_DB_PREFIX);
 			if (!$instance->connect()) {
@@ -65,18 +64,15 @@ class XoopsDatabaseFactory{
 		return $instance;
 	}
 
-
-
 	/**
 	 * Gets a reference to the only instance of database class. Currently
 	 * only being used within the installer.
 	 *
-   * @static
-   * @staticvar   object  The only instance of database class
-   * @return      object  Reference to the only instance of database class
+     * @static
+     * @staticvar   object  The only instance of database class
+     * @return      object  Reference to the only instance of database class
 	 */
-	function &getDatabase()
-	{
+	static public function &getDatabase(){
 		static $database;
 		if (!isset($database)) {
 			$file = XOOPS_ROOT_PATH.'/class/database/drivers/'.XOOPS_DB_TYPE.'/database.php';
@@ -86,7 +82,7 @@ class XoopsDatabaseFactory{
 			} else {
 				$class = 'Xoops'.ucfirst(XOOPS_DB_TYPE).'DatabaseProxy';
 			}
-			$database =& new $class();
+			$database = new $class();
 		}
 		return $database;
 	}
@@ -94,16 +90,31 @@ class XoopsDatabaseFactory{
 	/**
 	 * Gets the databaseupdater object.
 	 *
-   * @return	object  @link IcmsDatabaseUpdater
+     * @return	object  @link IcmsDatabaseUpdater
+     * @static
 	 */
-	function getDatabaseUpdater()
-	{
+	static public function getDatabaseUpdater(){
 		$file = XOOPS_ROOT_PATH.'/class/database/drivers/'.XOOPS_DB_TYPE.'/databaseupdater.php';
 		require_once $file;
 		$class = 'Icms'.ucfirst(XOOPS_DB_TYPE).'Databaseupdater';
-		$databaseUpdater =& new $class();
+		$databaseUpdater = new $class();
 		return $databaseUpdater;
 	}
 }
+
+/**
+ * XoopsDatabseFactory Class
+ *
+ * @package database
+ * @subpackage  main
+ * @since XOOPS
+ *
+ * @author		The XOOPS Project Community <http://www.xoops.org>
+ * @copyright   copyright (c) 2000-2003 XOOPS.org
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * 
+ * @deprecated
+ */
+class XoopsDatabaseFactory extends IcmsDatabaseFactory { /* For backwards compatibility */ } 
 
 ?>
