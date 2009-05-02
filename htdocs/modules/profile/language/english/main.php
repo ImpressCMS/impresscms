d301d280266aec0d6f195311a6ba7d6f9d8db661
Niels Drost <underdog@impresscms.org>
2009-05-02 14:21:42 +0000

    Ongoing process of cleaning up unused language constants for profile refs #128

--- htdocs/modules/profile/language/english/main.php
+++ htdocs/modules/profile/language/english/main.php
@@ -16,12 +16,12 @@
  */
 
 define("_PROFILE_MA_ERRORDURINGSAVE", "Error during save");
-define('_PROFILE_MA_USERREG','User Registration');
-define('_PROFILE_MA_NICKNAME','Username');
+//define('_PROFILE_MA_USERREG','User Registration');
+//define('_PROFILE_MA_NICKNAME','Username');
 define('_PROFILE_MA_REALNAME', 'Real Name');
 define('_PROFILE_MA_EMAIL','Email');
-define('_PROFILE_MA_ALLOWVIEWEMAIL','Allow other users to view my email address');
-define('_PROFILE_MA_TIMEZONE','Time Zone');
+//define('_PROFILE_MA_ALLOWVIEWEMAIL','Allow other users to view my email address');
+//define('_PROFILE_MA_TIMEZONE','Time Zone');
 define('_PROFILE_MA_AVATAR','Avatar');
 define('_PROFILE_MA_VERIFYPASS','Verify Password');
 define('_PROFILE_MA_SUBMIT','Submit');
@@ -29,7 +29,7 @@ define('_PROFILE_MA_USERNAME','Username');
 define('_PROFILE_MA_USERLOGINNAME','Login name');
 define('_PROFILE_MA_FINISH','Finish');
 define('_PROFILE_MA_REGISTERNG','Could not register new user.');
-define('_PROFILE_MA_MAILOK','Receive occasional email notices <br />from administrators and moderators?');
+//define('_PROFILE_MA_MAILOK','Receive occasional email notices <br />from administrators and moderators?');
 define('_PROFILE_MA_DISCLAIMER','Disclaimer');
 define('_PROFILE_MA_IAGREE','I agree to the above');
 define('_PROFILE_MA_UNEEDAGREE', 'Sorry, you have to agree to our disclaimer to get registered.');
@@ -54,17 +54,17 @@ define('_PROFILE_MA_HASJUSTREG','%s has just registered!');
 
 define('_PROFILE_MA_INVALIDMAIL','ERROR: Invalid email');
 define('_PROFILE_MA_EMAILNOSPACES','ERROR: Email addresses do not contain spaces.');
-define('_PROFILE_MA_INVALIDNICKNAME','ERROR: Invalid Username');
-define("_PROFILE_MA_INVALIDDISPLAYNAME", "ERROR: Invalid Displayname");
-define('_PROFILE_MA_NICKNAMETOOLONG','Username is too long. It must be less than %s characters.');
+//define('_PROFILE_MA_INVALIDNICKNAME','ERROR: Invalid Username');
+//define("_PROFILE_MA_INVALIDDISPLAYNAME", "ERROR: Invalid Displayname");
+//define('_PROFILE_MA_NICKNAMETOOLONG','Username is too long. It must be less than %s characters.');
 define('_PROFILE_MA_DISPLAYNAMETOOLONG','Displayname is too long. It must be less than %s characters.');
-define('_PROFILE_MA_NICKNAMETOOSHORT','Username is too short. It must be more than %s characters.');
+//define('_PROFILE_MA_NICKNAMETOOSHORT','Username is too short. It must be more than %s characters.');
 define('_PROFILE_MA_DISPLAYNAMETOOSHORT','Displayname is too short. It must be more than %s characters.');
-define('_PROFILE_MA_NAMERESERVED','ERROR: Name is reserved.');
+//define('_PROFILE_MA_NAMERESERVED','ERROR: Name is reserved.');
 define('_PROFILE_MA_DISPLAYNAMERESERVED','ERROR: Displayname is reserved.');
-define('_PROFILE_MA_NICKNAMENOSPACES','There cannot be any spaces in the Username.');
-define('_PROFILE_MA_DISPLAYNAMENOSPACES','There cannot be any spaces in the Displayname.');
-define('_PROFILE_MA_NICKNAMETAKEN','ERROR: Username taken.');
+//define('_PROFILE_MA_NICKNAMENOSPACES','There cannot be any spaces in the Username.');
+//define('_PROFILE_MA_DISPLAYNAMENOSPACES','There cannot be any spaces in the Displayname.');
+//define('_PROFILE_MA_NICKNAMETAKEN','ERROR: Username taken.');
 define('_PROFILE_MA_DISPLAYNAMETAKEN','ERROR: Displayname taken.');
 define('_PROFILE_MA_LOGINNAMETAKEN','ERROR: Loginname taken.');
 define('_PROFILE_MA_EMAILTAKEN','ERROR: Email address already registered.');
@@ -84,23 +84,23 @@ define("_PROFILE_MA_DEFAULT", "Basic Information");
 
 //%%%%%%		File Name userinfo.php 		%%%%%
 define('_PROFILE_MA_SELECTNG','No User Selected! Please go back and try again.');
-define('_PROFILE_MA_PM','PM');
+//define('_PROFILE_MA_PM','PM');
 define('_PROFILE_MA_EDITPROFILE','Edit Profile');
 define('_PROFILE_MA_LOGOUT','Logout');
-define('_PROFILE_MA_INBOX','Inbox');
+//define('_PROFILE_MA_INBOX','Inbox');
 define('_PROFILE_MA_ALLABOUT','All about %s');
-define('_PROFILE_MA_STATISTICS','Statistics');
-define('_PROFILE_MA_MYINFO','My Info');
-define('_PROFILE_MA_BASICINFO','Basic information');
-define('_PROFILE_MA_MOREABOUT','More About Me');
+//define('_PROFILE_MA_STATISTICS','Statistics');
+//define('_PROFILE_MA_MYINFO','My Info');
+//define('_PROFILE_MA_BASICINFO','Basic information');
+//define('_PROFILE_MA_MOREABOUT','More About Me');
 define('_PROFILE_MA_SHOWALL','Show All');
 
 //%%%%%%		File Name edituser.php 		%%%%%
 define('_PROFILE_MA_PROFILE','Profile');
 define('_PROFILE_MA_DISPLAYNAME','Displayname');
-define('_PROFILE_MA_SHOWSIG','Always attach my signature');
-define('_PROFILE_MA_CDISPLAYMODE','Comments Display Mode');
-define('_PROFILE_MA_CSORTORDER','Comments Sort Order');
+//define('_PROFILE_MA_SHOWSIG','Always attach my signature');
+//define('_PROFILE_MA_CDISPLAYMODE','Comments Display Mode');
+//define('_PROFILE_MA_CSORTORDER','Comments Sort Order');
 define('_PROFILE_MA_PASSWORD','Password');
 define('_PROFILE_MA_TYPEPASSTWICE','(type a new password twice to change it)');
 define('_PROFILE_MA_SAVECHANGES','Save Changes');
@@ -109,10 +109,10 @@ define('_PROFILE_MA_PASSNOTSAME','Both passwords are different. They must be ide
 define('_PROFILE_MA_PWDTOOSHORT','Sorry, your password must be at least <b>%s</b> characters long.');
 define("_PROFILE_MA_NOPASSWORD", "Please input a password");
 define('_PROFILE_MA_PROFUPDATED','Your Profile Updated!');
-define('_PROFILE_MA_USECOOKIE','Store my user name in a cookie for 1 year');
+//define('_PROFILE_MA_USECOOKIE','Store my user name in a cookie for 1 year');
 define('_PROFILE_MA_NO','No');
 define('_PROFILE_MA_DELACCOUNT','Delete Account');
-define('_PROFILE_MA_MYAVATAR', 'My Avatar');
+//define('_PROFILE_MA_MYAVATAR', 'My Avatar');
 define('_PROFILE_MA_UPLOADMYAVATAR', 'Upload Avatar');
 define('_PROFILE_MA_MAXPIXEL','Max Pixels');
 define('_PROFILE_MA_MAXIMGSZ','Max Image Size (Bytes)');
@@ -120,10 +120,10 @@ define('_PROFILE_MA_SELFILE','Select file');
 define('_PROFILE_MA_OLDDELETED','Your old avatar will be deleted!');
 define('_PROFILE_MA_CHOOSEAVT', 'Choose avatar from the available list');
 
-define('_PROFILE_MA_PRESSLOGIN', 'Press the button below to login');
+//define('_PROFILE_MA_PRESSLOGIN', 'Press the button below to login');
 
 define('_PROFILE_MA_ADMINNO', 'User in the webmasters group cannot be removed');
-define('_PROFILE_MA_GROUPS', 'User\'s Groups');
+//define('_PROFILE_MA_GROUPS', 'User\'s Groups');
 
 define('_PROFILE_MA_NOPERMISS','Sorry, you dont have the permission to perform this action!');
 define('_PROFILE_MA_SURETODEL','Are you sure you want to delete your account?');
--- htdocs/modules/profile/language/english/modinfo.php
+++ htdocs/modules/profile/language/english/modinfo.php
@@ -17,9 +17,9 @@
 
 define("_MI_PROFILE_NUMBPICT_TITLE","Number of Pictures");
 define("_MI_PROFILE_NUMBPICT_DESC" ,"Number of pictures a user can have in their page");
-define("_MI_PROFILE_ADMENU1","Home");
-define("_MI_PROFILE_ADMENU2" ,"About");
-define("_MI_PROFILE_SMNAME1" ,"Submit");
+//define("_MI_PROFILE_ADMENU1","Home");
+//define("_MI_PROFILE_ADMENU2" ,"About");
+//define("_MI_PROFILE_SMNAME1" ,"Submit");
 define("_MI_PROFILE_THUMW_TITLE" , "Thumb Width");
 define("_MI_PROFILE_THUMBW_DESC" , "Thumbnails width in pixels<br />This means your picture thumbnail will be<br />most of this size in width<br />All proportions are maintained");
 define("_MI_PROFILE_THUMBH_TITLE" , "Thumb Height");
@@ -49,8 +49,8 @@ define("_MI_PROFILE_PICTURE_NEWPIC_NOTIFYSBJ","{X_OWNER_NAME} has submitted a ne
 //define("_MI_PROFILE_HOTTEST_DESC","This block will show the hottest albums");
 //define("_MI_PROFILE_HOTFRIENDS","Hot Friends");
 //define("_MI_PROFILE_HOTFRIENDS_DESC","This block shows the users hot friends that have been added");
-define("_MI_PROFILE_PICTURE_TEMPLATEINDEXDESC","This template shows the pictures of the user");
-define("_MI_PROFILE_PICTURE_TEMPLATEFRIENDSDESC","This template shows the friends of the user");
+//define("_MI_PROFILE_PICTURE_TEMPLATEINDEXDESC","This template shows the pictures of the user");
+//define("_MI_PROFILE_PICTURE_TEMPLATEFRIENDSDESC","This template shows the friends of the user");
 define("_MI_PROFILE_MYFRIENDS","My Friends");
 define("_MI_PROFILE_FRIENDSPERPAGE_TITLE" , "Friends per page");
 define("_MI_PROFILE_FRIENDSPERPAGE_DESC" , "Set the number of friends to show per page<br />In the my Friends page");
@@ -62,19 +62,19 @@ define("_MI_PROFILE_DELETEPHYSICAL_TITLE","Delete files from the upload folder t
 define("_MI_PROFILE_DELETEPHYSICAL_DESC","Confirming yes here, will allow the script to delete the files from the uploaded data in the database as well.<br /> Be careful about this feature, if you exclude the files from the folder and not only in the database, some people who may have linked to the image directly in another part of the site may also lose their content;<br /> at the same time if you don't exclude them, you may use to much space in the server hard disk.<br />Configure this item well for your needs.");
 
 define("_MI_PROFILE_MYVIDEOS","My Videos");
-define("_MI_PROFILE_PICTURE_TEMPLATEALBUMDESC","Template for the picture gallery");
+//define("_MI_PROFILE_PICTURE_TEMPLATEALBUMDESC","Template for the picture gallery");
 define("_MI_PROFILE_MYPICTURES","My Photos");
 define("_MI_PROFILE_MODULEDESC","This module simulates a social network software like MySpace or Orkut.");
 define("_MI_PROFILE_TUBEW_TITLE","Width of the YouTube videos");
 define("_MI_PROFILE_TUBEW_DESC","The width in pixels of the YouTube video player");
 define("_MI_PROFILE_TUBEH_TITLE","Height of the YouTube videos");
 define("_MI_PROFILE_TUBEH_DESC","The height in pixels of the YouTube video player");
-define("_MI_PROFILE_PICTURE_TEMPLATESCRAPBOOKDESC","Template for the Scrapbook");
-define("_MI_PROFILE_PICTURE_TEMPLATESEUTUBODESC","Template for the videos section");
-define("_MI_PROFILE_PICTURE_TEMPLATETRIBESDESC","Template for the Tribes");
+//define("_MI_PROFILE_PICTURE_TEMPLATESCRAPBOOKDESC","Template for the Scrapbook");
+//define("_MI_PROFILE_PICTURE_TEMPLATESEUTUBODESC","Template for the videos section");
+//define("_MI_PROFILE_PICTURE_TEMPLATETRIBESDESC","Template for the Tribes");
 define("_MI_PROFILE_MYSCRAPS","My Scraps");
 define("_MI_PROFILE_MYTRIBES","My Tribes");
-define("_MI_PROFILE_TEMPLATENAVBARDESC","Template for the upper navbar used in all pages");
+//define("_MI_PROFILE_TEMPLATENAVBARDESC","Template for the upper navbar used in all pages");
 
 define("_MI_PROFILE_VIDEOSPERPAGE_TITLE","Videos per Page");
 define("_MI_PROFILE_VIDEO_NOTIFYTIT","Videos");
@@ -98,22 +98,22 @@ define("_MI_PROFILE_MAINTUBEH_DESC","Height of the video, that shows in the fron
 
 //24/09/2007
 define("_MI_PROFILE_MYCONFIGS","My Settings");
-define("_MI_PROFILE_PICTURE_TEMPLATECONFIGSDESC","Template settings for the user");
-define("_MI_PROFILE_PICTURE_TEMPLATEFOOTERDESC","Template for the footer of the module");
-define("_MI_PROFILE_PICTURE_TEMPLATEEDITTRIBE","Template for the Tribes page atributes");
-define("_MI_PROFILE_LICENSE",'');
+//define("_MI_PROFILE_PICTURE_TEMPLATECONFIGSDESC","Template settings for the user");
+//define("_MI_PROFILE_PICTURE_TEMPLATEFOOTERDESC","Template for the footer of the module");
+//define("_MI_PROFILE_PICTURE_TEMPLATEEDITTRIBE","Template for the Tribes page atributes");
+//define("_MI_PROFILE_LICENSE",'');
 
 //19/10/2007
 define("_MI_PROFILE_TRIBESPERPAGE_TITLE","Tribes per page");
 define("_MI_PROFILE_TRIBESPERPAGE_DESC","Tribes per page before pagination show up");
-define("_MI_PROFILE_PICTURE_TEMPLATESEARCHRESULTDESC","This template shows the results of a search for comunities");
-define("_MI_PROFILE_PICTURE_TEMPLATETRIBEDESC","This template shows a Tribe and its members");
+//define("_MI_PROFILE_PICTURE_TEMPLATESEARCHRESULTDESC","This template shows the results of a search for comunities");
+//define("_MI_PROFILE_PICTURE_TEMPLATETRIBEDESC","This template shows a Tribe and its members");
 
 //22/10/2007
 define("_MI_PROFILE_MYPROFILE","My Profile");
 define("_MI_PROFILE_SEARCH","Search Members");
-define("_MI_PROFILE_PICTURE_TEMPLATESEARCHRESULTSDESC","Template for the search results");
-define("_MI_PROFILE_PICTURE_TEMPLATESEARCHFORMDESC","Template for the search form");
+//define("_MI_PROFILE_PICTURE_TEMPLATESEARCHRESULTSDESC","Template for the search results");
+//define("_MI_PROFILE_PICTURE_TEMPLATESEARCHFORMDESC","Template for the search form");
 
 //26/10/2007
 define("_MI_PROFILE_ENABLEPICT_TITLE","Enable pictures section");
@@ -138,7 +138,7 @@ define("_MI_PROFILE_IMGORDER_TITLE", "Pictures Order");
 define("_MI_PROFILE_IMGORDER_DESC", "Show the newest pictures first?");
 
 //08/04/2008
-define("_MI_PROFILE_PICTURE_TEMPLATENOTIFICATIONS","Template for the notifications");
+//define("_MI_PROFILE_PICTURE_TEMPLATENOTIFICATIONS","Template for the notifications");
 
 //11/04/2008
 define("_MI_PROFILE_FRIENDSHIP_NOTIFYTIT","Friendships");
@@ -149,12 +149,12 @@ define("_MI_PROFILE_FRIEND_NEWPETITION_NOTIFYDSC","Notify me when someone ask fo
 define("_MI_PROFILE_FRIEND_NEWPETITION_NOTIFYSBJ","Someone has just asked to be your friend");
 
 //13/04/2008
-define("_MI_PROFILE_PICTURE_TEMPLATEFANS","Template for the fans page");
+//define("_MI_PROFILE_PICTURE_TEMPLATEFANS","Template for the fans page");
 
 //17/07/2008
 define("_MI_PROFILE_ENABLEAUDIO_TITLE","Enable audio section");
 define("_MI_PROFILE_ENABLEAUDIO_DESC","Enabling audio section for the users, will enable the audio playlist");
-define("_MI_PROFILE_PICTURE_TEMPLATEAUDIODESC","Template of audios page");
+//define("_MI_PROFILE_PICTURE_TEMPLATEAUDIODESC","Template of audios page");
 define("_MI_PROFILE_NUMBAUDIO_TITLE","Max number of audio for a user");
 define("_MI_PROFILE_AUDIOSPERPAGE_TITLE","Number of mp3 files per page");
 
@@ -179,8 +179,8 @@ define("_PROFILE_MI_PERMISSIONS", "Permissions");
 define("_PROFILE_MI_FINDUSER", "Find Users");
 
 //User Profile Category
-define("_PROFILE_MI_CATEGORY_TITLE", "User Profile");
-define("_PROFILE_MI_CATEGORY_DESC", "For those user fields");
+//define("_PROFILE_MI_CATEGORY_TITLE", "User Profile");
+//define("_PROFILE_MI_CATEGORY_DESC", "For those user fields");
 
 //User Profile Fields
 define("_PROFILE_MI_AIM_TITLE", "AIM");
@@ -205,8 +205,8 @@ define("_PROFILE_MI_OCCUPATION_TITLE", "Occupation");
 define("_PROFILE_MI_OCCUPATION_DESCRIPTION", "");
 define("_PROFILE_MI_URL_TITLE", "Website");
 define("_PROFILE_MI_URL_DESCRIPTION", "");
-define("_PROFILE_MI_NEWEMAIL_TITLE", "New Email");
-define("_PROFILE_MI_NEWEMAIL_DESCRIPTION", "Variable for storing a proposed new email address until confirmation comes from a mail sent to the old one. See modules/profile/changemail.php");
+//define("_PROFILE_MI_NEWEMAIL_TITLE", "New Email");
+//define("_PROFILE_MI_NEWEMAIL_DESCRIPTION", "Variable for storing a proposed new email address until confirmation comes from a mail sent to the old one. See modules/profile/changemail.php");
 
 //Configuration categories
 define('_PROFILE_MI_CAT_PERSONAL', 'Personal');
@@ -224,48 +224,48 @@ define("_PROFILE_MI_CAT_USER_DSC", "");
 
 //Configuration items
 define("_PROFILE_MI_PROFILE_SEARCH", "Show latest submissions by user on user profile");
-define("_PROFILE_MI_MAX_UNAME", "Maximum Username Length");
-define("_PROFILE_MI_MAX_UNAME_DESC", "This indicates the maximum number of characters, a username may have");
-define("_PROFILE_MI_MIN_UNAME", "Minimum Username Length");
-define("_PROFILE_MI_MIN_UNAME_DESC", "This indicates the minimum number of characters, a username must have");
-define("_PROFILE_MI_DISPLAY_DISCLAIMER", "Display Disclaimer");
-define("_PROFILE_MI_DISPLAY_DISCLAIMER_DESC", "If enabled, a disclaimer will be shown on the registration form");
-define("_PROFILE_MI_DISCLAIMER", "Disclaimer Text");
-define("_PROFILE_MI_DISCLAIMER_DESC", "This text will be shown on the registration form, if enabled above");
-define("_PROFILE_MI_BAD_UNAMES", "Enter names that should not be selected as username or displayname");
-define("_PROFILE_MI_BAD_UNAMES_DESC", "Separate each with a <b>|</b>, case insensitive, regex enabled.");
-define("_PROFILE_MI_BAD_EMAILS", "Enter emails that should not be selected");
-define("_PROFILE_MI_BAD_EMAILS_DESC", "Separate each with a <b>|</b>, case insensitive, regex enabled.");
-define("_PROFILE_MI_MINPASS", "Minimum password length");
-define("_PROFILE_MI_NEWUNOTIFY", "Notify by mail when a new user is registered?");
-define("_PROFILE_MI_NOTIFYTO", "Select group to which new user notification mail will be sent");
-define("_PROFILE_MI_ACTVTYPE", "Select activation type of newly registered users");
-define("_PROFILE_MI_USERACTV","Requires activation by user (recommended)");
-define("_PROFILE_MI_AUTOACTV","Activate automatically");
-define("_PROFILE_MI_ADMINACTV","Activation by administrators");
-define("_PROFILE_MI_ACTVGROUP", "Select group to which activation mail will be sent");
-define("_PROFILE_MI_ACTVGROUP_DESC", "Valid only when 'Activation by administrators' is selected");
-define("_PROFILE_MI_UNAMELVL","Select the level of strictness for username filtering");
-define("_PROFILE_MI_STRICT","Strict (only alphabets and numbers)");
-define("_PROFILE_MI_MEDIUM","Medium");
-define("_PROFILE_MI_LIGHT","Light (recommended for multi-byte chars)");
-define("_PROFILE_MI_ALLOWREG", "Allow new user registration?");
-define("_PROFILE_MI_ALLOWREG_DESC", "Select yes to accept new user registration");
-define("_PROFILE_MI_SELFDELETE", "Allow users to delete own account?");
-define("_PROFILE_MI_SELFDELETE_DESC", "");
-define("_PROFILE_MI_ALLOWCHGMAIL", "Allow users to change email address?");
-define("_PROFILE_MI_ALLOWCHGMAIL_DESC", "");
+//define("_PROFILE_MI_MAX_UNAME", "Maximum Username Length");
+//define("_PROFILE_MI_MAX_UNAME_DESC", "This indicates the maximum number of characters, a username may have");
+//define("_PROFILE_MI_MIN_UNAME", "Minimum Username Length");
+//define("_PROFILE_MI_MIN_UNAME_DESC", "This indicates the minimum number of characters, a username must have");
+//define("_PROFILE_MI_DISPLAY_DISCLAIMER", "Display Disclaimer");
+//define("_PROFILE_MI_DISPLAY_DISCLAIMER_DESC", "If enabled, a disclaimer will be shown on the registration form");
+//define("_PROFILE_MI_DISCLAIMER", "Disclaimer Text");
+//define("_PROFILE_MI_DISCLAIMER_DESC", "This text will be shown on the registration form, if enabled above");
+//define("_PROFILE_MI_BAD_UNAMES", "Enter names that should not be selected as username or displayname");
+//define("_PROFILE_MI_BAD_UNAMES_DESC", "Separate each with a <b>|</b>, case insensitive, regex enabled.");
+//define("_PROFILE_MI_BAD_EMAILS", "Enter emails that should not be selected");
+//define("_PROFILE_MI_BAD_EMAILS_DESC", "Separate each with a <b>|</b>, case insensitive, regex enabled.");
+//define("_PROFILE_MI_MINPASS", "Minimum password length");
+//define("_PROFILE_MI_NEWUNOTIFY", "Notify by mail when a new user is registered?");
+//define("_PROFILE_MI_NOTIFYTO", "Select group to which new user notification mail will be sent");
+//define("_PROFILE_MI_ACTVTYPE", "Select activation type of newly registered users");
+//define("_PROFILE_MI_USERACTV","Requires activation by user (recommended)");
+//define("_PROFILE_MI_AUTOACTV","Activate automatically");
+//define("_PROFILE_MI_ADMINACTV","Activation by administrators");
+//define("_PROFILE_MI_ACTVGROUP", "Select group to which activation mail will be sent");
+//define("_PROFILE_MI_ACTVGROUP_DESC", "Valid only when 'Activation by administrators' is selected");
+//define("_PROFILE_MI_UNAMELVL","Select the level of strictness for username filtering");
+//define("_PROFILE_MI_STRICT","Strict (only alphabets and numbers)");
+//define("_PROFILE_MI_MEDIUM","Medium");
+//define("_PROFILE_MI_LIGHT","Light (recommended for multi-byte chars)");
+//define("_PROFILE_MI_ALLOWREG", "Allow new user registration?");
+//define("_PROFILE_MI_ALLOWREG_DESC", "Select yes to accept new user registration");
+//define("_PROFILE_MI_SELFDELETE", "Allow users to delete own account?");
+//define("_PROFILE_MI_SELFDELETE_DESC", "");
+//define("_PROFILE_MI_ALLOWCHGMAIL", "Allow users to change email address?");
+//define("_PROFILE_MI_ALLOWCHGMAIL_DESC", "");
 define("_PROFILE_MI_SHOWEMPTY", "Show empty fields");
 define("_PROFILE_MI_SHOWEMPTY_DESC", "If set to 'no', fields without a value will not show up on user profiles");
 
 //Pages
-define("_PROFILE_MI_PAGE_INFO", "User Info");
-define("_PROFILE_MI_PAGE_EDIT", "Edit User");
-define("_PROFILE_MI_PAGE_SEARCH", "Search");
+//define("_PROFILE_MI_PAGE_INFO", "User Info");
+//define("_PROFILE_MI_PAGE_EDIT", "Edit User");
+//define("_PROFILE_MI_PAGE_SEARCH", "Search");
 
 //blocks
-define("_MI_SPROFILE_BLOCK_NEW_MEMBERS", "New members");
-define("_MI_SPROFILE_BLOCK_NEW_MEMBERS_DSC", "Recently subscribed members");
+//define("_MI_SPROFILE_BLOCK_NEW_MEMBERS", "New members");
+//define("_MI_SPROFILE_BLOCK_NEW_MEMBERS_DSC", "Recently subscribed members");
 
 define("_MI_SPROFILE_PERPAGE", "Users Per Page");
 define("_MI_SPROFILE_PERPAGE_DSC", "");
