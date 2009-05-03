51c695c3244ccc28376cae087f5c98c0a730b29d
Rene Sato <sato-san@impresscms.org>
2009-05-03 17:07:43 +0000

    size changed from 5 to 10

--- htdocs/class/icmsform/elements/icmsformpageelement.php
+++ htdocs/class/icmsform/elements/icmsformpageelement.php
@@ -15,7 +15,7 @@ class IcmsFormPageElement extends XoopsFormElementTray {
 	public function __construct($object, $key) {
 		$this->XoopsFormElementTray(_AM_VISIBLEIN , ' ', $key . '_password_tray');
     	$icms_page_handler = & xoops_gethandler('page');
-		$visible_label = new XoopsFormLabel('', '<select name="visiblein[]" id="visiblein[]" multiple="multiple" size="5">'.$this->getPageSelOptions($icms_page_handler, $object->getVar('visiblein')).'</select>');
+		$visible_label = new XoopsFormLabel('', '<select name="visiblein[]" id="visiblein[]" multiple="multiple" size="10">'.$this->getPageSelOptions($icms_page_handler, $object->getVar('visiblein')).'</select>');
 		$this->addElement($visible_label);
   	}
   	
