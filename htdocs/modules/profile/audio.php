fc1a1cc1be5b22cb877007220475526832c5863b
Sina Asghari <stranger@impresscms.org>
2009-07-21 18:53:13 +0000

    Now audios works too!

--- htdocs/modules/profile/audio.php
+++ htdocs/modules/profile/audio.php
@@ -13,29 +13,29 @@
 /**
  * Edit a Audio
  *
- * @param object $audioObj ProfileAudio object to be edited
+ * @param object $audiosObj ProfileAudio object to be edited
 */
-function editaudio($audioObj)
+function editaudios($audiosObj)
 {
-	global $profile_audio_handler, $xoTheme, $icmsTpl, $icmsUser;
+	global $profile_audios_handler, $xoTheme, $icmsTpl, $icmsUser;
 
-	if (!$audioObj->isNew()){
-		if (!$audioObj->userCanEditAndDelete()) {
-			redirect_header($audioObj->getItemLink(true), 3, _NOPERM);
+	if (!$audiosObj->isNew()){
+		if (!$audiosObj->userCanEditAndDelete()) {
+			redirect_header($audiosObj->getItemLink(true), 3, _NOPERM);
 		}
-		$audioObj->hideFieldFromForm(array('creation_time', 'uid_owner', 'meta_keywords', 'meta_description', 'short_url'));
-		$sform = $audioObj->getSecureForm(_MD_PROFILE_AUDIO_EDIT, 'addaudio');
-		$sform->assign($icmsTpl, 'profile_audioeditform');
-		$icmsTpl->assign('profile_category_path', $audioObj->getVar('title') . ' > ' . _EDIT);
+		$audiosObj->hideFieldFromForm(array('creation_time', 'uid_owner', 'meta_keywords', 'meta_description', 'short_url'));
+		$sform = $audiosObj->getSecureForm(_MD_PROFILE_AUDIOS_EDIT, 'addaudios');
+		$sform->assign($icmsTpl, 'profile_audioseditform');
+		$icmsTpl->assign('profile_category_path', $audiosObj->getVar('title') . ' > ' . _EDIT);
 	} else {
-		if (!$profile_audio_handler->userCanSubmit()) {
+		if (!$profile_audios_handler->userCanSubmit()) {
 			redirect_header(PROFILE_URL, 3, _NOPERM);
 		}
-		$audioObj->setVar('uid_owner', $icmsUser->uid());
-		$audioObj->setVar('creation_time', time());
-		$audioObj->hideFieldFromForm(array('creation_time', 'uid_owner', 'meta_keywords', 'meta_description', 'short_url'));
-		$sform = $audioObj->getSecureForm(_MD_PROFILE_AUDIO_SUBMIT, 'addaudio');
-		$sform->assign($icmsTpl, 'profile_audioform');
+		$audiosObj->setVar('uid_owner', $icmsUser->uid());
+		$audiosObj->setVar('creation_time', time());
+		$audiosObj->hideFieldFromForm(array('creation_time', 'uid_owner', 'meta_keywords', 'meta_description', 'short_url'));
+		$sform = $audiosObj->getSecureForm(_MD_PROFILE_AUDIOS_SUBMIT, 'addaudios');
+		$sform->assign($icmsTpl, 'profile_audiosform');
 		$icmsTpl->assign('profile_category_path', _SUBMIT);
 	}
 
@@ -46,7 +46,7 @@ function editaudio($audioObj)
 $profile_template = 'profile_audio.html';
 include_once 'header.php';
 
-$profile_audio_handler = icms_getModuleHandler('audio');
+$profile_audios_handler = icms_getModuleHandler('audio');
 
 /** Use a naming convention that indicates the source of the content of the variable */
 $clean_op = '';
@@ -56,40 +56,40 @@ if (isset($_POST['op'])) $clean_op = $_POST['op'];
 
 /** Again, use a naming convention that indicates the source of the content of the variable */
 global $icmsUser;
-$clean_audio_id = isset($_GET['audio_id']) ? intval($_GET['audio_id']) : 0 ;
+$clean_audios_id = isset($_GET['audios_id']) ? intval($_GET['audios_id']) : 0 ;
 $clean_uid = isset($_GET['uid']) ? intval($_GET['uid']) : 0 ;
 $real_uid = is_object($icmsUser)?intval($icmsUser->uid()):0;
-$audioObj = $profile_audio_handler->get($clean_audio_id);
+$audiosObj = $profile_audios_handler->get($clean_audios_id);
 /** Create a whitelist of valid values, be sure to use appropriate types for each value
  * Be sure to include a value for no parameter, if you have a default condition
  */
-$valid_op = array ('mod','addaudio','del','');
+$valid_op = array ('mod','addaudios','del','');
 /**
  * Only proceed if the supplied operation is a valid operation
  */
 if (in_array($clean_op,$valid_op,true)){
   switch ($clean_op) {
 	case "mod":
-		$audioObj = $profile_audio_handler->get($clean_audio_id);
-		if ($clean_audio_id > 0 && $audioObj->isNew()) {
+		$audiosObj = $profile_audios_handler->get($clean_audios_id);
+		if ($clean_audios_id > 0 && $audiosObj->isNew()) {
 			redirect_header(icms_getPreviousPage('index.php'), 3, _NOPERM);
 		}
-		editaudio($audioObj);
+		editaudios($audiosObj);
 		break;
 
-	case "addaudio":
+	case "addaudios":
         if (!$xoopsSecurity->check()) {
         	redirect_header(icms_getPreviousPage('index.php'), 3, _MD_PROFILE_SECURITY_CHECK_FAILED . implode('<br />', $xoopsSecurity->getErrors()));
         }
          include_once ICMS_ROOT_PATH.'/kernel/icmspersistablecontroller.php';
-        $controller = new IcmsPersistableController($profile_audio_handler);
-		$controller->storeFromDefaultForm(_MD_PROFILE_AUDIO_CREATED, _MD_PROFILE_AUDIO_MODIFIED);
+        $controller = new IcmsPersistableController($profile_audios_handler);
+		$controller->storeFromDefaultForm(_MD_PROFILE_AUDIOS_CREATED, _MD_PROFILE_AUDIOS_MODIFIED);
 		break;
 
 	case "del":
-		$audioObj = $profile_audio_handler->get($clean_audio_id);
-		if (!$audioObj->userCanEditAndDelete()) {
-			redirect_header($audioObj->getItemLink(true), 3, _NOPERM);
+		$audiosObj = $profile_audios_handler->get($clean_audios_id);
+		if (!$audiosObj->userCanEditAndDelete()) {
+			redirect_header($audiosObj->getItemLink(true), 3, _NOPERM);
 		}
 		if (isset($_POST['confirm'])) {
 		    if (!$xoopsSecurity->check()) {
@@ -97,32 +97,32 @@ if (in_array($clean_op,$valid_op,true)){
 		    }
 		}
   	    include_once ICMS_ROOT_PATH.'/kernel/icmspersistablecontroller.php';
-        $controller = new IcmsPersistableController($profile_audio_handler);
+        $controller = new IcmsPersistableController($profile_audios_handler);
 		$controller->handleObjectDeletionFromUserSide();
-		$icmsTpl->assign('profile_category_path', $audioObj->getVar('title') . ' > ' . _DELETE);
+		$icmsTpl->assign('profile_category_path', $audiosObj->getVar('title') . ' > ' . _DELETE);
 
 		break;
 
 	default:
 		$values = array();
 		if($real_uid){
-			$audioObj = $profile_audio_handler->get($clean_audio_id);
-			if ($clean_audio_id > 0 && $audioObj->isNew()) {
+			$audiosObj = $profile_audios_handler->get($clean_audios_id);
+			if ($clean_audios_id > 0 && $audiosObj->isNew()) {
 				redirect_header(icms_getPreviousPage('index.php'), 3, _NOPERM);
 			}
-			editaudio($audioObj);
+			editaudios($audiosObj);
 		}
-		if($clean_audio_id > 0){
-			$audioArray = $profile_audio_handler->getAudio($clean_audio_id);
-			$profile_audio_handler->updateCounter($clean_audio_id);
-			$icmsTpl->assign('profile_single_audio', $audioObj->toArray());
-			$icmsTpl->assign('profile_category_path', $audioArray['title']);
+		if($clean_audios_id > 0){
+			$audiosArray = $profile_audios_handler->getAudio($clean_audios_id);
+			$profile_audios_handler->updateCounter($clean_audios_id);
+			$icmsTpl->assign('profile_single_audio', $audiosObj->toArray());
+			$icmsTpl->assign('profile_category_path', $audiosArray['title']);
 		}elseif($clean_uid > 0){
-			$audioArray = $profile_audio_handler->getAudio(false, false, $clean_uid);
-			$icmsTpl->assign('profile_allaudio', $audioArray);
+			$audiosArray = $profile_audios_handler->getAudios(false, false, $clean_uid);
+			$icmsTpl->assign('profile_allaudios', $audiosArray);
 		}elseif($real_uid > 0){
-			$audioArray = $profile_audio_handler->getAudio(false, false, $real_uid);
-			$icmsTpl->assign('profile_allaudio', $audioArray);
+			$audiosArray = $profile_audios_handler->getAudios(false, false, $real_uid);
+			$icmsTpl->assign('profile_allaudios', $audiosArray);
 		}else{
 			redirect_header(PROFILE_URL);
 		}
@@ -131,7 +131,7 @@ if (in_array($clean_op,$valid_op,true)){
 		/**
 		 * Generating meta information for this page
 		 */
-		$icms_metagen = new IcmsMetagen($audioObj->getVar('title'), $audioObj->getVar('meta_keywords','n'), $audioObj->getVar('meta_description', 'n'));
+		$icms_metagen = new IcmsMetagen($audiosObj->getVar('title'), $audiosObj->getVar('meta_keywords','n'), $audiosObj->getVar('meta_description', 'n'));
 		$icms_metagen->createMetaTags();
 
 		break;
