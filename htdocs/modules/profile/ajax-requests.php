fd1a9cc058ecb451f01b6f88245f75f9e2020f56
Sina Asghari <stranger@impresscms.org>
2009-07-22 17:07:38 +0000

    Removed unneeded files and added ajax requests file for ajax requests to com in the module.

---
+++ htdocs/modules/profile/ajax-requests.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File supposed to work for ajax requests.
+ *
+ *
+ * @copyright       The ImpressCMS Project http://www.impresscms.org/
+ * @license         LICENSE.txt
+ * @license			GNU General Public License (GPL) http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @package         modules
+ * @since           1.3
+ * @author	   		Sina Asghari (aka stranger) <pesian_stranger@users.sourceforge.net>
+ * @version         $Id$
+ */
+
+die('Not yet activated.');
+?>
