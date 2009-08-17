f64e5d44db1fd89e625d10fe131cfb9ae33599b1
Rene Sato <sato-san@impresscms.org>
2009-08-17 07:00:55 +0000

    731: Recommends changed > modules/ added. close[p:core, t:731]

--- htdocs/install/language/english/welcome.php
+++ htdocs/install/language/english/welcome.php
@@ -22,7 +22,7 @@ $content .= '
 	<li>Setup the web server, PHP and database server properly.</li>
 	<li>Prepare a database for ImpressCMS. This can be an existing database as well as a newly created one.</li>
 	<li>Prepare a user account and grant this user access to the database (all rights).</li>
-	<li>Make the directories of uploads/, cache/, templates_c/, writable (chmod 777 or 755 - depending on servers).</li>
+	<li>Make the directories of uploads/, cache/, templates_c/, modules/ writable (chmod 777 or 755 - depending on servers).</li>
 	<li>Make the file mainfile.php writable (chmod 666 depending on server).</li>
 	<li>In your internet browser settings turn on the allowance of cookies and JavaScript.</li>
 </ul>
