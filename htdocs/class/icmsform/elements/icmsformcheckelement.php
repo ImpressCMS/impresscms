c07f62e33757f39340aaf02da59d687ed9d86fe4
Sina Asghari <stranger@impresscms.org>
2009-08-17 09:56:12 +0000

    IPF issue: icmsformtextelement.php, icmsformtextelement.php, icmsformcheckelement.php fixes [t:687]

--- htdocs/class/icmsform/elements/icmsformcheckelement.php
+++ htdocs/class/icmsform/elements/icmsformcheckelement.php
@@ -41,9 +41,12 @@ class IcmsFormCheckElement extends XoopsFormCheckBox {
 	 * @return	string    $js   the constructed javascript
 	 */
 	function renderValidationJS(){
+		$js = "";
 		$js .= "var hasSelections = false;";
 		//sometimes, there is an implicit '[]', sometimes not
 		$eltname = $this->getName();
+		$eltmsg = empty($eltcaption) ? sprintf( _FORM_ENTER, $eltname ) : sprintf( _FORM_ENTER, $eltcaption );
+		$eltmsg = str_replace('"', '\"', stripslashes( $eltmsg ) );
 		if(strpos($eltname, '[') === false){
 			$js .= "for(var i = 0; i < myform['{$eltname}[]'].length; i++){
 				if (myform['{$eltname}[]'][i].checked) {
