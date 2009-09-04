5f09ac26c7319e5a73ff47b0a6c0e4b05381bbe3
William Hall <mrtheme@impresscms.org>
2009-09-04 05:00:30 +0000

    fixing the ugly.

--- htdocs/install/language/english/finish.php
+++ htdocs/install/language/english/finish.php
@@ -5,9 +5,9 @@ $content .=
 <p>You can now access the <a href='../index.php'>home page of your site</a>.</p>
 <h3>Support</h3>
 <ul class=\"diags\">
-<li class=\"success\"><a href='http://www.impresscms.org/' target='_blank'>www.impresscms.org</a> - Project Homepage</p></li>" .
-"<li class=\"success\"><a href='http://community.impresscms.org/' target='_blank'>community.impresscms.org</a> - Support</li>" .
-"<li class=\"success\"><a href='http://addons.impresscms.org/' target='_blank'>addons.impresscms.org</a> - Modules & Themes</li>" .
-"<li class=\"success\"><a href='http://wiki.impresscms.org/' target='_blank'>wiki.impresscms.org</a> - Community Documentation</li>".
+<li><a href='http://www.impresscms.org/' target='_blank'>www.impresscms.org</a> - Project Homepage</p></li>" .
+"<li><a href='http://community.impresscms.org/' target='_blank'>community.impresscms.org</a> - Support</li>" .
+"<li><a href='http://addons.impresscms.org/' target='_blank'>addons.impresscms.org</a> - Modules & Themes</li>" .
+"<li><a href='http://wiki.impresscms.org/' target='_blank'>wiki.impresscms.org</a> - Community Documentation</li>".
 "</ul>";
 ?>
