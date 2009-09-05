811355f14e9cab4e50edb218d27bb179b3e56fa8
Steve Kenow <skenow@impresscms.org>
2009-09-05 15:39:22 +0000

    Setting version for the trunk to 1.2 RC 1
    Remember: Only bug fixes are to be committed to the trunk until we reach final. If you wish to add features, refactor classes, update to PHP5 strict - that is to be done in a new branch. Anything other than bug fixes will only delay our release. 
    
    http://wiki.impresscms.org/index.php?title=Release_Cycle

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
 * @version		$Id$
 */
 
-define('ICMS_VERSION_NAME','ImpressCMS 1.2 Beta');
+define('ICMS_VERSION_NAME','ImpressCMS 1.2 RC1');
 /**
  * To developers:
  * if you want to get the version number of the core, please use something like:
@@ -31,14 +31,14 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 2);
+define('ICMS_VERSION_STATUS', 3);
 
 /**
  * Build number
  *
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-define('ICMS_VERSION_BUILD', 29);
+define('ICMS_VERSION_BUILD', 30);
 
 /**
  * Latest dbversion of the System Module
