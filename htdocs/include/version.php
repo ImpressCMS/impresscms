4f25d77ab6f84025bc2db97ac9b3bbb1989e03e9
Marc-André Lanciault <marcan@impresscms.org>
2009-12-12 11:46:10 +0000

    #464: Preparing packahing of 1.2 final. refs[t:464]

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
 * @version		$Id$
 */
 
-define('ICMS_VERSION_NAME','ImpressCMS 1.2 RC2');
+define('ICMS_VERSION_NAME','ImpressCMS 1.2 Final');
 /**
  * To developers:
  * if you want to get the version number of the core, please use something like:
@@ -31,14 +31,14 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 3);
+define('ICMS_VERSION_STATUS', 10);
 
 /**
  * Build number
  *
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
-define('ICMS_VERSION_BUILD', 31);
+define('ICMS_VERSION_BUILD', 32);
 
 /**
  * Latest dbversion of the System Module
