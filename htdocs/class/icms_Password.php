65f03c32222cbd0466c83295e9c6d1bcd7783901
Vaughan Montgomery <vaughan@impresscms.org>
2009-12-25 05:37:51 +0000

    Bufix: when password expired, users are redirected to profile/user and not resetpass page.
    closes[t:895]

--- htdocs/class/icms_Password.php
+++ htdocs/class/icms_Password.php
@@ -48,9 +48,26 @@ final class icms_Password
         $db = Database::getInstance();
         if($uname !== '')
         {
-            $sql = $db->query("SELECT uname, pass_expired FROM ".$db->prefix('users')." WHERE
-                uname = '".@htmlspecialchars($uname, ENT_QUOTES, _CHARSET)."'");
-            list($uname, $pass_expired) = $db->fetchRow($sql);
+            include_once ICMS_ROOT_PATH.'/class/database/databaseupdater.php';
+            $table = new IcmsDatabasetable('users');
+            if($table->fieldExists('loginname'))
+            {
+                $sql = $db->query("SELECT loginname, pass_expired FROM ".$db->prefix('users')." WHERE
+                    loginname = '".@htmlspecialchars($uname, ENT_QUOTES, _CHARSET)."'");
+                list($loginname, $pass_expired) = $db->fetchRow($sql);
+            }
+            elseif($table->fieldExists('login_name'))
+            {
+                $sql = $db->query("SELECT login_name, pass_expired FROM ".$db->prefix('users')." WHERE
+                    login_name = '".@htmlspecialchars($uname, ENT_QUOTES, _CHARSET)."'");
+                list($login_name, $pass_expired) = $db->fetchRow($sql);
+            }
+            else
+            {
+                $sql = $db->query("SELECT uname, pass_expired FROM ".$db->prefix('users')." WHERE
+                    uname = '".@htmlspecialchars($uname, ENT_QUOTES, _CHARSET)."'");
+                list($uname, $pass_expired) = $db->fetchRow($sql);
+            }
         }
         else
         {
