d0e13035adb58e55417bba9e61f053221d12e178
Steve Kenow <skenow@impresscms.org>
2010-04-18 13:52:40 +0000

    Adding 2 more files needed by reflex ACP theme

---
+++ htdocs/suggest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * @copyright	http://www.impresscms.org/ The ImpressCMS Project
+ * @license	http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package	core
+ * @since		2009
+ * @author	Wiliam Hall (aka Mr. Theme) <mrtheme@impresscms.org>
+ * @version	$Id$
+ **/
+include 'mainfile.php';
+global $xoopsDB;
+if(isset($_POST['queryString'])) {
+	$queryString = mysql_real_escape_string($_POST['queryString']);
+
+	// Is the string length greater than 2?
+	if(strlen($queryString) > 2) {
+		$sql = "SELECT * FROM ".$xoopsDB->prefix("autosearch_list")." s INNER JOIN ".$xoopsDB->prefix("autosearch_cat")." c ON s.cat_id = c.cid WHERE name LIKE '%" . $queryString . "%' ORDER BY cat_id LIMIT 8";
+		$query = $xoopsDB->query($sql);
+		$num_results = mysql_num_rows($query);
+
+		echo "<ul id='searchresults'>";
+		if($query) {
+			if($num_results < 1) {
+				echo "<li><a href='javascript:void(0);'><img src='".ICMS_URL."/images/crystal/actions/exit.png' alt='no results found' /><span class='searchheading'>Sorry</span><span class='searchdesc'>No results were found that matched your query. Please try again.</span></a></li>";
+			} else {
+				$catid = 0;
+				while($result = $xoopsDB->fetchArray($query)) {
+					echo '<li><a href="' . XOOPS_URL . $result['url'] . '">';
+					echo '<img src="'.$result['img'].'" alt="" />';
+
+					$name = $result['name'];
+					if(strlen($name) > 35) {
+						$name = substr($name, 0, 35) . "...";
+					}
+					echo '<span class="searchheading">'.$name.'</span>';
+
+					$description = $result['desc'];
+					if(strlen($description) > 80) {
+						$description = substr($description, 0, 80) . "...";
+					}
+					echo '<span class="searchdesc">'.$description.'</span></a></li>';
+				}
+			}
+			echo '<li><span class="seperator"><a href="http://www.impresscms.org" rel="external">ImpressCMS Project</a></span><br /></li>';
+		} else {
+			echo '<li>ERROR: There was a problem with the query.</li>';
+		}
+		echo "</ul>";
+	} else {
+		// Dont do anything.
+	} // There is a queryString.
+} else {
+	echo 'There should be no direct access to this script!';
+}
+?>
