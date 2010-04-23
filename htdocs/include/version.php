c956ce3a3016e25c5d3707ffc74f914290373fb5
Steve Kenow <skenow@impresscms.org>
2010-04-23 13:51:44 +0000

    Updating changelog and version for the proper release info

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
  * @version		$Id$
  */
 
-define('ICMS_VERSION_NAME','ImpressCMS 1.3 Alpha');
+define('ICMS_VERSION_NAME','ImpressCMS 1.3 Beta');
 /**
  * To developers:
  * if you want to get the version number of the core, please use something like:
@@ -21,7 +21,7 @@ define('ICMS_VERSION_NAME','ImpressCMS 1.3 Alpha');
  */
 
 // For backward compatibility with XOOPS
-define('XOOPS_VERSION', ICMS_VERSION_NAME);
+define( 'XOOPS_VERSION', ICMS_VERSION_NAME );
 
 /**
  * Version Status
@@ -31,7 +31,7 @@ define('XOOPS_VERSION', ICMS_VERSION_NAME);
  * 10 = Final
  */
 
-define('ICMS_VERSION_STATUS', 1);
+define( 'ICMS_VERSION_STATUS', 2 );
 
 /**
  * Build number
@@ -39,7 +39,7 @@ define('ICMS_VERSION_STATUS', 1);
  * Every release has its own build number, incrementable by 1 everytime we make a release
  */
 // impresscms_1.3_beta
-define('ICMS_VERSION_BUILD', 33);
+define( 'ICMS_VERSION_BUILD', 36 );
 
 /**
  * Latest dbversion of the System Module
@@ -50,6 +50,6 @@ define('ICMS_VERSION_BUILD', 33);
  * So, developers, everytime you add an upgrade block in system/include/update.php to upgrade something in the DB,
  * pease also change this constant
  */
-define('ICMS_SYSTEM_DBVERSION', 39);
+define( 'ICMS_SYSTEM_DBVERSION', 40 );
 
 ?>
