160bc8c60a10c453fe12d34227bee6dee36e0c66
Vaughan Montgomery <vaughan@impresscms.org>
2010-05-21 13:04:55 +0000

    some small improvements in icms_Datafilter.
    these classes aren't yet implemented in the core at all.

--- htdocs/kernel/icms_DataFilter.php
+++ htdocs/kernel/icms_DataFilter.php
@@ -70,43 +70,102 @@ class icms_DataFilter
 	// -------- Public Functions --------
 
 	/*
-	 * Public Function checks if email is of correct formatting
+	 * Public Function checks Variables using specified filter type
 	 *
-	 * @param string     $email      The email address
-	 * @param string     $antispam   Generate an email address that is protected from spammers
-	 * @return string    $email      The generated email address
+	 * @param	string		$data		Data to be checked
+	 * @param	string		$type		Type of Filter To use for Validation
+	 *			Valid Filter Types:
+	 *					'url' = Checks & validates URL
+	 *					'email' = Checks & validates Email Addresses
+	 *					'ip' = Checks & validates IP Addresses
+	 *
+	 * @param	mixed		$options1	Options to use with specified filter
+	 *			Valid Filter Options:
+	 *				URL:
+	 *					'scheme' = URL must be an RFC compliant URL (like http://example)
+	 *					'host' = URL must include host name (like http://www.example.com)
+	 *					'path' = URL must have a path after the domain name (like www.example.com/example1/)
+	 *					'query' = URL must have a query string (like "example.php?name=Vaughan&age=34")
+	 *				EMAIL:
+	 *					'true' = Generate an email address that is protected from spammers
+	 *					'false' = Generate an email address that is NOT protected from spammers
+	 *				IP:
+	 *					'ip4' = Requires the value to be a valid IPv4 IP (like 255.255.255.255)
+	 *					'ip6' = Requires the value to be a valid IPv6 IP (like 2001:0db8:85a3:08d3:1319:8a2e:0370:7334)
+	 *					'rfc' = Requires the value to be a RFC specified private range IP (like 192.168.0.1)
+	 *					'res' = Requires that the value is not within the reserved IP range. both IPV4 and IPV6 values
+	 *
+	 * @param	mixed		$options2	Options to use with specified filter options1
+	 *				URL:
+	 *					'true' = URLEncode the URL (ie. http://www.example > http%3A%2F%2Fwww.example)
+	 *					'false' = Do Not URLEncode the URL
+	 *				EMAIL:
+	 *					NOT USED!
+	 *				IP:
+	 *					NOT USED!
+	 *
+	 * @return	mixed
 	 */
-	public function icms_CheckEmail($email, $antispam = false)
+	public function icms_CheckVar($data, $type, $options1 = '', $options2 = '')
 	{
-		if(!$email)
+		if(!$data || !$type)
 		{
 			return false;
 		}
-		return $this->icms_FilterEmail($email, $antispam);
-	}
+		$valid_types = array('url', 'email', 'ip');
+		if(!in_array($type, $valid_types))
+		{
+			return false;
+		}
+		else
+		{
+			if($type == 'url')
+			{
+				$valid_options1 = array('scheme', 'path', 'host', 'query');
+				$valid_options2 = array(0, 1);
 
-	/*
-	 * Public Function checks if URL is valid & clean
-	 *
-	 * @copyright The ImpressCMS Project <http://www.impresscms.org>
-	 *
-	 * @param string  $url  The URL to check
-	 * @param string  $options   Options to use during validation check
-	 *        valid options are  'scheme' > URL must be an RFC compliant URL (like http://example)
-	 *                           'host' > URL must include host name (like http://www.example.com)
-	 *                           'path' > URL must have a path after the domain name (like www.example.com/example1/)
-	 *                           'query' > URL must have a query string (like "example.php?name=Vaughan&age=34")
-	 * @param string  $encode    Whether to URLEncode the URL or not (ie. http://www.example > http%3A%2F%2Fwww.example)
-	 *
-	 * @return string  $url  The validated URL
-	 */
-	public function icms_CheckURL($url, $options = '', $encode = false)
-	{
-		if(!$url)
-		{
-			return false;
+				if(!isset($options1) || $options1 == '' || !in_array($options1, $valid_options1))
+				{
+					$options1 = '';
+				}
+				if(!isset($options2) || $options2 == '' || !in_array($options2, $valid_options2))
+				{
+					$options2 = 0;
+				}
+				else
+				{
+					$options2 = 1;
+				}
+			}
+
+			if($type == 'email')
+			{
+				$valid_options1 = array(0, 1);
+				$options2 = '';
+
+				if(!isset($options1) || $options1 == '' || !in_array($options1, $valid_options1))
+				{
+					$options1 = 0;
+				}
+				else
+				{
+					$options1 = 1;
+				}
+			}
+
+			if($type = 'ip')
+			{
+				$valid_options1 = array('ipv4', 'ipv6', 'rfc', 'res');
+				$options2 = '';
+
+				if(!isset($options1) || $options1 == '' || !in_array($options1, $valid_options1))
+				{
+					$options1 = 'ipv4';
+				}
+			}
 		}
-		return $this->icms_FilterURL($url, $options, $encode);
+
+		return $this->icms_FilterVar($data, $type, $options1, $options2);
 	}
 
 	/**
@@ -627,74 +686,101 @@ class icms_DataFilter
 	// -------- Private Functions --------
 
 	/*
-	 * Private Function checks if URL is valid & clean
-	 *
-	 * @copyright The ImpressCMS Project <http://www.impresscms.org>
-	 *
-	 * See public function icms_CheckURL() for parameters
-	 *
-	 * @return string  $url  The validated URL
-	 */
-	private function icms_FilterURL($url, $options, $encode)
+	* Private Function checks & Validates Data
+	*
+	* @copyright The ImpressCMS Project <http://www.impresscms.org>
+	*
+	* See public function icms_CheckVar() for parameters
+	*
+	* @return
+	*/
+	private function icms_FilterVar($data, $type, $options1, $options2)
 	{
-		$url = filter_var($url, FILTER_SANITIZE_URL);
-
-		if(isset($options) && $options !== '')
+		if($type == 'url')
 		{
-			if($options = 'scheme')
+			$data = filter_var($data, FILTER_SANITIZE_URL);
+
+			if($options1 == 'scheme')
 			{
-				$url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED);
+				$opt1 = FILTER_FLAG_SCHEME_REQUIRED;
 			}
-			elseif($options = 'host')
+			elseif($options == 'host')
 			{
-				$url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED);
+				$opt1 = FILTER_FLAG_HOST_REQUIRED;
 			}
-			elseif($options = 'path')
+			elseif($options == 'path')
 			{
-				$url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
+				$opt1 = FILTER_FLAG_PATH_REQUIRED;
 			}
-			elseif($options = 'query')
+			elseif($options == 'query')
 			{
-				$url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED);
+				$opt1 = FILTER_FLAG_QUERY_REQUIRED;
+			}
+
+			if(isset($opt1) && $opt1 !== '')
+			{
+				$data = filter_var($data, FILTER_VALIDATE_URL, $opt1);
 			}
 			else
 			{
-				$url = filter_var($url, FILTER_VALIDATE_URL);
+				$data = filter_var($data, FILTER_VALIDATE_URL);
 			}
-		}
-		else
-		{
-			$url = filter_var($url, FILTER_VALIDATE_URL);
+
+			if(is_set($options2) && $options2 == 1)
+			{
+				$data = filter_var($data, FILTER_SANITIZE_ENCODED);
+			}
+
+			return $data;
 		}
 
-		if($encode)
+		if($type == 'email')
 		{
-			$url = filter_var($url, FILTER_SANITIZE_ENCODED);
+			$data = filter_var($data, FILTER_SANITIZE_EMAIL);
+
+			if(!filter_var($data, FILTER_VALIDATE_EMAIL))
+			{
+				return false;
+			}
+			if(isset($options1) && $options1 == 1)
+			{
+				$data = str_replace('@', ' at ', $data);
+				$data = str_replace('.', ' dot ', $data);
+			}
+
+			return $data;
 		}
 
-		return $url;
-	}
+		if($type == 'ip')
+		{
+			if($options1 == 'ipv4')
+			{
+				$opt1 = FILTER_FLAG_IPV4;
+			}
+			elseif($options1 == 'ipv6')
+			{
+				$opt1 = FILTER_FLAG_IPV6;
+			}
+			elseif($options1 == 'rfc')
+			{
+				$opt1 = FILTER_FLAG_NO_PRIV_RANGE;
+			}
+			elseif($options1 == 'res')
+			{
+				$opt1 = FILTER_FLAG_NO_RES_RANGE;
+			}
 
-	/*
-	 * Private Function checks if email is of correct formatting
-	 *
-	 * @param string     $email      The email address
-	 * @param string     $antispam   Generate an email address that is protected from spammers
-	 * @return string    $email      The generated email address
-	 */
-	private function icms_FilterEmail($email, $antispam)
-	{
-		$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-		{
-			return false;
+			if(isset($opt1) && $opt1 !== '')
+			{
+				$data = filter_var($data, FILTER_VALIDATE_IP, $opt1);
+			}
+			else
+			{
+				$data = filter_var($data, FILTER_VALIDATE_IP);
+			}
+
+			return $ip;
 		}
-		if($antispam)
-		{
-			$email = str_replace('@', ' at ', $email);
-			$email = str_replace('.', ' dot ', $email);
-		}
-		return $email;
 	}
 
 	/**
