0a9a2d113521e7af744ce188297ee382ff1975b4
Marc-André Lanciault <marcan@impresscms.org>
2010-07-13 21:50:41 +0000

    typo in the deprecated message

--- htdocs/class/xoopsform/formcaptcha.php
+++ htdocs/class/xoopsform/formcaptcha.php
@@ -48,7 +48,7 @@ class IcmsFormCaptcha extends icms_form_elements_captcha {
 	private $_deprecated;
 	public function __construct() {
 		parent::__construct();
-		$this->_deprecated = icms_core_Debug::setDeprecated('icms_auth_Ads', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_form_elements_captcha', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
 
