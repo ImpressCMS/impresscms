09865d940b5085b7e0b4b2e290e28f6d4a29fd00
Marc-André Lanciault <marcan@impresscms.org>
2010-07-13 22:04:37 +0000

    removing the inclusion of formcheckbox.php

--- htdocs/class/xoopsformloader.php
+++ htdocs/class/xoopsformloader.php
@@ -23,7 +23,6 @@ include_once ICMS_ROOT_PATH."/class/xoopsform/form.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formlabel.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formselect.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formpassword.php";
-include_once ICMS_ROOT_PATH."/class/xoopsform/formcheckbox.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formhidden.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formfile.php";
 include_once ICMS_ROOT_PATH."/class/xoopsform/formradio.php";
