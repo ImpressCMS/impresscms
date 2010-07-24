575f8bf3b1370c32c903dd4bd862dceecb691893
Steve Kenow <skenow@impresscms.org>
2010-07-24 01:12:41 +0000

    Cleanup and PHP5.3 compliance for some more form elements - hidden fields

--- htdocs/libraries/icms/form/elements/Hidden.php
+++ htdocs/libraries/icms/form/elements/Hidden.php
@@ -1,44 +1,33 @@
 <?php
 /**
- * Creates a hidden form attribute
+ * Creates a hidden form field
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @package	XoopsForms
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @version	$Id: formhidden.php 19807 2010-07-13 22:41:04Z malanciault $
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		Form
+ * @subpackage	Elements
+ * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-/**
- * @package     kernel
- * @subpackage  form
- *
- * @author	    Kazumi Ono	<onokazu@xoops.org>
- * @copyright	copyright (c) 2000-2003 XOOPS.org
- */
 /**
  * A hidden field
  *
- * @package     kernel
- * @subpackage  form
+ * @category	ICMS
+ * @package     Form
+ * @subpackage  Elements
  *
  * @author	    Kazumi Ono	<onokazu@xoops.org>
- * @copyright	copyright (c) 2000-2003 XOOPS.org
  */
 class icms_form_elements_Hidden extends icms_form_Element {
 
 	/**
 	 * Value
 	 * @var	string
-	 * @access	private
 	 */
-	var $_value;
+	private $_value;
 
 	/**
 	 * Constructor
@@ -46,7 +35,7 @@ class icms_form_elements_Hidden extends icms_form_Element {
 	 * @param	string	$name	"name" attribute
 	 * @param	string	$value	"value" attribute
 	 */
-	function icms_form_elements_Hidden($name, $value) {
+	public function __construct($name, $value) {
 		$this->setName($name);
 		$this->setHidden();
 		$this->setValue($value);
@@ -59,7 +48,7 @@ class icms_form_elements_Hidden extends icms_form_Element {
 	 * @param	bool    $encode To sanitizer the text?
 	 * @return	string
 	 */
-	function getValue($encode = false) {
+	public function getValue($encode = false) {
 		return $encode ? htmlspecialchars($this->_value, ENT_QUOTES) : $this->_value;
 	}
 
@@ -68,7 +57,7 @@ class icms_form_elements_Hidden extends icms_form_Element {
 	 *
 	 * @param  $value	string
 	 */
-	function setValue($value) {
+	public function setValue($value) {
 		$this->_value = $value;
 	}
 
@@ -77,9 +66,9 @@ class icms_form_elements_Hidden extends icms_form_Element {
 	 *
 	 * @return	string	HTML
 	 */
-	function render() {
+	public function render() {
 		$ele_name = $this->getName();
-		return "<input type='hidden' name='".$ele_name."' id='".$ele_name."' value='".$this->getValue()."' />";
+		return "<input type='hidden' name='" . $ele_name . "' id='" . $ele_name . "' value='" . $this->getValue() . "' />";
 	}
 }
-?>
+
--- htdocs/libraries/icms/form/elements/Hiddentoken.php
+++ htdocs/libraries/icms/form/elements/Hiddentoken.php
@@ -2,30 +2,21 @@
 /**
 * Creates a hidden token form attribute
 *
-* @copyright	http://www.xoops.org/ The XOOPS Project
-* @copyright	XOOPS_copyrights.txt
 * @copyright	http://www.impresscms.org/ The ImpressCMS Project
-* @license	LICENSE.txt
-* @package	XoopsForms
-* @since	XOOPS
-* @author	http://www.xoops.org The XOOPS Project
-* @author	modified by UnderDog <underdog@impresscms.org>
-* @version	$Id: formhiddentoken.php 8662 2009-05-01 09:04:30Z pesianstranger $
+* @license		LICENSE.txt
+* @category		ICMS
+* @package		Form
+* @subpackage	Elements
+* @version		$Id$
 */
 
 if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 
 /**
- *
- * @author      Kazumi Ono  <onokazu@xoops.org>
- * @copyright   copyright (c) 2000-2005 XOOPS.org
- */
-/**
  * A hidden token field
  *
  *
  * @author      Kazumi Ono  <onokazu@xoops.org>
- * @copyright   copyright (c) 2000-2005 XOOPS.org
  */
 class icms_form_elements_Hiddentoken extends icms_form_elements_Hidden {
 
@@ -35,7 +26,8 @@ class icms_form_elements_Hiddentoken extends icms_form_elements_Hidden {
    * @param   string  $name       "name" attribute
    * @param   int     $timeout    timeout variable for the createToken function
    */
-  function icms_form_elements_Hiddentoken($name = _CORE_TOKEN, $timeout = 0){
-      $this->icms_form_elements_Hidden($name . '_REQUEST', $GLOBALS['xoopsSecurity']->createToken($timeout, $name));
+  public function icms_form_elements_Hiddentoken($name = _CORE_TOKEN, $timeout = 0) {
+      parent::__construct($name . '_REQUEST', $GLOBALS['xoopsSecurity']->createToken($timeout, $name));
   }
 }
+
