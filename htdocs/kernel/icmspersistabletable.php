e15f1536e6f2cbff67f8ea36d9fc1575276c1ee9
Marc-André Lanciault <marcan@impresscms.org>
2010-07-29 13:51:55 +0000

    ensuring backward compat

--- htdocs/kernel/icmspersistabletable.php
+++ htdocs/kernel/icmspersistabletable.php
@@ -10,8 +10,8 @@ if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
  */
 class IcmsPersistableColumn extends icms_ipf_view_Column {
 	private $_deprecated;
-	public function __construct() {
-		parent::__construct();
+	public function __construct($keyname, $align = _GLOBAL_LEFT, $width = false, $customMethodForValue = false, $param = false, $customCaption = false, $sortable = true) {
+		parent::__construct($keyname, $align = _GLOBAL_LEFT, $width = false, $customMethodForValue = false, $param = false, $customCaption = false, $sortable = true);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_view_Column', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
@@ -25,8 +25,8 @@ class IcmsPersistableColumn extends icms_ipf_view_Column {
  */
 class IcmsPersistableTable extends icms_ipf_view_Table {
 	private $_deprecated;
-	public function __construct() {
-		parent::__construct();
+	public function __construct(&$objectHandler, $criteria = false, $actions = array('edit', 'delete'), $userSide = false) {
+		parent::__construct(&$objectHandler, $criteria = false, $actions = array('edit', 'delete'), $userSide = false);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_view_Table', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
