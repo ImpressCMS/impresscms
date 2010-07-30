87751b07a9487fe74f5490117057af848efbcf3c
Marc-André Lanciault <marcan@impresscms.org>
2010-07-30 00:16:54 +0000

    passing the correct parameter in constructor

--- htdocs/kernel/icmspersistablecontroller.php
+++ htdocs/kernel/icmspersistablecontroller.php
@@ -7,8 +7,8 @@ if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
  */
 class IcmsPersistableController extends icms_ipf_Controller{
 	private $_deprecated;
-	public function __construct() {
-		parent::__construct();
+	public function __construct($handler) {
+		parent::__construct($handler);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_Controller', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
