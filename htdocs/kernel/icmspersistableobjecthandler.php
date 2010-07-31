d9517bc57e347be524f2f3ebcfb3492e85b4cae4
Marc-André Lanciault <marcan@impresscms.org>
2010-07-31 00:57:54 +0000

    old class did not have the correct class name

--- htdocs/kernel/icmspersistableobjecthandler.php
+++ htdocs/kernel/icmspersistableobjecthandler.php
@@ -6,7 +6,7 @@ if (!defined("ICMS_ROOT_PATH")) die("ImpressCMS root path not defined");
  * @todo		Remove in version 1.4
  *
  */
-class icms_ipf_ObjectHandler extends icms_ipf_Handler {
+class IcmsPersistableObjectHandler extends icms_ipf_Handler {
 	private $_deprecated;
 	public function __construct(&$db, $itemname, $keyname, $idenfierName, $summaryName, $modulename) {
 		parent::__construct($db, $itemname, $keyname, $idenfierName, $summaryName, $modulename);
