1dc2866b410183353546717c445d69206d78634a
Vaughan Montgomery <vaughan@impresscms.org>
2010-08-11 21:43:59 +0000

    Security Fix:
    Session data not destroyed after installation.
    
    closes #462
    
    Safer.

--- htdocs/install/page_end.php
+++ htdocs/install/page_end.php
@@ -34,12 +34,14 @@ $pageHasForm = false;
 $content = "";
 include "./language/$wizard->language/finish.php";
 
-// Destroy the Installation Session.
+// destroy all the installation session
 unset($_SESSION);
 if(isset($_COOKIE[session_name()]))
 {
 	setcookie(session_name(), '', time()-60);
 }
+session_unset();
+session_destroy();
 
 include 'install_tpl.php';
 ?>
