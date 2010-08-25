e85c085effea939d50851567910da9588bfe7a4a
Marc-André Lanciault <marcan@impresscms.org>
2010-08-25 14:43:11 +0000

    adding correct paramet in __construct

--- htdocs/kernel/icmspersistableexport.php
+++ htdocs/kernel/icmspersistableexport.php
@@ -6,8 +6,8 @@
  */
 class IcmsPersistableExport extends icms_ipf_export_Handler{
 	private $_deprecated;
-	public function __construct() {
-		parent::getInstance();
+	public function __construct(&$objectHandler, $criteria=null, $fields=false, $filename=false, $filepath=false, $format='csv', $options=false) {
+		parent::getInstance(&$objectHandler, $criteria, $fields, $filename, $filepath, $format, $options);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_export_Handler', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
