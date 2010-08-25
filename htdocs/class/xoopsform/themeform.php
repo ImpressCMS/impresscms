34074f88e72bcf170481aaa5fe2a9911c545f01f
Marc-André Lanciault <marcan@impresscms.org>
2010-08-25 19:09:41 +0000

    params

--- htdocs/class/xoopsform/themeform.php
+++ htdocs/class/xoopsform/themeform.php
@@ -39,8 +39,8 @@ defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
  */
 class XoopsThemeForm extends icms_form_Theme {
 	private $_deprecated;
-	public function __construct() {
-		parent::__construct();
+	public function __construct($title, $name, $action, $method = "post", $addtoken = false) {
+		parent::__construct($title, $name, $action, $method, $addtoken);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_form_Theme', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
