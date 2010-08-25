619d8f023e77730667041870d67508bd759a384f
Marc-André Lanciault <marcan@impresscms.org>
2010-08-25 14:40:10 +0000

    adding correct paramet in __construct

--- htdocs/kernel/blockposition.php
+++ htdocs/kernel/blockposition.php
@@ -6,8 +6,8 @@ defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
  */
 class IcmsBlockposition extends icms_block_position_Object {
 	private $_deprecated;
-	public function __construct() {
-		parent::__construct();
+	public function __construct(&$handler) {
+		parent::__construct($handler);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_block_position_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
