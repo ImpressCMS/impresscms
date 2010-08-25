43f31150a916048d514e9da17ae0826324f246e6
Marc-André Lanciault <marcan@impresscms.org>
2010-08-25 18:22:05 +0000

    no need to include form.php anymore

--- htdocs/libraries/icms/form/Table.php
+++ htdocs/libraries/icms/form/Table.php
@@ -24,10 +24,6 @@ defined('ICMS_ROOT_PATH') or die('ImpressCMS root path not defined');
  * @author	    Kazumi Ono	<onokazu@xoops.org>
  * @copyright	copyright (c) 2000-2003 XOOPS.org
  */
-/**
- * the base class
- */
-include_once ICMS_ROOT_PATH . '/class/xoopsform/form.php';
 
 /**
  * Form that will output formatted as a HTML table
