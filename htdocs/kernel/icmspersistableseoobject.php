765c75ec0cc6bcf2dc4ea03baf6788b8fd6ad7a1
Marc-André Lanciault <marcan@impresscms.org>
2010-08-25 14:10:45 +0000

    Improving this backward compat class to make it work with 1.2 modules

--- htdocs/kernel/icmspersistableseoobject.php
+++ htdocs/kernel/icmspersistableseoobject.php
@@ -5,15 +5,9 @@ if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
  * @todo		Remove in version 1.4
  *
  */
-class IcmsPersistableSeoObject extends IcmsPersistableObject {
+class IcmsPersistableSeoObject extends icms_ipf_seo_Object {
 	private $_deprecated;
 	public function __construct(&$handler) {
-		parent::__construct($handler);
-		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_seo_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
-	}
-
-	public function IcmsPersistableSeoObject(&$handler) {
-		parent::__construct($handler);
 		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_seo_Object', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
