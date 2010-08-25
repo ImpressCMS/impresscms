640230eceea0973b5bbcbb6ad8b19e81420fc6d4
Steve Kenow <skenow@impresscms.org>
2010-08-25 00:46:55 +0000

    Applying code standards, refs #197

--- htdocs/libraries/icms/core/TextFilter.php
+++ htdocs/libraries/icms/core/TextFilter.php
@@ -1,18 +1,27 @@
 <?php
 /**
  * Class to filter Text
- * @package      libraries
- * @subpackage   core
- * @since        1.3
- * @author       vaughan montgomery (vaughan@impresscms.org)
- * @author       ImpressCMS Project
- * @copyright    (c) 2007-2010 The ImpressCMS Project - www.impresscms.org
- * @version      $Id: TextFilter.php 19858 2010-07-15 12:01:13Z m0nty_ $
+ * @category	ICMS
+ * @package		Core
+ * @since		1.3
+ * @author		vaughan montgomery (vaughan@impresscms.org)
+ * @author		ImpressCMS Project
+ * @copyright	(c) 2007-2010 The ImpressCMS Project - www.impresscms.org
+ * @version		SVN: $Id$
  **/
-class icms_core_TextFilter extends icms_core_DataFilter
-{
-	public function __construct()
-	{
+/**
+ * Provide functions to filter text
+ *
+ * @category	ICMS
+ * @package		Core
+ *
+ */
+class icms_core_TextFilter extends icms_core_DataFilter {
+
+	/**
+	 * Constructor for the DataFilter object
+	 */
+	public function __construct() {
 		parent::__construct();
 	}
 
@@ -22,11 +31,9 @@ class icms_core_TextFilter extends icms_core_DataFilter
 	 * @static       $TextFilter_instance
 	 * @staticvar    object
 	 **/
-	public static function getInstance()
-	{
+	public static function getInstance() {
 		static $instance;
-		if(!isset($instance))
-		{
+		if (!isset($instance)) {
 			$instance = new icms_core_TextFilter();
 		}
 		return $instance;
@@ -34,11 +41,17 @@ class icms_core_TextFilter extends icms_core_DataFilter
 
 	// -------- Public Functions --------
 
-	public function checkVar($data, $type, $option1 = '', $option2 = '')
-	{
+	/**
+	 *
+	 * @param string $data
+	 * @param string $type
+	 * @param string $option1
+	 * @param string $option2
+	 */
+	public function checkVar($data, $type, $option1 = '', $option2 = '') {
 		return parent::checkVar($data, $type, $option1, $option2);
 	}
-	
+
 	/**
 	 * Filters textarea form data in DB for display purposes
 	 *
@@ -49,16 +62,14 @@ class icms_core_TextFilter extends icms_core_DataFilter
 	 * @param   bool	$br	 convert linebreaks?
 	 * @return  string
 	 **/
-	public function filterTarea($text, $smiley = 1, $imcode = 1, $image = 1, $br = 1)
-	{
+	public function filterTarea($text, $smiley = 1, $imcode = 1, $image = 1, $br = 1) {
 		// ################# Preload Trigger beforefilterTarea ##############
 		global $icmsPreloadHandler;
 
 		// FIXME: Review this fix, is not the best. I dont found the problem,
 		// it really should never worked in the admin side!
 		// Maybe the preload handler should be in the install kernel.
-		if(!is_object($icmsPreloadHandler))
-		{
+		if (!is_object($icmsPreloadHandler)) {
 			$icmsPreloadHandler = icms_preload_Handler::getInstance();
 		}
 		$icmsPreloadHandler->triggerEvent('beforeFilterTarea', array(&$text, $smiley, $imcode, $image, $br));
@@ -91,3 +102,4 @@ class icms_core_TextFilter extends icms_core_DataFilter
 	// -------- Private Functions --------
 
 }
+
