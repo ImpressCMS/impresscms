b205c9ff98351920ff82962cec8dab6239650c13
Markus Schutz <phoenyx@impresscms.de>
2010-09-19 13:48:17 +0000

    removing includes since we don't need them anymore (those files are included automatically via autoload)

--- htdocs/libraries/icms/db/legacy/Factory.php
+++ htdocs/libraries/icms/db/legacy/Factory.php
@@ -1,26 +1,15 @@
 <?php
 /**
- * Creates a database object and connection
+ * ImpressCMS Database Factory Class
  *
  * @category	ICMS
  * @package		Database
- *
  * @author      Gustavo Pilla  (aka nekro) <nekro@impresscms.org>
  * @copyright   The ImpressCMS Project <http://www.impresscms.org>
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @version		SVN: $Id$
  */
 
-/**
- * ImpressCMS Database Factory Class
- *
- * @category	ICMS
- * @package		Database
- *
- * @author      Gustavo Pilla  (aka nekro) <nekro@impresscms.org>
- * @copyright   The ImpressCMS Project <http://www.impresscms.org>
- * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
- */
 class icms_db_legacy_Factory {
 	/**
 	 * Constructor
@@ -42,12 +31,9 @@ class icms_db_legacy_Factory {
 	static public function &instance() {
 		static $instance;
 		if (!isset($instance)) {
-			$file = ICMS_ROOT_PATH . '/libraries/icms/db/legacy/' . XOOPS_DB_TYPE . '/Database.php';
-			require_once $file;
-			/* begin DB Layer Trapping patch */
 			if (defined('XOOPS_DB_ALTERNATIVE') && class_exists(XOOPS_DB_ALTERNATIVE)) {
 				$class = XOOPS_DB_ALTERNATIVE ;
-			} else /* end DB Layer Trapping patch */if (!defined('XOOPS_DB_PROXY')) {
+			} elseif (!defined('XOOPS_DB_PROXY')) {
 				$class = 'icms_db_legacy_' . XOOPS_DB_TYPE . '_Safe';
 			} else {
 				$class = 'icms_db_legacy_' . XOOPS_DB_TYPE . '_Proxy';
@@ -74,8 +60,6 @@ class icms_db_legacy_Factory {
 	static public function &getDatabase() {
 		static $database;
 		if (!isset($database)) {
-			$file = ICMS_ROOT_PATH . '/libraries/icms/db/legacy/' . XOOPS_DB_TYPE . '/Database.php';
-			require_once $file;
 			if (!defined('XOOPS_DB_PROXY')) {
 				$class = 'icms_db_legacy_' . XOOPS_DB_TYPE . '_Safe';
 			} else {
@@ -93,10 +77,6 @@ class icms_db_legacy_Factory {
 	 * @static
 	 */
 	static public function getDatabaseUpdater() {
-		$file = ICMS_ROOT_PATH . '/class/database/drivers/' . XOOPS_DB_TYPE . '/databaseupdater.php';
-		require_once $file;
-		$class = 'Icms' . ucfirst(XOOPS_DB_TYPE) . 'Databaseupdater';
-		$databaseUpdater = new $class();
-		return $databaseUpdater;
+		return new icms_db_legacy_updater_Handler();
 	}
 }
