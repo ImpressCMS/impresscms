73bba650afb33ff66bb0770040b3fe36b707e666
Markus Schutz <phoenyx@impresscms.de>
2010-10-10 15:57:14 +0000

    refactoring IcmsFormUser_sigElement to icms_ipf_form_elements_Signature

--- htdocs/class/icmsform/elements/icmsformuser_sigelement.php
+++ htdocs/class/icmsform/elements/icmsformuser_sigelement.php
@@ -5,34 +5,18 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormUser_sigElement extends icms_form_elements_Tray {
+class IcmsFormUser_sigElement extends icms_ipf_form_elements_Signature {
+	private $_deprecated;
 
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
-	function __construct($object, $key){
-
-		$var = $object->vars[$key];
-		$control = $object->controls[$key];
-
-		parent::__construct($var['form_caption'], '<br /><br />', $key . '_signature_tray');
-
-		$signature_textarea = new icms_form_elements_Dhtmltextarea('', $key, $object->getVar($key, 'e'));
-		$this->addElement($signature_textarea);
-
-		$attach_checkbox = new icms_form_elements_Checkbox('', 'attachsig', $object->getVar('attachsig', 'e'));
-		$attach_checkbox->addOption(1, _US_SHOWSIG);
-		$this->addElement($attach_checkbox);
+	public function __construct($object, $key) {
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Signature', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-
-?>
