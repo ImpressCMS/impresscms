895b9cd980e24dcb911f68ba686fa2347e3a0245
Markus Schutz <phoenyx@impresscms.de>
2010-10-10 17:08:06 +0000

    refactor IcmsFormSet_passwordElement in icms_ipf_form_elements_Passwordtray

--- htdocs/class/icmsform/elements/icmsformset_passwordelement.php
+++ htdocs/class/icmsform/elements/icmsformset_passwordelement.php
@@ -5,65 +5,18 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormSet_passwordElement extends icms_form_elements_Tray {
+class IcmsFormSet_passwordElement extends icms_ipf_form_elements_Passwordtray {
+	private $_deprecated;
 
-	/**
-	 * Size of the field.
-	 * @var	int
-	 * @access	private
-	 */
-	var $_size;
-
-	/**
-	 * Maximum length of the text
-	 * @var	int
-	 * @access	private
-	 */
-	var $_maxlength;
-
-	/**
-	 * Initial content of the field.
-	 * @var	string
-	 * @access	private
-	 */
-	var $_value;
-
-	/**
-	 * Constructor
-	 *
-	 * @param	string	$caption	Caption
-	 * @param	string	$name		"name" attribute
-	 * @param	int		$size		Size of the field
-	 * @param	int		$maxlength	Maximum length of the text
-	 * @param	int		$value		Initial value of the field.
-	 * 								<b>Warning:</b> this is readable in cleartext in the page's source!
-	 */
-
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
-	function __construct($object, $key){
-
-		$var = $object->vars[$key];
-		$control = $object->controls[$key];
-
-		parent::__construct($var['form_caption'] . '<br />' . _US_TYPEPASSTWICE, ' ', $key . '_password_tray');
-
-		$password_box1 = new icms_form_elements_Password('', $key . '1', 10, 32);
-		$this->addElement($password_box1);
-
-		$password_box2 = new icms_form_elements_Password('', $key . '2', 10, 32);
-		$this->addElement($password_box2);
+	public function __construct($object, $key) {
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Passwordtray', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-
-?>
