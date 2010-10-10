225dc306bb4c7e04875e06c5f4d6c14c1598a3bb
Markus Schutz <phoenyx@impresscms.de>
2010-10-10 17:11:29 +0000

    added Id tags and updated refactoring spreadsheet

--- htdocs/libraries/icms/ipf/form/elements/Time.php
+++ htdocs/libraries/icms/ipf/form/elements/Time.php
@@ -9,7 +9,7 @@
  * @subpackage	form
  * @since		1.1
  * @author		marcan <marcan@impresscms.org>
- * @version		$Id:$
+ * @version		$Id$
  */
 
 defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
