8516b6c4f12be9dbe1fbcadacc369fac10868fa6
Markus Schutz <phoenyx@impresscms.de>
2010-10-11 16:59:46 +0000

    refactoring IcmsFormSelectElement to icms_ipf_form_elements_Select

--- htdocs/class/icmsform/elements/icmsformselect_multielement.php
+++ htdocs/class/icmsform/elements/icmsformselect_multielement.php
@@ -5,26 +5,21 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-include_once ICMS_ROOT_PATH . "/class/icmsform/elements/icmsformselectelement.php" ;
-
-class IcmsFormSelect_multiElement extends IcmsFormSelectElement  {
-
+class IcmsFormSelect_multiElement extends icms_ipf_form_elements_Select  {
 	/**
 	 * Constructor
 	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
 	 * @param	string    $key      the form name
 	 */
 	function IcmsFormSelect_multiElement($object, $key) {
-		$this->multiple = true;
-		parent::IcmsFormSelectElement($object, $key);
+		$this->_multiple = true;
+		parent::__construct($object, $key);
 	}
 }
-
-?>
--- htdocs/class/icmsform/elements/icmsformselectelement.php
+++ htdocs/class/icmsform/elements/icmsformselectelement.php
@@ -5,68 +5,18 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormSelectElement extends icms_form_elements_Select {
-	var $multiple = false;
+class IcmsFormSelectElement extends icms_ipf_form_elements_Select {
+	private $_deprecated;
 
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
-	function IcmsFormSelectElement($object, $key) {
-		$var = $object->vars[$key];
-		$size = isset($var['size']) ? $var['size'] : ($this->multiple ? 5 : 1);
-
-		// Adding the options inside this SelectBox
-		// If the custom method is not from a module, than it's from the core
-		$control = $object->getControl($key);
-
-		$value = isset($control['value']) ? $control['value'] : $object->getVar($key, 'e');
-
-		parent::__construct($var['form_caption'], $key, $value, $size, $this->multiple);
-
-		if (isset($control['options'])) {
-			$this->addOptionArray($control['options']);
-		} else {
-			// let's find if the method we need to call comes from an already defined object
-			if (isset($control['object'])) {
-				if (method_exists($control['object'], $control['method'])) {
-					if ($option_array = $control['object']->$control['method']()) {
-						// Adding the options array to the select element
-						$this->addOptionArray($option_array);
-					}
-				}
-			} else {
-				// finding the itemHandler; if none, let's take the itemHandler of the $object
-				if (isset($control['itemHandler'])) {
-					if (!$control['module']) {
-						// Creating the specified core object handler
-						$control_handler = icms::handler($control['itemHandler']);
-					} else {
-						$control_handler =& icms_getModuleHandler($control['itemHandler'], $control['module']);
-					}
-				} else {
-					$control_handler =& $object->handler;
-				}
-
-				// Checking if the specified method exists
-				if (method_exists($control_handler, $control['method'])) {
-					// TODO : How could I pass the parameters in the following call ...
-					if ($option_array = $control_handler->$control['method']()) {
-						// Adding the options array to the select element
-						$this->addOptionArray($option_array);
-					}
-				}
-			}
-		}
+	public function __construct($object, $key) {
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Select', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-
-?>
