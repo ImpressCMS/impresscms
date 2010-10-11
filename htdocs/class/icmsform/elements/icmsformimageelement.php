80e2b88c88f7acd39d54606ae3567e771f34a458
Markus Schutz <phoenyx@impresscms.de>
2010-10-11 19:10:38 +0000

    refactoring IcmsFormImageElement in icms_ipf_form_elements_Image

--- htdocs/class/icmsform/elements/icmsformimageelement.php
+++ htdocs/class/icmsform/elements/icmsformimageelement.php
@@ -5,52 +5,18 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormImageElement extends icms_form_elements_Tray {
+class IcmsFormImageElement extends icms_ipf_form_elements_Image {
+	private $_deprecated;
 
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
-	function __construct($object, $key) {
-		$var = $object->vars[$key];
-		$control = $object->getControl($key);
-
-		$object_imageurl = $object->getImageDir();
-		parent::__construct( $var['form_caption'], ' ' );
-
-		if (isset($objectArray['image'])){
-			$objectArray['image'] = str_replace('{ICMS_URL}', ICMS_URL, $objectArray['image']);
-		}
-
-		if($object->getVar($key,'e') != '' && (substr($object->getVar($key,'e'), 0, 4) == 'http' || substr($object->getVar($key,'e'), 0, 11) == '{ICMS_URL}')){
-			$this->addElement( new icms_form_elements_Label( '', "<img src='" . str_replace('{ICMS_URL}', ICMS_URL, $object->getVar($key,'e')) . "' alt='' /><br/><br/>" ) );
-		}elseif($object->getVar($key,'e') != ''){
-			$this->addElement( new icms_form_elements_Label( '', "<img src='" . $object_imageurl . $object->getVar($key,'e') . "' alt='' /><br/><br/>" ) );
-		}
-
-		include_once ICMS_ROOT_PATH."/class/icmsform/elements/icmsformfileuploadelement.php";
-		$this->addElement(new IcmsFormFileUploadElement($object, $key));
-
-		if (!isset($control['nourl']) || !$control['nourl']) {
-			$this->addElement(new icms_form_elements_Label( '<div style="padding-top: 8px; font-size: 80%;">'._CO_ICMS_URL_FILE_DSC.'</div>', ''));
-			$this->addElement(new icms_form_elements_Label( '', '<br />' . _CO_ICMS_URL_FILE));
-			$this->addElement(new icms_form_elements_Text('', 'url_'.$key, 50, 500));
-		}
-		if (!$object->isNew()) {
-			include_once ICMS_ROOT_PATH."/class/icmsform/elements/icmsformcheckelement.php";
-			$this->addElement(new icms_form_elements_Label( '', '<br /><br />'));
-			$delete_check = new IcmsFormCheckElement('', 'delete_'.$key);
-			$delete_check->addOption(1, '<span style="color:red;">'._CO_ICMS_DELETE.'</span>');
-			$this->addElement($delete_check);
-		}
+	public function __construct($object, $key) {
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Image', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-?>
