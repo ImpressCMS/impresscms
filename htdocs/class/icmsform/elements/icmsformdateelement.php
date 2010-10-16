c1e6ba023ef72e08edbd90f2bab4b39ae52ae504
Markus Schutz <phoenyx@impresscms.de>
2010-10-16 09:01:57 +0000

    refactoring IcmsFormDateElement in icms_ipf_form_elements_Date

--- htdocs/class/icmsform/elements/icmsformdateelement.php
+++ htdocs/class/icmsform/elements/icmsformdateelement.php
@@ -5,23 +5,18 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		icms_ipf_Object
- * @since		  1.1
- * @author		  marcan <marcan@impresscms.org>
+ * @since		1.1
+ * @author		marcan <marcan@impresscms.org>
  * @version		$Id$
  */
 
-if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormDateElement extends icms_form_elements_Date {
+class IcmsFormDateElement extends icms_ipf_form_elements_Date {
+	private $_deprecated;
 
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
-	function IcmsFormDateElement($object, $key) {
-		parent::__construct($object->vars[$key]['form_caption'], $key, 15, $object->getVar($key, 'e'));
+	public function __construct($object, $key) {
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Date', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-
-?>
