afa0278936fcd34fe9f47aabec0ea65a915e1c7b
Markus Schutz <phoenyx@impresscms.de>
2010-10-16 16:02:59 +0000

    refactoring IcmsFormBlockoptionsElement in icms_ipf_form_elements_Blockoptions

--- htdocs/class/icmsform/elements/icmsformblockoptionselement.php
+++ htdocs/class/icmsform/elements/icmsformblockoptionselement.php
@@ -11,29 +11,13 @@
  * @version		$Id:$
  */
 
-if (!defined('ICMS_ROOT_PATH'))
-die("ImpressCMS root path not defined");
+defined('ICMS_ROOT_PATH') or die("ImpressCMS root path not defined");
 
-class IcmsFormBlockoptionsElement extends icms_form_elements_Tray {
+class IcmsFormBlockoptionsElement extends icms_ipf_form_elements_Blockoptions {
+	private $_deprecated;
 
-	/**
-	 * Constructor
-	 * @param	object    $object   reference to targetobject (@link icms_ipf_Object)
-	 * @param	string    $key      the form name
-	 */
 	public function __construct($object, $key) {
-		$var = $object->vars[$key];
-		parent::__construct($var['form_caption'], $key, $object->getVar($key, 'e'));
-		parent::__construct(_CO_SYSTEM_BLOCKSADMIN_OPTIONS, ' ', 'options' . '_password_tray');
-		$func = $object->getVar('edit_func');
-
-		require_once ICMS_ROOT_PATH . "/modules/" . $object->handler->getModuleDirname($object->getVar('mid', 'e')) . "/blocks/" . $object->getVar('func_file') ;
-		include_once ICMS_ROOT_PATH . "/class/xoopsformloader.php";
-		icms_loadLanguageFile($object->handler->getModuleDirname($object->getVar('mid', 'e')), 'blocks');
-
-		if (!function_exists($func)) return;
-		$visible_label = new icms_form_elements_Label('', $func (explode('|', $object->getVar('options'))));
-		$this->addElement($visible_label);
+		parent::__construct($object, $key);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_ipf_form_elements_Blockoptions', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-?>
