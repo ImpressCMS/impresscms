f331afd58a8d43165decce449402b982121f07c5
Steve Kenow <skenow@impresscms.org>
2010-12-02 01:52:19 +0000

    Removing the include for plugins.php

--- htdocs/modules/system/admin/rating/class/rating.php
+++ htdocs/modules/system/admin/rating/class/rating.php
@@ -13,8 +13,6 @@
 if (! defined ( "ICMS_ROOT_PATH" ))
 die ( "ImpressCMS root path not defined" );
 
-include_once ICMS_ROOT_PATH . "/class/plugins.php";
-
 class SystemRating extends icms_ipf_Object {
 
 	public $_modulePlugin=false;
