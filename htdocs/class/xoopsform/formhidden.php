cbb4da7d8e7c4bd0a4e5eb5ab130066ff901ce36
Steve Kenow <skenow@impresscms.org>
2010-12-30 16:16:12 +0000

    XoopsFormHidden had not been refactored properly - fixing that

--- htdocs/class/xoopsform/formhidden.php
+++ htdocs/class/xoopsform/formhidden.php
@@ -16,13 +16,6 @@
 if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
 
 /**
- * @package     kernel
- * @subpackage  form
- *
- * @author	    Kazumi Ono	<onokazu@xoops.org>
- * @copyright	copyright (c) 2000-2003 XOOPS.org
- */
-/**
  * A hidden field
  *
  * @package     kernel
@@ -30,56 +23,21 @@ if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
  *
  * @author	    Kazumi Ono	<onokazu@xoops.org>
  * @copyright	copyright (c) 2000-2003 XOOPS.org
+ * @deprecated	Use icms_form_elements_Hidden, instead
+ * @todo		Remove in version 1.4
  */
-class icms_form_elements_Hidden extends icms_form_Element {
+class XoopsFormHidden extends icms_form_elements_Hidden {
 
-	/**
-	 * Value
-	 * @var	string
-	 * @access	private
-	 */
-	var $_value;
-
+	private $_deprecated;
+	
 	/**
 	 * Constructor
 	 *
 	 * @param	string	$name	"name" attribute
 	 * @param	string	$value	"value" attribute
 	 */
-	function icms_form_elements_Hidden($name, $value) {
-		$this->setName($name);
-		$this->setHidden();
-		$this->setValue($value);
-		$this->setCaption("");
-	}
-
-	/**
-	 * Get the "value" attribute
-	 *
-	 * @param	bool    $encode To sanitizer the text?
-	 * @return	string
-	 */
-	function getValue($encode = false) {
-		return $encode ? htmlspecialchars($this->_value, ENT_QUOTES) : $this->_value;
-	}
-
-	/**
-	 * Sets the "value" attribute
-	 *
-	 * @param  $value	string
-	 */
-	function setValue($value) {
-		$this->_value = $value;
-	}
-
-	/**
-	 * Prepare HTML for output
-	 *
-	 * @return	string	HTML
-	 */
-	function render() {
-		$ele_name = $this->getName();
-		return "<input type='hidden' name='".$ele_name."' id='".$ele_name."' value='".$this->getValue()."' />";
+	function __construct($name, $value) {
+		parent::__construct($name, $value);
+		$this->_deprecated = icms_core_Debug::setDeprecated('icms_form_elements_Hidden', sprintf(_CORE_REMOVE_IN_VERSION, '1.4'));
 	}
 }
-?>
