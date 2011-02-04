4b7180ca1cb30527fa8ac9d984fdff148e6190b9
Steve Kenow <skenow@impresscms.org>
2011-02-04 01:45:20 +0000

    Fixing an issue where the module object is not loaded in the control panel by modules including cp_header after they have included mainfile. There was no ticket for this, but it was mentioned in the release article and the forums - http://community.impresscms.org/modules/newbb/viewtopic.php?post_id=41542#forumpost41542

--- htdocs/include/cp_header.php
+++ htdocs/include/cp_header.php
@@ -3,25 +3,26 @@
  * module files can include this file for admin authorization
  * the file that will include this file must be located under xoops_url/modules/module_directory_name/admin_directory_name/
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
- * @package	core
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by underdog <underdog@impresscms.org>
- * @version	$Id$
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package		core
+ * @version		SVN: $Id$
  */
 // Make sure the kernel launches the module in admin mode and checks the correct permissions
 define('ICMS_IN_ADMIN', 1);
 
-//error_reporting(0);
+/* if mainfile.php is loaded before this file, the icms::$module will not be loaded properly,
+ * causing a fatal error when trying to access any property or method of $icmsModule,
+ * $xoopsModule or icms::$module
+ */
+if (!icms::$module) {
+	icms_core_Debug::setDeprecated('', 'mainfile.php should not be loaded before including cp_header.php');
+	icms::loadService('module', array('icms_module_Handler', 'service'), array(TRUE));
+}
 /** Load the mainfile */
 include_once '../../../mainfile.php';
 /** Load the admin functions */
 include_once ICMS_ROOT_PATH . '/include/cp_functions.php';
 
 // include the default language file for the admin interface
-icms_loadLanguageFile($icmsModule->getVar('dirname'), 'admin');
-
+icms_loadLanguageFile(icms::$module->getVar('dirname'), 'admin');
