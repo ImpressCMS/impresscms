dfc36bee359447cc6a8169ef85500349d728ce75
Steve Kenow <skenow@impresscms.org>
2011-02-18 05:18:10 +0000

    Applying coding standards to the system module - smilies

--- htdocs/modules/system/admin/smilies/smileform.php
+++ htdocs/modules/system/admin/smilies/smileform.php
@@ -2,24 +2,20 @@
 /**
  * Administration of smilies, form file
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @package	Administration
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @version	$Id$
+ * @license		LICENSE.txt
+ * @package		System
+ * @subpackage	Smilies
+ * @version		SVN: $Id$
  */
 
-$smile_form = new icms_form_Theme($smiles['smile_form'], 'smileform', 'admin.php', 'post', true);
+$smile_form = new icms_form_Theme($smiles['smile_form'], 'smileform', 'admin.php', 'post', TRUE);
 $smile_form->setExtra('enctype="multipart/form-data"');
-$smile_form->addElement(new icms_form_elements_Text(_AM_SMILECODE, 'smile_code', 26, 25, $smiles['smile_code']), true);
-$smile_form->addElement(new icms_form_elements_Text(_AM_SMILEEMOTION, 'smile_desc', 26, 25, $smiles['smile_desc']), true);
+$smile_form->addElement(new icms_form_elements_Text(_AM_SMILECODE, 'smile_code', 26, 25, $smiles['smile_code']), TRUE);
+$smile_form->addElement(new icms_form_elements_Text(_AM_SMILEEMOTION, 'smile_desc', 26, 25, $smiles['smile_desc']), TRUE);
 $smile_select = new icms_form_elements_File('', 'smile_url', 5000000);
-$smile_label = new icms_form_elements_Label('', '<img src="'.ICMS_UPLOAD_URL.'/'.$smiles['smile_url'].'" alt="" />');
-$smile_tray = new icms_form_elements_Tray(_IMAGEFILE.'&nbsp;');
+$smile_label = new icms_form_elements_Label('', '<img src="' . ICMS_UPLOAD_URL . '/' . $smiles['smile_url'] . '" alt="" />');
+$smile_tray = new icms_form_elements_Tray(_IMAGEFILE . '&nbsp;');
 $smile_tray->addElement($smile_select);
 $smile_tray->addElement($smile_label);
 $smile_form->addElement($smile_tray);
@@ -28,4 +24,3 @@ $smile_form->addElement(new icms_form_elements_Hidden('id', $smiles['id']));
 $smile_form->addElement(new icms_form_elements_Hidden('op', $smiles['op']));
 $smile_form->addElement(new icms_form_elements_Hidden('fct', 'smilies'));
 $smile_form->addElement(new icms_form_elements_Button('', 'submit', _SUBMIT, 'submit'));
-?>
--- htdocs/modules/system/admin/smilies/xoops_version.php
+++ htdocs/modules/system/admin/smilies/xoops_version.php
@@ -2,21 +2,17 @@
 /**
  * Administration of smilies, versionfile
  *
- * @copyright	http://www.xoops.org/ The XOOPS Project
- * @copyright	XOOPS_copyrights.txt
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @package	Administration
- * @since	XOOPS
- * @author	http://www.xoops.org The XOOPS Project
- * @author	modified by UnderDog <underdog@impresscms.org>
- * @version	$Id$
+ * @license		LICENSE.txt
+ * @package		System
+ * @subpackage	Smilies
+ * @version		SVN: $Id$
  */
 
 $modversion = array(
 	'name' => _MD_AM_SMLS,
 	'version' => "",
-	'description' => "Smilies Settings",
+	'description' => _MD_AM_SMLS_DSC,
 	'author' => "",
 	'credits' => "The MPN SE Project",
 	'help' => "smilies.html",
