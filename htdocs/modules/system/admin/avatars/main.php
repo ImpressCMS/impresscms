e71e6e1cd5312f2e7b72d8a80bcbbce411dfeed8
Vaughan Montgomery <vaughan@impresscms.org>
2011-03-29 11:37:08 +0000

    adding filtering to $_POST & $_GET op

--- htdocs/modules/system/admin/avatars/main.php
+++ htdocs/modules/system/admin/avatars/main.php
@@ -15,9 +15,9 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 	if (!empty($_POST)) foreach ($_POST as $k => $v) ${$k} = StopXSS($v);
 	if (!empty($_GET)) foreach ($_GET as $k => $v) ${$k} = StopXSS($v);
 	$op = (isset($_GET['op'])) 
-		? trim(StopXSS($_GET['op'])) 
+		? trim(filter_input(INPUT_GET, 'op'))
 		: ((isset($_POST['op'])) 
-			? trim(StopXSS($_POST['op'])) 
+			? trim(filter_input(INPUT_POST, 'op'))
 			: 'list'
 		);
 	if ($op == 'list') {
