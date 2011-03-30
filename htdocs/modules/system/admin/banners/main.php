a5a060ff671eebacadda0dabf727b5ddfc1aa22f
Steve Kenow <skenow@impresscms.org>
2011-03-30 00:20:30 +0000

    This is an example of how to use the icms_core_DataFilter::checkVarArray method on input - * Specify all the get/post variables you will be expecting and their type * Optionally, use the options for the type for further control * You can leave out any variables that will be strings and then use the 3rd parameter to apply the default string validation. If you set the $strict parameter to TRUE, though, you will discard any variables not explicitly found in the filter array

--- htdocs/modules/system/admin/banners/main.php
+++ htdocs/modules/system/admin/banners/main.php
@@ -15,25 +15,25 @@ if (!is_object(icms::$user)
 {
 	exit('Access Denied');
 }
-include_once ICMS_ROOT_PATH . '/modules/system/admin/banners/banners.php';
+include_once ICMS_MODULES_PATH . '/system/admin/banners/banners.php';
 $allowedHTML = array('htmlcode');
 /*
  * valid POST variables
- * name
- * cid
- * imageurl
- * imptotal
- * htmlbanner
- * htmlcode
- * contact
- * email
- * login
- * passwd
- * extrainfo
- * bid
- * clickurl
- * op
- * impadded
+ * (str) 	name
+ * (int) 	cid
+ * (url) 	imageurl
+ * (int) 	imptotal
+ * (int) 	htmlbanner
+ * (html) 	htmlcode
+ * (str) 	contact
+ * (email) 	email
+ * (str) 	login
+ * (str) 	passwd
+ * (str) 	extrainfo
+ * (int) 	bid
+ * (url) 	clickurl
+ * (str) 	op
+ * (int) 	impadded
  * 
  * valid GET variables
  * op:	BannersAdmin, BannersAdd, BannerAddClient, BannerFinishDelete, 
@@ -42,33 +42,55 @@ $allowedHTML = array('htmlcode');
  * 		BannerClientChange
  * bid
  * cid
+ * fct	banners
  */
+/* This is an example of how to use the icms_core_DataFilter::checkVarArray method on input 
+ * Specify all the get/post variables you will be expecting and their type
+ * Optionally, use the options for the type for further control
+ * You can leave out any variables that will be strings and then use the 3rd parameter
+ * to apply the default string validation. If you set the $strict parameter to TRUE, though, 
+ * you will discard any variables not explicitly found in the filter array
+ */ 
+$filter_post = array(
+	'name'			=> 'str',
+	'cid' 			=> 'int',
+	'imageurl' 		=> 'url',
+	'imptotal' 		=> 'int',
+	'htmlbanner' 	=> 'int',
+	'htmlcode' 		=> 'html',
+	'contact' 		=> 'str',
+	'email' 		=> array('email', 'options' => array(0,1)),
+	'login'			=> 'str',
+	'passwd' 		=> 'str',
+	'extrainfo' 	=> 'str',
+	'bid' 			=> 'int',
+	'clickurl' 		=> 'url',
+	'op'			=> 'str',
+	'impadded' 		=> 'int',
+	'fct'			=> 'str',
+);
+$filter_get = array(
+	'bid' => 'int',
+	'cid' => 'int',
+	'fct' => 'str',
+	'op'  => 'str',
+);
+
+$name = $imageurl = $htmlcode = $contact = '';
+$email = $login = $passwd = $extrainfo = $clickurl = $op = '';
+$bid = $cid = $imptotal = $htmlbanner = $impadded = 0;
+
 if (!empty($_POST)) { 
-	foreach ($_POST as $k => $v) { 
-		if (!in_array($k, $allowedHTML)) {
-			${$k} = StopXSS($v);
-		} else {
-			${$k} = $v;
-		}
-	}
+	$clean_POST = icms_core_DataFilter::checkVarArray($_POST, $filter_post, FALSE);
+	extract($clean_POST);
 }
 if (!empty($_GET)) { 
-	foreach ($_GET as $k => $v) { 
-		if (!in_array($k, $allowedHTML)) {
-			${$k} = StopXSS($v);
-		} else {
-			${$k} = $v;
-		}
-	}
+	$clean_GET = icms_core_DataFilter::checkVarArray($_GET, $filter_get, FALSE);
+	extract($clean_GET);
 }
 
-$op = (isset($_GET['op'])) 
-	? trim(filter_input(INPUT_GET, 'op'))
-	: ((isset($_POST['op'])) 
-		? trim(filter_input(INPUT_POST, 'op'))
-		: 'BannersAdmin');
-
 switch($op) {
+	default:
 	case 'BannersAdmin':
 		BannersAdmin();
 		break;
@@ -77,13 +99,6 @@ switch($op) {
 		if (!icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$name = isset($_POST['name']) ? trim($_POST['name']) : '';
-		$cid = isset($_POST['cid']) ? (int) $_POST['cid'] : 0;
-		$imageurl = isset($_POST['imageurl']) ? trim($_POST['imageurl']) : '';
-		$clickurl = isset($_POST['clickurl']) ? trim($_POST['clickurl']) : '';
-		$imptotal = isset($_POST['imptotal']) ? (int) $_POST['imptotal'] : 0;
-		$htmlbanner = isset($_POST['htmlbanner']) ? (int) $_POST['htmlbanner'] : 0;
-		$htmlcode = isset($_POST['htmlcode']) ? trim($_POST['htmlcode']) : '';
 		if ($cid <= 0) {redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top');}
 		$db =& icms_db_Factory::instance();
 		$newid = $db->genId($db->prefix('banner') . '_bid_seq');
@@ -116,12 +131,6 @@ switch($op) {
 		if (!icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$name = isset($_POST['name']) ? trim($_POST['name']) : '';
-		$contact = isset($_POST['contact']) ? trim($_POST['contact']) : '';
-		$email = isset($_POST['email']) ? trim($_POST['email']) : '';
-		$login = isset($_POST['login']) ? trim($_POST['login']) : '';
-		$passwd = isset($_POST['passwd']) ? trim($_POST['passwd']) : '';
-		$extrainfo = isset($_POST['extrainfo']) ? trim($_POST['extrainfo']) : '';
 		$db =& icms_db_Factory::instance();
 		$newid = $db->genId(icms::$xoopsDB->prefix('bannerclient') . '_cid_seq');
 		$sql = sprintf("INSERT INTO %s (cid, name, contact, email, login, passwd, extrainfo) VALUES ('%d', %s, %s, %s, %s, %s, %s)",
@@ -140,12 +149,11 @@ switch($op) {
 
 	case 'BannerFinishDelete':
 		icms_cp_header();
-		icms_core_Message::confirm(array('op' => 'BannerFinishDelete2', 'bid' => (int) $_GET['bid'], 'fct' => 'banners'), 'admin.php', _AM_SUREDELE);
+		icms_core_Message::confirm(array('op' => 'BannerFinishDelete2', 'bid' => (int) $bid, 'fct' => 'banners'), 'admin.php', _AM_SUREDELE);
 		icms_cp_footer();
 		break;
 
 	case 'BannerFinishDelete2':
-		$bid = isset($_POST['bid']) ? (int) $_POST['bid'] : 0;
 		if ($bid <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
@@ -156,12 +164,10 @@ switch($op) {
 		break;
 
 	case 'BannerDelete':
-		$bid = isset($_GET['bid']) ? (int) $_GET['bid'] : 0;
 		if ($bid > 0) {BannerDelete($bid);}
 		break;
 
 	case 'BannerDelete2':
-		$bid = isset($_POST['bid']) ? (int) $_POST['bid'] : 0;
 		if ($bid <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
@@ -172,22 +178,13 @@ switch($op) {
 		break;
 
 	case 'BannerEdit':
-		$bid = isset($_GET['bid']) ? (int) $_GET['bid'] : 0;
 		if ($bid > 0) {BannerEdit($bid);}
 		break;
 
 	case 'BannerChange':
-		$bid = isset($_POST['bid']) ? (int) $_POST['bid'] : 0;
-		$cid = isset($_POST['cid']) ? (int) $_POST['cid'] : 0;
 		if (($cid <= 0 || $bid <= 0) | !icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$imageurl = isset($_POST['imageurl']) ? trim($_POST['imageurl']) : '';
-		$clickurl = isset($_POST['clickurl']) ? trim($_POST['clickurl']) : '';
-		$imptotal = isset($_POST['imptotal']) ? (int) $_POST['imptotal'] : 0;
-		$impadded = isset($_POST['impadded']) ? (int) $_POST['impadded'] : 0;
-		$htmlbanner = isset($_POST['htmlbanner']) ? (int) $_POST['htmlbanner'] : 0;
-		$htmlcode = isset($_POST['htmlcode']) ? trim($_POST['htmlcode']) : '';
 		$db =& icms_db_Factory::instance();
 		$sql = sprintf("UPDATE %s SET cid = '%d',
 										imptotal = '%d',
@@ -209,12 +206,10 @@ switch($op) {
 		break;
 
 	case 'BannerClientDelete':
-		$cid = isset($_GET['cid']) ? (int) $_GET['cid'] : 0;
 		if ($cid > 0) {BannerClientDelete($cid);}
 		break;
 
 	case 'BannerClientDelete2':
-		$cid = isset($_POST['cid']) ? (int) $_POST['cid'] : 0;
 		$db =& icms_db_Factory::instance();
 		if ($cid <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
@@ -227,21 +222,13 @@ switch($op) {
 		break;
 
 	case 'BannerClientEdit':
-		$cid = isset($_GET['cid']) ? (int) $_GET['cid'] : 0;
 		if ($cid > 0) {BannerClientEdit($cid);}
 		break;
 
 	case 'BannerClientChange':
-		$cid = isset($_POST['cid']) ? (int) $_POST['cid'] : 0;
 		if ($cid <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$name = isset($_POST['name']) ? trim($_POST['name']) : '';
-		$contact = isset($_POST['contact']) ? trim($_POST['contact']) : '';
-		$email = isset($_POST['email']) ? trim($_POST['email']) : '';
-		$login = isset($_POST['login']) ? trim($_POST['login']) : '';
-		$passwd = isset($_POST['passwd']) ? trim($_POST['passwd']) : '';
-		$extrainfo = isset($_POST['extrainfo']) ? trim($_POST['extrainfo']) : '';
 		$db =& icms_db_Factory::instance();
 		$sql = sprintf("UPDATE %s SET name = %s, contact = %s, email = %s, login = %s, passwd = %s, extrainfo = %s WHERE cid = '%d'",
 			$db->prefix("bannerclient"),
@@ -257,8 +244,5 @@ switch($op) {
 		redirect_header('admin.php?fct=banners&amp;op=BannersAdmin#top', 1, _AM_DBUPDATED);
 		break;
 
-	default:
-		BannersAdmin();
-		break;
 }
 
