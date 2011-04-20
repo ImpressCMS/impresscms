a9532ab93fb3821e8117c6bbfda1d14c65fa6aa1
Steve Kenow <skenow@impresscms.org>
2011-04-20 23:49:33 +0000

    Fix for #704 - multilanguage tags not being properly identified and displayed.
    As suspected, a global was not in scope when the function was loaded.

--- htdocs/plugins/preloads/multilanguage.php
+++ htdocs/plugins/preloads/multilanguage.php
@@ -1,23 +1,48 @@
 <?php
+/**
+ * Defines the Multilanguage tag handler and attaches it to the event handler during preload
+ * 
+ * @copyright	The ImpressCMS Project http://www.impresscms.org/
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @category	ICMS
+ * @package		:ibraries
+ * @since		1.3
+ * @version		$Id$
+ */
 
+/**
+ * Handles selection and display of multilanguage texts
+ * 
+ * @category	ICMS
+ * @package		Libraries
+ */
 class icms_MultilanguageEventHandler {
 
+	/**
+	 * Attaches the multilanguage handler to the event handler
+	 * 
+	 */
 	static public function setup() {
 		icms_Event::attach('icms', 'loadService-session', array(__CLASS__, 'initMultilang'));
 	}
 
+	/**
+	 * Initializes the multilanguage process
+	 * 
+	 */
 	static public function initMultilang() {
 		global $icmsConfigMultilang, $icmsConfig;
 		if ($icmsConfigMultilang['ml_enable']) {
-			require_once ICMS_ROOT_PATH . '/include/im_multilanguage.php' ;
+			require_once ICMS_INCLUDE_PATH . '/im_multilanguage.php' ;
 			$easiestml_langs = explode(',', $icmsConfigMultilang['ml_tags']);
 
-			$easiestml_langpaths = icms_core_Filesystem::getDirList(ICMS_ROOT_PATH."/language/");
+			$easiestml_langpaths = icms_core_Filesystem::getDirList(ICMS_ROOT_PATH . "/language/");
 			$langs = array_combine($easiestml_langs, explode(',', $icmsConfigMultilang['ml_names']));
 
 			if ($icmsConfigMultilang['ml_autoselect_enabled']
 				&& isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])
-				&& $_SERVER['HTTP_ACCEPT_LANGUAGE'] != "") {
+				&& $_SERVER['HTTP_ACCEPT_LANGUAGE'] != ""
+			) {
 				$autolang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
 				if (in_array($autolang, $easiestml_langs)) {
 					$icmsConfig['language'] = $langs[$autolang];
