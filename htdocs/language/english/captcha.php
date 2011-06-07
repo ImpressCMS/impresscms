64a41156696846ff07dd4ae6591774dd7ac6f604
blauer-fisch <blauer-fisch>
2011-06-07 05:28:16 +0000

    Sentence for captcha reduced.

--- htdocs/language/english/captcha.php
+++ htdocs/language/english/captcha.php
@@ -7,7 +7,7 @@ define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
 // For image mode
 define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
 define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "If the code is not clear enough, please click the image to refresh");
+define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
 
 // For text mode
 define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
