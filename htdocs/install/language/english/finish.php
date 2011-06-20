6a90ebf4de978a846cb8a207b908c42d7ee326dd
David Ledbury <david@impresscms.org>
2011-06-20 20:09:51 +0000

    Modification ref #740. Correcting link

--- htdocs/install/language/english/finish.php
+++ htdocs/install/language/english/finish.php
@@ -13,6 +13,6 @@ $content .=
 <p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
 
 <h3>Newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://eepurl.com/NQYV/' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
+<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
 ";
 ?>
