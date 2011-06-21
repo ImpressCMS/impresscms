7d81818b3834b8d5bb53032f46346be90e06d116
blauer-fisch <blauer-fisch>
2011-06-21 04:09:10 +0000

    Sentence changend, because the system use an other way now.

--- htdocs/install/modules/protector/trust_path/modules/protector/include/precheck_functions.php
+++ htdocs/install/modules/protector/trust_path/modules/protector/include/precheck_functions.php
@@ -3,7 +3,7 @@
 function protector_prepare()
 {
 	// check the access is from install/index.php
-	if( defined( '_INSTALL_CHARSET' ) && ! is_writable( XOOPS_ROOT_PATH.'/mainfile.php' ) ) die( 'To use installer, remove protector\'s lines from mainfile.php first.' ) ;
+	if( defined( '_INSTALL_CHARSET' ) && ! is_writable( XOOPS_ROOT_PATH.'/mainfile.php' ) ) die( 'To use installer, remove the follow plugin first: /plugins/preload/protector.php' ) ;
 
 	// Protector class
 	require_once dirname(dirname(__FILE__)).'/class/protector.php' ;
