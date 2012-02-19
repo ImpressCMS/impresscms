321964207f438405740255f205e243897f4e61ab
Steve Kenow <skenow@impresscms.org>
2012-02-19 05:01:12 +0000

    Relocating the block positions component of the system module to be standard IPF: see #415

---
+++ htdocs/modules/system/class/BlockspadminHandler.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Blocks position admin Handler class
+ *
+ * @copyright	The ImpressCMS Project <http://www.impresscms.org/>
+ * @license		LICENSE.txt
+ * @category	ICMS
+ * @package		Administration
+ * @subpackage	Block Positions
+ * @since		ImpressCMS 1.2
+ * @author		Rodrigo Pereira Lima (AKA TheRplima) <therplima@impresscms.org>
+ * @author		Gustavo Pilla (aka nekro) <nekro@impresscms.org>
+ * @author		modified by UnderDog <underdog@impresscms.org>
+ * @version		SVN: $Id$
+ */
+/**
+ * System Blockspadmin Handler Class
+ *
+ * @copyright	The ImpressCMS Project <http://www.impresscms.org/>
+ * @license		LICENSE.txt
+ * @package		Administration
+ * @subpackage	Block Positions
+ * @since		ImpressCMS 1.2
+ * @author		Gustavo Pilla (aka nekro) <nekro@impresscms.org>
+ */
+class mod_system_BlockspadminHandler extends icms_view_block_position_Handler {
+
+	/**
+	 * Constructor
+	 *
+	 * @param IcmsDatabase $db
+	 */
+	public function __construct(& $db) {
+		icms_ipf_Handler::__construct($db, 'blockspadmin', 'id', 'title', 'description', 'system');
+		$this->table = $this->db->prefix('block_positions');
+	}
+}
