11d74ba5bad6cd5978059cda2d966b557a987b9a
Steve Kenow <skenow@impresscms.org>
2012-02-19 05:26:17 +0000

    Relocating the symlinks (pages) component of the system module to be standard IPF: see #415

--- htdocs/modules/system/class/Pages.php
+++ htdocs/modules/system/class/Pages.php
@@ -15,7 +15,7 @@
  * @package		System
  * @subpackage	Symlinks
  */ 
-class SystemPages extends icms_data_page_Object {
+class mod_system_Pages extends icms_data_page_Object {
 
 	/**
 	 * Constructor
@@ -82,65 +82,5 @@ class SystemPages extends icms_data_page_Object {
 		}
 
 		return $ret;
-	}
-}
-
-/**
- * Symlinks handler
- * 
- * @package		System
- * @subpackage	Symlinks
- */
-class SystemPagesHandler extends icms_data_page_Handler {
-
-	/** */
-	private $modules_name;
-
-	/**
-	 * Constructor
-	 * 
-	 * @param $db
-	 */
-	public function __construct(& $db) {
-		icms_ipf_Handler::__construct($db, 'pages', 'page_id', 'page_title', '' , 'system');
-		$this->table = $db->prefix('icmspage');
-	}
-
-	/**
-	 * Get an array of installed modules
-	 * 
-	 * @param boolean $full
-	 * @return	array
-	 */
-	public function getModulesArray($full = FALSE) {
-		if (!count($this->modules_name)) {
-			$icms_module_handler = icms::handler('icms_module');
-			$installed_modules =& $icms_module_handler->getObjects();
-			foreach ($installed_modules as $module) {
-				$this->modules_name[$module->getVar('mid')]['name'] = $module->getVar('name');
-				$this->modules_name[$module->getVar('mid')]['dirname'] = $module->getVar('dirname');
-			}
-		}
-		$rtn = $this->modules_name;
-
-		if (!$full) {
-			foreach ($this->modules_name as $key => $module) {
-				$rtn[$key] = $module['name'];
-			}
-		}
-
-		return $rtn;
-	}
-
-	/**
-	 * Change the status of the symlink in the db
-	 * 
-	 * @param $page_id
-	 * @return	boolean	FALSE if failed, TRUE if successful
-	 */
-	public function changeStatus($page_id) {
-		$page = $this->get($page_id);
-		$page->setVar('page_status', !$page->getVar('page_status'));
-		return $this->insert($page, TRUE);
 	}
 }
