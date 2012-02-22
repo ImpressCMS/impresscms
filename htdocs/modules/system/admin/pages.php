d5c7df1f76015e058ac5d9b5517efe77a6801b2e
Steve Kenow <skenow@impresscms.org>
2012-02-22 03:52:31 +0000

    Standardizing symlinks admin in the system module. See #415

--- htdocs/modules/system/admin/pages.php
+++ htdocs/modules/system/admin/pages.php
@@ -13,9 +13,12 @@
  * @version		SVN: $Id$
  */
 
-if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar("mid"))) {
-	exit ("Access Denied");
-}
+/* set post and get filters before including admin_header */
+$filter_get = array('page_id' => 'int');
+$filter_post = array('page_id' => 'int');
+$page_id = 0;
+
+include 'admin_header.php';
 
 /**
  * Edit a symlink
@@ -23,17 +26,17 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
  * @param int $post_id Postid to be edited
  */
 function editpage($page_id = 0, $clone = FALSE) {
-	global $icms_page_handler, $icmsAdminTpl;
+	global $icms_admin_handler, $icmsAdminTpl;
 
-	$pageObj = $icms_page_handler->get($page_id);
+	$pageObj = $icms_admin_handler->get($page_id);
 
 	if (!$clone && !$pageObj->isNew()) {
-		$sform = $pageObj->getForm (_AM_SYSTEM_PAGES_EDIT, 'addpage');
-		$sform->assign ($icmsAdminTpl);
+		$sform = $pageObj->getForm(_AM_SYSTEM_PAGES_EDIT, 'addpage');
+		$sform->assign($icmsAdminTpl);
 
 	} else {
 		$pageObj->setVar('page_type', 'C');
-		$sform = $pageObj->getForm (_AM_SYSTEM_PAGES_CREATE, 'addpage');
+		$sform = $pageObj->getForm(_AM_SYSTEM_PAGES_CREATE, 'addpage');
 		$sform->assign ($icmsAdminTpl);
 
 	}
@@ -42,22 +45,15 @@ function editpage($page_id = 0, $clone = FALSE) {
 	$icmsAdminTpl->display('db:admin/pages/system_adm_pagemanager_index.html');
 }
 
-$icms_page_handler = icms_getModuleHandler('pages');
 /* Use a naming convention that indicates the source of the content of the variable */
-$clean_op = '';
+$clean_op = $op;
 /* Create a whitelist of valid values, be sure to use appropriate types for each value
  * Be sure to include a value for no parameter, if you have a default condition
  */
 $valid_op = array ('mod', 'changedField', 'addpage', 'del', 'status', '');
 
-if (isset($_GET['op']))
-$clean_op = htmlentities($_GET['op']);
-if (isset($_POST ['op']))
-$clean_op = htmlentities($_POST['op']);
-
-/** Again, use a naming convention that indicates the source of the content of the variable */
-$clean_page_id = isset($_GET['page_id']) ? (int) $_GET['page_id'] : 0;
-$clean_page_id = isset($_POST['page_id']) ? (int) $_POST['page_id'] : $clean_page_id;
+/* Again, use a naming convention that indicates the source of the content of the variable */
+$clean_page_id = $page_id;
 
 /*
  * in_array() is a native PHP function that will determine if the value of the
@@ -68,47 +64,48 @@ $clean_page_id = isset($_POST['page_id']) ? (int) $_POST['page_id'] : $clean_pag
 if (in_array($clean_op, $valid_op, TRUE)) {
 	switch ($clean_op) {
 		case 'status' :
-			$icms_page_handler->changeStatus( $clean_page_id );
+			$icms_admin_handler->changeStatus($clean_page_id);
+			if (isset($rtn)) redirect_header(ICMS_URL . base64_decode($rtn));
+			
 			$rtn = '/modules/system/admin.php?fct=pages';
-			if (isset($_GET['rtn'])) {
-				redirect_header(ICMS_URL . base64_decode($_GET['rtn']));
-			} else {
-				redirect_header(ICMS_URL . $rtn);
-			}
+			redirect_header(ICMS_URL . $rtn);
 			break;
 
 		case "clone" :
-			icms_cp_header ();
+			icms_cp_header();
 			editpage($clean_page_id, TRUE);
 			break;
 
 		case "mod" :
 		case "changedField" :
-			icms_cp_header ();
+			icms_cp_header();
 			editpage($clean_page_id);
 			break;
 
 		case "addpage" :
-			$controller = new icms_ipf_Controller($icms_page_handler);
+			$controller = new icms_ipf_Controller($icms_admin_handler);
 			$controller->storeFromDefaultForm(_AM_SYSTEM_PAGES_CREATED, _AM_SYSTEM_PAGES_MODIFIED);
 			break;
 
 		case "del" :
-			$controller = new icms_ipf_Controller ($icms_page_handler);
+			$controller = new icms_ipf_Controller($icms_admin_handler);
 			$controller->handleObjectDeletion();
 			break;
 
 		default :
 			icms_cp_header();
-			$objectTable = new icms_ipf_view_Table($icms_page_handler);
+			$objectTable = new icms_ipf_view_Table($icms_admin_handler);
+
 			$objectTable->addColumn(new icms_ipf_view_Column('page_status', 'center', FALSE, 'getCustomPageStatus'));
 			$objectTable->addColumn(new icms_ipf_view_Column('page_title', _GLOBAL_LEFT, FALSE, 'getAdminViewItemLink'));
 			$objectTable->addColumn(new icms_ipf_view_Column('page_url'));
 			$objectTable->addColumn(new icms_ipf_view_Column('page_moduleid', 'center', FALSE, 'getCustomPageModuleid'));
+
 			$objectTable->addIntroButton('addpost', 'admin.php?fct=pages&amp;op=mod', _AM_SYSTEM_PAGES_CREATE);
 			$objectTable->addCustomAction('getViewItemLink');
 			$objectTable->addQuickSearch(array ('page_title', 'page_url'));
 			$objectTable->addFilter('page_moduleid', 'getModulesArray');
+
 			$icmsAdminTpl->assign('icms_page_table', $objectTable->fetch());
 			$icmsAdminTpl->assign('icms_page_title', _AM_SYSTEM_PAGES_TITLE);
 			$icmsAdminTpl->display('db:admin/pages/system_adm_pagemanager_index.html');
