97595d5530ed1adf31bd0c6a2db7cc229c27bd76
Steve Kenow <skenow@impresscms.org>
2012-02-22 03:15:12 +0000

    Standardizing block position admin in the system module. See #415

--- htdocs/modules/system/admin/blockspadmin.php
+++ htdocs/modules/system/admin/blockspadmin.php
@@ -14,69 +14,63 @@
  * @version		SVN: $Id$
  */
 
-if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
-	exit(_CT_ACCESS_DENIED);
-}
+/* set get and post filters before including admin_header */
+$id = 0;
+
+$filter_get = array('id' => 'int');
+$filter_post = array('id' => 'int');
+
+include "admin_header.php";
 
 /**
  * Edit a block position
  * @param $id
  */
 function editblockposition($id = 0) {
-	global $icms_blockposition_handler, $icmsAdminTpl;
+	global $icms_admin_handler, $icmsAdminTpl;
 
-	$blockObj = $icms_blockposition_handler->get($id);
+	$blockObj = $icms_admin_handler->get($id);
 
 	if (!$blockObj->isNew()) {
 		$sform = $blockObj->getForm(_AM_SYSTEM_BLOCKSPADMIN_EDIT, 'addblockposition');
 		$sform->assign($icmsAdminTpl);
-
 	} else {
 		$sform = $blockObj->getForm(_AM_SYSTEM_BLOCKSPADMIN_CREATE, 'addblockposition');
 		$sform->assign($icmsAdminTpl);
-
 	}
 	$icmsAdminTpl->assign('id', $id);
 	$icmsAdminTpl->assign('lang_badmin', _AM_SYSTEM_BLOCKSPADMIN_TITLE);
 	$icmsAdminTpl->display('db:admin/blockspadmin/system_adm_blockspadmin.html');
 }
 
-$icms_blockposition_handler = icms_getModuleHandler('blockspadmin');
-
-$clean_op = '';
+$clean_op = $op;
+$clean_id = $id;
 
 $valid_op = array ('mod', 'changedField', 'addblockposition', 'del', '');
-
-if (isset($_GET['op'])) $clean_op = htmlentities($_GET['op']);
-if (isset($_POST['op'])) $clean_op = htmlentities($_POST['op']);
-
-$clean_id = isset($_GET['id']) ? (int) $_GET['id'] : 0 ;
-$clean_id = isset($_POST['id']) ? (int) $_POST['id'] : $clean_id;
 
 if (in_array($clean_op, $valid_op, TRUE)) {
 
 	switch ($clean_op) {
 		case "mod":
 		case "changedField":
-
 			icms_cp_header();
-
 			editblockposition($clean_id);
 			break;
 
 		case "addblockposition":
-			$controller = new icms_ipf_Controller($icms_blockposition_handler);
+			$controller = new icms_ipf_Controller($icms_admin_handler);
 			$controller->storeFromDefaultForm(_AM_SYSTEM_BLOCKSPADMIN_CREATED, _AM_SYSTEM_BLOCKSPADMIN_MODIFIED);
 			break;
 
 		case "del":
-			$controller = new icms_ipf_Controller($icms_blockposition_handler);
+			$controller = new icms_ipf_Controller($icms_admin_handler);
 			$controller->handleObjectDeletion();
 			break;
 
 		default:
 			icms_cp_header();
-			$objectTable = new icms_ipf_view_Table($icms_blockposition_handler, FALSE);
+			$objectTable = new icms_ipf_view_Table($icms_admin_handler, FALSE);
+
 			$objectTable->addColumn(new icms_ipf_view_Column('pname'), 'center');
 			$objectTable->addColumn(new icms_ipf_view_Column('title', FALSE, FALSE, 'getCustomTitle', FALSE, FALSE, FALSE));
 			$objectTable->addColumn(new icms_ipf_view_Column('description'));
@@ -85,7 +79,6 @@ if (in_array($clean_op, $valid_op, TRUE)) {
 			$objectTable->addQuickSearch(array('pname', 'title', 'description'));
 
 			$icmsAdminTpl->assign('icms_blockposition_table', $objectTable->fetch());
-
 			$icmsAdminTpl->assign('lang_badmin', _AM_SYSTEM_BLOCKSPADMIN_TITLE);
 			$icmsAdminTpl->assign('icms_blockposition_info', _AM_SYSTEM_BLOCKSPADMIN_INFO);
 
