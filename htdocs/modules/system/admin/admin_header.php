cb98507d59f8066a66568a034b771afae105d2ab
Steve Kenow <skenow@impresscms.org>
2012-04-08 02:25:51 +0000

    Sticking with convention over configuration - adding a class in the system module to extend the icms_module_Handler class. See #415

--- htdocs/modules/system/admin/admin_header.php
+++ htdocs/modules/system/admin/admin_header.php
@@ -44,8 +44,7 @@ if (!empty($_POST)) {
     extract($clean_POST);
 }
 
-/* @todo This really only works for IPF-based objects - will need to adjust */
-if ($fct !== "" && $fct !== 'modules') {
+if ($fct !== "") {
 	$icms_admin_handler = icms_getModuleHandler($fct, $module_dir);
 	icms_loadLanguageFile($module_dir, $fct, TRUE);
 }
