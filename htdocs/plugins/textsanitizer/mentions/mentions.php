bc429cc0ce519949c1e976bd2ec688060c9d129e
Steve Kenow <skenow@impresscms.org>
2012-04-14 21:06:27 +0000

    Adding hashtags and mentions plugins. These have been modified from their original form as shown in the tutorial video, so that you do not need to use square brackets unless you need to enclose multiple words. (multiple search terms or display names with spaces in them)
    Pages in the wiki have been started for both of these new features

---
+++ htdocs/plugins/textsanitizer/mentions/mentions.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Mentions TextSanitizer plugin
+ *
+ * @link		http://wiki.impresscms.org/modules/wiki/index.php?page=MentionsPlugin
+ * @copyright	http://www.impresscms.org/ The ImpressCMS Project
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package		plugins
+ * @version		$Id$
+ */
+define('MENTIONS_LINK',	ICMS_URL . '/userinfo.php?uid=%u'); // The link to user profile
+if (@defined(_ENTER_MENTION) === FALSE) {define('_ENTER_MENTION', 'Enter the user name to mention:');}
+
+/**
+ * Finds the mention in the text
+ *
+ * The mention can be of the form @username or @[User Name] and the username must be the display name of the user
+ *
+ * @param	obj	$ts
+ * @param	str	$text
+ * @return	str	String with the pattern replaced by a link
+ */
+function textsanitizer_mentions(&$ts, $text) {
+	$patterns[] = "#([\s\R])@(?|([\w\-]+)|\[([\w\s\-]+)\])#e";
+	$replacements[] = "mentions('\\2', '\\1')";
+	return preg_replace($patterns, $replacements, $text);
+}
+
+/**
+ * Generate a link to user profile from mention
+ *
+ * @param	str	$text
+ * @param	str	$prefix
+ * @return	str	link to search results for mention
+ */
+function mentions($text, $prefix) {
+	if (empty($text)) return $text;
+	icms_loadLanguageFile("core", "user");
+	$userHandler =& icms::handler('icms_member');
+	$criteria = new Criteria('uname', $text);
+	$userId = $userHandler->getUsers($criteria);
+	if (!$userId) return $prefix . "@" . $text;
+	$ret = $prefix . "<a href='" . sprintf(MENTIONS_LINK, $userId[0]->getVar('uid')) . "' title='" . sprintf(_US_ALLABOUT, $text) ."'>@" . $text . "</a>";
+	return $ret;
+}
+
+ /**
+  * Generates the code to add a button to the DHTML editor
+  *
+  * @param unknown_type $ele_name
+  * @return	arr
+  */
+function render_mentions($ele_name) {
+	global $xoTheme;
+	$dirname = basename(dirname(__FILE__));
+	if (isset($xoTheme)) {
+		$xoTheme->addScript(ICMS_PLUGINS_URL . '/textsanitizer/' . $dirname . '/' . $dirname . '.js',
+			array('type' => 'text/javascript'));
+	}
+	$code = "<img
+		onclick='javascript:icmsCodeMention(\"" . $ele_name . "\", \"" . htmlspecialchars(_ENTER_MENTION, ENT_QUOTES) . "\");'
+		onmouseover='style.cursor=\"pointer\"'
+		src='" . ICMS_PLUGINS_URL . "/textsanitizer/" . $dirname . "/" . $dirname . ".png'
+		alt='" . $dirname . "'
+		title='" . ucfirst($dirname) . "' />&nbsp;";
+	$javascript = 'plugins/textsanitizer/'. $dirname . '/' . $dirname . '.js';
+
+	return array($code, $javascript);
+}
+
+/**
+ * Pass css style to mentions
+ *
+ * @return	str	css style for mentions
+ */
+function style_mentions() {
+	$style_info = '';
+	return $style_info;
+}
