a87fb1e632c58e5d96b4d3e38c7db67b3ae1f6f7
Steve Kenow <skenow@impresscms.org>
2012-04-27 04:05:32 +0000

    Making pagination styles more extensible - no changes are necessary to core files to add a new style. Just place the appropriate css file in libraries/paginationstyles/css/
    See http://community.impresscms.org/modules/newbb/viewtopic.php?topic_id=5193

--- htdocs/libraries/paginationstyles/paginationstyles.php
+++ htdocs/libraries/paginationstyles/paginationstyles.php
@@ -7,37 +7,17 @@
 * @copyright	The ImpressCMS Project http://www.impresscms.org/
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @package		libraries
-* @since		  1.2
-* @author		  TheRplima <therplima@impresscms.org>
+* @since		1.2
+* @author		TheRplima <therplima@impresscms.org>
 * @version		$Id: paginationstyles.php 1742 2008-04-20 14:46:20Z real_therplima $
 */
 
-$count=0;
-$styles[$count]['name'] = 'Default';
-$styles[$count]['fcss'] = 'default';
+$style_list = icms_core_Filesystem::getFileList(ICMS_LIBRARIES_PATH . "/paginationstyles/css/", "", array("css"), TRUE);
 
-$count++;
-$styles[$count]['name'] = 'Badoo';
-$styles[$count]['fcss'] = 'badoo';
-
-$count++;
-$styles[$count]['name'] = 'Blue';
-$styles[$count]['fcss'] = 'blue';
-
-$count++;
-$styles[$count]['name'] = 'sato-orange';
-$styles[$count]['fcss'] = 'satoorange';
-
-$count++;
-$styles[$count]['name'] = 'Green-Black';
-$styles[$count]['fcss'] = 'greenblack';
-
-$count++;
-$styles[$count]['name'] = 'Viciao2k3';
-$styles[$count]['fcss'] = 'viciao2k3';
-
-$count++;
-$styles[$count]['name'] = 'Youtube';
-$styles[$count]['fcss'] = 'youtube';
-
-?>
+foreach ($style_list as $filename) {
+	$filename = str_ireplace(".css", "", $filename);
+	$styles[] = array(
+		'name' => ucfirst($filename),
+		'fcss' => $filename,
+	);
+}
