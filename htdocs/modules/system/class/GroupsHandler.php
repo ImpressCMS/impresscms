97442ee8466457ecd55bd6fb92f6e6245d4bd1a3
Steve Kenow <skenow@impresscms.org>
2012-05-16 03:20:39 +0000

    Some standardization of the docblock

--- htdocs/modules/system/class/GroupsHandler.php
+++ htdocs/modules/system/class/GroupsHandler.php
@@ -2,11 +2,11 @@
 /**
  * Wrapper for administering groups
  *
- * @copyright 	The ImpressCMS Project <http://www.impresscms.org>
- * @license		GNU General Public License (GPL) <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @category	ICMS
  * @package		Administration
  * @subpackage	Groups
+ * @copyright 	The ImpressCMS Project <http://www.impresscms.org>
+ * @license		GNU General Public License (GPL) <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @version		$Id$
  */
 
