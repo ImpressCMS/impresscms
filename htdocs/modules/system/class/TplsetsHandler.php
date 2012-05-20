b03337eb0819fdab6d6a1da3c7dbf36d7e0f454d
Steve Kenow <skenow@impresscms.org>
2012-05-20 03:55:16 +0000

    Refactoring template sets to follow the standard module architecture. See #415

---
+++ htdocs/modules/system/class/TplsetsHandler.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * wrapper for template management
+ *
+ * @category	ICMS
+ * @package		Administration
+ * @subpackage	Templates
+ * @copyright	http://www.impresscms.org/ The ImpressCMS Project
+ * @license		LICENSE.txt
+ * @version		$Id$
+ */
+
+/**
+ * Handler for managing images
+
+ * @category	ICMS
+ * @package		Administration
+ * @subpackage	Templates
+ */
+class mod_system_TplsetsHandler extends icms_view_template_set_Handler {
+
+	/**
+	 * Construct the handler
+	 * @@param  obj $db	database instance (@see icms_db_Factory::instance)
+	 */
+	public function __construct(&$db) {
+		parent::__construct($db);
+		/* overriding the default table name
+		 * @todo	complete refactoring and use standard table name
+		 */
+		$this->table = $this->db->prefix('tplset');
+	}
+}
