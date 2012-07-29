aa578ec107ff1879aed5b2ac0fb2a4a1262b953c
William Hall <mrtheme@impresscms.org>
2012-07-29 18:40:14 +0000

    Adding the user object

--- htdocs/libraries/jscore/icmsObject.php
+++ htdocs/libraries/jscore/icmsObject.php
@@ -16,14 +16,31 @@ $icmsJsConfigData = array(
   'url' => $icmsConfig['xoops_url'],
   'imageset' => ICMS_IMAGES_SET_URL,
   'gaActive' => $icmsConfigMetaFooter['use_google_analytics'],
-  'uiTheme' => 'smoothness'
+  'uiTheme' => 'smoothness',
+  'adminMenu' => false
 );
+
+$icmsJsUserData = array(
+  'isUser' => (!icms::$user) ? 0 : 1,
+  'uid' => (!icms::$user) ? 0 : icms::$user->vars['uid']['value'],
+  'name' => (!icms::$user) ? 0 : icms::$user->vars['name']['value'],
+  'uname' => (!icms::$user) ? 0 : icms::$user->vars['uname']['value'],
+  'email' => (!icms::$user) ? 0 : icms::$user->vars['email']['value'],
+  'user_avatar' => (!icms::$user) ? 0 : icms::$user->vars['user_avatar']['value'],
+  'user_regdate' => (!icms::$user) ? 0 : icms::$user->vars['user_regdate']['value'],
+  'posts' => (!icms::$user) ? 0 : icms::$user->vars['posts']['value'],
+  'timezone_offset' => (!icms::$user) ? 0 : icms::$user->vars['timezone_offset']['value'],
+  'last_login' => (!icms::$user) ? 0 : icms::$user->vars['last_login']['value'],
+  'language' => (!icms::$user) ? 0 : icms::$user->vars['language']['value']
+);
+
 $redirectMessage = (!empty($_SESSION['redirect_message'])) ? '"' . $_SESSION['redirect_message'] . '"' : 'false';
 unset( $_SESSION['redirect_message'] );
 
 $xoTheme->addScript(NULL, array('type' => 'text/javascript'),
   'var icms = {' .
     'config: ' . json_encode($icmsJsConfigData) .
+    ', user: ' . json_encode($icmsJsUserData) .
     ', redirectMessage: ' . $redirectMessage .
   '}' . 
   ', routeReady = {' .
