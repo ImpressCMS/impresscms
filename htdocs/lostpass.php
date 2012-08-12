5a96aec8b3ae06370c9873c5b066210344e68c15
Steve Kenow <skenow@impresscms.org>
2012-08-12 04:25:34 +0000

    Setting value for $code to eliminate warning and incorrect actions when requesting a new password, see #629

--- htdocs/lostpass.php
+++ htdocs/lostpass.php
@@ -20,6 +20,8 @@ include 'mainfile.php';
  *	$_GET parameters
  *	code
  */
+/* set default value for $code */
+$code = '';
 
 $filter_get = $filter_post = array('email' => array('email', 'options' => array(0, 0)));
 
@@ -47,7 +49,6 @@ if (empty($getuser)) {
 } else {
 	$icmspass = new icms_core_Password();
 
-//	$code = isset($_GET['code']) ? trim(filter_input(INPUT_GET, 'code')) : '';
 	$areyou = substr($getuser[0]->getVar('pass'), 0, 5);
 	if ($code != '' && $areyou == $code) {
 		$newpass = $icmspass->createSalt(8);
