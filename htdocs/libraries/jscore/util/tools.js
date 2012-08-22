68f45ac1ed98d80c85e19ec880feb7a2846a7ddf
William Hall <mrtheme@impresscms.org>
2012-08-22 01:24:36 +0000

    Adding new method to Tool I have found useful - it allows you to update an object literal. on submit pass the form to serializeJSON method and it will return well formed object.

--- htdocs/libraries/jscore/util/tools.js
+++ htdocs/libraries/jscore/util/tools.js
@@ -15,6 +15,14 @@ define(['jquery'], function ($) {
 			link.id = typeof id !== 'undefined' ? id : null;
 			document.getElementsByTagName("head")[0].appendChild(link);
 		}
+		
+    , serializeJSON: function(ele) {
+      var json = {};
+      $.map(ele.serializeArray(), function(n, i){
+        json[n['name']] = n['value'];
+      });
+      return json;
+    }
 
 	};
 	return module;
