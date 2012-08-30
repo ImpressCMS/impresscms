b3256f9e4bad44bdf6e09c7ee5296ffda1dab52a
William Hall <mrtheme@impresscms.org>
2012-08-30 19:53:56 +0000

    missed tools update.

--- htdocs/libraries/jscore/util/tools.js
+++ htdocs/libraries/jscore/util/tools.js
@@ -1,21 +1,21 @@
 define(['jquery'], function ($) {
-	var module = {
-		loadCSS:function(url, id) {
-			
-			if(typeof id !== 'undefined') {
-				if($(id).length) {
-					return;
-				}
-			}
+  var module = {
+    loadCSS:function(url, id) {
+      
+      if(typeof id !== 'undefined') {
+        if($(id).length) {
+          return;
+        }
+      }
 
-			var link = document.createElement("link");
-			link.type = "text/css";
-			link.rel = "stylesheet";
-			link.href = url;
-			link.id = typeof id !== 'undefined' ? id : null;
-			document.getElementsByTagName("head")[0].appendChild(link);
-		}
-		
+      var link = document.createElement("link");
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.href = url;
+      link.id = typeof id !== 'undefined' ? id : null;
+      document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
     , serializeJSON: function(ele) {
       var json = {}, result;
       $.map(ele.serializeArray(), function(n, i){
@@ -25,6 +25,15 @@ define(['jquery'], function ($) {
       return json;
     }
 
-	};
-	return module;
+    , serializeArgs: function(args) {
+      var json = {}, result, i;
+      args = args.split('&');
+      for(i=0;i<args.length; i++) {
+        result = args[i].split('=');
+        json[result[0]] = result[1];
+      }
+      return json;
+    }
+  };
+  return module;
 });
