ee732ea25090a06eeab7dc0946959c50e8b74a64
William Hall <mrtheme@impresscms.org>
2012-10-01 23:15:12 +0000

    prepping for latest ui refresh

--- htdocs/themes/core/media/js/boiler-core-scripts.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(document).ready(function() {
-  // Begin domReady
-    $("ul.sf-menu").superfish();
-    $('.sf-with-ul').parent().addClass('hasChildren');
-  // End DomReady
-  });
-}(jQuery));
