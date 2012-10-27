1b7ecd0118e3a9552a36cc8a037973d357e70b7f
William Hall <mrtheme@impresscms.org>
2012-10-27 06:01:26 +0000

    Updating protector with new advisory page - rewrote tests to be a bit more clean.

--- htdocs/install/modules/protector/trust_path/modules/protector/admin/advisory.php
+++ htdocs/install/modules/protector/trust_path/modules/protector/admin/advisory.php
@@ -4,15 +4,20 @@ $db =& icms_db_Factory::instance() ;
 
 // beggining of Output
 icms_cp_header();
+
+// title
+$moduleName = $xoopsModule->getVar('name');
+echo "<div class='CPbigTitle' style='background-image: url(../images/iconbig_icms.png)'><a href='#'>" . $moduleName . "</a></div>\n";
+
+// Menu
 include dirname(__FILE__).'/mymenu.php' ;
 
 	// for RTL users
 	@define( '_GLOBAL_LEFT' , @_ADM_USE_RTL == 1 ? 'right' : 'left' ) ;
 	@define( '_GLOBAL_RIGHT' , @_ADM_USE_RTL == 1 ? 'left' : 'right' ) ;
 
-// open table for ADVISORY
-echo "<style>dd {margin-"._GLOBAL_LEFT.": 32px;}</style>\n" ;
-echo "<br />\n<div style='border: 2px solid #2F5376;padding:8px;width:95%;' class='bg4'>\n" ;
+// open ADVISORY
+echo "<div class='protectorAdvisoryWrapper'>\n";
 
 // calculate the relative path between XOOPS_ROOT_PATH and XOOPS_TRUST_PATH
 $root_paths = explode( '/' , XOOPS_ROOT_PATH ) ;
@@ -23,109 +28,176 @@ foreach( $root_paths as $i => $rpath ) {
 $relative_path = str_repeat( '../' , count( $root_paths ) - $i ) . implode( '/' , array_slice( $trust_paths , $i ) ) ;
 
 // the path of XOOPS_TRUST_PATH accessible check
-echo "<dl><dt>'XOOPS_TRUST_PATH' : " ;
-echo "<img src='".XOOPS_URL.'/'.htmlspecialchars($relative_path)."/modules/protector/public_check.png' width='40' height='20' alt='' style='border:1px solid black;' /><br /><a href='".XOOPS_URL.'/'.htmlspecialchars($relative_path)."/modules/protector/public_check.php'>"._AM_ADV_TRUSTPATHPUBLICLINK."</a></dt>\n" ;
-echo "<dd>"._AM_ADV_TRUSTPATHPUBLIC."</dd></dl>" ;
+function url_exists($url) {
+  $h = get_headers($url);
+  $status = array();
+  preg_match('/HTTP\/.* ([0-9]+) .*/', $h[0] , $status);
+  return ($status[1] == 200);
+}
+$test = url_exists(XOOPS_URL . "/" . htmlspecialchars($relative_path) . "/modules/protector/public_check.png");
+if($test) {
+	// Accessible URL - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>ICMS_TRUST_PATH (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= "<img style='padding: 2px; border: 3px solid #F2DEDE; margin: 0 0 10px;' src='" . XOOPS_URL . "/" . htmlspecialchars($relative_path) . "/modules/protector/public_check.png' alt='' /><br />\n";
+			$safeCheck .= _AM_ADV_TRUSTPATHPUBLIC . "\n";
+			$safeCheck .= "<h3><a href='" . XOOPS_URL . '/' . htmlspecialchars($relative_path) . "/modules/protector/public_check.php'>" . _AM_ADV_TRUSTPATHPUBLICLINK . "</a></h3>\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
+} else {
+	// Inaccessible - Kickass.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>ICMS_TRUST_PATH - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
+}
+echo $safeCheck;
+
 
 // register_globals
-echo "<dl><dt>'register_globals' : " ;
-$safe = ! ini_get( "register_globals" ) ;
-if( $safe ) {
-	echo "off &nbsp; <span style='color:green;font-weight:bold;'>ok</span></dt>\n" ;
+$test = ini_get( "register_globals" ) ;
+if($test) {
+	// On - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>PHP: register_globals (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_REGISTERGLOBALS . "<br />" . XOOPS_ROOT_PATH . "/.htaccess<br />\n";
+			$safeCheck .= "<h3>php_flag &nbsp; register_globals &nbsp; off</em></h3>\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo "on  &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_REGISTERGLOBALS."<br /><br />
-			".XOOPS_ROOT_PATH."/.htaccess<br /><br />
-			<b>php_flag &nbsp; register_globals &nbsp; off</b>
-		</dd>" ;
+	// Off - Kickass.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>PHP: register_globals - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "</dl>\n" ;
+echo $safeCheck;
 
 
 // allow_url_fopen
-echo "<dl><dt>'allow_url_fopen' : " ;
-$safe = ! ini_get( "allow_url_fopen" ) ;
-if( $safe ) {
-	echo "off &nbsp; <span style='color:green;font-weight:bold;'>ok</span></dt>\n" ;
+$test = ini_get( "allow_url_fopen" ) ;
+if($test) {
+	// On - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>PHP: allow_url_fopen (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_ALLOWURLFOPEN . "\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo "on  &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_ALLOWURLFOPEN."</dd>" ;
+	// Off - Kickass.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>PHP: allow_url_fopen - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "</dl>\n" ;
+echo $safeCheck;
+
 
 
 // session.use_trans_sid
-echo "<dl><dt>'session.use_trans_sid' : " ;
-$safe = ! ini_get( "session.use_trans_sid" ) ;
-if( $safe ) {
-	echo "off &nbsp; <span style='color:green;font-weight:bold;'>ok</span></dt>\n" ;
+$test = ini_get( "session.use_trans_sid" ) ;
+if($test) {
+	// On - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>Server: session.use_trans_sid (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_USETRANSSID . "\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo "on  &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_USETRANSSID."</dd>" ;
+	// Off - Kickass.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>Server: session.use_trans_sid - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "</dl>\n" ;
+echo $safeCheck;
 
 
 // XOOPS_DB_PREFIX
-echo "<dl><dt>'XOOPS_DB_PREFIX' : " ;
-$safe = strtolower( XOOPS_DB_PREFIX ) != 'xoops' ;
-if( $safe ) {
-	echo XOOPS_DB_PREFIX." &nbsp; <span style='color:green;font-weight:bold;'>ok</span></dt>\n<dd>" ;
+$test = strtolower( XOOPS_DB_PREFIX ) != 'xoops' && strtolower( XOOPS_DB_PREFIX ) != 'icms';
+if(!$test) {
+	// The DB Prefix is way too easily guessed - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>XOOPS_DB_PREFIX = " . XOOPS_DB_PREFIX . " (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_DBPREFIX . "\n";
+			$safeCheck .= "<h3><a href='index.php?page=prefix_manager'>"._AM_ADV_LINK_TO_PREFIXMAN."</a></h3>\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo XOOPS_DB_PREFIX." &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_DBPREFIX."<br />\n" ;
+	// We can only hope that this prefix is bueno - PASS.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>XOOPS_DB_PREFIX = " . XOOPS_DB_PREFIX . " - <strong>Secure!</strong> :)</span>\n";
+		$safeCheck .= "<br /><a href='index.php?page=prefix_manager'>" . _AM_ADV_LINK_TO_PREFIXMAN . "</a>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "<a href='index.php?page=prefix_manager'>"._AM_ADV_LINK_TO_PREFIXMAN."</a></dd>" ;
-echo "</dl>\n" ;
+echo $safeCheck;
 
 
 // patch to mainfile.php
-echo "<dl><dt>'mainfile.php' : " ;
-if( ! defined( 'PROTECTOR_PRECHECK_INCLUDED' ) ) {
-	echo "missing precheck &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_MAINUNPATCHED."</dd>" ;
-} else if( ! defined( 'PROTECTOR_POSTCHECK_INCLUDED' ) ) {
-	echo "missing postcheck &nbsp; <span style='color:red;font-weight:bold;'>"._AM_ADV_NOTSECURE."</span></dt>\n" ;
-	echo "<dd>"._AM_ADV_MAINUNPATCHED."</dd>" ;
+$test = defined( 'PROTECTOR_PRECHECK_INCLUDED' ) && defined( 'PROTECTOR_POSTCHECK_INCLUDED' ) ? true : false;
+if(!$test) {
+	// Mainfile not patched - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";	
+		
+		$test2 = defined( 'PROTECTOR_PRECHECK_INCLUDED' ) ? true : false;
+		$test3 = defined( 'PROTECTOR_POSTCHECK_INCLUDED' ) ? true : false;
+		if(!$test2) {
+			// Missing PreCheck
+			$safeCheck .= "<h3>Mainfile.php: Missing PreCheck (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_MAINUNPATCHED . "\n";
+		} 
+		if(!$test3) {
+			// Missing PostCheck
+			$safeCheck .= "<h3>Mainfile.php: Missing PostCheck (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_MAINUNPATCHED . "\n";
+		}
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo "patched &nbsp; <span style='color:green;font-weight:bold;'>ok</span></dt>\n" ;
+	// PreCheck and PostCheck detected - PASS.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>Mainfile.php: PreCheck/PostCheck Detected - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "</dl>\n" ;
+echo $safeCheck;
+
 
 // patch to databasefactory.php
-echo "<dl><dt>'databasefactory.php' : " ;
 $db =& icms_db_Factory::instance() ;
-if( strtolower( get_class( $db ) ) != 'protectormysqldatabase' ) {
-	echo "<span style='color:red;font-weight:bold;'>"._AM_ADV_DBFACTORYUNPATCHED."</span></dt>\n" ;
+$test = strtolower( get_class( $db ) ) != 'protectormysqldatabase';
+if($test) {
+	// DBFactory not patched - UNSAFE!
+	$safeCheck = "<div class='coreMessage errorMsg'>\n";
+		$safeCheck .= "<p>\n";
+			$safeCheck .= "<h3>databasefactory.php: Not Patched (" ._AM_ADV_NOTSECURE . ")</h3>\n";
+			$safeCheck .= _AM_ADV_DBFACTORYUNPATCHED. "\n";
+		$safeCheck .= "</p>\n";
+	$safeCheck .= "</div>\n";
 } else {
-	echo _AM_ADV_DBFACTORYPATCHED."<span style='color:green;font-weight:bold;'>ok</span></dt>\n" ;
+	// We can only hope that this prefix is bueno - PASS.
+	$safeCheck = "<div class='coreMessage confirmMsg'>\n";
+		$safeCheck .= "<span>" . _AM_ADV_DBFACTORYPATCHED . " - <strong>Secure!</strong> :)</span>\n";
+	$safeCheck .= "</div>\n";	
 }
-echo "</dl>\n" ;
+echo $safeCheck;
 
-// close table for ADVISORY
-echo "</div><br />\n" ;
 
 
 
 // open table for PROTECTION CHECK
-echo "<br />\n<div style='border: 2px solid #2F5376;padding:8px;width:95%;' class='bg4'>\n" ;
-
-echo "<h3>"._AM_ADV_SUBTITLECHECK."</h3>\n" ;
-// Check contaminations
 $uri_contami = XOOPS_URL."/index.php?xoopsConfig%5Bnocommon%5D=1" ;
-echo "<dl><dt>"._AM_ADV_CHECKCONTAMI.":</dt>\n" ;
-echo "<dd><a href='$uri_contami' target='_blank'>$uri_contami</a></dd>" ;
-echo "</dl>\n" ;
-
-// Check isolated comments
 $uri_isocom = XOOPS_URL."/index.php?cid=".urlencode(",password /*") ;
-echo "<dl><dt>"._AM_ADV_CHECKISOCOM.":</dt>\n" ;
-echo "<dd><a href='$uri_isocom' target='_blank'>$uri_isocom</a></dd>" ;
-echo "</dl>\n" ;
-// close table for PROTECTION CHECK
-echo "</div>\n" ;
+$check = "<div class='coreMessage'>\n";
+	$check .= "<p>\n";
+		$check .= "<h3>" . _AM_ADV_SUBTITLECHECK . "</h3>\n";
+		$check .= "<span>" . _AM_ADV_CHECKCONTAMI . ": <a href='" . $uri_contami . "' target='_blank'>" . $uri_contami . "</a></span><br />\n";
+		$check .= "<span>" . _AM_ADV_CHECKISOCOM . ": <a href='" . $uri_isocom . "' target='_blank'>" . $uri_isocom . "</a></span>\n";
+	$check .= "</p>\n";	
+$check .= "</div>\n";
+echo $check;
 
-
-
+echo "</div>\n";
 icms_cp_footer();
 ?>
