a66dd843e95430cef666d01d03e1a37b0adfea3c
Steve Kenow <skenow@impresscms.org>
2012-11-12 16:40:42 -0600

    Updating SimplePie to v1.3, mirrors SVN commit 12108

--- htdocs/libraries/icms/feeds/Simplerss.php
+++ htdocs/libraries/icms/feeds/Simplerss.php
@@ -2,7 +2,7 @@
 /**
  * Class handling RSS feeds, using SimplePie class
  *
- * SimplePie is a very fast and easy-to-use class, written in PHP, that puts the ?simple? back into ?really simple syndication?.
+ * SimplePie is a very fast and easy-to-use class, written in PHP, that puts the 'simple' back into 'really simple syndication'.
  * Flexible enough to suit beginners and veterans alike, SimplePie is focused on speed, ease of use, compatibility and
  * standards compliance.
  *
@@ -13,47 +13,46 @@
  * @subpackage	RSS
  * @since		1.2
  * @author		malanciault <marcan@impresscms.org)
- * @version		$Id$
+ * @version		$Id: Simplerss.php 12108 2012-11-08 22:24:04Z skenow $
  */
 
-include_once ICMS_LIBRARIES_PATH . '/simplepie/simplepie.inc' ;
-include_once ICMS_LIBRARIES_PATH . '/simplepie/idn/idna_convert.class.php' ;
+include_once ICMS_LIBRARIES_PATH . '/simplepie/autoloader.php';
+include_once ICMS_LIBRARIES_PATH . '/simplepie/idn/idna_convert.class.php';
 
 class icms_feeds_Simplerss extends SimplePie {
 
 	/**
-	 * The IcmsSimpleRss class contains feed level data and options
+	 * The icms_feeds_Simplerss class contains feed level data and options
 	 *
-	 * There are two ways that you can create a new IcmsSimpleRss object. The first
-	 * is by passing a feed URL as a parameter to the IcmsSimpleRss constructor
+	 * There are two ways that you can create a new icms_feeds_Simplerss object. The first
+	 * is by passing a feed URL as a parameter to the icms_feeds_Simplerss constructor
 	 * (as well as optionally setting the cache expiry - The cache location is automatically set
 	 * as ICMS_CACHE_PATH). This will initialise the whole feed with all of the default settings, and you
 	 * can begin accessing methods and properties immediately.
 	 *
-	 * The second way is to create the IcmsSimpleRss object with no parameters
+	 * The second way is to create the icms_feeds_Simplerss object with no parameters
 	 * at all. This will enable you to set configuration options. After setting
 	 * them, you must initialise the feed using $feed->init(). At that point the
 	 * object's methods and properties will be available to you.
 	 *
 	 * @access public
-	 * @param string $feed_url This is the URL you want to parse.
+	 * @param str $feed_url This is the URL you want to parse.
 	 * @param int $cache_duration This is the number of seconds that you want to store the cache file for.
 	 */
-	public function __construct($feed_url = null, $cache_duration = null) {
-		// Other objects, instances created here so we can set options on them
-		$this->sanitize = new SimplePie_Sanitize;
+	public function __construct($feed_url = NULL, $cache_duration = NULL) {
+		/* SimplePie 1.3+ does not accept arguments in the constructor */
+		parent::__construct();
 
 		$this->set_cache_location(ICMS_CACHE_PATH);
 
-		if ($cache_duration !== null) {
+		if ($cache_duration !== NULL) {
 			$this->set_cache_duration($cache_duration);
 		}
 
 		// Only init the script if we're passed a feed URL
-		if ($feed_url !== null) {
+		if ($feed_url !== NULL) {
 			$this->set_feed_url($feed_url);
 			$this->init();
 		}
 	}
 }
-
