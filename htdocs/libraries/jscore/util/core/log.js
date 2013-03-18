c58b999c4e4ce6762a777b2f608fa218a6aef30e
William Hall <mrtheme@impresscms.org>
2013-03-18 15:28:50 -0500

    getting rid of old browser module - feature testing is handled by modernizr and we should not encourage browser sniffing. updating log module to more robust version.

--- htdocs/libraries/jscore/util/core/log.js
+++ htdocs/libraries/jscore/util/core/log.js
@@ -2,23 +2,22 @@
  @Module: log
  disabled console.log actions by default - logs are enabled when a url param of 'log' is true
 
- example of loggin turned on:
- http://www.fossil.com/webapp/wcs/stores/servlet/HomeView?langId=-1&storeId=12052&catalogId=25005&N=0&log=true
+ example of logging turned on:
+ http://www.impresscms.org?log=true
 
  @Method: initialize
  validates presence of logging param in url
 
  @Method: disableValidation
- stores window.console in noLog var - sets console.log to empty function
+ stores window.console in noConsole var - sets console.* to empty functions
 
  @Method: enableValidation
- checks if loggin is disabled and resets it to original value if it is
+ checks if logging is disabled and resets it to original value if it is
 */
 define(function(require) {
     var $ = require('jquery')
   , qs = require('util/mout/queryString')
-  , browser = require('util/core/browser')
-  , noLog = null
+  , noConsole = null
   , module = {
     initialize:function(){
       //Prevents IE from breaking on console entries.
@@ -29,7 +28,8 @@ define(function(require) {
         };
       }
 
-      if(qs.contains('log') && qs.getParam('log') === true) {
+      var url = window.location.href;
+      if(qs.contains(url, 'console') && qs.getParam(url, 'console') === true) {
         module.enableConsole();
       } else {
         module.disableConsole();
@@ -37,18 +37,36 @@ define(function(require) {
     }
 
     , disableConsole: function() {
-      noLog = window.console.log;
-      window['console']['log'] = function() {};
+      console.log('Logging disabled locally: use (?/&)console=true to enable');
+      noConsole = window.console;
+      window['console'] = {
+        log: function () {},
+        warn: function () {},
+        error: function () {},
+        dir: function () {},
+        group: function () {},
+        groupEnd: function () {},
+        debug: function () {},
+        info: function () {},
+        assert: function () {},
+        count: function () {},
+        dirxml: function () {},
+        profile: function () {},
+        profileEnd: function () {},
+        time: function () {},
+        timeEnd: function () {},
+        trace: function () {}
+      };
     }
 
     , enableConsole: function() {
-      if(noLog === null) {
+      if(noConsole === null) {
         return;
       } else {
-        window['console']['log'] = noLog;
+        window['console'] = noConsole;
       }
     }
-    };
+  };
 
-    return module;
+  return module;
 });
