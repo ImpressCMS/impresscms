967189515a375d96fd9a95c0005c14ac64e8269b
Steve Kenow <skenow@impresscms.org>
2014-06-07 18:59:08 -0500

    Using standard form class instead of hardcoded HTML; see #446

--- htdocs/readpmsg.php
+++ htdocs/readpmsg.php
@@ -68,119 +68,124 @@ if (!empty($_POST)) {
 /* begin page logic */
 if (!is_object(icms::$user)) {
 	redirect_header("user.php", 0);
-} else {
-	$pm_handler = icms::handler('icms_data_privmessage');
-	if ($delete != 0) {
-		if (!icms::$security->check()) {
-			echo implode('<br />', icms::$security->getErrors());
-			exit();
-		}
-		$pm =& $pm_handler->get($msg_id);
-		if (!is_object($pm) || $pm->getVar('to_userid') != icms::$user->getVar('uid') || !$pm_handler->delete($pm)) {
-			exit();
-		} else {
-			redirect_header("viewpmsg.php", 1, _PM_DELETED);
-			exit();
-		}
-	}
-
-	require ICMS_ROOT_PATH . '/header.php';
-
-	$criteria = new icms_db_criteria_Item('to_userid', (int) (icms::$user->getVar('uid')));
-	$criteria->setLimit(1);
-	$criteria->setStart($start);
-	$criteria->setSort('msg_time');
-	$pm_arr = $pm_handler->getObjects($criteria);
-
-	$echo = "<div><h4>". _PM_PRIVATEMESSAGE."</h4></div><br /><a href='userinfo.php?uid="
-		. (int) (icms::$user->getVar("uid")) ."'>". _PROFILE ."</a>&nbsp;
-		<span style='font-weight:bold;'>&raquo;&raquo;</span>&nbsp;
-		<a href='viewpmsg.php'>". _PM_INBOX ."</a>&nbsp;
-		<span style='font-weight:bold;'>&raquo;&raquo;</span>&nbsp;\n";
-
-	if (empty($pm_arr)) {
-		$echo .= '<br /><br />'._PM_YOUDONTHAVE;
-	} else {
-		if (!$pm_handler->setRead($pm_arr[0])) {
-			//echo "failed";
-		}
-
-		$echo .= $pm_arr[0]->getVar("subject")."<br />
-			<form action='readpmsg.php' method='post' name='delete".$pm_arr[0]->getVar("msg_id")."'>
-			<table border='0' cellpadding='4' cellspacing='1' class='outer' width='100%'>
-			<tr><th colspan='2'>". _PM_FROM ."</th></tr><tr class='even'>\n";
-
-		$poster = new icms_member_user_Object((int) $pm_arr[0]->getVar("from_userid"));
-
-		if (!$poster->isActive()) {
-			$poster = false;
-		}
-		$echo .= "<td valign='top'>";
-
-		if ($poster != false) { // we need to do this for deleted users
-			$echo .= "<a href='userinfo.php?uid=". (int) ($poster->getVar("uid"))."'>".$poster->getVar("uname")."</a><br />\n";
-
-			if ($poster->getVar("user_avatar") != "") {
-				$echo .= "<img src='uploads/".$poster->getVar("user_avatar")."' alt='' /><br />\n";
-			}
-
-			if ($poster->getVar("user_from") != "") {
-				$echo .= _PM_FROM .": " .$poster->getVar("user_from")."<br /><br />\n";
-			}
-
-			if ($poster->isOnline()) {
-				$echo .= "<span style='color:#ee0000;font-weight:bold;'>"._PM_ONLINE."</span><br /><br />\n";
-			}
-		} else {
-			$echo .= $icmsConfig['anonymous']; // we need to do this for deleted users
-		}
-
-		$echo .= "</td><td><img src='images/subject/".$pm_arr[0]->getVar("msg_image", "E")."' alt='' />&nbsp;
-			"._PM_SENTC."".formatTimestamp($pm_arr[0]->getVar("msg_time"));
-		$echo .= "<hr /><b>".$pm_arr[0]->getVar("subject")."</b><br /><br />\n";
-
-		$var = $pm_arr[0]->getVar('msg_text', 'N');
-
-		// see if the sender had permission to use wysiwyg for the system module - in 2.0, everyone will
-		/* @todo remove editor permission check in 2.0 */
-		$permHandler = icms::handler('icms_member_groupperm');
-		$filterType = $permHandler->checkRight('use_wysiwygeditor', 1, $poster->getGroups()) ? 'html' : 'text';
-
-		$echo .= icms_core_DataFilter::checkVar($var, $filterType, 'output') . "<br /><br /></td></tr>
-			<tr class='foot'><td width='20%' colspan='2' align='"._GLOBAL_LEFT."'>";
-
-		if ($poster != false) {
-		// we dont want to reply to a deleted user!
-			$echo .= "<a href='".ICMS_URL."/pmlite.php?reply=1&amp;msg_id="
-				. $pm_arr[0]->getVar("msg_id")."' class='cboxElement'>
-				<input type='button' class='formButton' value='" . _REPLY . "' /></a>\n";
-		}
-		$echo .= "<input type='hidden' name='delete' value='1' />";
-		$echo .= icms::$security->getTokenHTML();
-		$echo .= "<input type='hidden' name='msg_id' value='".$pm_arr[0]->getVar("msg_id")."' />";
-		$echo .= "<a href='#".$pm_arr[0]->getVar("msg_id")."' onclick='javascript:document.delete"
-			.$pm_arr[0]->getVar("msg_id").".submit();'>
-			<input type='button' class='formButton' value='" . _DELETE . "' /></a>";
-		$echo .= "</td></tr><tr><td colspan='2' align='"._GLOBAL_RIGHT."'>";
-
-		$previous = $start - 1;
-		$next = $start + 1;
-
-		if ($previous >= 0) {
-			$echo .= "<a href='readpmsg.php?start=". $previous . "&amp;total_messages=". $total_messages . "'>"._PM_PREVIOUS."</a> | ";
-		} else {
-			$echo .= _PM_PREVIOUS." | ";
-		}
-
-		if ($next < $total_messages) {
-			$echo .= "<a href='readpmsg.php?start=".  $next . "&amp;total_messages=". $total_messages . "'>"._PM_NEXT."</a>";
-		} else {
-			$echo .= _PM_NEXT;
-		}
-
-		$echo .= "</td></tr></table></form>\n";
-
-		echo $echo;
-	}
-	require "footer.php";
 }
+
+$pm_handler = icms::handler('icms_data_privmessage');
+
+if ($delete != 0) {
+	if (!icms::$security->check()) {
+		echo implode('<br />', icms::$security->getErrors());
+		exit();
+	}
+
+	$pm =& $pm_handler->get($msg_id);
+
+	if (!is_object($pm) || $pm->getVar('to_userid') != icms::$user->getVar('uid') || !$pm_handler->delete($pm)) {
+		exit();
+	}
+
+	redirect_header("viewpmsg.php", 1, _PM_DELETED);
+}
+
+require ICMS_ROOT_PATH . '/header.php';
+
+$criteria = new icms_db_criteria_Item('to_userid', (int) (icms::$user->getVar('uid')));
+$criteria->setLimit(1);
+$criteria->setStart($start);
+$criteria->setSort('msg_time');
+$pm_arr = $pm_handler->getObjects($criteria);
+
+$echo = "<div><h4>". _PM_PRIVATEMESSAGE."</h4></div><br /><a href='userinfo.php?uid="
+	. (int) (icms::$user->getVar("uid")) ."'>". _PROFILE
+	."</a>&nbsp;<span style='font-weight:bold;'>&raquo;&raquo;</span>&nbsp;<a href='viewpmsg.php'>"
+	. _PM_INBOX ."</a>&nbsp;<span style='font-weight:bold;'>&raquo;&raquo;</span>&nbsp;\n";
+
+if (empty($pm_arr)) {
+	$echo .= '<br /><br />'._PM_YOUDONTHAVE;
+} else {
+	if (!$pm_handler->setRead($pm_arr[0])) { /* echo "failed"; */ }
+
+	$echo .= $pm_arr[0]->getVar("subject")."<br /><table border='0' cellpadding='4' cellspacing='1' class='outer' width='100%'><tr><th colspan='1'>"
+				. _PM_FROM ."</th></tr><tr class='even'>\n";
+	$poster = new icms_member_user_Object((int) $pm_arr[0]->getVar("from_userid"));
+
+	if (!$poster->isActive()) {
+		$poster = false;
+	}
+
+	$echo .= "<td valign='top'>";
+
+	if ($poster != false) { // we need to do this for deleted users
+		$echo .= "<a href='userinfo.php?uid=". (int) ($poster->getVar("uid"))."'>".$poster->getVar("uname")."</a><br />\n";
+
+		if ($poster->getVar("user_avatar") != "") {
+			$echo .= "<img src='uploads/".$poster->getVar("user_avatar")."' alt='' /><br />\n";
+		}
+
+		if ($poster->getVar("user_from") != "") {
+			$echo .= _PM_FROM . ": " . $poster->getVar("user_from") . "<br /><br />\n";
+		}
+
+		if ($poster->isOnline()) {
+			$echo .= "<span style='color:#ee0000;font-weight:bold;'>" . _PM_ONLINE . "</span><br /><br />\n";
+		}
+	} else {
+		$echo .= $icmsConfig['anonymous']; // we need to do this for deleted users
+	}
+
+	$echo .= "<br /><img src='images/subject/" . $pm_arr[0]->getVar("msg_image", "E")
+		."' alt='' />&nbsp;"
+		._PM_SENTC . "" . formatTimestamp($pm_arr[0]->getVar("msg_time"));
+	$echo .= "<hr /><b>".$pm_arr[0]->getVar("subject")."</b><br /><br />\n";
+
+	$var = $pm_arr[0]->getVar('msg_text', 'N');
+
+	// see if the sender had permission to use wysiwyg for the system module - in 2.0, everyone will
+	/* @todo remove editor permission check in 2.0 */
+	$permHandler = icms::handler('icms_member_groupperm');
+	$filterType = $permHandler->checkRight('use_wysiwygeditor', 1, $poster->getGroups()) ? 'html' : 'text';
+
+	$echo .= icms_core_DataFilter::checkVar($var, $filterType, 'output')
+		. "<br /><br /></td></tr><tr class='foot'><td width='20%' align='"
+		. _GLOBAL_LEFT . "'>";
+
+	if ($poster != false) {
+	// we dont want to reply to a deleted user!
+		$echo .= "<a href='".ICMS_URL."/pmlite.php?reply=1&amp;msg_id="
+			. $pm_arr[0]->getVar("msg_id")
+			."' class='cboxElement'><input type='button' class='formButton' value='"
+			. _REPLY . "' /></a>\n";
+	}
+
+	$form = new icms_form_Simple('', 'delete', 'readpmsg.php', 'post', TRUE);
+	$form->addElement(new icms_form_elements_Hidden('delete', '1'));
+	$form->addElement(new icms_form_elements_Hidden('msg_id', $pm_arr[0]->getVar("msg_id")));
+	$form->addElement(new icms_form_elements_Button('', 'delete_message', _DELETE, 'submit'));
+
+	$echo .= $form->render();
+
+	$echo .= "</td></tr><tr><td>";
+
+	$previous = $start - 1;
+	$next = $start + 1;
+
+	if ($previous >= 0) {
+		$echo .= "<a href='readpmsg.php?start=" . $previous
+			. "&amp;total_messages=" . $total_messages
+			. "' style='float:" . _GLOBAL_LEFT . "'>"
+			. _PM_PREVIOUS . "</a> ";
+	}
+
+	if ($next < $total_messages) {
+		$echo .= " <a href='readpmsg.php?start=" .  $next
+			. "&amp;total_messages=" . $total_messages
+			. "' style='float:" . _GLOBAL_RIGHT . "'>"
+			. _PM_NEXT . "</a>";
+	}
+
+	$echo .= "</td></tr></table>\n";
+}
+
+echo $echo;
+
+require "footer.php";
