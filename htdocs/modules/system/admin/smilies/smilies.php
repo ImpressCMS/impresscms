2bb18944bc38dadaccc0959fb194deb040820fa5
unknown <jpenal3@penal3-53fdf64.cordoba.justicia.junta-andalucia.es>
2014-10-29 11:11:04 +0100

    Get out HTML code of smilies.php #838

--- htdocs/modules/system/admin/smilies/smilies.php
+++ htdocs/modules/system/admin/smilies/smilies.php
@@ -8,7 +8,6 @@
  * @subpackage	Smilies
  * @todo		Extract HTML and CSS to a template
  * @version		SVN: $Id$
- */
 
 if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
 	exit("Access Denied");
@@ -19,49 +18,26 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
  * 
  */
 function SmilesAdmin() {
+	global $icmsAdminTpl;
 	$db =& icms_db_Factory::instance();
 	$url_smiles = ICMS_UPLOAD_URL;
 	icms_cp_header();
-	echo '<div class="CPbigTitle" style="background-image: url(' . ICMS_MODULES_URL . '/system/admin/smilies/images/smilies_big.png)">' . _AM_SMILESCONTROL . '</div><br />';
 
 	if ($getsmiles = $db->query("SELECT * FROM " . $db->prefix("smiles"))) {
 		if (($numsmiles = $db->getRowsNum($getsmiles)) == "0") {
 			//EMPTY
 		} else {
-			echo '<form action="admin.php" method="post"><table width="100%" class="outer" cellpadding="4" cellspacing="1">'
-				. "<tr align='center'><th align='" . _GLOBAL_LEFT . "'>" ._AM_CODE . "</th>"
-				. "<th>" ._AM_SMILIE . "</th>"
-				. "<th>" . _AM_SMILEEMOTION . "</th>"
-				. "<th>" ._AM_DISPLAYF . "</th>"
-				. "<th>" . _AM_ACTION . "</th>"
-				. "</tr>\n";
 			$i = 0;
 			while ($smiles = $db->fetchArray($getsmiles)) {
-				if ($i % 2 == 0) {
-					$class = 'even';
-				} else {
-					$class= 'odd';
-				}
-				$smiles['code'] = icms_core_DataFilter::htmlSpecialChars($smiles['code']);
-				$smiles['smile_url'] = icms_core_DataFilter::htmlSpecialChars($smiles['smile_url']);
-				$smiles['smile_emotion'] = icms_core_DataFilter::htmlSpecialChars($smiles['emotion']);
-				echo "<tr align='center' class='$class'>"
-					. "<td align='" . _GLOBAL_LEFT . "'>" . $smiles['code'] . "</td>"
-					. "<td><img src='" . $url_smiles . "/" . $smiles['smile_url'] . "' alt='' /></td>"
-					. '<td>' . $smiles['smile_emotion'] . '</td>'
-					. '<td><input type="hidden" name="smile_id[' . $i . ']" value="' . $smiles['id'] . '" /><input type="hidden" name="old_display[' . $i . ']" value="' . $smiles['display'] . '" /><input type="checkbox" value="1" name="smile_display[' . $i . ']"';
-				if ($smiles['display'] == 1) {
-					echo ' checked="checked"';
-				}
-				echo " /></td><td><a href='admin.php?fct=smilies&amp;op=SmilesEdit&amp;id=" . $smiles['id'] . "'><img src='". ICMS_IMAGES_SET_URL . "/actions/edit.png' alt=" . _EDIT . " title=" . _EDIT . " /></a>&nbsp;"
-					. "<a href='admin.php?fct=smilies&amp;op=SmilesDel&amp;id=" . $smiles['id'] . "'><img src='". ICMS_IMAGES_SET_URL . "/actions/editdelete.png' alt=" . _DELETE . " title=" . _DELETE . " /></a></td>"
-					. "</tr>\n";
+				$smile[$i]['code'] = icms_core_DataFilter::htmlSpecialChars($smiles['code']);
+				$smile[$i]['smile_url'] = icms_core_DataFilter::htmlSpecialChars($smiles['smile_url']);
+				$smile[$i]['smile_emotion'] = icms_core_DataFilter::htmlSpecialChars($smiles['emotion']);
+				$smile[$i]['id'] = icms_core_DataFilter::htmlSpecialChars($smiles['id']);
+				$smile[$i]['display'] = icms_core_DataFilter::htmlSpecialChars($smiles['display']);
 				$i++;
+				$icmsAdminTpl->assign("smilesarray", $smile);
 			}
-			echo '<tr><td class="foot" colspan="5" align="center">'
-				. '<input type="hidden" name="op" value="SmilesUpdate" /><input type="hidden" name="fct" value="smilies" />' 
-				. icms::$security->getTokenHTML() 
-				. '<input type="submit" value="' . _SUBMIT . '" /></tr></table></form>';
+			$icmsAdminTpl->assign("security", icms::$security->getTokenHTML());		
 		}
 	} else {
 		echo _AM_CNRFTSD;
@@ -73,7 +49,7 @@ function SmilesAdmin() {
 	$smiles['smile_form'] = _AM_ADDSMILE;
 	$smiles['op'] = 'SmilesAdd';
 	$smiles['id'] = '';
-	echo "<br />";
+	$icmsAdminTpl->display(ICMS_MODULES_PATH . '/system/templates/admin/smilies/system_adm_smilies.html');
 	include ICMS_MODULES_PATH . '/system/admin/smilies/smileform.php';
 	$smile_form->display();
 	icms_cp_footer();
