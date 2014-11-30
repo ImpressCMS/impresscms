4b94ec62d8e7b16c30a21e13aeed9373f0b20915
debianus <debianus@gmail.com>
2014-11-30 10:38:51 +0100

    Re #613 Adding charset: no English languages had problem with special characters

--- htdocs/editors/tinymce/jscripts/plugins/icmsmlcontent/mlcontent.php
+++ htdocs/editors/tinymce/jscripts/plugins/icmsmlcontent/mlcontent.php
@@ -44,6 +44,7 @@ if (is_object(icms::$user) && $easiestml_exist) {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta charset="utf-8">
 <title>{#icmsmlcontent_dlg.title}</title>
 <script type="text/javascript" src="../../tiny_mce_popup.js"></script>
 <script type="text/javascript" src="js/icmsmlcontent.js"></script>
