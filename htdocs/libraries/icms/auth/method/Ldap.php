10993069989d1a2f1ceae9e16a9837fd47c6c046
David <david.j@impresscms.org>
2015-01-22 13:16:32 +0100

    Some missing XOOPS licenses See #687

--- htdocs/libraries/icms/auth/method/Ldap.php
+++ htdocs/libraries/icms/auth/method/Ldap.php
@@ -1,20 +1,51 @@
 <?php
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
 /**
- * Authentication classes, LDAP class file
+ * Authorization classes, LDAP class file
  *
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
  * @license		LICENSE.txt
  * @category	ICMS
- * @package		Authentication
- * @subpackage	LDAP
- * @version		SVN: $Id: Ldap.php 11730 2012-06-17 01:00:09Z skenow $
+ * @package		Auth
+ * @subpackage	Ldap
+ * @version		SVN: $Id: Ldap.php 12313 2013-09-15 21:14:35Z skenow $
  */
+
 /**
- * Authentication class for standard LDAP Server V2 or V3
+ * Authentification class for standard LDAP Server V2 or V3
  *
+ * @copyright	http://www.xoops.org/ The XOOPS Project
+ * @copyright	http://www.impresscms.org/ The ImpressCMS Project
+ * @license	    LICENSE.txt
+ * @author	    http://www.xoops.org The XOOPS Project
+ * @since       XOOPS
  * @category	ICMS
- * @package     Authentication
- * @subpackage  LDAP
+ * @package     Auth
+ * @subpackage  Ldap
  * @author	    Pierre-Eric MENUET	<pemphp@free.fr>
  */
 class icms_auth_method_Ldap extends icms_auth_Object {
