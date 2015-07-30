e3233d7203d52d769ef45be3ff84c66b0f0759d8
Raimondas Rimkevičius <github@mekdrop.name>
2015-07-30 11:15:11 +0300

    groups  handler in system module use class_alias

--- htdocs/modules/system/class/GroupsHandler.php
+++ htdocs/modules/system/class/GroupsHandler.php
@@ -10,25 +10,4 @@
  * @version		$Id$
  */
 
-/**
- * Handles group administration
- *
- * @category	ICMS
- * @package		Administration
- * @subpackage	Groups
- */
-class mod_system_GroupsHandler extends icms_member_group_Handler {
-
-	/**
-	 * Constructs the handler class for groups
-	 *
-	 * @param  obj $db	database instance (@see icms_db_Factory::instance)
-	 */
-	public function __construct(&$db) {
-		parent::__construct($db);
-		/* overriding the default table name
-		 * @todo	complete refactoring and use standard table name
-		 */
-		$this->table = $this->db->prefix('groups');
-	}
-}
+class_alias('icms_member_group_Handler', 'mod_system_GroupsHandler', true);
