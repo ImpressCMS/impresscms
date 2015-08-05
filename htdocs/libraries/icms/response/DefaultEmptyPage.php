58898b4e45012504b8c2aac3ffde3bd4ef71415b
Raimondas Rimkevičius <github@mekdrop.name>
2015-08-06 01:27:40 +0300

    added DefaultEmptyPage response

---
+++ htdocs/libraries/icms/response/DefaultEmptyPage.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* Creates response of PHP serialize type
+ *
+ * @author Raimondas Rimkevičius <mekdrop@impresscms.org>
+ */
+class icms_response_DefaultEmptyPage
+    extends icms_response_HTML {    
+    
+    /**
+     * Constructor
+     */
+    public function __construct() {    
+        global $xoopsOption;
+        ob_start();
+        $xoopsOption['show_cblock'] = 1;
+	/** Included to start page rendering */
+	include ICMS_ROOT_PATH . DIRECTORY_SEPARATOR . "header.php";
+	/** Included to complete page rendering */
+	include ICMS_ROOT_PATH . DIRECTORY_SEPARATOR . "footer.php";
+        $msg = ob_get_clean();
+        parent::__construct($msg);
+    }
+    
+}
