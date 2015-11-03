92d309ce10f5e58806673c40add8ef831936c55b
Raimondas Rimkevičius <github@mekdrop.name>
2015-11-04 00:59:33 +0200

    Added feeds test

--- tests/libraries/icms/TextsanitizerTest.php
+++ tests/libraries/icms/TextsanitizerTest.php
@@ -5,7 +5,7 @@ namespace ImpressCMS\Tests\Libraries\ICMS;
 class TextsanitizerTest extends \PHPUnit_Framework_TestCase {
     
     /**
-     * Test if icms_core_DataFilter is available
+     * Test if is available
      */
     public function testAvailability() {
         $this->assertTrue(class_exists('icms_core_Textsanitizer', true), "icms_core_Textsanitizer class doesn't exist");        
