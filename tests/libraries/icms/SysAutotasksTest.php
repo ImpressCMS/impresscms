741e4a673bf46d81dd7169a21b7edd7b32df7b07
Raimondas Rimkevičius <github@mekdrop.name>
2015-11-15 02:04:55 +0200

    only one assert for last test

--- tests/libraries/icms/SysAutotasksTest.php
+++ tests/libraries/icms/SysAutotasksTest.php
@@ -7,24 +7,8 @@ class SysAutotasksTest extends \PHPUnit_Framework_TestCase {
     /**
      * Test if is available
      */
-    public function testAvailability() {
-        $this->assertTrue(class_exists('icms_sys_autotasks_System', true), 'icms_sys_autotasks_System does\'t exist');
-        $instance = $this->getClassInstance('icms_sys_autotasks_System');          
-        $this->assertTrue($instance instanceof \icms_sys_autotasks_ISystem, 'icms_sys_autotasks_System doesn\'t implements icms_sys_autotasks_ISystem');
+    public function testAvailability() {        
+        $this->assertTrue(class_exists('icms_sys_autotasks_System', true), 'icms_sys_autotasks_System does\'t exist');  
     }
-    
-    /**
-     * Gets instance of class from classname
-     * 
-     * @param string $class     ClassName
-     * 
-     * @return object
-     */
-    private function getClassInstance($class) {
-        $instance = $this->getMockBuilder($class)
-                    ->disableOriginalConstructor()
-                    ->getMock();
-        return $instance;
-    }   
     
 }
