db35e244fa2927e09de4b67157328ab56140ec0f
Raimondas Rimkevičius <github@mekdrop.name>
2015-12-04 21:52:09 +0200

    Improved test

--- tests/libraries/icms/ImageTest.php
+++ tests/libraries/icms/ImageTest.php
@@ -14,7 +14,9 @@ class ImageTest extends \PHPUnit_Framework_TestCase {
                 'icms_image_set_Handler' => 'icms_ipf_Handler',
                 'icms_image_set_Object' => 'icms_ipf_Object',
                 'icms_image_Handler' => 'icms_ipf_Handler',
-                'icms_image_Object' => 'icms_ipf_Object'
+                'icms_image_Object' => 'icms_ipf_Object',
+                'icms_image_body_Handler' => 'icms_ipf_Handler',
+                'icms_image_body_Object' => 'icms_ipf_Object'
             ] as $class => $must_be_instance_of) {
                 $this->assertTrue(class_exists($class, true), $class . " class doesn't exist");        
             if ($must_be_instance_of !== null) {
@@ -24,6 +26,21 @@ class ImageTest extends \PHPUnit_Framework_TestCase {
                 $this->assertTrue( $instance instanceof $must_be_instance_of, $class . " is not instanceof " . $must_be_instance_of);
             }
         }
-    }  
+    }
+    
+    /**
+     * Tests image body functionality
+     */
+    public function testImageBody() {
+        $image_handler = \icms::handler('icms_image');
+        $image = $image_handler->create();
+        $this->assertTrue(property_exists($image, 'image_body'), 'icms_image_Object doesn\'t have image_body property');
+        $test_var = sha1(microtime(true));
+        $image->image_body = $test_var;
+        $this->assertSame($test_var, $image->getVar('image_body'), 'getVar for icms_image_Object doesn\'t work as expected (I)');
+        $image->image_body = null;
+        $image->setVar('image_body', $test_var);
+        $this->assertSame($test_var, $image->getVar('image_body'), 'getVar for icms_image_Object doesn\'t work as expected (II)');
+    }
     
 }
