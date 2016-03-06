8e0ac86cce0ad7c039af2bb9acda48d2019d4ddc
skenow <skenow@impresscms.org>
2016-03-06 14:54:41 -0600

    Restoring class definitions instead of using class_alias. This aids composer in building the autoloader files

--- htdocs/modules/system/class/ImagesHandler.php
+++ htdocs/modules/system/class/ImagesHandler.php
@@ -3,8 +3,28 @@
  * wrapper for image management
  *
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
- * @see         \icms_image_Handler
+ * @license		LICENSE.txt
  * @package     ImpressCMS\Modules\System\Class\Images
  */
-class_alias('icms_image_Handler', 'mod_system_ImagesHandler', true);
+
+/**
+ * Handler for managing images
+
+ * @category	ICMS
+ * @package		Administration
+ * @subpackage	Images
+ */
+class mod_system_ImagesHandler extends icms_image_Handler {
+
+	/**
+	 * Construct the handler
+	 * @@param  obj $db	database instance (@see icms_db_Factory::instance)
+	 */
+	public function __construct(&$db) {
+		parent::__construct($db);
+		/* overriding the default table name
+		 * @todo	complete refactoring and use standard table name
+		 */
+		$this->table = $this->db->prefix('images');
+	}
+}
--- htdocs/modules/system/class/TplsetsHandler.php
+++ htdocs/modules/system/class/TplsetsHandler.php
@@ -3,8 +3,28 @@
  * wrapper for template management
  *
  * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license	LICENSE.txt
+ * @license		LICENSE.txt
  * @package     ImpressCMS\Modules\System\Class\TemplatesSets
- * @see         \icms_view_template_set_Handler
  */
-class_alias('icms_view_template_set_Handler', 'mod_system_TplsetsHandler', true);
+
+/**
+ * Handler for managing images
+
+ * @category	ICMS
+ * @package		Administration
+ * @subpackage	Templates
+ */
+class mod_system_TplsetsHandler extends icms_view_template_set_Handler {
+
+	/**
+	 * Construct the handler
+	 * @@param  obj $db	database instance (@see icms_db_Factory::instance)
+	 */
+	public function __construct(&$db) {
+		parent::__construct($db);
+		/* overriding the default table name
+		 * @todo	complete refactoring and use standard table name
+		 */
+		$this->table = $this->db->prefix('tplset');
+	}
+}
