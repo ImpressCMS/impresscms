7563acab39783e621b493106be95c2a6e3a66c98
Raimondas Rimkevičius <github@mekdrop.name>
2018-01-29 23:13:59 +0200

    Removed manual Geshi inclusion from some other files

--- htdocs/plugins/textsanitizer/syntaxhighlightphp/syntaxhighlightphp.php
+++ htdocs/plugins/textsanitizer/syntaxhighlightphp/syntaxhighlightphp.php
@@ -29,7 +29,6 @@ function textsanitizer_syntaxhighlightphp(&$ts, $text) {
  * @param $source
  */
 function textsanitizer_geshi_php_highlight( $source) {
-	if (!@include_once ICMS_LIBRARIES_PATH . '/geshi/geshi.php' ) return false;
 	$source = icms_core_DataFilter::undoHtmlSpecialChars($source);
 
 	// Create the new GeSHi object, passing relevant stuff
