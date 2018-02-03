4a59c31cb575bf70dfffd5b5abdd60161d78bc00
Raimondas Rimkevičius <github@mekdrop.name>
2018-02-04 01:14:30 +0200

    mainfile now is not modified and is more clean

--- htdocs/mainfile.php
+++ htdocs/mainfile.php
@@ -1,44 +1,35 @@
 <?php
-// $Id: mainfile.php 12313 2013-09-15 21:14:35Z skenow $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-
 /**
-* All information in order to connect to database are going through here.
-*
-* Be careful if you are changing data's in this file.
-*
-* @copyright	http://www.xoops.org/ The XOOPS Project
-* @copyright	http://www.impresscms.org/ The ImpressCMS Project
-* @license	http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
-* @since	XOOPS
-* @package	ImpressCMS\Core
-*/
+ * All information in order to connect to database are going through here.
+ *
+ * Be careful if you are changing data's in this file.
+ */
 
 // ImpressCMS is not installed yet.
-if (!defined('XOOPS_INSTALL')) {
-    header('Location: install/index.php');
-    exit();
+if (is_dir('install') && !defined('XOOPS_INSTALL')) {
+	header('Location: install/index.php');
+	exit();
+} elseif (!defined("ICMS_MAINFILE_INCLUDED")) {
+	define("ICMS_MAINFILE_INCLUDED", true);
+
+	// (optional) Physical path to script that logs database queries.
+	// Example: define('ICMS_LOGGING_HOOK', ICMS_ROOT_PATH . '/modules/foobar/logging_hook.php');
+	define('ICMS_LOGGING_HOOK', '');
+
+	foreach (array('GLOBALS', '_SESSION', 'HTTP_SESSION_VARS', '_GET', 'HTTP_GET_VARS', '_POST', 'HTTP_POST_VARS', '_COOKIE', 'HTTP_COOKIE_VARS', '_REQUEST', '_SERVER', 'HTTP_SERVER_VARS', '_ENV', 'HTTP_ENV_VARS', '_FILES', 'HTTP_POST_FILES', 'icmsConfig') as $bad_global) {
+		if (isset($_REQUEST[$bad_global])) {
+			http_response_code(400);
+			exit();
+		}
+	}
+
+	define('ICMS_GROUP_ADMIN', 1);
+	define('ICMS_GROUP_USERS', 2);
+	define('ICMS_GROUP_ANONYMOUS', 3);
+
+	define('ICMS_ROOT_PATH', __DIR__);
+
+	if (!isset($xoopsOption['nocommon']) && ICMS_ROOT_PATH != '') {
+		include ICMS_ROOT_PATH . "/include/common.php";
+	}
 }
