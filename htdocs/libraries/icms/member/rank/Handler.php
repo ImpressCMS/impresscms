766b6b5bb306f3a0850a1f27e0ac9c6750e385ca
Raimondas Rimkevičius <github@mekdrop.name>
2018-02-05 14:09:33 +0200

    Fixed: It seems like you are calling the size function count() as part of the test condition. You might want to compute the size beforehand, and not on each iteration.

--- htdocs/libraries/icms/member/rank/Handler.php
+++ htdocs/libraries/icms/member/rank/Handler.php
@@ -77,8 +77,8 @@ class icms_member_rank_Handler extends icms_ipf_Handler {
 	public function MoveAllRanksImagesToProperPath() {
 		$sql = "SELECT rank_image FROM " . $this->table;
 		$Query = $this->query($sql, FALSE);
-		for ($i = 0; $i < count($Query); $i++) {
-			$values[] = $Query[$i]["rank_image"];
+		foreach ($Query as $qpart) {
+			$values[] = $qpart["rank_image"];
 		}
 
 		foreach ($values as $value) {
