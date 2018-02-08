0f1687ea0b0667499851c4131d230e915345e298
Raimondas Rimkevičius <github@mekdrop.name>
2018-02-08 16:02:41 +0200

    test bootstrap fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
 $_SERVER['REMOTE_ADDR'] = '0.0.0.0';
+
+define('ICMS_PUBLIC_PATH', dirname(__DIR__) . '/htdocs');
+
 require_once dirname(__DIR__) . '/mainfile.php';
 
 error_reporting(E_ALL & E_STRICT & ~E_DEPRECATED);
