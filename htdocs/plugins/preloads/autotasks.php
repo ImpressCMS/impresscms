97607dc55cdc2599f63f351fdefa1966ca2cb5b1
Raimondas Rimkevičius <github@mekdrop.name>
2018-03-13 21:42:26 +0200

    Remove var-dump from autotask preload

--- htdocs/plugins/preloads/autotasks.php
+++ htdocs/plugins/preloads/autotasks.php
@@ -26,7 +26,6 @@ class IcmsPreloadAutotasks extends icms_preload_Item {
 			$rez = $handler->execTasks();
 			$handler->startIfNeeded();
 			if ($handler->needExit()) {
-				var_dump($rez);
 				exit(0);
 			}
 		}
