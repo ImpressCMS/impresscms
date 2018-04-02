5881f7885cff6d9ef4ae855b667aee8d04bbe3fd
Raimondas Rimkevičius <github@mekdrop.name>
2018-04-02 13:26:17 +0300

    tests fix

--- tests/libraries/icms/SessionTest.php
+++ tests/libraries/icms/SessionTest.php
@@ -2,6 +2,9 @@
 
 namespace ImpressCMS\Tests\Libraries\ICMS;
 
+use ImpressCMS\Core\Providers\SessionServiceProvider;
+use League\Container\Container;
+
 /**
 * @backupGlobals disabled
 * @backupStaticAttributes disabled
@@ -20,15 +23,18 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
      * Tests service method
      */
     public function testService() {
-        $this->assertTrue(method_exists('icms_core_Session', 'service'), 'static method service doesn\'t exists for icms_core_Session');
-        $this->assertTrue(\icms_core_Session::service() instanceof \icms_core_Session, 'service method doesn\'t return instanceod icms_core_Session type');
+		$container = new Container();
+		$container->addServiceProvider(SessionServiceProvider::class);
+		$this->assertTrue($container->get('session') instanceof \icms_core_Session, 'service method doesn\'t return instanceod icms_core_Session type');
     }
 
     /**
      * Checks if all required variables are available
      */
     public function testVariables() {
-        $instance = \icms_core_Session::service();
+		$container = new Container();
+		$container->addServiceProvider(SessionServiceProvider::class);
+		$instance = $container->get('session');
         $this->assertInternalType('int', $instance->ipv6securityLevel, 'ipv6securityLevel must be int');
         $this->assertInternalType('int', $instance->securityLevel, 'securityLevel must be int');
         $this->assertInternalType('bool', $instance->enableRegenerateId, 'enableRegenerateId must be bool');
@@ -38,7 +44,9 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
      * Checks if all required methods are available
      */
     public function testMethodsAvailability() {
-        $instance = \icms_core_Session::service();
+		$container = new Container();
+		$container->addServiceProvider(SessionServiceProvider::class);
+		$instance = $container->get('session');
         foreach ([ 'open', 'close', 'read', 'write', 'destroy', 'gc', 'gc_force', 'icms_sessionRegenerateId', 'update_cookie', 'createFingerprint', 'checkFingerprint', 'sessionOpen', 'removeExpiredCustomSession', 'sessionClose', 'sessionStart' ] as $method) {
             $this->assertTrue(method_exists($instance, $method), $method . ' doesn\'t exists');
         }
