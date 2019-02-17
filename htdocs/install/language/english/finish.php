65da516838beb3aee9aed8ba7048af37dad5b573
Steve Kenow <skenow@impresscms.org>
2019-02-16 22:18:17 -0600

    More fixes with strings, found by Code Climate, See #373

--- htdocs/install/language/english/finish.php
+++ htdocs/install/language/english/finish.php
@@ -3,11 +3,11 @@
 $content .=
 "<h3>Support</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>"
-"<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>"
-"<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>"
-"<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>"
-"</ul>
+<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>
+<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+</ul>
 
 <h3>Newsletter</h3>
 <p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
