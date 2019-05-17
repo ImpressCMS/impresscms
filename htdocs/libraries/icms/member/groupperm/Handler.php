bf008a3eb4c44b19edfc04ba7821bfb238378687
Raimondas Rimkevičius <github@mekdrop.name>
2019-05-17 13:53:07 +0300

    This probably will fix good enough #367 issue (#443)
    close #367

--- htdocs/libraries/icms/member/groupperm/Handler.php
+++ htdocs/libraries/icms/member/groupperm/Handler.php
@@ -379,15 +379,15 @@ class icms_member_groupperm_Handler extends icms_core_ObjectHandler {
 	public function getGroupIds($gperm_name, $gperm_itemid, $gperm_modid = 1) {
 		$ret = array();
 		$perms = array();
-		if (isset($this->_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid])) {
-  			$perms = $this->_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid];
+		if (isset(self::_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid])) {
+			$perms = self::_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid];
 		} else {
 			$criteria = new icms_db_criteria_Compo(new icms_db_criteria_Item('gperm_name', $gperm_name));
 			$criteria->add(new icms_db_criteria_Item('gperm_itemid', (int) $gperm_itemid));
 			$criteria->add(new icms_db_criteria_Item('gperm_modid', (int) $gperm_modid));
 			$perms = $this->getObjects($criteria, true);
 			foreach ( $perms as $perm) {
-		  		$this->_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid][] = $perm;
+				self::_cachedRights[$gperm_name][$gperm_itemid][$gperm_modid][] = $perm;
 	  		}
 		}
 		foreach ( array_keys($perms) as $i) {
