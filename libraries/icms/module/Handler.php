db8f929a44a1298ec944fc1f41bccc73c884f421
Raimondas Rimkevičius <github@mekdrop.name>
2019-11-12 00:08:49 +0200

    module templates module instalation

--- libraries/icms/module/Handler.php
+++ libraries/icms/module/Handler.php
@@ -394,48 +394,7 @@ class icms_module_Handler
 					$logger->info(
 						sprintf(_MD_AM_MOD_DATA_INSERT_SUCCESS,  icms_conv_nr2local($newmid) )
 					);
-					$tplfile_handler = & icms::handler('icms_view_template_file');
-					$templates = $module->getInfo('templates');
-					if ($templates !== false) {
-						$logger->info(_MD_AM_TEMPLATES_ADDING);
-						foreach ($templates as $tpl) {
-							$tplfile = & $tplfile_handler->create();
-							$tpldata = & xoops_module_gettemplate($dirname, $tpl['file']);
-							$tplfile->setVar('tpl_source', $tpldata, true);
-							$tplfile->setVar('tpl_refid', $newmid);
 
-							$tplfile->setVar('tpl_tplset', 'default');
-							$tplfile->setVar('tpl_file', $tpl['file']);
-							$tplfile->setVar('tpl_desc', $tpl['description'], true);
-							$tplfile->setVar('tpl_module', $dirname);
-							$tplfile->setVar('tpl_lastmodified', time());
-							$tplfile->setVar('tpl_lastimported', 0);
-							$tplfile->setVar('tpl_type', 'module');
-							if (!$tplfile_handler->insert($tplfile)) {
-								$logger->error(
-									sprintf( '  ' . _MD_AM_TEMPLATE_INSERT_FAIL , $tpl['file'] )
-								);
-							} else {
-								$newtplid = $tplfile->getVar('tpl_id');
-								$logger->info(
-									sprintf('  ' . _MD_AM_TEMPLATE_INSERTED, $tpl['file'] ,   $newtplid )
-								);
-
-								// generate compiled file
-								if (!icms_view_Tpl::template_touch($newtplid)) {
-									$logger->error(
-										sprintf('  ' . _MD_AM_TEMPLATE_COMPILE_FAIL ,$tpl['file'],  $newtplid )
-									);
-								} else {
-									$logger->info(
-										sprintf('  ' . _MD_AM_TEMPLATE_COMPILED, $tpl['file'] )
-									);
-								}
-							}
-							unset($tpldata);
-						}
-					}
-					icms_view_Tpl::template_clear_module_cache($newmid);
 					$blocks = $module->getInfo('blocks');
 					if ($blocks !== false) {
 						$logger->info(_MD_AM_BLOCKS_ADDING);
