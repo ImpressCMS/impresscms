aba2e8d9304ec3d976826851a9dfba77e1b10d25
David Janssens <david.j@impresscms.org>
2019-12-19 14:15:17 +0100

    update system module version to 1.4.0 final

--- htdocs/modules/system/icms_version.php
+++ htdocs/modules/system/icms_version.php
@@ -62,11 +62,11 @@ $modversion = array(
 	'image'=> "images/system_slogo.png", /* for backward compatibility */
 
 /**  Development information */
-	'status_version'=> "RC",
-	'status'=> "RC",
-	'date'=> "16 Dec 2019",
+	'status_version'=> "Final",
+	'status'=> "Final",
+	'date'=> "19 Dec 2019",
 	'author_word'=> "",
-	'warning'=>_CO_ICMS_WARNING_RC,
+	'warning'=>_CO_ICMS_WARNING_FINAL,
 
 /** Contributors */
 	'developer_website_url' => "https://www.impresscms.org",
