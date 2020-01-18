e78fada5fa110a5610162df27f637836182310bd
Raimondas Rimkevičius <github@mekdrop.name>
2020-01-19 00:06:23 +0200

    now should build

--- .github/packages/docker/nginx-php-mysql/bin/do-custom-vars-tasks.sh
+++ .github/packages/docker/nginx-php-mysql/bin/do-custom-vars-tasks.sh
@@ -2,8 +2,8 @@
 
 # Sets some variables to default as in container
 export DB_TYPE=pdo.mysql
-export DB_USER=icms
+export DB_USER=root
 export DB_PORT=3306
 export DB_NAME=impresscms
 export DB_HOST=127.0.0.1
-export DB_PASS=docker
+export DB_PASS=icmsdocker
