0baed58d22f70aa4caa4b0edf917d13230c0b10b
Raimondas Rimkevičius <github@mekdrop.name>
2020-01-21 00:07:47 +0200

    Nginx+MySQL+PHP & Nginx+PHP docker images builds for releases (#591)
    * initial docker build test

---
+++ .github/packages/docker/nginx-php/bin/random-file-env-var.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+ENV_CURRENT_VALUE=$1
+ENV_VAR_NAME=$2
+ENV_VAR_LENGTH=$3
+
+mkdir -p /etc/impresscms
+
+ENV_VAR_FILE="/etc/impresscms/$ENV_VAR_NAME.cfg"
+
+if [ -z "$ENV_CURRENT_VALUE" ]; then
+  if [ ! -f "$ENV_VAR_FILE" ]; then
+  	echo "Generating random $ENV_VAR_NAME..."
+    echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $ENV_VAR_LENGTH | head -n 1) > $ENV_VAR_FILE
+    chmod 0444 $ENV_VAR_FILE
+  fi;
+  echo "Reading $ENV_VAR_NAME from file..."
+  export "$ENV_VAR_NAME"=$(cat $ENV_VAR_FILE)
+fi;
