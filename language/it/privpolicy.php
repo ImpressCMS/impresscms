0b7cf5a03d4272b9172160616637c936316df58f
David Janssens <fiammybe@gmail.com>
2020-02-05 16:10:08 +0100

    New translations privpolicy.php (Italian)

--- language/it/privpolicy.php
+++ language/it/privpolicy.php
@@ -1,3 +1,3 @@
 <?php
 //%%%%%%		File Name privpolicy.php 		%%%%%
-define('_PRV_PRIVACY_POLICY', 'Informativa sul trattamento dei dati personali <br>(Art. 13 D.lgs 196/2003)');
+define('_PRV_PRIVACY_POLICY', 'Privacy Policy');
