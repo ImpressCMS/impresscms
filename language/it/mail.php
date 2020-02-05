78da371c33eb90d6fd2c94e7b32b702d71e40c5b
David Janssens <fiammybe@gmail.com>
2020-02-05 16:10:16 +0100

    New translations mail.php (Italian)

--- language/it/mail.php
+++ language/it/mail.php
@@ -1,7 +1,7 @@
 <?php
-define("_MAIL_MSGBODY", "Il corpo testo del messaggio &egrave; vuoto.");
-define("_MAIL_FAILOPTPL", "Impossibile aprire il file di template!");
-define("_MAIL_SENDMAILNG", "Non &egrave; stato possibile inviare un'email a %s!");
-define("_MAIL_MAILGOOD", "Email per %s inviata con successo.");
-define("_MAIL_SENDPMNG", "Non &egrave; stato possibile inviare un messaggio privato a %s!");
-define("_MAIL_PMGOOD", "Messaggio privato per %s inviato con successo.");
+define("_MAIL_MSGBODY", "Message body is not set.");
+define("_MAIL_FAILOPTPL", "Failed opening template file.");
+define("_MAIL_SENDMAILNG", "Could not send mail to %s.");
+define("_MAIL_MAILGOOD", "Mail sent to %s.");
+define("_MAIL_SENDPMNG", "Could not send private message to %s.");
+define("_MAIL_PMGOOD", "Private message sent to %s.");
