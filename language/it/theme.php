86520cda225815c16ea4cb9b6e7d913b1b79e837
David Janssens <fiammybe@gmail.com>
2020-02-05 16:10:05 +0100

    New translations theme.php (Italian)

--- language/it/theme.php
+++ language/it/theme.php
@@ -1,46 +1,46 @@
 <?php
 // general
-define("THEME_UPTOP","Torna su");
-define("THEME_VALIDCSS","Questo tema &egrave; validato da W3C per CSS 1 and 2");
-define("THEME_VALIDXHTML","Questo tema &egrave; validato da W3C per XHTML 1.01");
-define("THEME_LICENSE","Vedi Creative Commons Licence");
-define("THEME_W3C","Validato W3C - XHTML 1.01 and CSS 1/2/3");
+define("THEME_UPTOP","Back to Top");
+define("THEME_VALIDCSS","This theme template is W3C Validated for CSS 1 and 2");
+define("THEME_VALIDXHTML","This theme template is W3C Validated for XHTML 1.01");
+define("THEME_LICENSE","See Creative Commons Licence");
+define("THEME_W3C","Valid W3C - XHTML 1.01 and CSS 1/2/3");
 
 // userbar.html
-define("THEME_REGISTER","Registrazione utente");
-define("THEME_REGISTERATION",'Registrazione Utente');
-define("THEME_REMEMBER"," Ricordami");
+define("THEME_REGISTER","Register");
+define("THEME_REGISTERATION",'User Registration');
+define("THEME_REMEMBER"," Remember me ?");
 define("THEME_PSEUDO"," Login");
-define("THEME_DESC_PSEUDO"," Inserisci il tuo login");
+define("THEME_DESC_PSEUDO"," Write your login");
 define("THEME_PASSWORD"," Password");
-define("THEME_DESC_PASSWORD"," Inserisci la tua password");
+define("THEME_DESC_PASSWORD"," Write your password");
 
-define("THEME_WELLCOME","Benvenuto");
-define("THEME_PROFILE","Vedi profilo");
-define("THEME_NOTIFICATION","Vedi notifiche");
-define("THEME_MESSAGE","Vedi messaggi");
+define("THEME_WELLCOME","Welcome");
+define("THEME_PROFILE","Your profile");
+define("THEME_NOTIFICATION","Bookmarks");
+define("THEME_MESSAGE","Show your inbox content");
 define("THEME_INBOX","Inbox");
-define("THEME_YOUHAVE","Tu hai ");
-define("THEME_NOTREAD","messaggio(i) da leggere");
-define("THEME_MYACCOUNT","Il mio account");
+define("THEME_YOUHAVE","You have ");
+define("THEME_NOTREAD","unread message(s)");
+define("THEME_MYACCOUNT","My profile");
 define("THEME_LOGOUT","Logout");
-define("THEME_ADMIN","Amministratore");
-define("THEME_OR","o");
+define("THEME_ADMIN","Admin");
+define("THEME_OR","or");
 
 // searchbar.html
-define("THEME_SEARCH","Cerca");
-define("THEME_DESC_SEARCH","Cerca in questo sito");
-define("THEME_KEYWORDS","Inserisci le chiavi di ricerca");
+define("THEME_SEARCH","Search");
+define("THEME_DESC_SEARCH","Search this site");
+define("THEME_KEYWORDS","Put the keywords");
 
 // globalnav.html
-define("THEME_CONTACT","Contatta");
-define("THEME_CONTACT_DESC","Contattaci?");
-define("THEME_SITEMAP","Mappa del sito");
-define("THEME_RSS","RSS Feeds");
+define("THEME_CONTACT","Contact");
+define("THEME_CONTACT_DESC","Contact us ?");
+define("THEME_SITEMAP","Sitemap");
+define("THEME_RSS","RSS");
 
-define("THEME_NAVTITLE","Navigazione");
+define("THEME_NAVTITLE","Navigation");
 define("THEME_HOME","Home");
-define("THEME_HOME_DESC","Tornare alla home");
+define("THEME_HOME_DESC","Back to home");
 
 // Theme accessibility
 define("THEME_Aa","Aa");
@@ -48,11 +48,11 @@ define("THEME_Amin","-");
 define("THEME_Aplus","+");
 
 // ImpressCMS Description Links
-define("THEME_Support_DESC","Supporta il progetto ImpressCMS!");
-define("THEME_Blogs_DESC","Leggi i blogs di ImpressCMS");
-define("THEME_Wiki_DESC","Impara con i wiki di ImpressCMS");
-define("THEME_Addons_DESC","Download i moduli, temi e altro ancora");
-define("THEME_Community_DESC","Unisciti alla ImpressCMS Community");
-define("THEME_ImpressCMS_DESC","Visita il sito principale di ImpressCMS");
+define("THEME_Support_DESC","Support the ImpressCMS Project !");
+define("THEME_Blogs_DESC","Read the ImpressCMS blogs");
+define("THEME_Wiki_DESC","Learn more in the community-driven wiki for ImpressCMS");
+define("THEME_Addons_DESC","Download modules, themes and more documentation");
+define("THEME_Community_DESC","Join the ImpressCMS Community");
+define("THEME_ImpressCMS_DESC","Visit the ImpressCMS main site");
 define("THEME_FEEDS","Feeds");
 define("THEME_PARTNERS","Partners");
