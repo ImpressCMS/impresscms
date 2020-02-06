00b67fac7efc100000334f38e97521d5e3d4ccf6
David Janssens <fiammybe@gmail.com>
2020-02-06 16:06:31 +0100

    New translations global.php (Russian)

--- language/ru/global.php
+++ language/ru/global.php
@@ -1,168 +1,168 @@
 <?php
 
 //%%%%%%	File Name mainfile.php 	%%%%%
-define('_PLEASEWAIT','Please Wait');
-define('_FETCHING','Loading...');
-define('_TAKINGBACK','Taking you back to where you were....');
-define('_LOGOUT','Logout');
-define('_SUBJECT','Subject');
-define('_MESSAGEICON','Message Icon');
-define('_COMMENTS','Comments');
-define('_POSTANON','Post Anonymously');
-define('_DISABLESMILEY','Disable smiley');
-define('_DISABLEHTML','Disable html');
-define('_PREVIEW','Preview');
+define('_PLEASEWAIT','Пожалуйста, подождите');
+define('_FETCHING','Загрузка...');
+define('_TAKINGBACK','Вернитесь назад, где были....');
+define('_LOGOUT','Выход');
+define('_SUBJECT','Заголовок');
+define('_MESSAGEICON','Иконка сообщения');
+define('_COMMENTS','Комментарии');
+define('_POSTANON','Послать анонимно');
+define('_DISABLESMILEY','Отключить смайлики');
+define('_DISABLEHTML','Отключить html');
+define('_PREVIEW','Просмотр');
 
-define('_GO','Go!');
-define('_NESTED','Nested');
-define('_NOCOMMENTS','No Comments');
-define('_FLAT','Flat');
-define('_THREADED','Threaded');
-define('_OLDESTFIRST','Oldest First');
-define('_NEWESTFIRST','Newest First');
-define('_MORE','more...');
-define('_IFNOTRELOAD','If the page does not automatically reload, please click <a href="%s">here</a>');
-define('_WARNINSTALL2','WARNING: Directory %s exists on your server. <br />Please remove this directory for security reasons.');
-define('_WARNINWRITEABLE','WARNING: File %s is writeable by the server. <br />Please change the permission of this file for security reasons.<br /> in Unix (444), in Win32 (read-only)');
-define('_WARNINNOTWRITEABLE','WARNING: File %s is not writeable by the server. <br />Please change the permission of this file for functionality reasons.<br /> in Unix (777), in Win32 (writeable)');
+define('_GO','Выполнить');
+define('_NESTED','Уплотненный');
+define('_NOCOMMENTS','Без комментариев');
+define('_FLAT','Плоский');
+define('_THREADED','Нитевидный');
+define('_OLDESTFIRST','Старые первыми');
+define('_NEWESTFIRST','Новые первыми');
+define('_MORE','Далее...');
+define('_IFNOTRELOAD','Если страница автоматически не перезагрузится, пожалуйста нажмите <a href=\'%s\'>сюда</a>');
+define('_WARNINSTALL2','ПРЕДУПРЕЖДЕНИЕ: Каталог %s существует на Вашем сервере. <br />Пожалуйста, удалите этот каталог из соображений безопасности.');
+define('_WARNINWRITEABLE','ПРЕДУПРЕЖДЕНИЕ: Файл %s доступен для записи. <br />Пожалуйста, измените права доступа к этому файлу из соображений безопасности.<br /> В Unix (444), в Win32 (только чтение)');
+define('_WARNINNOTWRITEABLE','ПРЕДУПРЕЖДЕНИЕ: Файл %s недоступен для записи. <br />Пожалуйста, измените права доступа к этому файлу из соображений функциональности.<br /> В Unix (777), в Win32 (запись)');
 
 // Error messages issued by icms_core_Object::cleanVars()
-define( '_XOBJ_ERR_REQUIRED', '%s is required' );
-define( '_XOBJ_ERR_SHORTERTHAN', '%s must be shorter than %d characters.' );
+define( '_XOBJ_ERR_REQUIRED', 'Требуется %s' );
+define( '_XOBJ_ERR_SHORTERTHAN', '%s должен быть короче %d символов.' );
 
 //%%%%%%	File Name themeuserpost.php 	%%%%%
-define('_PROFILE','Profile');
-define('_POSTEDBY','Posted by');
-define('_VISITWEBSITE','Visit Website');
-define('_SENDPMTO','Send Private Message to %s');
-define('_SENDEMAILTO','Send Email to %s');
-define('_ADD','Add');
-define('_REPLY','Reply');
-define('_DATE','Date');   // Posted date
+define('_PROFILE','Профиль');
+define('_POSTEDBY','Отправитель');
+define('_VISITWEBSITE','Посетить web-сайт');
+define('_SENDPMTO','Отправить личное сообщение %s');
+define('_SENDEMAILTO','Отправить Email %s');
+define('_ADD','Добавить');
+define('_REPLY','Ответить');
+define('_DATE','Дата');   // Posted date
 
 //%%%%%%	File Name admin_functions.php 	%%%%%
-define('_MAIN','Main');
-define('_MANUAL','Manual');
-define('_INFO','Info');
-define('_CPHOME','Admin Control Panel');
-define('_YOURHOME','Home Page');
+define('_MAIN','Главная');
+define('_MANUAL','Руководство');
+define('_INFO','Информация');
+define('_CPHOME','Панель управления');
+define('_YOURHOME','Главная');
 
 //%%%%%%	File Name misc.php (who's-online popup)	%%%%%
-define('_WHOSONLINE','Who\'s Online');
-define('_GUESTS', 'Guests');
-define('_MEMBERS', 'Members');
-define('_ONLINEPHRASE','<b>%s</b> user(s) are online');
-define('_ONLINEPHRASEX','<b>%s</b> user(s) are browsing <b>%s</b>');
-define('_CLOSE','Close');  // Close window
+define('_WHOSONLINE','Кто в онлайне');
+define('_GUESTS', 'Гости');
+define('_MEMBERS', 'Участники');
+define('_ONLINEPHRASE','<b>%s</b> пользователь(ей) в онлайне');
+define('_ONLINEPHRASEX','<b>%s</b> пользователь(ей) просматривают <b>%s</b>');
+define('_CLOSE','Закрыть');  // Close window
 
 //%%%%%%	File Name module.textsanitizer.php 	%%%%%
 define('_QUOTEC','Quote:');
 
 //%%%%%%	File Name admin.php 	%%%%%
-define("_NOPERM","Sorry, you don't have permission to access this area.");
+define("_NOPERM","Извините, Вы не имеете разрешения на доступ в эту зону.");
 
 //%%%%%		Common Phrases		%%%%%
-define("_NO","No");
-define("_YES","Yes");
-define("_EDIT","Edit");
-define("_DELETE","Delete");
-define("_SUBMIT","Submit");
-define("_MODULENOEXIST","Selected module does not exist!");
-define("_ALIGN","Align");
-define("_LEFT","Left");
-define("_CENTER","Center");
-define("_RIGHT","Right");
-define("_FORM_ENTER", "Please enter %s");
+define("_NO","Нет");
+define("_YES","Да");
+define("_EDIT","Редактировать");
+define("_DELETE","Удалить");
+define("_SUBMIT","Выполнить");
+define("_MODULENOEXIST","Выбранный модуль не существует!");
+define("_ALIGN","Выравнивание");
+define("_LEFT","Слева");
+define("_CENTER","По центру");
+define("_RIGHT","Справа");
+define("_FORM_ENTER", "Пожалуйста, введите %s");
 // %s represents file name
-define("_MUSTWABLE","File %s must be writable by the server!");
+define("_MUSTWABLE","Файл %s должен быть доступен на запись сервером!");
 // Module info
-define('_PREFERENCES', 'Preferences');
-define("_VERSION", "Version");
-define("_DESCRIPTION", "Description");
-define("_ERRORS", "Errors");
-define("_NONE", "None");
+define('_PREFERENCES', 'Настройки');
+define("_VERSION", "Версия");
+define("_DESCRIPTION", "Описание");
+define("_ERRORS", "Ошибки");
+define("_NONE", "Ничего");
 define('_ON','on');
-define('_READS','reads');
-define('_SEARCH','Search');
-define('_ALL', 'All');
-define('_TITLE', 'Title');
-define('_OPTIONS', 'Options');
+define('_READS','прочтений');
+define('_SEARCH','Поиск');
+define('_ALL', 'Все');
+define('_TITLE', 'Заголовок');
+define('_OPTIONS', 'Опции');
 define('_QUOTE', 'Quote');
-define('_HIDDENC', 'Hidden Content:');
+define('_HIDDENC', 'Скрытый контент:');
 define('_HIDDENTEXT', 'This content is hidden for anonymous users, please <a href="'.ICMS_URL.'/register.php" title="Registration at ' . htmlspecialchars ( $icmsConfig ['sitename'], ENT_QUOTES ) . '">register</a> to be able to see it.');
-define('_LIST', 'List');
-define('_LOGIN','User Login');
-define('_USERNAME','Username: ');
-define('_PASSWORD','Password: ');
-define("_SELECT","Select");
-define("_IMAGE","Image");
-define("_SEND","Send");
-define("_CANCEL","Cancel");
-define("_ASCENDING","Ascending order");
-define("_DESCENDING","Descending order");
-define('_BACK', 'Back');
-define('_NOTITLE', 'No title');
+define('_LIST', 'Список');
+define('_LOGIN','Вход');
+define('_USERNAME','Имя пользователя: ');
+define('_PASSWORD','Пароль: ');
+define("_SELECT","Выбор");
+define("_IMAGE","Рисунок");
+define("_SEND","Отправить");
+define("_CANCEL","Отменить");
+define("_ASCENDING","По возрастанию");
+define("_DESCENDING","По убывания");
+define('_BACK', 'Назад');
+define('_NOTITLE', 'Нет заголовка');
 
 /* Image manager */
-define('_IMGMANAGER','Image Manager');
-define('_NUMIMAGES', '%s images');
-define('_ADDIMAGE','Add Image File');
-define('_IMAGENAME','Name:');
-define('_IMGMAXSIZE','Max size allowed (bytes):');
-define('_IMGMAXWIDTH','Max width allowed (pixels):');
-define('_IMGMAXHEIGHT','Max height allowed (pixels):');
-define('_IMAGECAT','Category:');
-define('_IMAGEFILE','Image file:');
-define('_IMGWEIGHT','Order:');
-define('_IMGDISPLAY','Display this image?');
-define('_IMAGEMIME','MIME type:');
-define('_FAILFETCHIMG', 'Could not get uploaded file %s');
-define('_FAILSAVEIMG', 'Failed storing image %s into the database');
-define('_NOCACHE', 'No Cache');
-define('_CLONE', 'Clone');
-define('_INVISIBLE', 'Invisible');
+define('_IMGMANAGER','Менеджер рисунков');
+define('_NUMIMAGES', '%s рисунков');
+define('_ADDIMAGE','Добавить файл рисунка');
+define('_IMAGENAME','Имя:');
+define('_IMGMAXSIZE','Макс допустимый размер (байты):');
+define('_IMGMAXWIDTH','Макс допустимая ширина (пикселы):');
+define('_IMGMAXHEIGHT','Макс допустимая высота (пикселы):');
+define('_IMAGECAT','Категория:');
+define('_IMAGEFILE','Файл рисунка:');
+define('_IMGWEIGHT','Порядок отображения в менеджере рисунков:');
+define('_IMGDISPLAY','Показать этот рисунок?');
+define('_IMAGEMIME','MIME тип:');
+define('_FAILFETCHIMG', 'Не могу взять загруженный файл %s');
+define('_FAILSAVEIMG', 'Ошибка сохранения рисунка %s в базу данных');
+define('_NOCACHE', 'Не кэшировать');
+define('_CLONE', 'Клонировать');
+define('_INVISIBLE', 'Скрыть');
 
 //%%%%%	File Name class/xoopsform/formmatchoption.php 	%%%%%
-define("_STARTSWITH", "Starts with");
-define("_ENDSWITH", "Ends with");
-define("_MATCHES", "Matches");
-define("_CONTAINS", "Contains");
+define("_STARTSWITH", "Начинается с");
+define("_ENDSWITH", "Заканчивается");
+define("_MATCHES", "Совпадения");
+define("_CONTAINS", "Содержит");
 
 //%%%%%%	File Name commentform.php 	%%%%%
-define("_REGISTER","Register");
+define("_REGISTER","Регистрация");
 
 //%%%%%%	File Name xoopscodes.php 	%%%%%
-define("_SIZE","SIZE");  // font size
-define("_FONT","FONT");  // font family
-define("_COLOR","COLOR");  // font color
-define("_EXAMPLE","SAMPLE");
-define("_ENTERURL","Enter the URL of the link you want to add:");
-define("_ENTERWEBTITLE","Enter the web site title:");
-define("_ENTERIMGURL","Enter the URL of the image you want to add.");
-define("_ENTERIMGPOS","Now, enter the position of the image.");
-define("_IMGPOSRORL","'R' or 'r' for right, 'L' or 'l' for left, 'C' or 'c' for center, or leave it blank.");
-define("_ERRORIMGPOS","ERROR! Enter the position of the image.");
-define("_ENTEREMAIL","Enter the email address you want to add.");
-define("_ENTERCODE","Enter the codes that you want to add.");
-define("_ENTERQUOTE","Enter the text that you want to be quoted.");
-define("_ENTERHIDDEN","Enter the text that you want to be hidden for anonymous users.");
-define("_ENTERTEXTBOX","Please input text into the textbox.");
+define("_SIZE","РАЗМЕР");  // font size
+define("_FONT","ШРИФТ");  // font family
+define("_COLOR","ЦВЕТ");  // font color
+define("_EXAMPLE","ПРИМЕР");
+define("_ENTERURL","Введите адрес ссылки которую Вы хотите добавить:");
+define("_ENTERWEBTITLE","Введите название сайта:");
+define("_ENTERIMGURL","Введите адрес рисунка который Вы хотите добавить.");
+define("_ENTERIMGPOS","Теперь введите расположение рисунка.");
+define("_IMGPOSRORL","'R' или 'r' если справа, 'L' или 'l' если слева, или оставьте поле пустым.");
+define("_ERRORIMGPOS","ОШИБКА! Введите расположение рисунка.");
+define("_ENTEREMAIL","Введите e-mail адрес который Вы хотите добавить.");
+define("_ENTERCODE","Введите коды, которые Вы хотите добавить.");
+define("_ENTERQUOTE","Введите текст, который вы хотите цитировать.");
+define("_ENTERHIDDEN","Введите текст, который вы хотите скрыть от гостей.");
+define("_ENTERTEXTBOX","Пожалуйста, введите текст в поле ввода.");
 
 //%%%%%		TIME FORMAT SETTINGS   %%%%%
-define('_SECOND', '1 second');
-define('_SECONDS', '%s seconds');
-define('_MINUTE', '1 minute');
-define('_MINUTES', '%s minutes');
-define('_HOUR', '1 hour');
-define('_HOURS', '%s hours');
-define('_DAY', '1 day');
-define('_DAYS', '%s days');
-define('_WEEK', '1 week');
-define('_MONTH', '1 month');
+define('_SECOND', '1 секунда');
+define('_SECONDS', '%s секунд');
+define('_MINUTE', '1 минута');
+define('_MINUTES', '%s минут');
+define('_HOUR', '1 час');
+define('_HOURS', '%s часов');
+define('_DAY', '1 день');
+define('_DAYS', '%s дней');
+define('_WEEK', '1 неделя');
+define('_MONTH', '1 месяц');
 
-define("_DATESTRING","Y/n/j G:i:s");
-define("_MEDIUMDATESTRING","Y/n/j G:i");
-define("_SHORTDATESTRING","Y/n/j");
+define("_DATESTRING","d.m.Y H:i");
+define("_MEDIUMDATESTRING","d.m.Y H:i");
+define("_SHORTDATESTRING","d.m.Y");
 /*
  The following characters are recognized in the format string:
  a - "am" or "pm"
@@ -195,53 +195,53 @@ define("_SHORTDATESTRING","Y/n/j");
 
 //%%%%%		LANGUAGE SPECIFIC SETTINGS   %%%%%
 define('_CHARSET', 'utf-8');
-define('_LANGCODE', 'en');
+define('_LANGCODE', 'ru');
 
 // change 0 to 1 if this language is a multi-bytes language
 define("XOOPS_USE_MULTIBYTES", "0");
 // change 0 to 1 if this language is a RTL (right to left) language
 define("_ADM_USE_RTL","0");
 
-define('_MODULES','Modules');
-define('_SYSTEM','System');
-define('_IMPRESSCMS_NEWS','News');
-define('_ABOUT','ImpressCMS Project');
-define('_IMPRESSCMS_HOME','Project Home');
-define('_IMPRESSCMS_COMMUNITY','Community');
-define('_IMPRESSCMS_ADDONS','Addons');
-define('_IMPRESSCMS_WIKI','Wiki');
-define('_IMPRESSCMS_BLOG','Blog');
-define('_IMPRESSCMS_DONATE','Donate!');
-define("_IMPRESSCMS_Support","Support the project !");
-define('_IMPRESSCMS_SOURCEFORGE','SourceForge Project');
-define('_IMPRESSCMS_ADMIN','Administration of');
+define('_MODULES','Модули');
+define('_SYSTEM','Система');
+define('_IMPRESSCMS_NEWS','Новости');
+define('_ABOUT','Проект ImpressCMS');
+define('_IMPRESSCMS_HOME','Главная');
+define('_IMPRESSCMS_COMMUNITY','Сообщество');
+define('_IMPRESSCMS_ADDONS','Дополнения');
+define('_IMPRESSCMS_WIKI','Вики');
+define('_IMPRESSCMS_BLOG','Блог');
+define('_IMPRESSCMS_DONATE','Поддержите!');
+define("_IMPRESSCMS_Support","Поддержите проект!");
+define('_IMPRESSCMS_SOURCEFORGE','Проект на SourceForge');
+define('_IMPRESSCMS_ADMIN','Администрирование ');
 /** The default separator used in icms_view_Tree::getNicePathFromId */
 define('_BRDCRMB_SEP','&nbsp;:&nbsp;');
 //Content Manager
-define('_CT_NAV','Home');
-define('_CT_RELATEDS','Related pages');
+define('_CT_NAV','Главная');
+define('_CT_RELATEDS','Связанные страницы');
 //Security image (captcha)
-define("_SECURITYIMAGE_GETCODE","Enter the security code");
-define("_WARNINGUPDATESYSTEM","Congratulations, you have just successfully upgraded your site to the latest version of ImpressCMS!<br />Therefor to finish the upgrade process you'll need to click here and update your system module.<br />Click here to process the upgrade.");
+define("_SECURITYIMAGE_GETCODE","Введите защитный код");
+define("_WARNINGUPDATESYSTEM","Поздравляем, сейчас Вы провели обновление Вашего сайта до последней версии ImpressCMS!<br />Для завершения процесса апгрейда Вам необходимо кликнуть здесь и обновить системный модуль.<br />Нажать здесь для продолжения.");
 
 // This shows local support site in ImpressCMS menu, (if selected language is not English)
-define('_IMPRESSCMS_LOCAL_SUPPORT', 'https://www.impresscms.org'); //add the local support site's URL
-define('_IMPRESSCMS_LOCAL_SUPPORT_TITLE','Local support site');
-define("_ALLEFTCON","Enter the text to be aligned on the Left side.");
-define("_ALCENTERCON","Enter the text to be aligned on the Center side.");
-define("_ALRIGHTCON","Enter the text to be aligned on the Right side.");
+define('_IMPRESSCMS_LOCAL_SUPPORT', 'http://www.impresscms.ru'); //add the local support site's URL
+define('_IMPRESSCMS_LOCAL_SUPPORT_TITLE','Сайт локальной поддержки');
+define("_ALLEFTCON","Введите текст, который будет распологаться слева.");
+define("_ALCENTERCON","Введите текст, который будет распологаться в центре.");
+define("_ALRIGHTCON","Введите текст, который будет распологаться справа.");
 
-define('_MODABOUT_ABOUT', 'About');
+define('_MODABOUT_ABOUT', 'О модуле');
 // if you have troubles with this font on your language or it is not working, download tcpdf from: http://www.tecnick.com/public/code/cp_dpage.php?aiocp_dp=tcpdf and add the required font in libraries/tcpdf/fonts then write down the font name here. system will then load this font for your language.
 define('_PDF_LOCAL_FONT', '');
 define('_CALENDAR_TYPE',''); // this value is for the local calendar used in this system, if you're not sure about this leave this value as it is!
-define('_CALENDAR','Calendar');
-define('_RETRYPOST','Sorry, a time-out occured. Would you like to post again ?'); // autologin hack GIJ
+define('_CALENDAR','Календарь');
+define('_RETRYPOST','Извините, время истекло. Желаете повторить снова ?'); // autologin hack GIJ
 
 ############# added since 1.2 #############
 define('_QSEARCH','Quick Search');
 define('_PREV','Prev');
-define('_NEXT','Next');
+define('_NEXT','След. >>');
 define('_LCL_NUM0','0');
 define('_LCL_NUM1','1');
 define('_LCL_NUM2','2');
@@ -274,7 +274,7 @@ define('_ENTERMEDIAURL', 'Enter media url:');
 define("_TODAY", "	\\o\\d\\a\\y G:i");
 define("_YESTERDAY", "\\Y\e\\s\\t\e\\r\\d\\a\\y G:i");
 define("_MONTHDAY", "n/j G:i");
-define("_YEARMONTHDAY", "Y/n/j G:i");
+define("_YEARMONTHDAY", "d.m.Y H:i");
 define("_ELAPSE", "%s ago");
 define('_VISIBLE', 'Visible');
 define('_UP', 'Up');
@@ -289,7 +289,7 @@ define('_CHECKALL', 'Check all');
 define('_COPYRIGHT', 'Copyright');
 define("_LONGDATESTRING", "F jS Y, h:iA");
 define('_AUTHOR', 'Author');
-define("_CREDITS", "Credits");
+define("_CREDITS", "Участники");
 define("_LICENSE", "License");
 define("_LOCAL_FOOTER", 'Powered by ImpressCMS &copy; 2007-' . date('Y', time()) . ' <a href=\"https://www.impresscms.org/\" rel=\"external\">The ImpressCMS Project</a><br />Hosting by <a href="http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15">SiteGround</a>'); //footer Link to local support site
 define("_BLOCK_ID", "Block ID");
@@ -307,6 +307,6 @@ define("_FILTERS_MSG4","Output Filter (HTMLPurifier): ");
 // added in 2.0
 define('_ENTER_MENTION', 'Enter the user name to mention:');
 define( '_ENTER_HASHTAG', 'Enter the term(s) to tag:');
-define('_NAME', 'Name');
+define('_NAME', 'Имя');
 
-define('_OR', 'or');
+define('_OR', 'или');
