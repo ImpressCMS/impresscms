a93b58c3782efaff441d029d3db3fbb1ac283ffc
David Janssens <fiammybe@gmail.com>
2020-02-06 16:03:08 +0100

    New translations databaseupdater.php (Dutch)

--- language/nl/databaseupdater.php
+++ language/nl/databaseupdater.php
@@ -5,40 +5,40 @@
  * Author: The SmartFactory <www.smartfactory.ca>
  * Licence: GNU
  */
-define("_DATABASEUPDATER_IMPORT", "Import");
-define("_DATABASEUPDATER_CURRENTVER", "Current version: <span class='currentVer'>%s</span>");
-define("_DATABASEUPDATER_DBVER", "Database Version %s");
-define("_DATABASEUPDATER_MSG_ADD_DATA", "Data added in table %s");
-define("_DATABASEUPDATER_MSG_ADD_DATA_ERR", "Error adding data in table %s");
-define("_DATABASEUPDATER_MSG_CHGFIELD", "Changing field %s in table %s");
-define("_DATABASEUPDATER_MSG_CHGFIELD_ERR", "Error changing field %s in table %s");
-define("_DATABASEUPDATER_MSG_CREATE_TABLE", "Table %s created");
-define("_DATABASEUPDATER_MSG_CREATE_TABLE_ERR", "Error creating table %s");
-define("_DATABASEUPDATER_MSG_NEWFIELD", "Successfully added field %s");
-define("_DATABASEUPDATER_MSG_NEWFIELD_ERR", "Error adding field %s");
-define("_DATABASEUPDATER_NEEDUPDATE", "Your database is out-of-date. Please upgrade your database tables!<br /><b>Note : The ImpressCMS strongly recommends you to backup all your database tables before running this upgrade script.</b>");
-define("_DATABASEUPDATER_NOUPDATE", "Your database is up-to-date. No updates are necessary.");
-define("_DATABASEUPDATER_UPDATE_DB", "Updating Database");
-define("_DATABASEUPDATER_UPDATE_ERR", "Errors updating to version %s");
-define("_DATABASEUPDATER_UPDATE_NOW", "Update Now!");
-define("_DATABASEUPDATER_UPDATE_OK", "Successfully updated to version %s");
-define("_DATABASEUPDATER_UPDATE_TO", "Updating to version %s");
-define("_DATABASEUPDATER_UPDATE_UPDATING_DATABASE", "Updating database...");
+define("_DATABASEUPDATER_IMPORT", "Importeer");
+define("_DATABASEUPDATER_CURRENTVER", "Huidige versie: <span class='currentVer'>%s</span>");
+define("_DATABASEUPDATER_DBVER", "Database Versie %s");
+define("_DATABASEUPDATER_MSG_ADD_DATA", "Data toegevoegd in tabel %s");
+define("_DATABASEUPDATER_MSG_ADD_DATA_ERR", "Fout bij toevoegen data in tabel %s");
+define("_DATABASEUPDATER_MSG_CHGFIELD", "Wijzigen van veld %s in tabel %s");
+define("_DATABASEUPDATER_MSG_CHGFIELD_ERR", "Fout bij wijzigen van veld %s in tabel %s");
+define("_DATABASEUPDATER_MSG_CREATE_TABLE", "Tabel %s is aangemaakt");
+define("_DATABASEUPDATER_MSG_CREATE_TABLE_ERR", "Fout bij aanmaken van tabel %s");
+define("_DATABASEUPDATER_MSG_NEWFIELD", "Veld %s is succesvol toegevoegd");
+define("_DATABASEUPDATER_MSG_NEWFIELD_ERR", "Fout bij toevoegen van veld %s");
+define("_DATABASEUPDATER_NEEDUPDATE", "Uw database is niet up-to-date. Wij verzoeken u uw database tabellen bij te werken!<br /><b>Opmerking : ImpressCMS adviseery u ten zeerste eerst een backup te maken van al uw database tabellen alvorens u dit bijwerk script uitvoert.</b><br />");
+define("_DATABASEUPDATER_NOUPDATE", "Uw database is up-to-date. Er zijn geen bijwerkingen noodzakelijk.");
+define("_DATABASEUPDATER_UPDATE_DB", "Bijwerken van de database");
+define("_DATABASEUPDATER_UPDATE_ERR", "Fouten tijdens bijwerken naar versie %s");
+define("_DATABASEUPDATER_UPDATE_NOW", "Nu bijwerken!");
+define("_DATABASEUPDATER_UPDATE_OK", "Succesvol bijgewerkt naar versie %s");
+define("_DATABASEUPDATER_UPDATE_TO", "Bijwerken naar versie %s");
+define("_DATABASEUPDATER_UPDATE_UPDATING_DATABASE", "Bijwerken van database...");
 
-define("_DATABASEUPDATER_MSG_UPDATE_TABLE", "Records of table %s were successfully updated");
-define("_DATABASEUPDATER_MSG_UPDATE_TABLE_ERR", "An error occured while updating records in table %s");
-define("_DATABASEUPDATER_MSG_DELETE_TABLE", "Specified records of table %s were successfully deleted");
-define("_DATABASEUPDATER_MSG_DELETE_TABLE_ERR", "An error occured while deleting specified records in table %s");
+define("_DATABASEUPDATER_MSG_UPDATE_TABLE", "Data van tabel %s zijn succesvol bijgewerkt");
+define("_DATABASEUPDATER_MSG_UPDATE_TABLE_ERR", "Er is een fout opgetreden tijdens het bijwerken van de data in tabel %s");
+define("_DATABASEUPDATER_MSG_DELETE_TABLE", "De specifieke data van tabel %s zijn succesvol verwijderd");
+define("_DATABASEUPDATER_MSG_DELETE_TABLE_ERR", "Er is een fout opgetreden tijdens het verwijderen van spefieke data uit tabel %s");
 ############# added since 1.2 #############
-define("_DATABASEUPDATER_MSG_DB_VERSION_ERR", "Unable to update module dbversion");
-define("_DATABASEUPDATER_LATESTVER", "Latest database version : <span class='currentVer'>%s</span>");
-define("_DATABASEUPDATER_MSG_CONFIG_ERR", "Unable to insert config %s");
-define("_DATABASEUPDATER_MSG_CONFIG_SCC", "Successfully inserted %s config");
+define("_DATABASEUPDATER_MSG_DB_VERSION_ERR", "Bijwerken van module dbversie niet mogelijk");
+define("_DATABASEUPDATER_LATESTVER", "Laatste database versie : <span class='currentVer'>%s</span><br />");
+define("_DATABASEUPDATER_MSG_CONFIG_ERR", "Niet mogelijk om config %s in te voegen<br />");
+define("_DATABASEUPDATER_MSG_CONFIG_SCC", "%s config succesvol ingevoegd<br />");
 
 /* added in 1.3 */
 define( '_DATABASEUPDATER_MSG_FROM_112', "<code><h3>You have updated your site from ImpressCMS 1.1.x to ImpressCMS 1.2 so you <strong>must install the new Content module</strong> to update the core content manager. You will be redirected to the installation process in 20 seconds. If this does not happen click <a href='" . ICMS_URL . "/modules/system/admin.php?fct=modules&op=install&module=content&from_112=1'>here</a>.</h3></code>" );
 define('_DATABASEUPDATER_MSG_DROPFIELD_ERR', 'An error occured while deleting specified fields %1$s from table %2$s');
-define("_DATABASEUPDATER_MSG_DROPFIELD", 'Successfully dropped field %1$s from table %2$s');
+define("_DATABASEUPDATER_MSG_DROPFIELD", 'Veld %s succesvol verwijderd');
 
 // Added in 1.2.7/1.3.1
 define("_DATABASEUPDATER_MSG_DROP_TABLE", "Successfully dropped database table %s");
