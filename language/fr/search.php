3cf5e6e8d0cd64836cd46577398f734bc4c0bf4f
David Janssens <fiammybe@gmail.com>
2020-02-06 16:07:29 +0100

    New translations search.php (French)

--- language/fr/search.php
+++ language/fr/search.php
@@ -1,24 +1,24 @@
 <?php
 
 //%%%%%%	File Name search.php 	%%%%%
-define("_SR_SEARCH","Search");
-define("_SR_PLZENTER","Please enter all required data!");
-define("_SR_SEARCHRESULTS","Search Results");
-define("_SR_NOMATCH","No Match Found for your Query");
+define("_SR_SEARCH","Chercher");
+define("_SR_PLZENTER","Merci de saisir toutes les donn&eacute;es requises!");
+define("_SR_SEARCHRESULTS","R&eacute;sultats de la recherche");
+define("_SR_NOMATCH","Pas de correspondance trouv&eacute;e pour votre recherche");
 //define("_SR_FOUND","Found <b>%s</b> match(es)");
-define("_SR_SHOWING","(Showing %d - %d)");
-define("_SR_ANY","Any (OR)");
-define("_SR_ALL","All (AND)");
-define("_SR_EXACT","Exact Match");
-define("_SR_SHOWALLR","Show all results");
-define("_SR_NEXT","Next");
-define("_SR_PREVIOUS","Previous");
-define("_SR_KEYWORDS","Keyword(s)");
-define("_SR_TYPE","Search Type");
-define("_SR_SEARCHIN","Search in");
-define('_SR_KEYTOOSHORT', 'Keywords must be at least <b>%s</b> characters long');
-define('_SR_KEYIGNORE', 'Keywords shorter than <b>%s</b> characters will be ignored');
-define('_SR_SEARCHRULE', 'Search Rule');
-define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minimum length (%u chars) and were not included in your search:');
+define("_SR_SHOWING","(Afficher %d - %d)");
+define("_SR_ANY","N'importe quel terme");
+define("_SR_ALL","Tous");
+define("_SR_EXACT","Correspondance exacte");
+define("_SR_SHOWALLR","Afficher tous les r&eacute;sultats de cette section");
+define("_SR_NEXT","Suivant >>");
+define("_SR_PREVIOUS","<< Pr&eacute;c&eacute;dent");
+define("_SR_KEYWORDS","Mots cl&eacute;s");
+define("_SR_TYPE","Type");
+define("_SR_SEARCHIN","Chercher dans");
+define('_SR_KEYTOOSHORT', 'Les mots cl&eacute;s doivent avoir au moins <b>%s</b> caract&egrave;res!');
+define('_SR_KEYIGNORE', 'Les mots cl&eacute;s de moins de <b>%s</b> caract&egrave;res seront ignor&eacute;s');
+define('_SR_SEARCHRULE', 'R&egrave;gle de recherche');
+define('_SR_IGNOREDWORDS', 'Les mots suivants sont plus courts que la taille minimum autoris&eacute;e (%u caract&egrave;res) et n\'ont pas &eacute;t&eacute; inclus dans votre recherche:');
 ############# added since 1.2 #############
-define('_SR_HITSRETURNED', 'hit(s) returned');
+define('_SR_HITSRETURNED', 'r&eacute;sultat(s) retourn&eacute;(s)');
