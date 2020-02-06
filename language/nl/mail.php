e5a18b80b7d9bb4dc5ff1e6a87a00a8f11b36b67
David Janssens <fiammybe@gmail.com>
2020-02-06 16:03:31 +0100

    New translations mail.php (Dutch)

--- language/nl/mail.php
+++ language/nl/mail.php
@@ -1,7 +1,7 @@
 <?php
-define("_MAIL_MSGBODY", "Message body is not set.");
-define("_MAIL_FAILOPTPL", "Failed opening template file.");
-define("_MAIL_SENDMAILNG", "Could not send mail to %s.");
-define("_MAIL_MAILGOOD", "Mail sent to %s.");
-define("_MAIL_SENDPMNG", "Could not send private message to %s.");
-define("_MAIL_PMGOOD", "Private message sent to %s.");
+define("_MAIL_MSGBODY", "Body is niet ingevuld.");
+define("_MAIL_FAILOPTPL", "MISLUKT: Openen van mail template.");
+define("_MAIL_SENDMAILNG", "Het was onmogelijk mail te verzenden naar %s.");
+define("_MAIL_MAILGOOD", "Mail verzonden naar %s.");
+define("_MAIL_SENDPMNG", "Kon PM naar %s niet verzenden.");
+define("_MAIL_PMGOOD", "PM verzonden naar %s.");
