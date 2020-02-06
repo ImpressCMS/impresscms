37f744bcc67d871c30cda887a92e59cfb5226fff
David Janssens <fiammybe@gmail.com>
2020-02-06 16:03:03 +0100

    New translations captcha.php (Dutch)

--- language/nl/captcha.php
+++ language/nl/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
-define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
+define("ICMS_CAPTCHA_CAPTION", "Bevestigingscode");
+define("ICMS_CAPTCHA_INVALID_CODE", "Ongeldige bevestigingscode!");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Te veel pogingen!");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum aantal pogingen: %d");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "De code is hoofdletter gevoelig");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "De code is hoofdletter ongevoelig");
+define("ICMS_CAPTCHA_REFRESH", "Is de code onduidelijk, klik dan op de afbeelding om deze te vernieuwen");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
+define("ICMS_CAPTCHA_RULE_TEXT", "Vul de weergegeven code uit de afbeelding in");
