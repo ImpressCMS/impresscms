e153480f6ffe7f784cd18c9479493854bc5d91d0
David Janssens <fiammybe@gmail.com>
2020-02-06 16:03:06 +0100

    New translations core.php (Dutch)

--- language/nl/core.php
+++ language/nl/core.php
@@ -12,53 +12,53 @@
  * @version		$Id
  */
 
-define('_CORE_MEMORYUSAGE', 'Memory usage');
+define('_CORE_MEMORYUSAGE', 'Geheugen gebruik');
 define('_CORE_BYTES', 'bytes');
-define('_CORE_KILOBYTES', 'Kilo Bytes');
-define('_CORE_MEGABYTES', 'Mega Bytes');
-define('_CORE_GIGABYTES', 'Giga Bytes');
+define('_CORE_KILOBYTES', 'Kilobytes');
+define('_CORE_MEGABYTES', 'Megabytes');
+define('_CORE_GIGABYTES', 'Gigabytes');
 define('_CORE_KILOBYTES_SHORTEN', 'Kb');
 define('_CORE_MEGABYTES_SHORTEN', 'Mb');
 define('_CORE_GIGABYTES_SHORTEN', 'Gb');
-define('_CORE_MODULEHANDLER_NOTAVAILABLE', 'Handler does not exist<br />Module: %s<br />Name: %s');
-define('_CORE_COREHANDLER_NOTAVAILABLE', 'Class <b>%s</b> does not exist<br />Handler Name: %s');
-define('_CORE_NOMODULE', 'No Module is loaded');
-define('_CORE_PAGENOTDISPLAYED', 'This page cannot be displayed due to an internal error.<br/><br/>You can provide the following information to the administrators of this site to help them solve the problem:<br /><br />Error: %s<br />');
+define('_CORE_MODULEHANDLER_NOTAVAILABLE', 'Handler bestaat niet<br />Module: %s<br />Naam: %s');
+define('_CORE_COREHANDLER_NOTAVAILABLE', 'Klasse <b>%s</b> bestaat niet<br />Handler Naam: %s');
+define('_CORE_NOMODULE', 'Er is geen module geladen');
+define('_CORE_PAGENOTDISPLAYED', 'Deze pagina kan niet worden weergegeven door een interne fout.<br/><br/>U kunt de volgende informatie verstrekken aan de beheerder van deze website om het probleem te helpen oplossen:<br /><br />Fout: %s<br />');
 define('_CORE_TOKEN', 'XOOPS_TOKEN');
-define('_CORE_TOKENVALID', 'Token Validation');
-define('_CORE_TOKENNOVALID', 'No valid token found in request/session');
-define('_CORE_TOKENINVALID', 'No valid token found in request/session');
-define('_CORE_TOKENISVALID', 'Valid token found');
-define('_CORE_TOKENEXPIRED', 'Valid token expired');
-define('_CORE_CLASSNOTINSTANIATED', 'This class cannot be instantiated!');
+define('_CORE_TOKENVALID', 'Teken beoordelen');
+define('_CORE_TOKENNOVALID', 'Geen geldig teken in het verzoek/sessie gevonden');
+define('_CORE_TOKENINVALID', 'Geen geldig teken in het verzoek/sessie gevonden');
+define('_CORE_TOKENISVALID', 'Geldig teken gevonden');
+define('_CORE_TOKENEXPIRED', 'Geldig teken is verlopen');
+define('_CORE_CLASSNOTINSTANIATED', 'Deze class kan niet worden verzocht!');
 
-define('_CORE_DB_NOTRACE', 'notrace:mysql extension not loaded');
-define('_CORE_DB_NOTALLOWEDINGET', 'Database updates are not allowed during processing of a GET request');
-define('_CORE_DB_NOTRACEDB', 'notrace:Unable to connect to database');
-define('_CORE_DB_INVALIDEMAIL', 'Invalid Email');
-define('_CORE_PASSLEVEL1','Too short');
-define('_CORE_PASSLEVEL2','Weak');
-define('_CORE_PASSLEVEL3','Good');
-define('_CORE_PASSLEVEL4','Strong');
-define('_CORE_UNAMEPASS_IDENTIC','Username and Password identical.');
+define('_CORE_DB_NOTRACE', 'notrace:mysql extensie niet geladen');
+define('_CORE_DB_NOTALLOWEDINGET', 'Database updates zijn niet toegestaan gedurende de uitvoering van een GET verzoek');
+define('_CORE_DB_NOTRACEDB', 'notrace: Niet mogelijk om te verbinden met database');
+define('_CORE_DB_INVALIDEMAIL', 'Ongeldige e-mail');
+define('_CORE_PASSLEVEL1','Te kort');
+define('_CORE_PASSLEVEL2','Zwak');
+define('_CORE_PASSLEVEL3','Goed');
+define('_CORE_PASSLEVEL4','Sterk');
+define('_CORE_UNAMEPASS_IDENTIC','Gebruikernaam en wachtwoord zijn identiek.');
 
 /* Added in 1.3 */
 
-define('_CORE_CHECKSUM_FILES_ADDED',' files have been added');
-define('_CORE_CHECKSUM_FILES_REMOVED',' files have been removed');
-define('_CORE_CHECKSUM_ALTERED_REMOVED',' files have been altered or removed');
-define('_CORE_CHECKSUM_CHECKFILE','Checking against the file ');
-define('_CORE_CHECKSUM_PERMISSIONS_ALTERED',' files have had their permissions altered');
-define('_CORE_CHECKSUM_CHECKFILE_UNREADABLE', 'The file containing the checksums is unavailable or unreadable. Validation cannot be completed');
-define('_CORE_CHECKSUM_ADDING',' Adding');
+define('_CORE_CHECKSUM_FILES_ADDED',' bestanden zijn toegevoegd');
+define('_CORE_CHECKSUM_FILES_REMOVED',' bestanden werden verwijderd');
+define('_CORE_CHECKSUM_ALTERED_REMOVED',' bestanden werden aangepast of verwijderd');
+define('_CORE_CHECKSUM_CHECKFILE','Controleren op bestand ');
+define('_CORE_CHECKSUM_PERMISSIONS_ALTERED',' de rechten van de bestanden zijn gewijzigd');
+define('_CORE_CHECKSUM_CHECKFILE_UNREADABLE', 'Het bestand met de checksums is niet beschikbaar of onleesbaar. De validatie kan niet worden voltooid');
+define('_CORE_CHECKSUM_ADDING',' Toevoegen');
 define('_CORE_CHECKSUM_CHECKSUM',' Checksum');
-define('_CORE_CHECKSUM_PERMISSIONS',' Permissions');
+define('_CORE_CHECKSUM_PERMISSIONS',' Rechten');
 
-define('_CORE_DEPRECATED', 'Deprecated');
-define('_CORE_DEPRECATED_REPLACEMENT', 'use %s instead');
+define('_CORE_DEPRECATED', 'Verouderd');
+define('_CORE_DEPRECATED_REPLACEMENT', 'gebruik %s in de plaats');
 define('_CORE_DEPRECATED_CALLSTACK', '<br />Call Stack: <br />');
-define('_CORE_DEPRECATED_MSG', '%s in %s, line %u <br />');
-define('_CORE_DEPRECATED_CALLEDBY', 'Called by: ');
-define('_CORE_REMOVE_IN_VERSION', 'This will be removed in version %s');
+define('_CORE_DEPRECATED_MSG', '%s in %s, regel %u <br />');
+define('_CORE_DEPRECATED_CALLEDBY', 'Oproep door: ');
+define('_CORE_REMOVE_IN_VERSION', 'Dit zal worden verwijderd in versie %s');
 define('_CORE_DEBUG', 'Debug');
-define('_CORE_DEVELOPER_DASHBOARD', 'Developer Dashboard');
+define('_CORE_DEVELOPER_DASHBOARD', 'Ontwikkelaar Dashboard');
