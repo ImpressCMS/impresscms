3c4921f43c418e40ed38fce3ed9d302fee671da5
David Janssens <fiammybe@gmail.com>
2020-02-06 16:02:57 +0100

    New translations admin.php (Dutch)

--- language/nl/admin.php
+++ language/nl/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 //%%%%%%	File Name  admin.php 	%%%%%
-define("_AD_NORIGHT","You don't have the right to access this area");
-define('_AD_LOGINADMIN', 'Logging you in..');
-define('_AD_INSTALLEDMODULES','Installed Modules');
+define("_AD_NORIGHT","U heeft niet voldoende rechten om deze pagina te openen");
+define('_AD_LOGINADMIN', 'U wordt nu ingelogd...');
+define('_AD_INSTALLEDMODULES','Geïnstalleerde modules');
