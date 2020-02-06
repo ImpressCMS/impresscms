f6d8897ca59c9e222a38afce279173cc421c5b12
David Janssens <fiammybe@gmail.com>
2020-02-06 16:05:44 +0100

    New translations search.php (Russian)

--- language/ru/search.php
+++ language/ru/search.php
@@ -1,24 +1,24 @@
 <?php
 
 //%%%%%%	File Name search.php 	%%%%%
-define("_SR_SEARCH","Search");
-define("_SR_PLZENTER","Please enter all required data!");
-define("_SR_SEARCHRESULTS","Search Results");
-define("_SR_NOMATCH","No Match Found for your Query");
+define("_SR_SEARCH","Поиск по сайту");
+define("_SR_PLZENTER","Пожалуйста заполните все требуемые поля!");
+define("_SR_SEARCHRESULTS","Результаты поиска");
+define("_SR_NOMATCH","Не найдено соответствий Вашему запросу");
 //define("_SR_FOUND","Found <b>%s</b> match(es)");
-define("_SR_SHOWING","(Showing %d - %d)");
-define("_SR_ANY","Any (OR)");
-define("_SR_ALL","All (AND)");
-define("_SR_EXACT","Exact Match");
-define("_SR_SHOWALLR","Show all results");
-define("_SR_NEXT","Next");
-define("_SR_PREVIOUS","Previous");
-define("_SR_KEYWORDS","Keyword(s)");
-define("_SR_TYPE","Search Type");
-define("_SR_SEARCHIN","Search in");
-define('_SR_KEYTOOSHORT', 'Keywords must be at least <b>%s</b> characters long');
-define('_SR_KEYIGNORE', 'Keywords shorter than <b>%s</b> characters will be ignored');
-define('_SR_SEARCHRULE', 'Search Rule');
-define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minimum length (%u chars) and were not included in your search:');
+define("_SR_SHOWING","(Показано %d - %d)");
+define("_SR_ANY","Любое (ИЛИ)");
+define("_SR_ALL","Все (И)");
+define("_SR_EXACT","Точное совпадение");
+define("_SR_SHOWALLR","Показать все результаты");
+define("_SR_NEXT","След. >>");
+define("_SR_PREVIOUS","<< Пред.");
+define("_SR_KEYWORDS","Ключевые слова");
+define("_SR_TYPE","Тип");
+define("_SR_SEARCHIN","Область поиска");
+define('_SR_KEYTOOSHORT', 'Ключевые слова должны быть длиной не менее <b>%s</b> символов!');
+define('_SR_KEYIGNORE', 'Ключевые слова короче <b>%s</b> символов будут игнорированы');
+define('_SR_SEARCHRULE', 'Правила поиска');
+define('_SR_IGNOREDWORDS', 'Перечисленные слова короче разрешенного минимума (%u символов) и не будут включены в Вашу строку поиска:');
 ############# added since 1.2 #############
 define('_SR_HITSRETURNED', 'hit(s) returned');
