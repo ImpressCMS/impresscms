beec0d322482c94d977eaf2913a3178cc889d911
David Janssens <fiammybe@gmail.com>
2020-02-06 16:06:41 +0100

    New translations calendar.php (Russian)

--- language/ru/calendar.php
+++ language/ru/calendar.php
@@ -1,70 +1,70 @@
 <?php
 //%%%%%		Time Zone	%%%%
-define("_CAL_SUNDAY", "Sunday");
-define("_CAL_MONDAY", "Monday");
-define("_CAL_TUESDAY", "Tuesday");
-define("_CAL_WEDNESDAY", "Wednesday");
-define("_CAL_THURSDAY", "Thursday");
-define("_CAL_FRIDAY", "Friday");
-define("_CAL_SATURDAY", "Saturday");
-define("_CAL_JANUARY", "January");
-define("_CAL_FEBRUARY", "February");
-define("_CAL_MARCH", "March");
-define("_CAL_APRIL", "April");
-define("_CAL_MAY", "May");
-define("_CAL_JUNE", "June");
-define("_CAL_JULY", "July");
-define("_CAL_AUGUST", "August");
-define("_CAL_SEPTEMBER", "September");
-define("_CAL_OCTOBER", "October");
-define("_CAL_NOVEMBER", "November");
-define("_CAL_DECEMBER", "December");
-define("_CAL_TGL1STD", "Toggle first day of week");
-define("_CAL_PREVYR", "Prev. year");
-define("_CAL_PREVMNTH", "Prev. month");
-define("_CAL_GOTODAY", "Go Today");
-define("_CAL_NXTMNTH", "Next month");
-define("_CAL_NEXTYR", "Next year");
-define("_CAL_SELDATE", "Select date");
-define("_CAL_DRAGMOVE", "Drag to move");
-define("_CAL_TODAY", "Today");
-define("_CAL_DISPM1ST", "Display Monday first");
-define("_CAL_DISPS1ST", "Display Sunday first");
+define("_CAL_SUNDAY", "Воскресенье");
+define("_CAL_MONDAY", "Понедельник");
+define("_CAL_TUESDAY", "Вторник");
+define("_CAL_WEDNESDAY", "Среда");
+define("_CAL_THURSDAY", "Четверг");
+define("_CAL_FRIDAY", "Пятница");
+define("_CAL_SATURDAY", "Суббота");
+define("_CAL_JANUARY", "Январь");
+define("_CAL_FEBRUARY", "Февраль");
+define("_CAL_MARCH", "Март");
+define("_CAL_APRIL", "Апрель");
+define("_CAL_MAY", "Май");
+define("_CAL_JUNE", "Июнь");
+define("_CAL_JULY", "Июль");
+define("_CAL_AUGUST", "Август");
+define("_CAL_SEPTEMBER", "Сентябрь");
+define("_CAL_OCTOBER", "Октябрь");
+define("_CAL_NOVEMBER", "Ноябрь");
+define("_CAL_DECEMBER", "Декабрь");
+define("_CAL_TGL1STD", "Перейти на первый день недели");
+define("_CAL_PREVYR", "Предыдущий год (удерживайте для меню)");
+define("_CAL_PREVMNTH", "Предыдущий месяц (удерживайте для меню)");
+define("_CAL_GOTODAY", "На сегодня");
+define("_CAL_NXTMNTH", "Следующий месяц (удерживайте для меню)");
+define("_CAL_NEXTYR", "Следующий год (удерживайте для меню)");
+define("_CAL_SELDATE", "Выбор даты");
+define("_CAL_DRAGMOVE", "Перетащите");
+define("_CAL_TODAY", "Сегодня");
+define("_CAL_DISPM1ST", "Неделя начинается с понедельника");
+define("_CAL_DISPS1ST", "Неделя начинается с воскресенья");
 
 ############# added since 1.1.2 #############
-define("_CAL_SUN", "Sun");
-define("_CAL_MON", "Mon");
-define("_CAL_TUE", "Tue");
-define("_CAL_WED", "Wed");
-define("_CAL_THU", "Thu");
-define("_CAL_FRI", "Fri");
-define("_CAL_SAT", "Sat");
+define("_CAL_SUN", "Вск");
+define("_CAL_MON", "Пнд");
+define("_CAL_TUE", "Втр");
+define("_CAL_WED", "Срд");
+define("_CAL_THU", "Чтв");
+define("_CAL_FRI", "Птн");
+define("_CAL_SAT", "Сбт");
 // First day of the week. "0" means display Sunday first, "1" means display Monday first, etc...
 define("_CAL_FIRSTDAY", "1");
-define("_CAL_JAN", "Jan");
-define("_CAL_FEB", "Feb");
-define("_CAL_MAR", "Mar");
-define("_CAL_APR", "Apr");
-define("_CAL_JUN", "Jun");
-define("_CAL_JUL", "Jul");
-define("_CAL_AUG", "Aug");
-define("_CAL_SEP", "Sept");
-define("_CAL_OCT", "Oct");
-define("_CAL_NOV", "Nov");
-define("_CAL_DEC", "Dec");
+define("_CAL_JAN", "Янв");
+define("_CAL_FEB", "Фев");
+define("_CAL_MAR", "Мар");
+define("_CAL_APR", "Апр");
+define("_CAL_JUN", "Июн");
+define("_CAL_JUL", "Июл");
+define("_CAL_AUG", "Авг");
+define("_CAL_SEP", "Сен");
+define("_CAL_OCT", "Окт");
+define("_CAL_NOV", "Ноя");
+define("_CAL_DEC", "Дек");
 // Direction of the calendar, ltr for left to right and rtl for roght to left (for Persian, Arabic, etc.)
 define("_CAL_DIRECTION", "ltr");
 define("_CAL_AM", "am");
 define("_CAL_AM_CAPS", "AM");
 define("_CAL_PM", "pm");
-define("_CAL_PM_CAPS", "PM");
-define("_CAL_TIME", "Time");
-define("_CAL_WK", "wk"); // shorten of week-end
+define("_CAL_PM_CAPS", "Личные сообщения");
+define("_CAL_TIME", "Время");
+define("_CAL_WK", "вх"); // shorten of week-end
 // This may be local-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 define("_CAL_WEEKEND", "0,6");
-define("_CAL_DSPFIRST", "Display %s first");
+define("_CAL_DSPFIRST", "Показать %s первым");
 //This constants are for the jalali calendar included in the library
 define('_CAL_FARVARDIN','Farvardin');
 define('_CAL_ORDIBEHESHT','Ordibehest');
@@ -75,7 +75,7 @@ define('_CAL_SHAHRIVAR','Shahrivar');
 define('_CAL_MEHR','Mehr');
 define('_CAL_ABAN','Aban');
 define('_CAL_AZAR','Azar');
-define('_CAL_DEY','Day');
+define('_CAL_DEY','День');
 define('_CAL_BAHMAN','Bahman');
 define('_CAL_ESFAND','Esfand');
 define("_CAL_NUMS_ARRAY", "'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'"); // numeric values can differ in different languages
