e7275e28288d6cd0a07b1ef48a817d5745f0e50d
David Janssens <fiammybe@gmail.com>
2020-02-06 16:07:50 +0100

    New translations search.php (Italian)

--- language/it/search.php
+++ language/it/search.php
@@ -1,24 +1,24 @@
 <?php
 
 //%%%%%%	File Name search.php 	%%%%%
-define("_SR_SEARCH","Search");
-define("_SR_PLZENTER","Please enter all required data!");
-define("_SR_SEARCHRESULTS","Search Results");
-define("_SR_NOMATCH","No Match Found for your Query");
+define("_SR_SEARCH","Cerca");
+define("_SR_PLZENTER","Si prega di riempire tutti i campi richiesti!");
+define("_SR_SEARCHRESULTS","Risultati della ricerca");
+define("_SR_NOMATCH","Nessun risultato");
 //define("_SR_FOUND","Found <b>%s</b> match(es)");
-define("_SR_SHOWING","(Showing %d - %d)");
-define("_SR_ANY","Any (OR)");
-define("_SR_ALL","All (AND)");
-define("_SR_EXACT","Exact Match");
-define("_SR_SHOWALLR","Show all results");
-define("_SR_NEXT","Next");
-define("_SR_PREVIOUS","Previous");
-define("_SR_KEYWORDS","Keyword(s)");
-define("_SR_TYPE","Search Type");
-define("_SR_SEARCHIN","Search in");
-define('_SR_KEYTOOSHORT', 'Keywords must be at least <b>%s</b> characters long');
-define('_SR_KEYIGNORE', 'Keywords shorter than <b>%s</b> characters will be ignored');
-define('_SR_SEARCHRULE', 'Search Rule');
-define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minimum length (%u chars) and were not included in your search:');
+define("_SR_SHOWING","(Visualizza %d - %d)");
+define("_SR_ANY","Alcuni (OR)");
+define("_SR_ALL","Tutti (AND)");
+define("_SR_EXACT","Corrispondenza esatta");
+define("_SR_SHOWALLR","Mostra tutti i risultati della ricerca");
+define("_SR_NEXT","Successiva >>");
+define("_SR_PREVIOUS","<< Precedente");
+define("_SR_KEYWORDS","Parole chiave");
+define("_SR_TYPE","Tipo");
+define("_SR_SEARCHIN","Cerca in");
+define('_SR_KEYTOOSHORT', 'Le parole chiave devono essere essere composte da almeno <strong>%s</strong> caratteri!');
+define('_SR_KEYIGNORE', 'Le parole chiave pi&ugrave; corte di <b>%s</b> caratteri saranno ignorate durante la ricerca');
+define('_SR_SEARCHRULE', 'Regole per la ricerca');
+define('_SR_IGNOREDWORDS', 'Le seguenti parole sono di lunghezza inferiore alla minima richiesta (%u caratteri) e non sono state incluse nella ricerca:');
 ############# added since 1.2 #############
-define('_SR_HITSRETURNED', 'hit(s) returned');
+define('_SR_HITSRETURNED', 'occorrenze restituite');
