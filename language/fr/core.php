74410b80edfb26a145ba85ca28d1139de928b9d9
David Janssens <fiammybe@gmail.com>
2020-02-06 16:07:17 +0100

    New translations core.php (French)

--- language/fr/core.php
+++ language/fr/core.php
@@ -12,53 +12,53 @@
  * @version		$Id
  */
 
-define('_CORE_MEMORYUSAGE', 'Memory usage');
-define('_CORE_BYTES', 'bytes');
-define('_CORE_KILOBYTES', 'Kilo Bytes');
-define('_CORE_MEGABYTES', 'Mega Bytes');
-define('_CORE_GIGABYTES', 'Giga Bytes');
-define('_CORE_KILOBYTES_SHORTEN', 'Kb');
-define('_CORE_MEGABYTES_SHORTEN', 'Mb');
-define('_CORE_GIGABYTES_SHORTEN', 'Gb');
-define('_CORE_MODULEHANDLER_NOTAVAILABLE', 'Handler does not exist<br />Module: %s<br />Name: %s');
-define('_CORE_COREHANDLER_NOTAVAILABLE', 'Class <b>%s</b> does not exist<br />Handler Name: %s');
-define('_CORE_NOMODULE', 'No Module is loaded');
-define('_CORE_PAGENOTDISPLAYED', 'This page cannot be displayed due to an internal error.<br/><br/>You can provide the following information to the administrators of this site to help them solve the problem:<br /><br />Error: %s<br />');
+define('_CORE_MEMORYUSAGE', 'Consommation mémoire');
+define('_CORE_BYTES', 'octets');
+define('_CORE_KILOBYTES', 'Kilo Octets');
+define('_CORE_MEGABYTES', 'Mega Octets');
+define('_CORE_GIGABYTES', 'Giga octets');
+define('_CORE_KILOBYTES_SHORTEN', 'Ko');
+define('_CORE_MEGABYTES_SHORTEN', 'Mo');
+define('_CORE_GIGABYTES_SHORTEN', 'Go');
+define('_CORE_MODULEHANDLER_NOTAVAILABLE', 'Le gestionnaire n\'existe pas<br />Module : %s<br />Nom : %s');
+define('_CORE_COREHANDLER_NOTAVAILABLE', 'La classe <b>%s</b> n\'existe pas<br />Nom du gestionnaire : %s');
+define('_CORE_NOMODULE', 'Aucun module n\'est chargé');
+define('_CORE_PAGENOTDISPLAYED', 'Cette page ne peut pas être affichée en raison d\'une erreur interne.<br/><br/>Vous pouvez fournir les informations suivantes aux administrateurs de ce site pour les aider à résoudre le problème :<br /><br />Erreur : %s<br />');
 define('_CORE_TOKEN', 'XOOPS_TOKEN');
-define('_CORE_TOKENVALID', 'Token Validation');
-define('_CORE_TOKENNOVALID', 'No valid token found in request/session');
-define('_CORE_TOKENINVALID', 'No valid token found in request/session');
-define('_CORE_TOKENISVALID', 'Valid token found');
-define('_CORE_TOKENEXPIRED', 'Valid token expired');
-define('_CORE_CLASSNOTINSTANIATED', 'This class cannot be instantiated!');
+define('_CORE_TOKENVALID', 'Validation du jeton');
+define('_CORE_TOKENNOVALID', 'Aucun jeton valide trouvé dans la requête/session');
+define('_CORE_TOKENINVALID', 'Aucun jeton valide trouvé dans la requête/session');
+define('_CORE_TOKENISVALID', 'Jeton valide trouvé');
+define('_CORE_TOKENEXPIRED', 'Jeton valide expiré');
+define('_CORE_CLASSNOTINSTANIATED', 'Cette classe ne peut pas être instanciée !');
 
-define('_CORE_DB_NOTRACE', 'notrace:mysql extension not loaded');
-define('_CORE_DB_NOTALLOWEDINGET', 'Database updates are not allowed during processing of a GET request');
-define('_CORE_DB_NOTRACEDB', 'notrace:Unable to connect to database');
-define('_CORE_DB_INVALIDEMAIL', 'Invalid Email');
-define('_CORE_PASSLEVEL1','Too short');
-define('_CORE_PASSLEVEL2','Weak');
-define('_CORE_PASSLEVEL3','Good');
-define('_CORE_PASSLEVEL4','Strong');
-define('_CORE_UNAMEPASS_IDENTIC','Username and Password identical.');
+define('_CORE_DB_NOTRACE', 'notrace:extension mysql non chargée');
+define('_CORE_DB_NOTALLOWEDINGET', 'Les mises à jour de la base de données ne sont pas autorisées lors du traitement d\'une requête GET');
+define('_CORE_DB_NOTRACEDB', 'notrace:Impossible de se connecter à la base de données');
+define('_CORE_DB_INVALIDEMAIL', 'Addresse email invalide');
+define('_CORE_PASSLEVEL1','Trop court');
+define('_CORE_PASSLEVEL2','Faible');
+define('_CORE_PASSLEVEL3','Bon');
+define('_CORE_PASSLEVEL4','Fort');
+define('_CORE_UNAMEPASS_IDENTIC','Nom d\'utilisateur et mot de passe identiques.');
 
 /* Added in 1.3 */
 
-define('_CORE_CHECKSUM_FILES_ADDED',' files have been added');
-define('_CORE_CHECKSUM_FILES_REMOVED',' files have been removed');
-define('_CORE_CHECKSUM_ALTERED_REMOVED',' files have been altered or removed');
-define('_CORE_CHECKSUM_CHECKFILE','Checking against the file ');
-define('_CORE_CHECKSUM_PERMISSIONS_ALTERED',' files have had their permissions altered');
-define('_CORE_CHECKSUM_CHECKFILE_UNREADABLE', 'The file containing the checksums is unavailable or unreadable. Validation cannot be completed');
-define('_CORE_CHECKSUM_ADDING',' Adding');
-define('_CORE_CHECKSUM_CHECKSUM',' Checksum');
+define('_CORE_CHECKSUM_FILES_ADDED',' les fichiers ont été ajoutés');
+define('_CORE_CHECKSUM_FILES_REMOVED',' les fichiers ont été supprimés');
+define('_CORE_CHECKSUM_ALTERED_REMOVED',' les fichiers ont été modifiés ou supprimés');
+define('_CORE_CHECKSUM_CHECKFILE','Vérification par rapport au fichier ');
+define('_CORE_CHECKSUM_PERMISSIONS_ALTERED',' les fichiers ont eu leurs permissions modifiées');
+define('_CORE_CHECKSUM_CHECKFILE_UNREADABLE', 'Le fichier contenant les sommes de contrôle est indisponible ou illisible. La validation ne peut pas être terminée');
+define('_CORE_CHECKSUM_ADDING',' Ajouter');
+define('_CORE_CHECKSUM_CHECKSUM',' Somme de contrôle');
 define('_CORE_CHECKSUM_PERMISSIONS',' Permissions');
 
-define('_CORE_DEPRECATED', 'Deprecated');
-define('_CORE_DEPRECATED_REPLACEMENT', 'use %s instead');
+define('_CORE_DEPRECATED', 'Déprécié');
+define('_CORE_DEPRECATED_REPLACEMENT', 'utiliser %s à la place');
 define('_CORE_DEPRECATED_CALLSTACK', '<br />Call Stack: <br />');
-define('_CORE_DEPRECATED_MSG', '%s in %s, line %u <br />');
-define('_CORE_DEPRECATED_CALLEDBY', 'Called by: ');
-define('_CORE_REMOVE_IN_VERSION', 'This will be removed in version %s');
+define('_CORE_DEPRECATED_MSG', '%s en %s, ligne %u <br />');
+define('_CORE_DEPRECATED_CALLEDBY', 'Appelé par: ');
+define('_CORE_REMOVE_IN_VERSION', 'Ce sera supprimé dans la version %s');
 define('_CORE_DEBUG', 'Debug');
-define('_CORE_DEVELOPER_DASHBOARD', 'Developer Dashboard');
+define('_CORE_DEVELOPER_DASHBOARD', 'Tableau de bord développeur');
