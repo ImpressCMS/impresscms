a910ae663b07b6960bf8089360d1447fd830980e
David Janssens <fiammybe@gmail.com>
2020-02-06 16:08:30 +0100

    New translations captcha.php (Italian)

--- language/it/captcha.php
+++ language/it/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
-define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
+define("ICMS_CAPTCHA_CAPTION", "Codice di conferma");
+define("ICMS_CAPTCHA_INVALID_CODE", "Codice di conferma non valido!");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Troppi tentativi!");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "Massimo n. tentativi: %d");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "Il codice è case-sensitive (distingue maiuscole e minuscole)");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "Il codice è case-insensitive (non distingue maiuscole e minuscole)");
+define("ICMS_CAPTCHA_REFRESH", "Se il codice non è abbastanza comprensibile, sei pregato di cliccare sull'immagine per cambiarlo");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
+define("ICMS_CAPTCHA_RULE_TEXT", "Inserisci il risultato di questa espressione");
