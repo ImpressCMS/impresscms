afa37e5eeed5c39ca0177ba0923dcee6457edb4c
David Janssens <fiammybe@gmail.com>
2020-02-06 16:07:25 +0100

    New translations admin.php (French)

--- language/fr/admin.php
+++ language/fr/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 //%%%%%%	File Name  admin.php 	%%%%%
-define("_AD_NORIGHT","You don't have the right to access this area");
-define('_AD_LOGINADMIN', 'Logging you in..');
-define('_AD_INSTALLEDMODULES','Installed Modules');
+define("_AD_NORIGHT","Vous n'avez pas les droits pour acc&eacute;der &agrave; cette zone");
+define('_AD_LOGINADMIN', 'Connexion...');
+define('_AD_INSTALLEDMODULES','Modules install&eacute;s');
