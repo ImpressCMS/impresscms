47b67714dc1b21e7ccfebb06fa915395a8994511
David Janssens <fiammybe@gmail.com>
2020-02-06 16:08:46 +0100

    New translations theme.php (Spanish)

--- language/es/theme.php
+++ language/es/theme.php
@@ -1,46 +1,46 @@
 <?php
 // general
-define("THEME_UPTOP","Back to Top");
-define("THEME_VALIDCSS","This theme template is W3C Validated for CSS 1 and 2");
-define("THEME_VALIDXHTML","This theme template is W3C Validated for XHTML 1.01");
-define("THEME_LICENSE","See Creative Commons Licence");
-define("THEME_W3C","Valid W3C - XHTML 1.01 and CSS 1/2/3");
+define("THEME_UPTOP","Volver arriba");
+define("THEME_VALIDCSS","Esta plantilla de tema está W3C validada para CSS 1 y 2");
+define("THEME_VALIDXHTML","Esta plantilla de tema está W3C validada para XHTML 1.01");
+define("THEME_LICENSE","Ver licencia de Creative Commons");
+define("THEME_W3C","Válido W3C - XHTML 1.01 y CSS 1/2/3");
 
 // userbar.html
-define("THEME_REGISTER","Register");
-define("THEME_REGISTERATION",'User Registration');
-define("THEME_REMEMBER"," Remember me ?");
-define("THEME_PSEUDO"," Login");
-define("THEME_DESC_PSEUDO"," Write your login");
-define("THEME_PASSWORD"," Password");
-define("THEME_DESC_PASSWORD"," Write your password");
+define("THEME_REGISTER","Registrarse");
+define("THEME_REGISTERATION",'Registro de usuario');
+define("THEME_REMEMBER"," ¿Recordarme?");
+define("THEME_PSEUDO"," Ingresar");
+define("THEME_DESC_PSEUDO"," Escriba su nombre de usuario");
+define("THEME_PASSWORD"," Contraseña");
+define("THEME_DESC_PASSWORD"," Escribe tu contraseña");
 
-define("THEME_WELLCOME","Welcome");
-define("THEME_PROFILE","Your profile");
-define("THEME_NOTIFICATION","Bookmarks");
-define("THEME_MESSAGE","Show your inbox content");
-define("THEME_INBOX","Inbox");
-define("THEME_YOUHAVE","You have ");
-define("THEME_NOTREAD","unread message(s)");
-define("THEME_MYACCOUNT","My profile");
-define("THEME_LOGOUT","Logout");
+define("THEME_WELLCOME","Bienvenida");
+define("THEME_PROFILE","Tu perfil");
+define("THEME_NOTIFICATION","Marcadores");
+define("THEME_MESSAGE","Mostrar el contenido de tu bandeja de entrada");
+define("THEME_INBOX","Entrada");
+define("THEME_YOUHAVE","Tienes ");
+define("THEME_NOTREAD","mensaje(s) sin leer");
+define("THEME_MYACCOUNT","Mi perfil");
+define("THEME_LOGOUT","Cerrar sesión");
 define("THEME_ADMIN","Admin");
-define("THEME_OR","or");
+define("THEME_OR","o");
 
 // searchbar.html
-define("THEME_SEARCH","Search");
-define("THEME_DESC_SEARCH","Search this site");
-define("THEME_KEYWORDS","Put the keywords");
+define("THEME_SEARCH","Buscar");
+define("THEME_DESC_SEARCH","Buscar en este sitio");
+define("THEME_KEYWORDS","Poner las palabras clave");
 
 // globalnav.html
-define("THEME_CONTACT","Contact");
-define("THEME_CONTACT_DESC","Contact us ?");
+define("THEME_CONTACT","Contacto");
+define("THEME_CONTACT_DESC","Contáctenos ?");
 define("THEME_SITEMAP","Sitemap");
 define("THEME_RSS","RSS");
 
 define("THEME_NAVTITLE","Navigation");
-define("THEME_HOME","Home");
-define("THEME_HOME_DESC","Back to home");
+define("THEME_HOME","Inicio");
+define("THEME_HOME_DESC","Volver a casa");
 
 // Theme accessibility
 define("THEME_Aa","Aa");
@@ -48,11 +48,11 @@ define("THEME_Amin","-");
 define("THEME_Aplus","+");
 
 // ImpressCMS Description Links
-define("THEME_Support_DESC","Support the ImpressCMS Project !");
-define("THEME_Blogs_DESC","Read the ImpressCMS blogs");
-define("THEME_Wiki_DESC","Learn more in the community-driven wiki for ImpressCMS");
-define("THEME_Addons_DESC","Download modules, themes and more documentation");
-define("THEME_Community_DESC","Join the ImpressCMS Community");
-define("THEME_ImpressCMS_DESC","Visit the ImpressCMS main site");
-define("THEME_FEEDS","Feeds");
-define("THEME_PARTNERS","Partners");
+define("THEME_Support_DESC","Soporte al proyecto ImpressCMS !");
+define("THEME_Blogs_DESC","Leer los blogs ImpressCMS");
+define("THEME_Wiki_DESC","Aprende más en el wiki para ImpressCMS");
+define("THEME_Addons_DESC","Descargar módulos, temas y más documentación");
+define("THEME_Community_DESC","Únete a la comunidad ImpressCMS");
+define("THEME_ImpressCMS_DESC","Visite el sitio principal de ImpressCMS");
+define("THEME_FEEDS","Alimentos");
+define("THEME_PARTNERS","Socios");
