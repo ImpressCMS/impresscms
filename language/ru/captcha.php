a6f70f04a2c0764b2f1d44f7193171cdab41af31
David Janssens <fiammybe@gmail.com>
2020-02-06 16:06:27 +0100

    New translations captcha.php (Russian)

--- language/ru/captcha.php
+++ language/ru/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
-define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
+define("ICMS_CAPTCHA_CAPTION", "Проверочный код");
+define("ICMS_CAPTCHA_INVALID_CODE", "Неправильный проверочный код!");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Много попыток!");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "(max. %d попыток)");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "Чувствительный к регистру");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "Нечувствительный к регистру");
+define("ICMS_CAPTCHA_REFRESH", "Обновить картинку");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
+define("ICMS_CAPTCHA_RULE_TEXT", "Введите результат запроса");
