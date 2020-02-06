f3bb247bc574d5cc76c0fb98f3e24485b6e2692c
David Janssens <fiammybe@gmail.com>
2020-02-06 16:07:28 +0100

    New translations pmsg.php (French)

--- language/fr/pmsg.php
+++ language/fr/pmsg.php
@@ -1,45 +1,45 @@
 <?php
 
 //%%%%%%	File Name readpmsg.php 	%%%%%
-define("_PM_DELETED","Your message(s) has been deleted");
-define("_PM_PRIVATEMESSAGE","Private Messages");
-define("_PM_INBOX","Inbox"); // could use _US_INBOX
-define("_PM_FROM","From");
-define("_PM_YOUDONTHAVE","You don't have any private messages");
+define("_PM_DELETED","Votre (vos) message(s) a (ont) &eacute;t&eacute; effac&eacute;(s)");
+define("_PM_PRIVATEMESSAGE","Message priv&eacute;");
+define("_PM_INBOX","Mes Messages"); // could use _US_INBOX
+define("_PM_FROM","De la part de");
+define("_PM_YOUDONTHAVE","Vous n'avez aucun message priv&eacute;");
 //define("_PM_FROMC","From: "); 								//replaced with _PM_FROM with colon
-define("_PM_SENTC","Sent: "); // The date of message sent
+define("_PM_SENTC","Envoy&eacute; le :&nbsp;"); // The date of message sent
 //define("_PM_PROFILE","Profile"); 								// replaced with global: _PROFILE
 
 // %s is a username
-define("_PM_PREVIOUS","Previous Message");
-define("_PM_NEXT","Next Message");
+define("_PM_PREVIOUS","Message pr&eacute;c&eacute;dent");
+define("_PM_NEXT","Message suivant");
 
 //%%%%%%	File Name pmlite.php 	%%%%%
-define("_PM_SORRY","Sorry! You are not a registered user.");
-define("_PM_REGISTERNOW","Register Now!");
+define("_PM_SORRY","D&eacute;sol&eacute;! Vous n'&ecirc;tes pas un utilisateur enregistr&eacute;.");
+define("_PM_REGISTERNOW","Inscrivez-vous maintenant !");
 //define("_PM_GOBACK","Go Back"); 									//Never used. If needed, use global: _BACK
-define("_PM_USERNOEXIST","The selected user doesn't exist in the database.");
-define("_PM_PLZTRYAGAIN","Please check the name and try again.");
-define("_PM_MESSAGEPOSTED","Your message has been posted");
-define("_PM_CLICKHERE","You can click here to view your private messages");
-define("_PM_ORCLOSEWINDOW","Or click here to close this window.");
-define("_PM_USERWROTE","%s wrote:");
-define("_PM_TO","To: "); // with colon
+define("_PM_USERNOEXIST","Le membre s&eacute;lectionn&eacute; n'existe pas dans la base de donn&eacute;es.");
+define("_PM_PLZTRYAGAIN","Merci de v&eacute;rifier le nom et de recommencer.");
+define("_PM_MESSAGEPOSTED","Votre message a &eacute;t&eacute; post&eacute;");
+define("_PM_CLICKHERE","Vous pouvez cliquer ici pour voir vos messages priv&eacute;s");
+define("_PM_ORCLOSEWINDOW","Ou cliquer ici pour fermer cette fen&ecirc;tre.");
+define("_PM_USERWROTE","%s a &eacute;crit :");
+define("_PM_TO","A:&nbsp;"); // with colon
 //define("_PM_SUBJECTC","Subject: "); 								//Never used. If needed, use global: _SUBJECT with colon
-define("_PM_MESSAGEC","Message: "); 								// (with colon) Duplicated in comments _CM_MESSAGE
-define("_PM_CLEAR","Clear");										// Duplicated in notifications _NOT_CLEAR
+define("_PM_MESSAGEC","Message:&nbsp;"); 								// (with colon) Duplicated in comments _CM_MESSAGE
+define("_PM_CLEAR","Vider");										// Duplicated in notifications _NOT_CLEAR
 //define("_PM_CANCELSEND","Cancel Send"); 							// Never used. If needed, use globals: _CANCEL + _SEND
 //define("_PM_SUBMIT","Submit"); 									// replaced with global: _SUBMIT
 
 //%%%%%%	File Name viewpmsg.php 	%%%%%
 //define("_PM_SUBJECT","Subject"); 									// replaced with global: _SUBJECT
 //define("_DATE","Date"); 											// replaced with global: _DATE
-define("_PM_NOTREAD","Not Read");
+define("_PM_NOTREAD","Pas lu");
 //define("_PM_SEND","Send"); 										// replaced with global: _SEND
 //define("_PM_DELETE","Delete"); 									// replaced with global: _DELETE
 //define("_PM_REPLY", "Reply"); 									// replaced with global: _REPLY
-define("_PM_PLZREG","Please register first to send private messages!");
+define("_PM_PLZREG","Merci de vous enregistrer en premier pour envoyer des messages priv&eacute;s!");
 
-define("_PM_ONLINE", "Online");
+define("_PM_ONLINE", "En ligne");
 
-define("_PM_MESSAGEPOSTED_EMAILSUBJ","[%s] Private Message Notification");
+define("_PM_MESSAGEPOSTED_EMAILSUBJ","\"[%s] Notification de message priv");
