62614e6de4fd5ec00a3115827bce87b6896a4037
David Janssens <fiammybe@gmail.com>
2020-02-07 16:49:30 +0100

    New translations welcome.php (Dutch)

--- htdocs/install/language/nl/welcome.php
+++ htdocs/install/language/nl/welcome.php
@@ -2,29 +2,28 @@
 
 $content .= '
 <p>
-	ImpressCMS is a Web publishing system written in PHP. It is an ideal tool for
-	developing small to large dynamic community websites, intra company portals, corporate portals, weblogs and much more.
+	ImpressCMS is een publicatiesysteem voor het Web, geschreven in PHP. Het is het ideale hulpmiddel om zowel kleine als grote dynamische community websites, bedrijfsportalen, weblogs en veel meer te ontwikkelen.
 </p>
 <p>
-	ImpressCMS is released under the terms of the
+	ImpressCMS wordt ter beschikking gesteld van de 
 	<a href="http://www.gnu.org/copyleft/gpl.html" rel="external">GNU General Public License (GPL)</a>
-	and is free to use and modify.
-	It is free to redistribute as long as you abide by the distribution terms of the GPL.
+and is vrij om te gebruiken of te wijzigen.
+Het is vrij om te herdistribueren zolang er aan de voorwaarden van de GPL wordt voldaan.
 </p>
-<h3>Requirements</h3>
+<h3>Vereisten</h3>
 <ul>
-	<li>- WWW Server: <a href="http://www.apache.org/" rel="external">Apache</a>, IIS, Roxen, etc</li>
-	<li>- Script: <a href="http://www.php.net/" rel="external">PHP</a> 5.2.8 or higher and 16mb minimum memory allocation</li>
+	<li>- WWW Server: <a href="http://www.apache.org/" rel="external">Apache</a>, Nginx, Roxen, etc</li>
+	<li>- Script: <a href="http://www.php.net/" rel="external">PHP</a> 7.0 of hoger en minimaal 32 MB RAM</li>
 	<li>- Database: <a href="http://www.mysql.com/" rel="external">MySQL</a> 4.1.0 or higher, <a href="https://mariadb.org/" rel="external">MariaDB</a> 5.1 or higher</li>
 </ul>
-<h3>Before you install</h3>
+<h3>Voor je installeert</h3>
 <ul>
-	<li>Setup the web server, PHP and database server properly.</li>
-	<li>Prepare a database for ImpressCMS. This can be an existing database as well as a newly created one.</li>
-	<li>Prepare a user account and grant this user access to the database (all rights).</li>
-	<li>Make the directories of uploads/, ../storage/templates_c, ../storage/cache, ../storage/log, ../storage/htmlpurifier, ../modules/ writable (chmod 777 or 755 - depending on servers).</li>
-	<li>Make the file ../.env writable (chmod 666 depending on server).</li>
-	<li>In your internet browser settings turn on the allowance of cookies and JavaScript.</li>
+	<li>Zet de web server, PHP en database server op zoals het hoort.</li>
+	<li>Bereid een databank voor om door ImpressCMS te laten gebruiken. Dat kan een bestaande databank zijn, of een volledig nieuwe.</li>
+	<li>Bereid een databank gebruiker voor en geef hem alle rechten op de databank waarin ImpressCMS terecht zal komen.</li>
+	<li>Maak de folders uploads/, ../storage/templates_c, ../storage/cache, ../storage/log, ../storage/htmlpurifier, ../modules/ schrijfbaar (chmod 777 or 755 - hangt af van het type server).</li>
+	<li>Maak de file ../.env schrijfbaar(chmod 666 - hangt af van de server).</li>
+	<li>In je internet browser instellingen, sta toe dat Cookies en Javascript gebruikt worden.</li>
 </ul>
 ';
 
