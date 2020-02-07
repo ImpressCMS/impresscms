52759bbd73147803b19e3453284e8555a44305f8
David Janssens <fiammybe@gmail.com>
2020-02-07 01:02:34 +0100

    New translations finish.php (French)

--- htdocs/install/language/fr/finish.php
+++ htdocs/install/language/fr/finish.php
@@ -3,18 +3,18 @@
 $content .=
 "<h3>Support</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>
-<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>
-<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>
-<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+<li><a href='https://www.impresscms.org/' target='_blank'>Notre Accueil - Page d'accueil du projet</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Forums de la communauté - Support</a></li>
+<li><li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Téléchargements - Modules & Thèmes</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Documentation de la Communauté</a></li>
 </ul>
 
 <h3>Newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
+<p>N'oubliez pas de vous abonner à la <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>Newsletter ImpressCMS</a>, pour les nouvelles de nouveaux modules, thèmes, mises à jour importantes et plus encore !</p>
 
-<h3>Hosting Sponsor</h3>
-<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> provides hosting for the ImpressCMS websites and offers environments tailored to your needs and ImpressCMS</p>
+<h3>Sponsor Hébergeur</h3>
+<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> fournit un hébergement pour les sites Web ImpressCMS et propose des environnements adaptés à vos besoins et ImpressCMS</p>
 
-<h3>Your site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
+<h3>Votre site</h3>
+<p>Vous pouvez maintenant accéder à la page d'accueil de votre site, en cliquant sur le bouton ci-dessous et en terminant l'assistant.</p>
 ";
