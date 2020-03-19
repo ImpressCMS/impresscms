0cf664971cb4fa9997edbc85243d08ff60567ce0
David Janssens <fiammybe@gmail.com>
2020-03-19 10:28:11 +0100

    New translations finish.php (Dutch)

--- htdocs/install/language/nl/finish.php
+++ htdocs/install/language/nl/finish.php
@@ -1,20 +1,17 @@
 <?php
 // $Id: finish.php 12168 2013-05-22 13:25:59Z skenow $
 $content .=
-"<h3>Support</h3>
+"<h3>Ondersteuning</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>
-<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>
-<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>
-<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+<li><a href='https://www.impresscms.org/' target='_blank'>Project Startpagina</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Ondersteuning</a></li>
+<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Thema's</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Documentatie</a></li>
 </ul>
 
-<h3>Newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
+<h3>Nieuwsbrief</h3>
+<p>Schrijf je nu in op onze <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Nieuwsbrief</a>, om maandelijks het laatste nieuws te ontvangen over thema's, modules en belangrijke updates!</p>
 
-<h3>Hosting Sponsor</h3>
-<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> provides hosting for the ImpressCMS websites and offers environments tailored to your needs and ImpressCMS</p>
-
-<h3>Your site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
+<h3>Jouw website</h3>
+<p>Je kan nu naar de startpagina van je site, door te klikken op de knop hieronder en de installatie te voltooien.</p>
 ";
