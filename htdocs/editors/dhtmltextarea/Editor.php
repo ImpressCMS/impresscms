2ec182cf55c6dca5a810eb45b3485249993cc129
Raimondas Rimkevičius <github@mekdrop.name>
2020-03-22 13:43:44 +0200

    editors and sourceseditors now can be defined as services with tags editor.content and editor.source

---
+++ htdocs/editors/dhtmltextarea/Editor.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace ImpressCMS\Editors\DHTMLTextArea;
+
+use icms\plugins\EditorInterface;
+
+/**
+ * Defines DHTMLTextArea editor
+ *
+ * @package ImpressCMS\Editors\DHTMLTextArea
+ */
+class Editor implements EditorInterface
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getVersion(): string
+	{
+		return '1.0.0';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getLicense(): string
+	{
+		return 'GPLv2';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function create(array $configs, $checkCompatible = false): \icms_form_elements_Textarea
+	{
+		require_once __DIR__ . '/dhtmltextarea.php';
+		return new \FormDhtmlTextArea($configs, $checkCompatible);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getOrder(): ?int
+	{
+		return 0;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function supportsHTML(): bool
+	{
+		return false;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getTitle(): string
+	{
+		global $icmsConfig;
+
+		if (!defined('_XOOPS_EDITOR_DHTMLTEXTAREA')) {
+			$langPath = __DIR__ . '/language/';
+			$lang = $icmsConfig['language'];
+
+			if (!file_exists($langPath . $lang . '.php')) {
+				$lang = 'english';
+			}
+
+			require_once $langPath . $lang . '.php';
+		}
+
+		return _XOOPS_EDITOR_DHTMLTEXTAREA;
+	}
+}
