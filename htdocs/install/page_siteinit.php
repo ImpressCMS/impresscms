c328a570468d4950237bb4fdb109f8ad1fbb1ed0
David Janssens <david.j@impresscms.org>
2020-06-22 14:51:21 +0200

    Fix the "Trying to access array offset on value of type null" error in PHP 7.4 by checking if the variable is is set or not first

--- htdocs/install/page_siteinit.php
+++ htdocs/install/page_siteinit.php
@@ -57,58 +57,58 @@ ob_start();
 ?>
 <?php if (!empty( $error ) ) echo '<div class="x2-note error">' . $error . "</div>\n"; ?>
 
-<script
-	type="text/javascript"
-	src="../libraries/jquery/password_strength_plugin.js"></script>
-<script type="text/javascript">
-                $(document).ready( function() {
-                    $.fn.shortPass = "<?php echo _CORE_PASSLEVEL1;?>";
-                    $.fn.badPass = "<?php echo _CORE_PASSLEVEL2;?>";
-                    $.fn.goodPass = "<?php echo _CORE_PASSLEVEL3;?>";
-                    $.fn.strongPass = "<?php echo _CORE_PASSLEVEL4;?>";
-                    $.fn.samePassword = "Username and Password identical.";
-                    $.fn.resultStyle = "";
-				$(".password_adv").passStrength({
-					shortPass: 		"top_shortPass",
-					badPass:		"top_badPass",
-					goodPass:		"top_goodPass",
-					strongPass:		"top_strongPass",
-					baseStyle:		"top_testresult",
-					userid:			"#adminlogin_name",
-					messageloc:		0
+	<script
+		type="text/javascript"
+		src="../libraries/jquery/password_strength_plugin.js"></script>
+	<script type="text/javascript">
+		$(document).ready( function() {
+			$.fn.shortPass = "<?php echo _CORE_PASSLEVEL1;?>";
+			$.fn.badPass = "<?php echo _CORE_PASSLEVEL2;?>";
+			$.fn.goodPass = "<?php echo _CORE_PASSLEVEL3;?>";
+			$.fn.strongPass = "<?php echo _CORE_PASSLEVEL4;?>";
+			$.fn.samePassword = "Username and Password identical.";
+			$.fn.resultStyle = "";
+			$(".password_adv").passStrength({
+				shortPass: 		"top_shortPass",
+				badPass:		"top_badPass",
+				goodPass:		"top_goodPass",
+				strongPass:		"top_strongPass",
+				baseStyle:		"top_testresult",
+				userid:			"#adminlogin_name",
+				messageloc:		0
 
-				});
 			});
+		});
 	</script>
-<fieldset>
-<h3><?php echo LEGEND_ADMIN_ACCOUNT; ?></h3>
-<div class="blokinit">
-<div class="dbconn_line"><label for="adminname"><?php echo ADMIN_DISPLAY_LABEL; ?></label>
-<div class="clear">&nbsp;</div>
-<input type="text" name="adminname" id="adminname" maxlength="25"
-	value="<?php echo htmlspecialchars( $vars['adminname'], ENT_QUOTES ); ?>" />
-</div>
-<div class="dbconn_line"><label for="adminlogin_name"><?php echo ADMIN_LOGIN_LABEL; ?></label>
-<div class="clear">&nbsp;</div>
-<input class="adminlogin_name" type="text" name="adminlogin_name"
-	id="adminlogin_name" maxlength="25"
-	value="<?php echo htmlspecialchars( $vars['adminlogin_name'], ENT_QUOTES ); ?>" />
-</div>
-<div class="dbconn_line"><label for="adminmail"><?php echo ADMIN_EMAIL_LABEL; ?></label>
-<div class="clear">&nbsp;</div>
-<input type="text" name="adminmail" id="adminmail" maxlength="255"
-	value="<?php echo htmlspecialchars( $vars['adminmail'], ENT_QUOTES ); ?>" />
-</div>
-<div class="dbconn_line"><label for="adminpass"><?php echo ADMIN_PASS_LABEL; ?></label>
-<div class="clear">&nbsp;</div>
-<input class="password_adv" type="password" name="adminpass"
-	id="adminpass" maxlength="255" value="" /></div>
-<div class="dbconn_line"><label for="adminpass2"><?php echo ADMIN_CONFIRMPASS_LABEL; ?></label>
-<div class="clear">&nbsp;</div>
-<input type="password" name="adminpass2" id="adminpass2" maxlength="255"
-	value="" /></div>
-</div>
-</fieldset>
+	<fieldset>
+		<h3><?php echo LEGEND_ADMIN_ACCOUNT; ?></h3>
+		<div class="blokinit">
+			<div class="dbconn_line"><label for="adminname"><?php echo ADMIN_DISPLAY_LABEL; ?></label>
+				<div class="clear">&nbsp;</div>
+				<input type="text" name="adminname" id="adminname" maxlength="25"
+					   value="<?php if(isset($vars['adminname'])) { if(isset($vars['adminname'])) echo htmlspecialchars( $vars['adminname'], ENT_QUOTES );} ?>" />
+			</div>
+			<div class="dbconn_line"><label for="adminlogin_name"><?php echo ADMIN_LOGIN_LABEL; ?></label>
+				<div class="clear">&nbsp;</div>
+				<input class="adminlogin_name" type="text" name="adminlogin_name"
+					   id="adminlogin_name" maxlength="25"
+					   value="<?php if(isset($vars['adminlogin_name'])) echo htmlspecialchars( $vars['adminlogin_name'], ENT_QUOTES ); ?>" />
+			</div>
+			<div class="dbconn_line"><label for="adminmail"><?php echo ADMIN_EMAIL_LABEL; ?></label>
+				<div class="clear">&nbsp;</div>
+				<input type="text" name="adminmail" id="adminmail" maxlength="255"
+					   value="<?php if(isset($vars['adminmail'])) echo htmlspecialchars( $vars['adminmail'], ENT_QUOTES ); ?>" />
+			</div>
+			<div class="dbconn_line"><label for="adminpass"><?php echo ADMIN_PASS_LABEL; ?></label>
+				<div class="clear">&nbsp;</div>
+				<input class="password_adv" type="password" name="adminpass"
+					   id="adminpass" maxlength="255" value="" /></div>
+			<div class="dbconn_line"><label for="adminpass2"><?php echo ADMIN_CONFIRMPASS_LABEL; ?></label>
+				<div class="clear">&nbsp;</div>
+				<input type="password" name="adminpass2" id="adminpass2" maxlength="255"
+					   value="" /></div>
+		</div>
+	</fieldset>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
