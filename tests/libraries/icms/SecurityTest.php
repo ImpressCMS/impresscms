03c77301a2ea97b8ab8d3fbc4d0f6dc0a3b22396
Raimondas Rimkevičius (aka MekDrop) <mekdrop@impresscms.org>
2020-08-02 02:54:20 +0300

    token test fix

--- tests/libraries/icms/SecurityTest.php
+++ tests/libraries/icms/SecurityTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ImpressCMS\Tests\Libraries\ICMS;
+use Aura\Session\SessionFactory;
 use ImpressCMS\Core\Providers\SecurityServiceProvider;
 use League\Container\Container;
 
@@ -46,9 +47,20 @@ class SecurityTest extends \PHPUnit_Framework_TestCase {
     public function testToken() {
 		$container = new Container();
 		$container->addServiceProvider(SecurityServiceProvider::class);
+		/**
+		 * @var \icms_core_Security $instance
+		 */
 		$instance = $container->get('security');
         $new_token = $instance->createToken();
+
         $this->assertInternalType('string', $new_token, 'Generated token is not type of string');
         $this->assertTrue($instance->validateToken($new_token), 'Can\'t validate correct token');
     }
+
+	protected function setUp()
+	{
+		\icms::$session = (new SessionFactory())->newInstance([]);
+
+		parent::setUp();
+	}
 }
