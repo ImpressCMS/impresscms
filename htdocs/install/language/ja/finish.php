fbe10f0476a2040e41e1ad0e5c6e0d789a2a4c1b
David Janssens <fiammybe@gmail.com>
2020-10-22 02:23:55 +0200

    New translations finish.php (Japanese)

--- htdocs/install/language/ja/finish.php
+++ htdocs/install/language/ja/finish.php
@@ -1,20 +1,20 @@
 <?php
 // $Id: finish.php 12168 2013-05-22 13:25:59Z skenow $
 $content .=
-"<h3>Support</h3>
+"<h3>サポートt</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>
-<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>
-<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>
-<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+<li><a href='https://www.impresscms.org/' target='_blank'>ImpressCMS公式ページ</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>フォーラム</a></li>
+<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>モジュール・テーマダウンロード</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wikiドキュメント</a></li>
 </ul>
 
-<h3>Newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
+<h3>ニュースレター</h3>
+<p>こちらの <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>にぜひご登録ください。新しいモジュールやテーマ、その他のアップデート情報をお送りさせていただきます!</p>
 
-<h3>Hosting Sponsor</h3>
-<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> provides hosting for the ImpressCMS websites and offers environments tailored to your needs and ImpressCMS</p>
+<h3>スポンサー</h3>
+<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> ImpressCMS環境を整えたレンタルサーバー</p>
 
-<h3>Your site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
+<h3>作成されたサイト</h3>
+<p>これでインストールは完了です。下のボタンをクリックして作成されたサイトをご覧ください。</p>
 ";
