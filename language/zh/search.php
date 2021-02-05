d6fa3531d2cbb7132fb77c0bffe3100cefa05f53
David Janssens <fiammybe@gmail.com>
2021-02-05 22:03:16 +0100

    New translations search.php (Chinese Traditional)

--- language/zh/search.php
+++ language/zh/search.php
@@ -1,24 +1,24 @@
 <?php
 
 //%%%%%%	File Name search.php 	%%%%%
-define("_SR_SEARCH","搜索");
-define("_SR_PLZENTER","请输入!");
-define("_SR_SEARCHRESULTS","搜索结果");
-define("_SR_NOMATCH","无匹配的搜索结果");
+define("_SR_SEARCH","Search");
+define("_SR_PLZENTER","Please enter all required data!");
+define("_SR_SEARCHRESULTS","Search Results");
+define("_SR_NOMATCH","No Match Found for your Query");
 //define("_SR_FOUND","Found <b>%s</b> match(es)");
-define("_SR_SHOWING","(显示%d - %d)");
-define("_SR_ANY","任何(或者)");
-define("_SR_ALL","全部(和)");
-define("_SR_EXACT","提取匹配结果");
-define("_SR_SHOWALLR","显示全部搜索结果");
-define("_SR_NEXT","下一页");
-define("_SR_PREVIOUS","上一页");
-define("_SR_KEYWORDS","关键词");
-define("_SR_TYPE","搜索类型");
-define("_SR_SEARCHIN","搜索于");
-define('_SR_KEYTOOSHORT', '关键词最少<b>%s</b>个字符');
-define('_SR_KEYIGNORE', '少于<b>%s</b>个字符的关键词将被忽略');
-define('_SR_SEARCHRULE', '搜索规则');
-define('_SR_IGNOREDWORDS', '下列字词因少于(%u chars)个字符，将不会出现在搜索结果中:');
+define("_SR_SHOWING","(Showing %d - %d)");
+define("_SR_ANY","Any (OR)");
+define("_SR_ALL","All (AND)");
+define("_SR_EXACT","Exact Match");
+define("_SR_SHOWALLR","Show all results");
+define("_SR_NEXT","Next");
+define("_SR_PREVIOUS","Previous");
+define("_SR_KEYWORDS","Keyword(s)");
+define("_SR_TYPE","Search Type");
+define("_SR_SEARCHIN","Search in");
+define('_SR_KEYTOOSHORT', 'Keywords must be at least <b>%s</b> characters long');
+define('_SR_KEYIGNORE', 'Keywords shorter than <b>%s</b> characters will be ignored');
+define('_SR_SEARCHRULE', 'Search Rule');
+define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minimum length (%u chars) and were not included in your search:');
 ############# added since 1.2 #############
-define('_SR_HITSRETURNED', '点击返回');
+define('_SR_HITSRETURNED', 'hit(s) returned');
