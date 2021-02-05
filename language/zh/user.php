b82a1caf5087fccfff87790fba67a07e699814a2
David Janssens <fiammybe@gmail.com>
2021-02-05 19:44:49 +0100

    New translations user.php (Chinese Simplified)

--- language/zh/user.php
+++ language/zh/user.php
@@ -1,191 +1,191 @@
 <?php
 
 //%%%%%%		File Name user.php 		%%%%%
-define('_US_NOTREGISTERED','Not registered?  Click <a href="register.php">here</a>.');
-define('_US_LOSTPASSWORD','Lost your password?');
-define('_US_NOPROBLEM','No problem. Simply enter the e-mail address we have on file for your account.');
-define('_US_YOUREMAIL','Your Email: ');
-define('_US_SENDPASSWORD','Send Password');
-define('_US_LOGGEDOUT','You are now logged out');
-define('_US_THANKYOUFORVISIT','Thank you for your visit to our site!');
-define('_US_INCORRECTLOGIN','Incorrect Login!');
-define('_US_LOGGINGU','Thank you for logging in, %s.');
-define('_US_RESETPASSWORD','Reset your password');
-define('_US_SUBRESETPASSWORD','Reset Password');
-define('_US_RESETPASSTITLE','Your password has expired!');
-define('_US_RESETPASSINFO','Please complete the following form in order to reset your password. If your email, login name and current password all match our record, your password will be changed instantly and you will be able to log back in!');
-define('_US_PASSEXPIRED','Your password has expired.<br />You will now be redirected to a form where you will be able to reset your password.');
-define('_US_SORRYUNAMENOTMATCHEMAIL','The login name entered is not associated with the given Email address!');
-define('_US_PWDRESET','Your password has been reset successfully!');
-define('_US_SORRYINCORRECTPASS','You have entered your current password incorrectly!');
+define('_US_NOTREGISTERED','没有注册?  点击 <a href="register.php">这里</a>开始注册');
+define('_US_LOSTPASSWORD','忘记密码?');
+define('_US_NOPROBLEM','没问题，输入注册时的电子邮箱地址吧。');
+define('_US_YOUREMAIL','电子邮箱地址: ');
+define('_US_SENDPASSWORD','发送密码');
+define('_US_LOGGEDOUT','你已登出');
+define('_US_THANKYOUFORVISIT','欢迎光临!');
+define('_US_INCORRECTLOGIN','不正确的登录!');
+define('_US_LOGGINGU','感谢登录, %s.');
+define('_US_RESETPASSWORD','重设密码');
+define('_US_SUBRESETPASSWORD','重设密码');
+define('_US_RESETPASSTITLE','密码超时!');
+define('_US_RESETPASSINFO','请完整填写下列表格以重设密码，如果邮箱地址、用户名、当前密码都正确，密码就会重设，你也就可以再次登录了。');
+define('_US_PASSEXPIRED','密码超时<br />将会指向一个表格页面，以便重设密码.');
+define('_US_SORRYUNAMENOTMATCHEMAIL','用户名、邮箱地址不匹配!');
+define('_US_PWDRESET','密码已重设!');
+define('_US_SORRYINCORRECTPASS','当前密码输入不正确!');
 
 // 2001-11-17 ADD
-define('_US_NOACTTPADM','The selected user has been deactivated or has not been activated yet.<br />Please contact the administrator for details.');
-define('_US_ACTKEYNOT','Activation key not correct!');
-define('_US_ACONTACT','Selected account is already activated!');
-define('_US_ACTLOGIN','Your account has been activated. Please login with the registered password.');
-define('_US_NOPERMISS','Sorry, you dont have the permission to perform this action!');
-define('_US_SURETODEL','Are you sure you want to delete your account?');
-define('_US_REMOVEINFO','This will remove all your info from our database.');
-define('_US_BEENDELED','Your account has been deleted.');
-define('_US_REMEMBERME', 'Remember me');
+define('_US_NOACTTPADM','所选用户未激活.<br />请联系管理员.');
+define('_US_ACTKEYNOT','激活码不正确!');
+define('_US_ACONTACT','所选帐号已激活!');
+define('_US_ACTLOGIN','帐号已激活，请用密码登录.');
+define('_US_NOPERMISS','对不起，权限不足，不能执行此动作。');
+define('_US_SURETODEL','你真的要删除这个帐号吗?');
+define('_US_REMOVEINFO','将从数据库中删除所有信息');
+define('_US_BEENDELED','帐号将被删除');
+define('_US_REMEMBERME', '记住我');
 
 //%%%%%%		File Name register.php 		%%%%%
-define('_US_USERREG','User Registration');
-define('_US_EMAIL','Email');
-define('_US_ALLOWVIEWEMAIL','Allow other users to view my email address');
-define('_US_WEBSITE','Website');
-define('_US_TIMEZONE','Time Zone');
-define('_US_AVATAR','Avatar');
-define('_US_VERIFYPASS','Verify Password');
-define('_US_SUBMIT','Submit');
-define('_US_LOGINNAME','Username');
-define('_US_FINISH','Finish');
-define('_US_REGISTERNG','Could not register new user.');
-define('_US_MAILOK','Receive occasional email notices from administrators and moderators?');
-define('_US_DISCLAIMER','Disclaimer');
-define('_US_IAGREE','I agree to the above');
-define('_US_UNEEDAGREE', 'Sorry, you have to agree to our disclaimer to get registered.');
-define('_US_NOREGISTER','Sorry, we are currently closed for new user registrations');
+define('_US_USERREG','用户注册');
+define('_US_EMAIL','电子邮箱');
+define('_US_ALLOWVIEWEMAIL','允许我的电子邮箱地址对其他用户可见');
+define('_US_WEBSITE','网站');
+define('_US_TIMEZONE','时区');
+define('_US_AVATAR','头像');
+define('_US_VERIFYPASS','验证密码');
+define('_US_SUBMIT','提交');
+define('_US_LOGINNAME','用户名');
+define('_US_FINISH','完成');
+define('_US_REGISTERNG','不能继续新用户');
+define('_US_MAILOK','允许接收管理员发送的邮件?');
+define('_US_DISCLAIMER','免责声明');
+define('_US_IAGREE','同意');
+define('_US_UNEEDAGREE', '对不起，只有同意免责声明才能继续注册');
+define('_US_NOREGISTER','对不起，新用户注册功能已关闭');
 
 // %s is username. This is a subject for email
-define('_US_USERKEYFOR','User activation key for %s');
+define('_US_USERKEYFOR','用户 %s 的验证码');
 
-define('_US_YOURREGISTERED','You are now registered. An email containing a user activation key has been sent to the email account you provided. Please follow the instructions in the mail to activate your account. ');
-define('_US_YOURREGMAILNG','You are now registered. However, we were unable to send the activation mail to your email account due to an internal error that had occurred on our server. We are sorry for the inconvenience, please send the webmaster an email notifying him/her of the situation.');
-define('_US_YOURREGISTERED2','You are now registered.  Please wait for your account to be activated by the adminstrators.  You will receive an email once you are activated.  This could take a while so please be patient.');
+define('_US_YOURREGISTERED','你已注册，一封包含验证码的邮件已发送到你的邮箱，请根据邮件中的提示来完成注册。 ');
+define('_US_YOURREGMAILNG','你已注册，但因服务器内部错误，验证邮件未能发送。由此带来的不便，我们深表遗憾。请向网站管理员发送邮件反映。');
+define('_US_YOURREGISTERED2','你已注册，请等候管理员的激活，如果帐号被激活，我们会发送电子邮件通知你，等候激活有可能需要较长的时间。');
 
 // %s is your site name
-define('_US_NEWUSERREGAT','New user registration at %s');
+define('_US_NEWUSERREGAT','新用户已在 %s注册');
 // %s is a username
-define('_US_HASJUSTREG','%s has just registered!');
+define('_US_HASJUSTREG','%s 刚刚注册!');
 
-define('_US_INVALIDMAIL','ERROR: Invalid email');
-define('_US_INVALIDNICKNAME','ERROR: Invalid Loginname, please try an other Loginname.');
-define('_US_NICKNAMETOOLONG','Username is too long. It must be less than %s characters.');
-define('_US_NICKNAMETOOSHORT','Username is too short. It must be more than %s characters.');
-define('_US_NAMERESERVED','ERROR: Name is reserved.');
-define('_US_NICKNAMENOSPACES','There cannot be any spaces in the Username.');
-define('_US_LOGINNAMETAKEN','ERROR: Username taken.');
-define('_US_NICKNAMETAKEN','ERROR: Display Name taken.');
-define('_US_EMAILTAKEN','ERROR: Email address already registered.');
-define('_US_ENTERPWD','ERROR: You must provide a password.');
-define('_US_SORRYNOTFOUND','Sorry, no corresponding user info was found.');
+define('_US_INVALIDMAIL','错误: 无效的邮箱');
+define('_US_INVALIDNICKNAME','错误: 无效的用户名, 请换个用户名');
+define('_US_NICKNAMETOOLONG','用户名太长，不应大于 %s 个字符.');
+define('_US_NICKNAMETOOSHORT','用户名太短，不应小于 %s 个字符.');
+define('_US_NAMERESERVED','错误: 用户名不可用');
+define('_US_NICKNAMENOSPACES','用户名中不能包含空格');
+define('_US_LOGINNAMETAKEN','错误: 用户名已被注册');
+define('_US_NICKNAMETAKEN','错误: 显示名称已被别人使用');
+define('_US_EMAILTAKEN','错误: 该邮箱地址已被注册');
+define('_US_ENTERPWD','错误: 必须设置密码');
+define('_US_SORRYNOTFOUND','对不起，找不到用户.');
 
-define('_US_USERINVITE', 'Membership invitation');
-define('_US_INVITENONE','ERROR: Registration is by invitation only.');
-define('_US_INVITEINVALID','ERROR: Incorrect invitation code.');
-define('_US_INVITEEXPIRED','ERROR: Invitation code is already used or expired.');
+define('_US_USERINVITE', '友情邀请');
+define('_US_INVITENONE','错误: 只能邀请注册');
+define('_US_INVITEINVALID','错误: 邀请码不正确');
+define('_US_INVITEEXPIRED','错误: 邀请码已过期');
 
-define('_US_INVITEBYMEMBER','Only an existing member can invite new members; please request an invitation email from some registered member.');
-define('_US_INVITEMAILERR','We were unable to send the mail with registration link to your email account due to an internal error that had occurred on our server. We are sorry for the inconvenience, please try again and if problem persists, do send the webmaster an email notifying him/her of the situation. <br />');
-define('_US_INVITEDBERR','We were unable to process your registration request due to an internal error. We are sorry for the inconvenience, please try again and if problem persists, do send the webmaster an email notifying him/her of the situation. <br />');
-define('_US_INVITESENT','An email containing registration link has been sent to the email account you provided. Please follow the instructions in the mail to register your account. This could take few minutes so please be patient.');
+define('_US_INVITEBYMEMBER','只有现有会员才能邀请新会员; 请要求现有会员发送邀请邮件。');
+define('_US_INVITEMAILERR','服务器出错，含有注册链接的邮件未能发送。由此带来的不便，我们深表歉意。请再试一次，如还有问题，请发送邮件向管理员反映 <br />');
+define('_US_INVITEDBERR','因为内部错误，你的注册请求未能执行。请再试一次，如还有问题，请发送邮件向管理员反映。 <br />');
+define('_US_INVITESENT','一封含有注册链接的邮件已发送去你所提交的电子邮箱，请依照邮件中的提示完成注册。');
 // %s is your site name
-define('_US_INVITEREGLINK','Registration invitation from %s');
+define('_US_INVITEREGLINK','来自 %s 的注册邀请');
 
 // %s is your site name
-define('_US_NEWPWDREQ','New Password Request at %s');
-define('_US_YOURACCOUNT', 'Your account at %s');
+define('_US_NEWPWDREQ',' %s 的新密码请求');
+define('_US_YOURACCOUNT', '你在  %s 的帐号');
 
-define('_US_MAILPWDNG','mail_password: could not update user entry. Contact the Administrator');
-define('_US_RESETPWDNG','reset_password: could not update user entry. Contact the Administrator');
+define('_US_MAILPWDNG','邮箱密码：不能更新，请联系管理员');
+define('_US_RESETPWDNG','重设密码：不能更新，请联系管理员');
 
-define('_US_RESETPWDREQ','Reset Password Request at %s');
-define('_US_MAILRESETPWDNG','reset_password: could not update user entry. Contact the Administrator');
-define('_US_NEWPASSWORD','New Password');
-define('_US_YOURUSERNAME','Your Username');
-define('_US_CURRENTPASS','Your Current Password');
-define('_US_BADPWD','Bad Password, Password can not contain username.');
+define('_US_RESETPWDREQ',' %s 的重设密码请求');
+define('_US_MAILRESETPWDNG','重设密码：不能更新，请联系管理员');
+define('_US_NEWPASSWORD','新密码');
+define('_US_YOURUSERNAME','你的用户名');
+define('_US_CURRENTPASS','你当前的密码');
+define('_US_BADPWD','不安全的密码，密码中不能包含用户名');
 
 // %s is a username
-define('_US_PWDMAILED','Password for %s mailed.');
-define('_US_CONFMAIL','Confirmation Mail for %s mailed.');
-define('_US_ACTVMAILNG', 'Failed sending notification mail to %s');
-define('_US_ACTVMAILOK', 'Notification mail to %s sent.');
+define('_US_PWDMAILED',' %s 的密码已发送');
+define('_US_CONFMAIL',' %s 的确认邮件已发送');
+define('_US_ACTVMAILNG', '发送邮件给 %s 失败');
+define('_US_ACTVMAILOK', '给 %s 的通知邮件已发送');
 
 //%%%%%%		File Name userinfo.php 		%%%%%
-define('_US_SELECTNG','No User Selected! Please go back and try again.');
-define('_US_PM','PM');
+define('_US_SELECTNG','没有用户被选中，请返回重试');
+define('_US_PM','下午');
 define('_US_ICQ','ICQ');
 define('_US_AIM','AIM');
 define('_US_YIM','YIM');
 define('_US_MSNM','MSNM');
-define('_US_LOCATION','Location');
-define('_US_OCCUPATION','Occupation');
-define('_US_INTEREST','Interest');
-define('_US_SIGNATURE','Signature');
-define('_US_EXTRAINFO','Extra Info');
-define('_US_EDITPROFILE','Edit Profile');
-define('_US_LOGOUT','Logout');
-define('_US_INBOX','Inbox');
-define('_US_MEMBERSINCE','Member Since');
-define('_US_RANK','Rank');
-define('_US_POSTS','Comments/Posts');
-define('_US_LASTLOGIN','Last Login');
-define('_US_ALLABOUT','All about %s');
-define('_US_STATISTICS','Statistics');
-define('_US_MYINFO','My Info');
-define('_US_BASICINFO','Basic information');
-define('_US_MOREABOUT','More About Me');
-define('_US_SHOWALL','Show All');
+define('_US_LOCATION','地址');
+define('_US_OCCUPATION','住址');
+define('_US_INTEREST','兴趣');
+define('_US_SIGNATURE','签名');
+define('_US_EXTRAINFO','其他信息');
+define('_US_EDITPROFILE','编辑个人资料');
+define('_US_LOGOUT','登出');
+define('_US_INBOX','收件箱');
+define('_US_MEMBERSINCE','注册自');
+define('_US_RANK','等级');
+define('_US_POSTS','发言/留言');
+define('_US_LASTLOGIN','上次登录');
+define('_US_ALLABOUT','%s的全部信息');
+define('_US_STATISTICS','统计');
+define('_US_MYINFO','我的资料');
+define('_US_BASICINFO','基本资料');
+define('_US_MOREABOUT','更多资料');
+define('_US_SHOWALL','显示全部');
 
 //%%%%%%		File Name edituser.php 		%%%%%
-define('_US_PROFILE','Profile');
-define('_US_REALNAME','Real Name');
-define('_US_SHOWSIG','Always attach my signature');
-define('_US_CDISPLAYMODE','Comments Display Mode');
-define('_US_CSORTORDER','Comments Sort Order');
-define('_US_PASSWORD','Password');
-define('_US_TYPEPASSTWICE','(type a new password twice to change it)');
-define('_US_SAVECHANGES','Save Changes');
-define('_US_NOEDITRIGHT',"Sorry, you don't have the right to edit this user's info.");
-define('_US_PASSNOTSAME','Both passwords are different. They must be identical.');
-define('_US_PWDTOOSHORT','Sorry, your password must be at least <b>%s</b> characters long.');
-define('_US_PROFUPDATED','Your Profile Updated!');
-define('_US_USECOOKIE','Store my user name in a cookie for 1 year');
+define('_US_PROFILE','简介');
+define('_US_REALNAME','真实姓名');
+define('_US_SHOWSIG','总是使用签名');
+define('_US_CDISPLAYMODE','留言显示模式');
+define('_US_CSORTORDER','留言排列顺序');
+define('_US_PASSWORD','密码');
+define('_US_TYPEPASSTWICE','(将新密码输入两次以更改)');
+define('_US_SAVECHANGES','保存更改');
+define('_US_NOEDITRIGHT',"对不起，你没有更改该用户资料的权限");
+define('_US_PASSNOTSAME','两次输入的密码不匹配. ');
+define('_US_PWDTOOSHORT','对不起, 密码至少<b>%s</b>个字符');
+define('_US_PROFUPDATED','你的资料已更新!');
+define('_US_USECOOKIE','将我的用户名在cookie中保存1年');
 //define('_US_NO','No');
-define('_US_DELACCOUNT','Delete Account');
-define('_US_MYAVATAR', 'My Avatar');
-define('_US_UPLOADMYAVATAR', 'Upload Avatar');
-define('_US_MAXPIXEL','Max Pixels');
-define('_US_MAXIMGSZ','Max Image Size (Bytes)');
-define('_US_SELFILE','Select file');
-define('_US_OLDDELETED','Your old avatar will be deleted!');
-define('_US_CHOOSEAVT', 'Choose avatar from the available list');
-define('_US_SELECT_THEME', 'Default Theme');
-define('_US_SELECT_LANG', 'Default Language');
+define('_US_DELACCOUNT','删除用户');
+define('_US_MYAVATAR', '我的头像');
+define('_US_UPLOADMYAVATAR', '上传头像');
+define('_US_MAXPIXEL','最大像素');
+define('_US_MAXIMGSZ','图片最大尺寸 (Bytes)');
+define('_US_SELFILE','选择文件');
+define('_US_OLDDELETED','你的旧头像将被删除!');
+define('_US_CHOOSEAVT', '在列表中选择头像');
+define('_US_SELECT_THEME', '默认主图');
+define('_US_SELECT_LANG', '默认语言');
 
-define('_US_PRESSLOGIN', 'Press the button below to login');
+define('_US_PRESSLOGIN', '点击下面的按钮登录');
 
-define('_US_ADMINNO', 'User in the webmasters group cannot be removed');
-define('_US_GROUPS', 'User\'s Groups');
+define('_US_ADMINNO', '管理员组中的用户不能被删除');
+define('_US_GROUPS', '用户所在组');
 
-define('_US_YOURREGISTRATION', 'Your registration at %s');
-define('_US_WELCOMEMSGFAILED', 'Error while sending the welcome email.');
-define('_US_NEWUSERNOTIFYADMINFAIL', 'Notification to admin about new user registration failed.');
-define('_US_REGFORM_NOJAVASCRIPT', 'To log in at the site it\'s necessary that your browser has javascript enabled.');
-define('_US_REGFORM_WARNING', 'To register at the site you need to use a secure password. Try to create your password by using a mixture of letters (upper and lowercase), numbers and symbols. Try to create a password the more complex as possible although you can remember it.');
-define('_US_CHANGE_PASSWORD', 'Change Password?');
-define('_US_POSTSNOTENOUGH','Sorry, at least you need to have <b>%s</b> posts, to be able to upload your avatar.');
-define('_US_UNCHOOSEAVT', 'Until you reach this amount you can choose avatar from the list below.');
+define('_US_YOURREGISTRATION', '你在%s注册');
+define('_US_WELCOMEMSGFAILED', '在发送欢迎邮件时发生错误');
+define('_US_NEWUSERNOTIFYADMINFAIL', '注册失败，通知管理员');
+define('_US_REGFORM_NOJAVASCRIPT', '请开启JavaScript以确保能够正确登录');
+define('_US_REGFORM_WARNING', '尽量使用一个安全的密码。尝试使用混合大小写字母、数字和符号来创建密码。密码尽可能复杂，当然你要能记住它');
+define('_US_CHANGE_PASSWORD', '更改密码?');
+define('_US_POSTSNOTENOUGH','对不起，需要最少<b>%s</b>条留言，才能更新头像.');
+define('_US_UNCHOOSEAVT', '只有达到这个数量，才能从下面表中选择头像');
 
 
-define('_US_SERVER_PROBLEM_OCCURRED','There was an issue while checking for spammers list!');
-define('_US_INVALIDIP','ERROR: This IP adress is not allowed to register');
+define('_US_SERVER_PROBLEM_OCCURRED','检测滥发广告黑名单时发生错误!');
+define('_US_INVALIDIP','出错: 你的IP地址不允许注册');
 
 ######################## Added in 1.2 ###################################
-define('_US_LOGIN_NAME', "Login Name");
-define('_US_OLD_PASSWORD', "Old Password");
-define('_US_NICKNAME','Display Name');
-define('_US_MULTLOGIN', 'It was not possible to login on the site!! <br />
+define('_US_LOGIN_NAME', "登录名");
+define('_US_OLD_PASSWORD', "旧密码");
+define('_US_NICKNAME','显示昵称');
+define('_US_MULTLOGIN', '不能登录!! <br />
         <p align="left" style="color:red;">
-        Possible causes:<br />
-         - You are already logged in on the site.<br />
-         - Someone else logged in on the site using your username and password.<br />
-         - You left the site or close the browser window without clicking the logout button.<br />
+           出错原因：<br />
+         - 你已登录<br />
+         - 已有其他人使用你的用户帐户登录<br />
+         - 在你关闭浏览器窗口时，没有登出<br />
         </p>
-        Wait a few minutes and try again later. If the problems still persists contact the site administrator.');
+           请稍等并重试，如果还不能登录，请联系管理员');
 
 // added in 1.3
 define('_US_NOTIFICATIONS', "Notifications");
@@ -201,17 +201,17 @@ define("_MA_USER_ADD_SELECTED", "Add selected users");
 
 define("_MA_USER_GROUP", "Group");
 define("_MA_USER_LEVEL", "Level");
-define("_MA_USER_LEVEL_ACTIVE", "Active");
+define("_MA_USER_LEVEL_ACTIVE", "激活");
 define("_MA_USER_LEVEL_INACTIVE", "Inactive");
 define("_MA_USER_LEVEL_DISABLED", "Disabled");
-define("_MA_USER_RANK", "Rank");
+define("_MA_USER_RANK", "等级");
 
 define("_MA_USER_FINDUS","Find Users");
-define("_MA_USER_REALNAME","Real Name");
+define("_MA_USER_REALNAME","真实姓名");
 define("_MA_USER_REGDATE","Joined Date");
-define("_MA_USER_EMAIL","Email");
-define("_MA_USER_PREVIOUS","Previous");
-define("_MA_USER_NEXT","Next");
+define("_MA_USER_EMAIL","电子邮件");
+define("_MA_USER_PREVIOUS","上一页");
+define("_MA_USER_NEXT","下一页");
 define("_MA_USER_USERSFOUND","%s user(s) found");
 
 define("_MA_USER_ACTUS", "Active Users: %s");
@@ -230,18 +230,18 @@ define("_MA_USER_SORT","Sort by");
 define("_MA_USER_ORDER","Order");
 define("_MA_USER_LASTLOGIN","Last login");
 define("_MA_USER_POSTS","Number of posts");
-define("_MA_USER_ASC","Ascending order");
-define("_MA_USER_DESC","Descending order");
+define("_MA_USER_ASC","按字母排序");
+define("_MA_USER_DESC","按字母降序");
 define("_MA_USER_LIMIT","Number of users per page");
 define("_MA_USER_RESULTS", "Search results");
 define("_MA_USER_SHOWMAILOK", "Type of users to show");
 define("_MA_USER_MAILOK","Only users that accept mail");
 define("_MA_USER_MAILNG","Only users that don't accept mail");
-define("_MA_USER_BOTH", "All");
+define("_MA_USER_BOTH", "全部");
 
 define("_MA_USER_RANGE_LAST_LOGIN","Logged in past <span style='color:#ff0000;'>X</span>days");
 define("_MA_USER_RANGE_USER_REGDATE","Registered in past <span style='color:#ff0000;'>X</span>days");
-define("_MA_USER_RANGE_POSTS","Posts");
+define("_MA_USER_RANGE_POSTS","留言");
 
 define("_MA_USER_HASAVATAR", "Has avatar");
 define("_MA_USER_MODE_SIMPLE", "Simple mode");
