9e59ee532e762fb028581757f18614489a36c126
David Janssens <fiammybe@gmail.com>
2021-02-05 21:04:20 +0100

    New translations misc.php (Chinese Simplified)

--- language/zh/misc.php
+++ language/zh/misc.php
@@ -1,23 +1,23 @@
 <?php
 
-define("_MSC_YOURNAMEC","Your Name: ");
-define("_MSC_YOUREMAILC","Your Email: ");
-define("_MSC_FRIENDNAMEC","Friend Name: ");
-define("_MSC_FRIENDEMAILC","Friend Email: ");
-define("_MSC_RECOMMENDSITE","Recommend this Site to a Friend");
+define("_MSC_YOURNAMEC","你的名字: ");
+define("_MSC_YOUREMAILC","电子邮箱: ");
+define("_MSC_FRIENDNAMEC","朋友: ");
+define("_MSC_FRIENDEMAILC","朋友邮箱: ");
+define("_MSC_RECOMMENDSITE","向朋友推荐这个网站");
 // %s is your site name
-define("_MSC_INTSITE","Interesting Site: %s");
-define("_MSC_REFERENCESENT","The reference to our site has been sent to your friend. Thanks!");
-define("_MSC_ENTERYNAME","Please enter your name");
-define("_MSC_ENTERFNAME","Please enter your friend's name");
-define("_MSC_ENTERFMAIL","Please enter your friend's email address");
-define("_MSC_NEEDINFO","You need to enter required info!");
-define("_MSC_INVALIDEMAIL1","The email address you provided is not a valid address.");
-define("_MSC_INVALIDEMAIL2","Please check the address and try again.");
+define("_MSC_INTSITE","感兴趣的网站: %s");
+define("_MSC_REFERENCESENT","我们网站的相关信息已发送给了你朋友，谢谢！");
+define("_MSC_ENTERYNAME","输入你的名字");
+define("_MSC_ENTERFNAME","输入你朋友的名字");
+define("_MSC_ENTERFMAIL","输入你朋友的电子邮箱地址");
+define("_MSC_NEEDINFO","必须填写相关信息!");
+define("_MSC_INVALIDEMAIL1","不是有效的邮箱地址.");
+define("_MSC_INVALIDEMAIL2","请检查邮箱地址并重试.");
 
-define("_MSC_AVAVATARS","Available Avatars");
+define("_MSC_AVAVATARS","头像可用");
 
-define("_MSC_SMILIES","Smilies");
-define("_MSC_CLICKASMILIE","Click a smilie to insert it into your message.");
-define("_MSC_CODE","Code");
-define("_MSC_EMOTION","Emotion");
+define("_MSC_SMILIES","表情符");
+define("_MSC_CLICKASMILIE","点击表情符可插入到信息中.");
+define("_MSC_CODE","代码");
+define("_MSC_EMOTION","心情");
