f4f121eedf4a2fe964a5f99eb06d8742353167d4
David Janssens <fiammybe@gmail.com>
2021-02-06 08:21:10 +0100

    New translations captcha.php (Portuguese)

--- language/pt/captcha.php
+++ language/pt/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Código de Confirmação");
-define("ICMS_CAPTCHA_INVALID_CODE", "Código de confirmação Inválido!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Foram feitas muitas tentativas erradas!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Número Máximo de tentativas: %d");
+define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
+define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "O código é sensível a maiúsculas e minúsculas");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "Tanto faz se for maiúsculas ou minúsculas");
-define("ICMS_CAPTCHA_REFRESH", "Se o código não for claro o suficiente, clique na imagem para renovar o código");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
+define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Informe o resultado da expressão");
+define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
