69e6f852420e5aae2e1a75b2aff4ce9d8bd2b4be
David Janssens <fiammybe@gmail.com>
2021-02-06 04:25:16 +0100

    New translations calendar.php (Chinese Traditional)

--- language/zh/calendar.php
+++ language/zh/calendar.php
@@ -1,70 +1,70 @@
 <?php
 //%%%%%		Time Zone	%%%%
-define("_CAL_SUNDAY", "星期天");
-define("_CAL_MONDAY", "星期一");
-define("_CAL_TUESDAY", "星期二");
-define("_CAL_WEDNESDAY", "星期三");
-define("_CAL_THURSDAY", "星期四");
-define("_CAL_FRIDAY", "星期五");
-define("_CAL_SATURDAY", "星期六");
-define("_CAL_JANUARY", "一月");
-define("_CAL_FEBRUARY", "二月");
-define("_CAL_MARCH", "三月");
-define("_CAL_APRIL", "四月");
-define("_CAL_MAY", "五月");
-define("_CAL_JUNE", "六月");
-define("_CAL_JULY", "七月");
-define("_CAL_AUGUST", "八月");
-define("_CAL_SEPTEMBER", "九月");
-define("_CAL_OCTOBER", "十月");
-define("_CAL_NOVEMBER", "十一月");
-define("_CAL_DECEMBER", "十二月");
-define("_CAL_TGL1STD", "设置每周的第一天");
-define("_CAL_PREVYR", "上一. 年");
-define("_CAL_PREVMNTH", "上一. 月");
-define("_CAL_GOTODAY", "跳到今天");
-define("_CAL_NXTMNTH", "下一月");
-define("_CAL_NEXTYR", "下一年");
-define("_CAL_SELDATE", "选择日期");
-define("_CAL_DRAGMOVE", "拖拽移动");
-define("_CAL_TODAY", "今天");
-define("_CAL_DISPM1ST", "首先显示星期一");
-define("_CAL_DISPS1ST", "首先显示星期天");
+define("_CAL_SUNDAY", "Sunday");
+define("_CAL_MONDAY", "Monday");
+define("_CAL_TUESDAY", "Tuesday");
+define("_CAL_WEDNESDAY", "Wednesday");
+define("_CAL_THURSDAY", "Thursday");
+define("_CAL_FRIDAY", "Friday");
+define("_CAL_SATURDAY", "Saturday");
+define("_CAL_JANUARY", "January");
+define("_CAL_FEBRUARY", "February");
+define("_CAL_MARCH", "March");
+define("_CAL_APRIL", "April");
+define("_CAL_MAY", "May");
+define("_CAL_JUNE", "June");
+define("_CAL_JULY", "July");
+define("_CAL_AUGUST", "August");
+define("_CAL_SEPTEMBER", "September");
+define("_CAL_OCTOBER", "October");
+define("_CAL_NOVEMBER", "November");
+define("_CAL_DECEMBER", "December");
+define("_CAL_TGL1STD", "Toggle first day of week");
+define("_CAL_PREVYR", "Prev. year");
+define("_CAL_PREVMNTH", "Prev. month");
+define("_CAL_GOTODAY", "Go Today");
+define("_CAL_NXTMNTH", "Next month");
+define("_CAL_NEXTYR", "Next year");
+define("_CAL_SELDATE", "Select date");
+define("_CAL_DRAGMOVE", "Drag to move");
+define("_CAL_TODAY", "Today");
+define("_CAL_DISPM1ST", "Display Monday first");
+define("_CAL_DISPS1ST", "Display Sunday first");
 
 ############# added since 1.1.2 #############
-define("_CAL_SUN", "日");
-define("_CAL_MON", "一");
-define("_CAL_TUE", "二");
-define("_CAL_WED", "三");
-define("_CAL_THU", "四");
-define("_CAL_FRI", "五");
-define("_CAL_SAT", "六");
+define("_CAL_SUN", "Sun");
+define("_CAL_MON", "Mon");
+define("_CAL_TUE", "Tue");
+define("_CAL_WED", "Wed");
+define("_CAL_THU", "Thu");
+define("_CAL_FRI", "Fri");
+define("_CAL_SAT", "Sat");
 // First day of the week. "0" means display Sunday first, "1" means display Monday first, etc...
-define("_CAL_FIRSTDAY", "0");
-define("_CAL_JAN", "一月");
-define("_CAL_FEB", "二月");
-define("_CAL_MAR", "三月");
-define("_CAL_APR", "四月");
-define("_CAL_JUN", "六月");
-define("_CAL_JUL", "七月");
-define("_CAL_AUG", "八月");
-define("_CAL_SEP", "九月");
-define("_CAL_OCT", "十月");
-define("_CAL_NOV", "十一月");
-define("_CAL_DEC", "十二月");
+define("_CAL_FIRSTDAY", "1");
+define("_CAL_JAN", "Jan");
+define("_CAL_FEB", "Feb");
+define("_CAL_MAR", "Mar");
+define("_CAL_APR", "Apr");
+define("_CAL_JUN", "Jun");
+define("_CAL_JUL", "Jul");
+define("_CAL_AUG", "Aug");
+define("_CAL_SEP", "Sept");
+define("_CAL_OCT", "Oct");
+define("_CAL_NOV", "Nov");
+define("_CAL_DEC", "Dec");
 // Direction of the calendar, ltr for left to right and rtl for roght to left (for Persian, Arabic, etc.)
 define("_CAL_DIRECTION", "ltr");
-define("_CAL_AM", "上午");
-define("_CAL_AM_CAPS", "上午");
-define("_CAL_PM", "下午");
-define("_CAL_PM_CAPS", "下午");
-define("_CAL_TIME", "时间");
-define("_CAL_WK", "周"); // shorten of week-end
+define("_CAL_AM", "am");
+define("_CAL_AM_CAPS", "AM");
+define("_CAL_PM", "pm");
+define("_CAL_PM_CAPS", "PM");
+define("_CAL_TIME", "Time");
+define("_CAL_WK", "wk"); // shorten of week-end
 // This may be local-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 define("_CAL_WEEKEND", "0,6");
-define("_CAL_DSPFIRST", "首先显示 %s ");
+define("_CAL_DSPFIRST", "Display %s first");
 //This constants are for the jalali calendar included in the library
 define('_CAL_FARVARDIN','Farvardin');
 define('_CAL_ORDIBEHESHT','Ordibehest');
@@ -81,5 +81,5 @@ define('_CAL_ESFAND','Esfand');
 define("_CAL_NUMS_ARRAY", "'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'"); // numeric values can differ in different languages
 ############# added since 1.2 #############
 define("_CAL_SUFFIX", "th");
-define('_CAL_AM_LONG','上午');
-define('_CAL_PM_LONG','下午');
+define('_CAL_AM_LONG','before noon');
+define('_CAL_PM_LONG','after noon');
