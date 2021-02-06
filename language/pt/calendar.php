92c1aec8958d2c2cac4e64af24b9049952b1c3e2
David Janssens <fiammybe@gmail.com>
2021-02-06 05:03:10 +0100

    New translations calendar.php (Portuguese, Brazilian)

--- language/pt/calendar.php
+++ language/pt/calendar.php
@@ -1,70 +1,70 @@
 <?php
 //%%%%%		Time Zone	%%%%
-define("_CAL_SUNDAY", "Sunday");
-define("_CAL_MONDAY", "Monday");
-define("_CAL_TUESDAY", "Tuesday");
-define("_CAL_WEDNESDAY", "Wednesday");
-define("_CAL_THURSDAY", "Thursday");
-define("_CAL_FRIDAY", "Friday");
-define("_CAL_SATURDAY", "Saturday");
-define("_CAL_JANUARY", "January");
-define("_CAL_FEBRUARY", "February");
-define("_CAL_MARCH", "March");
-define("_CAL_APRIL", "April");
-define("_CAL_MAY", "May");
-define("_CAL_JUNE", "June");
-define("_CAL_JULY", "July");
-define("_CAL_AUGUST", "August");
-define("_CAL_SEPTEMBER", "September");
-define("_CAL_OCTOBER", "October");
-define("_CAL_NOVEMBER", "November");
-define("_CAL_DECEMBER", "December");
-define("_CAL_TGL1STD", "Toggle first day of week");
-define("_CAL_PREVYR", "Prev. year");
-define("_CAL_PREVMNTH", "Prev. month");
-define("_CAL_GOTODAY", "Go Today");
-define("_CAL_NXTMNTH", "Next month");
-define("_CAL_NEXTYR", "Next year");
-define("_CAL_SELDATE", "Select date");
-define("_CAL_DRAGMOVE", "Drag to move");
-define("_CAL_TODAY", "Today");
-define("_CAL_DISPM1ST", "Display Monday first");
-define("_CAL_DISPS1ST", "Display Sunday first");
+define("_CAL_SUNDAY", "Domingo");
+define("_CAL_MONDAY", "Segunda");
+define("_CAL_TUESDAY", "Terça");
+define("_CAL_WEDNESDAY", "Quarta");
+define("_CAL_THURSDAY", "Quinta");
+define("_CAL_FRIDAY", "Sexta");
+define("_CAL_SATURDAY", "Sábado");
+define("_CAL_JANUARY", "Janeiro");
+define("_CAL_FEBRUARY", "Fevereiro");
+define("_CAL_MARCH", "Março");
+define("_CAL_APRIL", "Abril");
+define("_CAL_MAY", "Maio");
+define("_CAL_JUNE", "Junho");
+define("_CAL_JULY", "Julho");
+define("_CAL_AUGUST", "Agosto");
+define("_CAL_SEPTEMBER", "Setembro");
+define("_CAL_OCTOBER", "Outubro");
+define("_CAL_NOVEMBER", "Novembro");
+define("_CAL_DECEMBER", "Dezembro");
+define("_CAL_TGL1STD", "Primeiro dia da semana");
+define("_CAL_PREVYR", "Ano (selecione no menu)");
+define("_CAL_PREVMNTH", "Mês (selecione no menu)");
+define("_CAL_GOTODAY", "Hoje");
+define("_CAL_NXTMNTH", "Próximo Mês (selecione no menu)");
+define("_CAL_NEXTYR", "Próximo Ano (selecione no menu)");
+define("_CAL_SELDATE", "Selecione a data");
+define("_CAL_DRAGMOVE", "Arraste para mover");
+define("_CAL_TODAY", "Hoje");
+define("_CAL_DISPM1ST", "Mostrar 'segunda' como o primeiro dia da semana");
+define("_CAL_DISPS1ST", "Mostrar 'domingo' como o primeiro dia da semana");
 
 ############# added since 1.1.2 #############
-define("_CAL_SUN", "Sun");
-define("_CAL_MON", "Mon");
-define("_CAL_TUE", "Tue");
-define("_CAL_WED", "Wed");
-define("_CAL_THU", "Thu");
-define("_CAL_FRI", "Fri");
-define("_CAL_SAT", "Sat");
+define("_CAL_SUN", "Dom");
+define("_CAL_MON", "Seg");
+define("_CAL_TUE", "Ter");
+define("_CAL_WED", "Qua");
+define("_CAL_THU", "Qui");
+define("_CAL_FRI", "Sex");
+define("_CAL_SAT", "Sab");
 // First day of the week. "0" means display Sunday first, "1" means display Monday first, etc...
 define("_CAL_FIRSTDAY", "1");
 define("_CAL_JAN", "Jan");
-define("_CAL_FEB", "Feb");
+define("_CAL_FEB", "Fev");
 define("_CAL_MAR", "Mar");
-define("_CAL_APR", "Apr");
+define("_CAL_APR", "Abr");
 define("_CAL_JUN", "Jun");
 define("_CAL_JUL", "Jul");
-define("_CAL_AUG", "Aug");
-define("_CAL_SEP", "Sept");
-define("_CAL_OCT", "Oct");
+define("_CAL_AUG", "Ago");
+define("_CAL_SEP", "Set");
+define("_CAL_OCT", "Out");
 define("_CAL_NOV", "Nov");
-define("_CAL_DEC", "Dec");
+define("_CAL_DEC", "Dez");
 // Direction of the calendar, ltr for left to right and rtl for roght to left (for Persian, Arabic, etc.)
 define("_CAL_DIRECTION", "ltr");
 define("_CAL_AM", "am");
 define("_CAL_AM_CAPS", "AM");
 define("_CAL_PM", "pm");
 define("_CAL_PM_CAPS", "PM");
-define("_CAL_TIME", "Time");
-define("_CAL_WK", "wk"); // shorten of week-end
+define("_CAL_TIME", "Hora");
+define("_CAL_WK", "Sem"); // shorten of week-end
 // This may be local-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 define("_CAL_WEEKEND", "0,6");
-define("_CAL_DSPFIRST", "Display %s first");
+define("_CAL_DSPFIRST", "Mostrar %s primeiro");
 //This constants are for the jalali calendar included in the library
 define('_CAL_FARVARDIN','Farvardin');
 define('_CAL_ORDIBEHESHT','Ordibehest');
