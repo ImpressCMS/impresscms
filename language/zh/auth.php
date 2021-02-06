e1c0205cf8877560d86518ef47babb60b5609243
David Janssens <fiammybe@gmail.com>
2021-02-06 05:05:09 +0100

    New translations auth.php (Chinese Traditional)

--- language/zh/auth.php
+++ language/zh/auth.php
@@ -2,12 +2,12 @@
 
 //%%%%%%		File Name auth.php 		%%%%%
 
-define('_AUTH_MSG_AUTH_METHOD',"使用 %s 授权模式.");
-define('_AUTH_LDAP_EXTENSION_NOT_LOAD','PHP LDAP 扩展未加载(检查PHP配置文件php.ini)');
-define('_AUTH_LDAP_SERVER_NOT_FOUND',"不能连接到服务器");
-define('_AUTH_LDAP_USER_NOT_FOUND',"会员 %s 在服务器 (%s) %s 目录中未能找到");
-define('_AUTH_LDAP_CANT_READ_ENTRY',"不能读取 %s");
+define('_AUTH_MSG_AUTH_METHOD',"using %s authentication method.");
+define('_AUTH_LDAP_EXTENSION_NOT_LOAD','PHP LDAP extension not loaded (verify your PHP configuration file php.ini)');
+define('_AUTH_LDAP_SERVER_NOT_FOUND',"Can't connect to the server");
+define('_AUTH_LDAP_USER_NOT_FOUND',"Member %s not found in the directory server (%s) in %s");
+define('_AUTH_LDAP_CANT_READ_ENTRY',"Can't read entry %s");
 define('_AUTH_LDAP_XOOPS_USER_NOTFOUND',"Sorry no corresponding user information has been found in the ImpressCMS database for connection: %s <br />" .
 		"Please verify your user datas or set on the automatic provisionning");
-define('_AUTH_LDAP_START_TLS_FAILED',"打开TLS连接时失败");
+define('_AUTH_LDAP_START_TLS_FAILED',"Failed to open a TLS connection");
 
