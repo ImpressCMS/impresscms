f65d32e3a44d96c6615f471e81eff345876d70b0
David Janssens <fiammybe@gmail.com>
2021-02-06 03:02:57 +0100

    New translations admin.php (Chinese Simplified)

--- language/zh/admin.php
+++ language/zh/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 //%%%%%%	File Name  admin.php 	%%%%%
-define("_AD_NORIGHT","You don't have the right to access this area");
-define('_AD_LOGINADMIN', 'Logging you in..');
-define('_AD_INSTALLEDMODULES','Installed Modules');
+define("_AD_NORIGHT","权限不足");
+define('_AD_LOGINADMIN', '请登录..');
+define('_AD_INSTALLEDMODULES','安装模块');
