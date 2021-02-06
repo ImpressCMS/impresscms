026ea9155d642b5a80338f5b20af63110a270fc6
David Janssens <fiammybe@gmail.com>
2021-02-06 04:24:11 +0100

    New translations captcha.php (Chinese Simplified)

--- language/zh/captcha.php
+++ language/zh/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
-define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
+define("ICMS_CAPTCHA_CAPTION", "确认验证码");
+define("ICMS_CAPTCHA_INVALID_CODE", "无效验证码!");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "尝试次数过多!");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "最多尝试次数: %d");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "区分大小写");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "不区分大小写");
+define("ICMS_CAPTCHA_REFRESH", "点击图片刷新.");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
+define("ICMS_CAPTCHA_RULE_TEXT", "请输入验证码");
