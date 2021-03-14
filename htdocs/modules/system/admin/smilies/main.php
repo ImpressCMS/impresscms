3d17aa9af30f4508b388d144391ea622d61732b6
Steve Kenow <skenow@impresscms.org>
2021-03-13 18:39:20 -0600

    Standard input filter for submodule - smilies

--- htdocs/modules/system/admin/smilies/main.php
+++ htdocs/modules/system/admin/smilies/main.php
@@ -1,41 +1,41 @@
 <?php
 // $Id: main.php 12313 2013-09-15 21:14:35Z skenow $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
+// ------------------------------------------------------------------------ //
+// XOOPS - PHP Content Management System //
+// Copyright (c) 2000 XOOPS.org //
+// <http://www.xoops.org/> //
+// ------------------------------------------------------------------------ //
+// This program is free software; you can redistribute it and/or modify //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation; either version 2 of the License, or //
+// (at your option) any later version. //
+// //
+// You may not change or alter any portion of this comment or credits //
+// of supporting developers from this source code or any supporting //
+// source code which is considered copyrighted (c) material of the //
+// original comment or credit authors. //
+// //
+// This program is distributed in the hope that it will be useful, //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the //
+// GNU General Public License for more details. //
+// //
+// You should have received a copy of the GNU General Public License //
+// along with this program; if not, write to the Free Software //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+// ------------------------------------------------------------------------ //
+// Author: Kazumi Ono (AKA onokazu) //
 // URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
+// Project: The XOOPS Project //
 // ------------------------------------------------------------------------- //
 /**
  * Administration of smilies, main file
  *
- * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license		LICENSE.txt
- * @package		System
- * @subpackage	Smilies
- * @version		SVN: $Id: main.php 12313 2013-09-15 21:14:35Z skenow $
+ * @copyright http://www.impresscms.org/ The ImpressCMS Project
+ * @license LICENSE.txt
+ * @package System
+ * @subpackage Smilies
+ * @version SVN: $Id: main.php 12313 2013-09-15 21:14:35Z skenow $
  */
 if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
 	exit("Access Denied");
@@ -46,33 +46,47 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
  * (str) op
  *
  * POST variables
+ * (int) id
  * (str) op
  * (int|arr) smile_id
- * (arr) smile_display
+ * (arr|arr) smile_display
  * (arr) old_display
- * (str) xoops_upload_file
+ * (str|array) xoops_upload_file
  * (str) smile_code
  * (str) smile_desc
- * id
+ *
  */
 include_once ICMS_MODULES_PATH . "/system/admin/smilies/smilies.php";
 
-if (!empty($_POST)) foreach ($_POST as $k => $v) ${$k} = StopXSS($v);
-if (!empty($_GET)) foreach ($_GET as $k => $v) ${$k} = StopXSS($v);
-$op = (isset($_GET['op']))
-? trim(filter_input(INPUT_GET, 'op', FILTER_SANITIZE_STRING))
-	: ((isset($_POST['op']))
-			? trim(filter_input(INPUT_POST, 'op', FILTER_SANITIZE_STRING))
-		: 'SmilesAdmin');
+/* default values */
+$op = 'SmilesAdmin';
+$id = 0;
 
-switch($op) {
+$filter_get = array('id' => 'int');
+
+$filter_post = array('id' => 'int', 'smile_display' => 'int', 'smile_id' => 'int', 'old_display' => 'int');
+
+/* filter the user input */
+if (!empty($_GET)) {
+	// in places where strict mode is not used for checkVarArray, make sure filter_ vars are not overwritten
+	if (isset($_GET['filter_post'])) unset($_GET['filter_post']);
+	$clean_GET = icms_core_DataFilter::checkVarArray($_GET, $filter_get, false);
+	extract($clean_GET);
+}
+
+if (!empty($_POST)) {
+	$clean_POST = icms_core_DataFilter::checkVarArray($_POST, $filter_post, false);
+	extract($clean_POST);
+}
+
+switch ($op) {
 	case "SmilesUpdate":
 		if (!icms::$security->check()) {
 			redirect_header('admin.php?fct=smilies', 3, implode('<br />', icms::$security->getErrors()));
 		}
 		$count = (!empty($_POST['smile_id']) && is_array($_POST['smile_id'])) ? count($_POST['smile_id']) : 0;
-		$db =& icms_db_Factory::instance();
-		for ($i = 0; $i < $count; $i++) {
+		$db = &icms_db_Factory::instance();
+		for ($i = 0; $i < $count; $i++ ) {
 			$smile_id = (int) $_POST['smile_id'][$i];
 			if (empty($smile_id)) {
 				continue;
@@ -89,7 +103,7 @@ switch($op) {
 		if (!icms::$security->check()) {
 			redirect_header('admin.php?fct=smilies', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$db =& icms_db_Factory::instance();
+		$db = &icms_db_Factory::instance();
 		$uploader = new icms_file_MediaUploadHandler(ICMS_UPLOAD_PATH, array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/x-png'), 100000, 120, 120);
 		$uploader->setPrefix('smil');
 		if ($uploader->fetchMedia($_POST['xoops_upload_file'][0])) {
@@ -120,21 +134,19 @@ switch($op) {
 		break;
 
 	case "SmilesEdit":
-		$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 		if ($id > 0) {
 			SmilesEdit($id);
 		}
 		break;
 
 	case "SmilesSave":
-		$id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
 		if ($id <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=smilies', 3, implode('<br />', icms::$security->getErrors()));
 		}
 		$smile_code = icms_core_DataFilter::stripSlashesGPC($_POST['smile_code']);
 		$smile_desc = icms_core_DataFilter::stripSlashesGPC($_POST['smile_desc']);
 		$smile_display = (int) $_POST['smile_display'] > 0 ? 1 : 0;
-		$db =& icms_db_Factory::instance();
+		$db = &icms_db_Factory::instance();
 		if ($_FILES['smile_url']['name'] != "") {
 			$uploader = new icms_file_MediaUploadHandler(ICMS_UPLOAD_PATH, array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/x-png'), 100000, 120, 120);
 			$uploader->setPrefix('smil');
@@ -173,7 +185,6 @@ switch($op) {
 		break;
 
 	case "SmilesDel":
-		$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 		if ($id > 0) {
 			icms_cp_header();
 			icms_core_Message::confirm(array('fct' => 'smilies', 'op' => 'SmilesDelOk', 'id' => $id), 'admin.php', _AM_WAYSYWTDTS);
@@ -182,11 +193,10 @@ switch($op) {
 		break;
 
 	case "SmilesDelOk":
-		$id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
 		if ($id <= 0 | !icms::$security->check()) {
 			redirect_header('admin.php?fct=smilies', 3, implode('<br />', icms::$security->getErrors()));
 		}
-		$db =& icms_db_Factory::instance();
+		$db = &icms_db_Factory::instance();
 		$sql = sprintf("DELETE FROM %s WHERE id = '%u'", $db->prefix('smiles'), $id);
 		$db->query($sql);
 		redirect_header("admin.php?fct=smilies&amp;op=SmilesAdmin", 2, _AM_DBUPDATED);
