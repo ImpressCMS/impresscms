ae0c9df60267f5951b2f2a1883bd65d55f3646b3
Steve Kenow <skenow@impresscms.org>
2021-03-13 18:33:22 -0600

    Standard input filtering for submodule ratings

--- htdocs/modules/system/admin/rating/main.php
+++ htdocs/modules/system/admin/rating/main.php
@@ -2,21 +2,20 @@
 /**
  * ImpressCMS Ratings
  *
- * @copyright	The ImpressCMS Project http://www.impresscms.org/
- * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
- * @package		System
- * @subpackage	Ratings
- * @since		1.2
- * @todo		Complete this feature? You cannot add a rating and there are no ratings to modify
- * @version		SVN: $Id: main.php 11147 2011-03-30 14:12:26Z m0nty_ $
+ * @copyright The ImpressCMS Project http://www.impresscms.org/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package System
+ * @subpackage Ratings
+ * @since 1.2
+ * @todo Complete this feature? You cannot add a rating and there are no ratings to modify
+ * @version SVN: $Id: main.php 11147 2011-03-30 14:12:26Z m0nty_ $
  */
-
 if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
 	exit("Access Denied");
 }
 
 function editrating($showmenu = FALSE, $ratingid = 0) {
-	global $icms_rating_handler, $icmsAdminTpl;
+	global $icms_rating_handler, $icmsAdminTpl, $op, $changedField;
 
 	icms_cp_header();
 
@@ -32,16 +31,14 @@ function editrating($showmenu = FALSE, $ratingid = 0) {
 	} else {
 		$ratingObj->hideFieldFromForm(array('item', 'itemid', 'uid', 'date', 'rate'));
 
-		if (isset($_POST['op'])) {
-			$controller = new icms_ipf_Controller($icms_rating_handler);
-			$controller->postDataToObject($ratingObj);
+		$controller = new icms_ipf_Controller($icms_rating_handler);
+		$controller->postDataToObject($ratingObj);
 
-			if ($_POST['op'] == 'changedField') {
-				switch($_POST['changedField']) {
-					case 'dirname' :
-						$ratingObj->showFieldOnForm(array('item', 'itemid', 'uid', 'date', 'rate'));
-						break;
-				}
+		if ($op == 'changedField') {
+			switch ($changedField) {
+				case 'dirname':
+					$ratingObj->showFieldOnForm(array('item', 'itemid', 'uid', 'date', 'rate'));
+					break;
 			}
 		}
 
@@ -56,36 +53,57 @@ icms_loadLanguageFile('system', 'common');
 
 $icms_rating_handler = icms_getmodulehandler('rating');
 
-if (!empty($_POST)) foreach ($_POST as $k => $v) ${$k} = StopXSS($v);
-if (!empty($_GET)) foreach ($_GET as $k => $v) ${$k} = StopXSS($v);
-$op = (isset($_POST['op']))
-? trim(filter_input(INPUT_POST, 'op', FILTER_SANITIZE_STRING))
-	: ((isset($_GET['op']))
-			? trim(filter_input(INPUT_GET, 'op', FILTER_SANITIZE_STRING))
-		: '');
+/*
+ * GET variables
+ * (string) op
+ *
+ * POST variables
+ *
+ */
+
+/* default values */
+$op = '';
+
+$filter_get = array();
+
+$filter_post = array();
+
+/* filter the user input */
+if (!empty($_GET)) {
+	// in places where strict mode is not used for checkVarArray, make sure filter_ vars are not overwritten
+	if (isset($_GET['filter_post'])) unset($_GET['filter_post']);
+	$clean_GET = icms_core_DataFilter::checkVarArray($_GET, $filter_get, FALSE);
+	extract($clean_GET);
+}
+
+if (!empty($_POST)) {
+	$clean_POST = icms_core_DataFilter::checkVarArray($_POST, $filter_post, FALSE);
+	extract($clean_POST);
+}
 
 switch ($op) {
-	/*	case "mod":
-	 case "changedField";
-
-		$ratingid = isset($_GET['ratingid']) ? (int) ($_GET['ratingid']) : 0 ;
-
-		editrating(true, $ratingid);
-
-		break;
-
-		case "clone":
-
-		$ratingid = isset($_GET['ratingid']) ? (int) ($_GET['ratingid']) : 0 ;
-
-		editrating(true, $ratingid, true);
-		break;
-
-		case "addrating":
-		$controller = new icms_ipf_Controller($icms_rating_handler);
-		$controller->storeFromDefaultForm(_CO_ICMS_RATINGS_CREATED, _CO_ICMS_RATINGS_MODIFIED, ICMS_URL . '/modules/system/admin.php?fct=rating');
-		break;
-		*/
+	/*
+	 * case "mod":
+	 * case "changedField";
+	 *
+	 * $ratingid = isset($_GET['ratingid']) ? (int) ($_GET['ratingid']) : 0 ;
+	 *
+	 * editrating(true, $ratingid);
+	 *
+	 * break;
+	 *
+	 * case "clone":
+	 *
+	 * $ratingid = isset($_GET['ratingid']) ? (int) ($_GET['ratingid']) : 0 ;
+	 *
+	 * editrating(true, $ratingid, true);
+	 * break;
+	 *
+	 * case "addrating":
+	 * $controller = new icms_ipf_Controller($icms_rating_handler);
+	 * $controller->storeFromDefaultForm(_CO_ICMS_RATINGS_CREATED, _CO_ICMS_RATINGS_MODIFIED, ICMS_URL . '/modules/system/admin.php?fct=rating');
+	 * break;
+	 */
 	case "del":
 		$controller = new icms_ipf_Controller($icms_rating_handler);
 		$controller->handleObjectDeletion();
@@ -102,9 +120,9 @@ switch ($op) {
 		$objectTable->addColumn(new icms_ipf_view_Column('item', _GLOBAL_LEFT, false, 'getItemValue'));
 		$objectTable->addColumn(new icms_ipf_view_Column('date', 'center', 150));
 		$objectTable->addColumn(new icms_ipf_view_Column('rate', 'center', 60, 'getRateValue'));
-		//$objectTable->addIntroButton('addrating', 'admin.php?fct=rating&op=mod', _CO_ICMS_RATINGS_CREATE);
+		// $objectTable->addIntroButton('addrating', 'admin.php?fct=rating&op=mod', _CO_ICMS_RATINGS_CREATE);
 
-		//$objectTable->addQuickSearch(array('title', 'summary', 'description'));
+		// $objectTable->addQuickSearch(array('title', 'summary', 'description'));
 
 		$icmsAdminTpl->assign('icms_rating_table', $objectTable->fetch());
 
