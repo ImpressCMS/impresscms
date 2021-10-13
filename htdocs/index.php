e1a2d6bd16380b2a825a9af05754c891bd25525c
Raimondas Rimkevičius <mekdrop@gmail.com>
2021-10-13 18:47:33 +0300

    Better non installed icms detection (#1008)
    * removed old index.php
    
    * Improved non installed state handling + finally index.php doesn't contains anything from old Xoops code
    
    Co-authored-by: Raimondas Rimkevičius (aka MekDrop) <mekdrop@impresscms.org>

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,51 +1,14 @@
 <?php
 
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-
-/**
- * Site index aka home page.
- * redirects to installation, if ImpressCMS is not installed yet
- *
- * @copyright    http://www.xoops.org/ The XOOPS Project
- * @copyright    http://www.impresscms.org/ The ImpressCMS Project
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
- * @package    ImpressCMS\Core
- * @since    XOOPS
- * @author    skalpa <psk@psykaos.net>
- * @author    Sina Asghari(aka stranger) <pesian_stranger@users.sourceforge.net>
- */
-
-use Aura\Session\Session;
+use Aura\Session\SessionFactory;
 use GuzzleHttp\Psr7\ServerRequest;
 use ImpressCMS\Core\Controllers\DefaultController;
+use ImpressCMS\Core\Database\DatabaseConnection;
+use ImpressCMS\Core\Facades\Config;
 use League\Route\Http\Exception as HttpException;
 use League\Route\Router;
-use Aura\Session\SessionFactory;
-
-/** mainfile is required, if it doesn't exist - installation is needed */
+use Middlewares\BasePath;
+use Narrowspark\HttpEmitter\SapiEmitter;
 
 define('ICMS_PUBLIC_PATH', __DIR__);
 
@@ -54,11 +17,11 @@ if (is_dir('install') && strpos($_SERVER['REQUEST_URI'], '/install') === false) 
 	try {
 		include_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'mainfile.php';
 		/**
-		 * @var \icms_db_Connection $dbm
+		 * @var DatabaseConnection $dbm
 		 */
-		$dbm = \icms::getInstance()->get('db-connection-1');
-		$isInstalled = $dbm->fetchCol('SELECT COUNT(*) FROM `' . $dbm->prefix('users') . '`;') > 0;
-	} catch (\Exception $exception) {
+		$dbm = icms::getInstance()->get('db-connection-1');
+		$isInstalled = $dbm->fetchCol('SELECT 1 FROM `' . $dbm->prefix('users') . '` LIMIT 1;') > 0;
+	} catch (Throwable $exception) {
 		$isInstalled = false;
 	}
 	if (!$isInstalled) {
@@ -72,7 +35,7 @@ if (is_dir('install') && strpos($_SERVER['REQUEST_URI'], '/install') === false) 
 /**
  * @var Router $router
  */
-$router = \icms::getInstance()->get('router');
+$router = icms::getInstance()->get('router');
 
 $basePath = parse_url(
 	env('URL'),
@@ -80,13 +43,13 @@ $basePath = parse_url(
 );
 if ($basePath !== '/' && $basePath !== null) {
 	$router->middleware(
-		(new \Middlewares\BasePath($basePath))->fixLocation()
+		(new BasePath($basePath))->fixLocation()
 	);
 	if (substr($basePath, -1) !== '/') {
 		$basePath .= '/';
 	}
 	if ($basePath[0] !== '/') {
-		return '/'.$basePath;
+		return '/' . $basePath;
 	}
 	$request = ServerRequest::fromGlobals();
 	$uri = $request->getUri();
@@ -111,33 +74,38 @@ try {
 } catch (HttpException $httpException) {
 	try {
 		/**
-		 * @var icms_config_Handler $configHandler
+		 * @var Config $configHandler
 		 */
 		$configHandler = icms::handler('icms_config');
-		$mainConfig = $configHandler->getConfigsByCat(icms_config_Handler::CATEGORY_MAIN);
+		$mainConfig = $configHandler->getConfigsByCat(Config::CATEGORY_MAIN);
 	} catch (Exception $exception) {
 		$mainConfig = [];
 	}
 
 	$sessionName = ($mainConfig['use_mysession'] && $mainConfig['session_name']) ? $mainConfig['session_name'] : 'ICMSSESSION';
-	\icms::$session = (new SessionFactory())->newInstance(
+	icms::$session = (new SessionFactory())->newInstance(
 		$request->getCookieParams()
 	);
 
 	$defController = new DefaultController();
 	$_GET['e'] = $httpException->getStatusCode();
+	$serverRequest = new ServerRequest(
+		$request->getMethod(),
+		$request->getUri(),
+		$request->getHeaders(),
+		$request->getBody(),
+		$request->getProtocolVersion()
+	);
 	$response = $defController->getError(
-		(new ServerRequest(
-			$request->getMethod(),
-			$request->getUri(),
-			$request->getHeaders(),
-			$request->getBody(),
-			$request->getProtocolVersion()
-		))
+		$serverRequest
 			->withQueryParams($_GET)
 			->withParsedBody($_POST)
-			->withAttribute('session', \icms::$session)
+			->withAttribute('session', icms::$session)
 	);
 }
 
-\icms::getInstance()->get('sapi-emitter')->emit($response);
+/**
+ * @var SapiEmitter $sapiEmitter
+ */
+$sapiEmitter = icms::getInstance()->get('sapi-emitter');
+$sapiEmitter->emit($response);
