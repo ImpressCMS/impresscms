3477c3a4e929c1ec118afded36cc3e01c86401ab
David Janssens <fiammybe@gmail.com>
2021-11-02 13:27:28 +0100

    New translations auth.php (German)

--- language/de/auth.php
+++ language/de/auth.php
@@ -2,12 +2,12 @@
 
 //%%%%%%		File Name auth.php 		%%%%%
 
-define('_AUTH_MSG_AUTH_METHOD',"benutze %s Authentifizierungsmethode.");
-define('_AUTH_LDAP_EXTENSION_NOT_LOAD','PHP-LDAP-Erweiterung nicht geladen (überprüfen Sie Ihre PHP-Konfigurationsdatei php.ini)');
-define('_AUTH_LDAP_SERVER_NOT_FOUND',"Keine Verbindung zum Server möglich");
-define('_AUTH_LDAP_USER_NOT_FOUND',"Mitglied %s wurde nicht auf dem Verzeichnisserver (%s) in %s gefunden");
-define('_AUTH_LDAP_CANT_READ_ENTRY',"Konnte Eintrag %s nicht lesen");
+define('_AUTH_MSG_AUTH_METHOD',"benutze %s Authentifizierungsmethode. ");
+define('_AUTH_LDAP_EXTENSION_NOT_LOAD','PHP-LDAP-Erweiterung nicht geladen (überprüfen Sie Ihre PHP-Konfigurationsdatei php.ini). ');
+define('_AUTH_LDAP_SERVER_NOT_FOUND',"Keine Verbindung zum Server möglich. ");
+define('_AUTH_LDAP_USER_NOT_FOUND',"Mitglied %s wurde nicht auf dem Verzeichnisserver (%s) in %s gefunden. ");
+define('_AUTH_LDAP_CANT_READ_ENTRY',"Konnte Eintrag %s nicht lesen. ");
 define('_AUTH_LDAP_XOOPS_USER_NOTFOUND',"Sorry no corresponding user information has been found in the ImpressCMS database for connection: %s <br />" .
 		"Please verify your user datas or set on the automatic provisionning");
-define('_AUTH_LDAP_START_TLS_FAILED',"Fehler beim Öffnen einer TLS-Verbindung");
+define('_AUTH_LDAP_START_TLS_FAILED',"Fehler beim Öffnen einer TLS-Verbindung. ");
 
