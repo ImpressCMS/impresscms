72e66376e29b7e53b21881d4ae2a4e967562d03b
David Janssens <fiammybe@gmail.com>
2021-11-02 12:51:34 +0100

    New translations finish.php (German)

--- htdocs/install/language/de/finish.php
+++ htdocs/install/language/de/finish.php
@@ -1,20 +1,17 @@
 <?php
 // $Id: finish.php 12168 2013-05-22 13:25:59Z skenow $
 $content .=
-"<h3>Unterstützung-Hilfe</h3>
+"<h3>Ondersteuning</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>ImpressCMS.org - Project Homepage</a></li>
-<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>- Nutzen Sie das Forum und die Anleitungen</a></li>
-<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>- Modules, Themes und Erweiterungen</a></li>
-<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+<li><a href='https://www.impresscms.org/' target='_blank'>Project Startpagina</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Ondersteuning</a></li>
+<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Thema's</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Documentatie</a></li>
 </ul>
 
-<h3>Newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, für Neuigkeiten über neue Module, Themen, wichtige Updates und Vieles mehr!</p>
+<h3>Nieuwsbrief</h3>
+<p>Schrijf je nu in op onze <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Nieuwsbrief</a>, om maandelijks het laatste nieuws te ontvangen over thema's, modules en belangrijke updates!</p>
 
-<h3>Hosting Sponsor</h3>
-<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> bietet Hosting für die ImpressCMS-Websites und auf Ihre Bedürfnisse zugeschnittene Umgebungen für ImpressCMS</p>
-
-<h3>Deine Webseite</h3>
-<p>Sie können jetzt auf die Startseite Ihrer Site zugreifen, indem Sie auf die Schaltfläche unten klicken und den Assistenten beenden.</p>
+<h3>Ihre Webseite</h3>
+<p>Je kan nu naar de startpagina van je site, door te klikken op de knop hieronder en de installatie te voltooien.</p>
 ";
