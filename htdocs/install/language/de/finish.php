08bb24a2aa5493b2cf59fda4be3ab525386c64e8
David Janssens <fiammybe@gmail.com>
2021-11-02 10:15:20 +0100

    New translations finish.php (German)

--- htdocs/install/language/de/finish.php
+++ htdocs/install/language/de/finish.php
@@ -1,4 +1,20 @@
 <?php
 // $Id: finish.php 12168 2013-05-22 13:25:59Z skenow $
 $content .=
-"Unterstützung";
+"<h3>Unterstützung-Hilfe</h3>
+<ul class=\"diags\">
+<li><a href='https://www.impresscms.org/' target='_blank'>ImpressCMS.org - Project Homepage</a></li>
+<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>- Nutzen Sie das Forum und die Anleitungen</a></li>
+<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>- Modules, Themes und Erweiterungen</a></li>
+<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>
+</ul>
+
+<h3>Newsletter</h3>
+<p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, für Neuigkeiten über neue Module, Themen, wichtige Updates und Vieles mehr!</p>
+
+<h3>Hosting Sponsor</h3>
+<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> bietet Hosting für die ImpressCMS-Websites und auf Ihre Bedürfnisse zugeschnittene Umgebungen für ImpressCMS</p>
+
+<h3>Deine Webseite</h3>
+<p>Sie können jetzt auf die Startseite Ihrer Site zugreifen, indem Sie auf die Schaltfläche unten klicken und den Assistenten beenden.</p>
+";
