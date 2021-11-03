1057309d847bf08c3325fa32f87f12565cf2e46a
David Janssens <fiammybe@gmail.com>
2021-11-03 09:47:44 +0100

    New translations timezone.php (German)

--- language/de/timezone.php
+++ language/de/timezone.php
@@ -1,8 +1,8 @@
 <?php
 
 //%%%%%		Time Zone	%%%%
-define("_TZ_GMTM12", "(GMT-12:00) Eniwetok, Kwajalein");
-define("_TZ_GMTM11", "(GMT-11:00) Midway Island, Samoa");
+define("_TZ_GMTM12", "(GMT-12:00) Eniwetok, Kwajalein ");
+define("_TZ_GMTM11", "(GMT-11:00) Midway Island, Samoa ");
 define("_TZ_GMTM10", "(GMT-10:00) Hawaii");
 define("_TZ_GMTM9", "(GMT-9:00) Alaska");
 define("_TZ_GMTM8", "(GMT-8:00) Pazifische Zeit (US &amp; Kanada)");
@@ -13,21 +13,21 @@ define("_TZ_GMTM4", "(GMT-4:00) Atlantic Time (Kanada), Caracas, La Paz");
 define("_TZ_GMTM35", "(GMT-3:30) Neufundland");
 define("_TZ_GMTM3", "(GMT-3:00) Brasilia, Buenos Aires, Georgetown");
 define("_TZ_GMTM2", "(GMT-2:00) Mittelatlantik");
-define("_TZ_GMTM1", "(GMT-1:00) Azoren, Kapverdeninseln");
+define("_TZ_GMTM1", "(GMT-1:00) Azoren, Kapverdeninseln ");
 define("_TZ_GMT0", "(GMT) Greenwich Mean Time, London, Dublin, Lissabon, Casablanca, Monrovia");
 define("_TZ_GMTP1", "(GMT+1:00) Amsterdam, Berlin, Rom, Kopenhagen, Brüssel, Madrid, Paris");
 define("_TZ_GMTP2", "(GMT+2:00) Athen, Istanbul, Minsk, Helsinki, Jerusalem, Südafrika");
 define("_TZ_GMTP3", "(GMT+3:00) Baghdad, Kuwait, Riyadh, Moscow, St. Petersburg");
 define("_TZ_GMTP35", "(GMT+3:30) Teheran");
-define("_TZ_GMTP4", "(GMT+4:00) Abu Dhabi, Muscat, Baku, Tbilisi");
+define("_TZ_GMTP4", "(GMT+4:00) Abu  Dhabi, Muscat, Baku, Tbilisi");
 define("_TZ_GMTP45", "(GMT+4:30) Kabul");
-define("_TZ_GMTP5", "(GMT+5:00) Ekaterinburg, Islamabad, Karachi, Tashkent");
-define("_TZ_GMTP55", "(GMT+5:30) Mumbai, Kolkata, Chennai, New Delhi");
+define("_TZ_GMTP5", "(GMT+5:00) Jekaterinburg, Islamabad, Karachi, Tashkent");
+define("_TZ_GMTP55", "(GMT+5:30) Mumbai,Kalkutta, Chennai, New Delhi");
 define("_TZ_GMTP6", "(GMT+6:00) Almaty, Dhaka, Colombo");
 define("_TZ_GMTP7", "(GMT+7:00) Bangkok, Hanoi, Jakarta");
-define("_TZ_GMTP8", "(GMT+8:00) Peking, Perth, Singapur, Hongkong, Ürümqi, Taipei");
+define("_TZ_GMTP8", "(GMT+8:00) Peking, Perth, Singapur, Hongkong, Urumtschi, Taipei");
 define("_TZ_GMTP9", "(GMT+9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk");
 define("_TZ_GMTP95", "(GMT+9:30) Adelaide, Darwin");
-define("_TZ_GMTP10", "(GMT+10:00) Brisbane, Canberra, Melbourne, Sydney, Guam, Vlasdiostok");
-define("_TZ_GMTP11", "(GMT+11:00) Magadan, Salomonen, Neukaledonien");
+define("_TZ_GMTP10", "(GMT+10:00) Brisbane, Canberra, Melbourne, Sydney, Guam, Wladiwostok");
+define("_TZ_GMTP11", "(GMT+11:00)  Magadan, Salomonen, Neukaledonien");
 define("_TZ_GMTP12", "(GMT+12:00) Auckland, Wellington, Fidschi, Kamtschatka, Marshall-Insel");
