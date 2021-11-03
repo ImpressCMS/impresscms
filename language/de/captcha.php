5868137a6d73d0c9eca8ae58f7707d97085415ed
David Janssens <fiammybe@gmail.com>
2021-11-03 08:24:58 +0100

    New translations captcha.php (German)

--- language/de/captcha.php
+++ language/de/captcha.php
@@ -1,13 +1,13 @@
 <?php
-define("ICMS_CAPTCHA_CAPTION", "Confirmation Code");
-define("ICMS_CAPTCHA_INVALID_CODE", "Invalid confirmation code!");
-define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Too many attempts!");
-define("ICMS_CAPTCHA_MAXATTEMPTS", "Maximum attempts: %d");
+define("ICMS_CAPTCHA_CAPTION", "Visuelle Verifizierung");
+define("ICMS_CAPTCHA_INVALID_CODE", "falsche Verifizierung");
+define("ICMS_CAPTCHA_TOOMANYATTEMPTS", "Zu viele Versuche! Bitte versuchen Sie es zu einem spÃ¤teren Zeitpunkt erneut, oder wenden Sie sich bitte an den Webmaster.");
+define("ICMS_CAPTCHA_MAXATTEMPTS", "(max. %d Versuche)");
 
 // For image mode
-define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "The code is case-sensitive");
-define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "The code is case-insensitive");
-define("ICMS_CAPTCHA_REFRESH", "Click the image for a new code.");
+define("ICMS_CAPTCHA_RULE_CASESENSITIVE", "Groß und Kleinschreibung beachten");
+define("ICMS_CAPTCHA_RULE_CASEINSENSITIVE", "Groß und Kleinschreibung nicht beachten");
+define("ICMS_CAPTCHA_REFRESH", "Neue Verifizierung anfordern");
 
 // For text mode
-define("ICMS_CAPTCHA_RULE_TEXT", "Input the result from the expression");
+define("ICMS_CAPTCHA_RULE_TEXT", "Geben Sie die Zeichen aus der Verifizierung ein");
