ddca94aa85edcebf3bc998c1460b8280ebf35d6f
David Janssens <fiammybe@gmail.com>
2021-11-03 11:06:22 +0100

    New translations finish.php (German)

--- htdocs/install/language/de/finish.php
+++ htdocs/install/language/de/finish.php
@@ -13,7 +13,8 @@ $content .=
 <p>Please remember to subscribe to the <a href='http://impresscms.us1.list-manage.com/subscribe?u=55aacd489c3081ba60a7b5c09&id=77e43dd1a9' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
 
 <h3>Hosting Sponsor</h3>
-<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> provides hosting for the ImpressCMS websites and offers environments tailored to your needs and ImpressCMS</p>
+<p><a href='http://www.siteground.com/impresscms-hosting.htm?afcode=7e9aa639d30265c079823a498f5b8f15'>SiteGround</a> provides hosting for the ImpressCMS websites and offers environments tailored to your needs and ImpressCMS </p>
 
 <h3>Your site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>";
+<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
+";
