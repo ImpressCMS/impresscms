b3296cbdcc803f94a794972071ffad3f39b7831a
David Janssens <fiammybe@gmail.com>
2021-11-03 11:06:27 +0100

    New translations uploader.php (German)

--- language/de/uploader.php
+++ language/de/uploader.php
@@ -1,15 +1,15 @@
 <?php
-define("_ER_UP_MIMETYPELOAD", "Fehler beim Laden des Mime-Types ( Dateityp)");
+define("_ER_UP_MIMETYPELOAD", "Fehler beim Laden des Mime-Types - Dateityp");
 define("_ER_UP_FILENOTFOUND", "Datei nicht gefunden");
 define("_ER_UP_INVALIDFILESIZE", "Ungültige Dateigröße");
 define("_ER_UP_FILENAMEEMPTY", "Dateiname ist leer");
 define("_ER_UP_NOFILEUPLOADED", "Keine Datei hochgeladen");
-define("_ER_UP_ERROROCCURRED", "Ein Fehler ist aufgetreten: Fehler Nr.%s");
-define("_ER_UP_UPLOADDIRNOTSET", "Upload-Verzeichnis nicht gesetzt");
-define("_ER_UP_FAILEDOPENDIR", "Fehler beim Öffnen des Verzeichnises: %s");
+define("_ER_UP_ERROROCCURRED", "Ein Fehler ist aufgetreten: Fehler #%s");
+define("_ER_UP_UPLOADDIRNOTSET", "Upload - Verzeichnis nicht gesetzt");
+define("_ER_UP_FAILEDOPENDIR", "Fehler beim Öffnen des Verzeichnisses: %s");
 define("_ER_UP_FAILEDOPENDIRWRITE", "Fehler beim Öffnen des Verzeichnisses mit Schreibrechten: %s");
 define("_ER_UP_FILESIZETOOLARGE", "Dateigröße zu groß (maximal %u Bytes): %u Bytes");
-define("_ER_UP_FILEWIDTHTOOLARGE", "Dateibreite zu groß (Maximal %u px): %u px");
+define("_ER_UP_FILEWIDTHTOOLARGE", "Dateibreite zu groß (Maximum %u px): %u px");
 define("_ER_UP_FILEHEIGHTTOOLARGE", "Dateigröße zu groß (Maximum %u px): %u px");
 define("_ER_UP_MIMETYPENOTALLOWED", "MIME Typ nicht zulässig: %s");
 define("_ER_UP_FAILEDFETCHIMAGESIZE", "Fehler beim ermitteln der Bildgröße der Datei %s, überspringe Überprüfung.");
@@ -20,6 +20,6 @@ define("_ER_UP_SUSPICIOUSREFUSED", "Das Hochladen wurde abgebrochen, bitte wende
 define("_ER_UP_INVALIDFILENAME", "Ungültiger Dateiname");
 define("_ER_UP_FAILEDSAVEFILE", "Fehler beim Speichern der Datei in %s");
 // added in 1.3
-define("_ER_UP_PARTIALLY", "Die hochgeladene Datei wurde nur teilweise hochgeladen.");
+define("_ER_UP_PARTIALLY", "Die Datei wurde nur teilweise hochgeladen.");
 define("_ER_UP_NO_TMP_DIR", "Es wurde kein temporäres Verzeichnis gefunden. Bitte kontaktieren Sie den Administrator. Vielen Dank.");
 define("_ER_UP_CANT_WRITE", "Datei konnte nicht auf die Festplatte geschrieben werden. Bitte kontaktieren Sie den Administrator. Vielen Dank.");
