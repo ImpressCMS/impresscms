8ab091b524382e1c85c3ce8c4671c15308be915a
David Janssens <fiammybe@gmail.com>
2021-11-03 09:24:08 +0100

    New translations misc.php (German)

--- language/de/misc.php
+++ language/de/misc.php
@@ -7,17 +7,17 @@ define("_MSC_FRIENDEMAILC","E-Mail Adresse Ihres Freundes: ");
 define("_MSC_RECOMMENDSITE","Empfehlen Sie diese Website einem Freund");
 // %s is your site name
 define("_MSC_INTSITE","Interessante Webseite: %s");
-define("_MSC_REFERENCESENT","The reference to our site has been sent to your friend. Thanks!");
-define("_MSC_ENTERYNAME","Please enter your name");
-define("_MSC_ENTERFNAME","Please enter your friend's name");
-define("_MSC_ENTERFMAIL","Please enter your friend's email address");
-define("_MSC_NEEDINFO","You need to enter required info!");
-define("_MSC_INVALIDEMAIL1","The email address you provided is not a valid address.");
-define("_MSC_INVALIDEMAIL2","Please check the address and try again.");
+define("_MSC_REFERENCESENT","Der Verweis auf unsere Website wurde an Ihren Freund gesendet. Vielen Dank!");
+define("_MSC_ENTERYNAME","Bitte geben Sie Ihren Namen ein");
+define("_MSC_ENTERFNAME","Bitte geben Sie den Namen Ihres Freundes ein");
+define("_MSC_ENTERFMAIL","Bitte geben Sie die E-Mail-Adresse Ihres Freundes ein");
+define("_MSC_NEEDINFO","Sie müssen die erforderlichen Informationen eingeben!");
+define("_MSC_INVALIDEMAIL1","Die angegebene E-Mail-Adresse ist keine gültige Adresse.");
+define("_MSC_INVALIDEMAIL2","Bitte überprüfen Sie die Adresse und versuchen Sie es erneut.");
 
-define("_MSC_AVAVATARS","Available Avatars");
+define("_MSC_AVAVATARS","Verfügbare Avatare");
 
 define("_MSC_SMILIES","Smilies");
-define("_MSC_CLICKASMILIE","Click a smilie to insert it into your message.");
+define("_MSC_CLICKASMILIE","Klicken Sie auf einen Smiley, um ihn in Ihre Nachricht einzufügen.");
 define("_MSC_CODE","Code");
-define("_MSC_EMOTION","Emotion");
+define("_MSC_EMOTION","Emoticon");
