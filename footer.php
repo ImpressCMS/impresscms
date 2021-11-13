9388597550ae6019e35ae9a537e0d23b084e4451
Raimondas Rimkevičius <mekdrop@impresscms.org>
2021-11-14 00:32:07 +0200

    Fixed namespaces issues for never PHP in footer.php and header.php

--- footer.php
+++ footer.php
@@ -2,17 +2,19 @@
 /**
  * This file is used as footer for all places where content is generated not in object way
  *
- * @copyright	http://www.impresscms.org/ The ImpressCMS Project
+ * @copyright    http://www.impresscms.org/ The ImpressCMS Project
  * @package     ImpressCMS/Core
  *
  * @todo        Remove this file in the future
  */
 
+use Aura\Session\Session;
+
 $_SESSION['ad_sess_regen'] = false;
 /**
- * @var \Aura\Session\Session $session
+ * @var Session $session
  */
-$session = \icms::$session;
+$session = icms::$session;
 if (isset($_SESSION['sess_regen']) && $_SESSION['sess_regen']) {
 	$session->start();
 	$session->regenerateId();
@@ -21,14 +23,14 @@ if (isset($_SESSION['sess_regen']) && $_SESSION['sess_regen']) {
 }
 
 // ################# Preload Trigger beforeFooter ##############
-\icms::$preload->triggerEvent('beforeFooter');
+icms::$preload->triggerEvent('beforeFooter');
 
-\icms::$logger->stopTime('Module display');
+icms::$logger->stopTime('Module display');
 
 global $xoopsOption;
 
 $response = $xoopsOption['response']->getBody();
 
-\icms::$logger->stopTime();
+icms::$logger->stopTime();
 
 echo $response;
