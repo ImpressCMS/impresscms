4852a3bcc5dba07613f3283f4e6186ced89fa197
David Janssens <david.j@impresscms.org>
2021-11-29 15:31:46 +0100

    add CKEditor 4.17.1 (#1095)

---
+++ htdocs/editors/CKeditor/xoops_version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* CKEditor version file (legacy)
+*
+* @deprecated
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		LICENSE.txt
+* @package		Editors
+* @subpackage	CKEditor
+* @author		modified by MrTheme <mrtheme@impresscms.org>, Steve K <skenow@impresscms.org>
+* @version		$Id$
+*/
+
+require dirname(__FILE__) . '/icms_version.php';
