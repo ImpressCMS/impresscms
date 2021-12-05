d84c124a741971ae15396dce6864a0199eb65470
David Janssens <david.j@impresscms.org>
2021-12-05 07:47:51 +0100

    Preparations for the 1.4.3 RC release (#1099)
    * Update version.php
    
    * Update icms_version.php
    
    change version number
    
    * Delete .tx directory
    
    We are no longer using Transifex,
    
    * Adding version number in README.md
    
    * Update and rename readme.txt to UPGRADE.md
    
    * FCKEditor is no longer included. Removing Extra
    
    * Update readme.txt
    
    update wiki url, remove old download url

--- htdocs/modules/system/icms_version.php
+++ htdocs/modules/system/icms_version.php
@@ -46,7 +46,7 @@ defined("ICMS_ROOT_PATH") || die("ICMS root path not defined");
  */
 $modversion = array (
 		'name' => _MI_SYSTEM_NAME,
-		'version' => '1.4.2',
+		'version' => '1.4.3 RC',
 		'description' => _MI_SYSTEM_DESC,
 		'author' => "fiammybe",
 		'credits' => "The ImpressCMS Project",
@@ -64,11 +64,11 @@ $modversion = array (
 		'image' => "images/system_slogo.png", /* for backward compatibility */
 
 /**  Development information */
-	'status_version' => "Final",
-		'status' => "Final",
-		'date' => "24 Dec 2020",
+	'status_version' => "RC",
+		'status' => "RC",
+		'date' => "04 Dec 2021",
 		'author_word' => "",
-		'warning' => _CO_ICMS_WARNING_FINAL,
+		'warning' => _CO_ICMS_WARNING_RC,
 
 		/**
 		 * Contributors
