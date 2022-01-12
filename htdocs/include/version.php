6842a59260933ba207d38b6ad70fb5555c292399
David Janssens <david.j@impresscms.org>
2022-01-12 09:57:01 +0100

    update version to 1.4.3 RC2
    build = 102

--- htdocs/include/version.php
+++ htdocs/include/version.php
@@ -10,7 +10,7 @@
  * @version		$Id: version.php 12500 2015-06-15 20:03:56Z fiammy $
  */
 
-define('ICMS_VERSION_NAME', 'ImpressCMS 1.4.3 RC');
+define('ICMS_VERSION_NAME', 'ImpressCMS 1.4.3 RC2');
 
 // For backward compatibility with XOOPS
 define('XOOPS_VERSION', ICMS_VERSION_NAME);
@@ -32,7 +32,7 @@ define('ICMS_VERSION_STATUS', 3);
  */
 // impresscms_1.3.10 = 82, 2.0.0 alpha 4 = 73, 1.4,1 beta : 94, 1.4.1 RC = 98
 
-define('ICMS_VERSION_BUILD', 101);
+define('ICMS_VERSION_BUILD', 102);
 
 /**
  * Latest dbversion of the System Module
