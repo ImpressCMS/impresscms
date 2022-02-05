3d2e2921a0be2d49d1acd6c459a9f975fe47f5ce
Raimondas Rimkevičius <github@mekdrop.name>
2022-02-06 01:41:14 +0200

    Console commands now registered from CommandsProvider and not composer.json (#1126)
    Co-authored-by: David Janssens <david.j@impresscms.org>

---
+++ core/Providers/CommandsProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace ImpressCMS\Core\Providers;
+
+use ImpressCMS\Core\Commands\CacheClearCommand;
+use ImpressCMS\Core\Commands\GenerateAppKeyCommand;
+use ImpressCMS\Core\Commands\ModuleInstallCommand;
+use ImpressCMS\Core\Commands\ModuleUninstallCommand;
+use ImpressCMS\Core\Commands\ModuleUpdateCommand;
+use ImpressCMS\Core\Commands\TemplatesCacheClearCommand;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use Sunrise\Http\Router\Command\RouteListCommand;
+
+/**
+ * Provides system console commands
+ */
+class CommandsProvider extends AbstractServiceProvider
+{
+	public const CONSOLE_COMMAND_TAG = "console.command";
+
+	/**
+	 * @inheritdoc
+	 */
+	protected $provides = [
+		self::CONSOLE_COMMAND_TAG,
+	];
+
+    /**
+     * @inheritDoc
+     */
+    public function register()
+    {
+		$this->leagueContainer
+			->add(GenerateAppKeyCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(CacheClearCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(RouteListCommand::class)
+			->addArgument('router')
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(TemplatesCacheClearCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(ModuleInstallCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(ModuleUpdateCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+		$this->leagueContainer
+			->add(ModuleUninstallCommand::class)
+			->addTag(self::CONSOLE_COMMAND_TAG)
+		;
+    }
+}
