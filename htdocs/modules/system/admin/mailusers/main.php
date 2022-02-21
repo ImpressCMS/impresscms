beadb4c26492eb7a0538a90d2d9c48aa90276f34
Steve Kenow <skenow@impresscms.org>
2022-02-21 15:51:44 -0600

    Additional input filtering - mailusers, findusers, checkVarArray inner elements (#1163)
    * Applying the filters to inner elements of arrays
    
    Since the keys were not defined, the method was applying the default
    string filter.
    
    * Adjusting filter_input parameters and applying to ordersel too
    
    * Adding input filters instead of defaults
    
    * Input filtering for the admin mailusers submodule
    
    * Changing filter method for admin pages submodule for consistency
    
    * Moving the authentication test to the top of the file
    
    * Changing filter methods for consistency.
    
    Replaced StopXSS with core datafilter
    
    * Making sure the session is valid before sending
    
    * The security check causes failure for valid mailing - reverting
    
    * This should do it - send succeeds within the session, is rejected if not

--- htdocs/modules/system/admin/mailusers/main.php
+++ htdocs/modules/system/admin/mailusers/main.php
@@ -38,6 +38,9 @@
  * @version		SVN: $Id: main.php 12313 2013-09-15 21:14:35Z skenow $
  * @todo	scrub the input arrays (GET and POST)
  */
+if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
+	exit("Access Denied");
+}
 
 /*
  * GET variables
@@ -54,23 +57,58 @@
  * (int) mail_idle_less
  * (int) mail_regd_min
  * (int) mail_regd_max
- * (array) mail_to_group
- * (array) mail_to_user
+ * (array - int) mail_to_group
+ * (array - int) mail_to_user
  * (int) mail_start
  * (str) mail_fromname
  * (str) mail_subject
  * (str) mail_body
  *
  */
-if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin($icmsModule->getVar('mid'))) {
-	exit("Access Denied");
-} else {
-	$op = "form";
-	$limit = 100;
 
-	if (!empty($_POST['op']) && $_POST['op'] == "send") {
-		$op =  $_POST['op'];
-	}
+/* default values */
+$op = "form";
+$mail_send_to = $mail_fromname = $mail_subject = $mail_body = '';
+$mail_inactive = $mail_mailok = $mail_lastlog_min = $mail_lastlog_max = 0;
+$mail_idle_more = $mail_idle_less = $mail_regd_min = $mail_regd_max = 0;
+$mail_to_group = $mail_to_user = $mail_start = 0;
+
+/* Set filters */
+$filter_get = array();
+
+$filter_post = array(
+	'op' => 'str',
+	'mail_send_to' => 'str',
+	'mail_inactive' => 'int',
+	'mail_mailok' => 'int',
+	'mail_lastlog_min' => 'int',
+	'mail_lastlog_max' => 'int',
+	'mail_idle_more' => 'int',
+	'mail_idle_less' => 'int',
+	'mail_regd_min' => 'int',
+	'mail_regd_max' => 'int',
+	'mail_to_group' => 'int',
+	'mail_to_user' => 'int',
+	'mail_start' => 'int',
+	'mail_fromname' => 'str',
+	'mail_subject' => 'str',
+	'mail_body' => 'str'
+);
+
+/* filter the user input */
+if (!empty($_GET)) {
+	// in places where strict mode is not used for checkVarArray, make sure filter_ vars are not overwritten
+	if (isset($_GET['filter_post'])) unset($_GET['filter_post']);
+	$clean_GET = icms_core_DataFilter::checkVarArray($_GET, $filter_get, false);
+	extract($clean_GET);
+}
+
+if (!empty($_POST)) {
+	$clean_POST = icms_core_DataFilter::checkVarArray($_POST, $filter_post, false);
+	extract($clean_POST);
+}
+
+	$limit = 100;
 
 	if (!icms::$security->check() || $op == "form") {
 		icms_cp_header();
@@ -82,43 +120,41 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 		include ICMS_MODULES_PATH . "/system/admin/mailusers/mailform.php";
 		$form->display();
 		icms_cp_footer();
-	}
-
-	if ($op == "send" && !empty($_POST['mail_send_to'])) {
+	} elseif ($op == "send" && !empty($mail_send_to)) {
 		$added = array();
 		$added_id = array();
 		$criteria = array();
 		$count_criteria = 0; // user count via criteria;
-		if (!empty($_POST['mail_inactive'])) {
+		if (!empty($mail_inactive)) {
 			$criteria[] = "level = 0";
 		} else {
-			if (!empty($_POST['mail_mailok'])) {
+			if (!empty($mail_mailok)) {
 				$criteria[] = 'user_mailok = 1';
 			}
-			if (!empty($_POST['mail_lastlog_min'])) {
-				$f_mail_lastlog_min = trim($_POST['mail_lastlog_min']);
+			if (!empty($mail_lastlog_min)) {
+				$f_mail_lastlog_min = trim($mail_lastlog_min);
 				$time = mktime(0, 0, 0, substr($f_mail_lastlog_min, 5, 2), substr($f_mail_lastlog_min, 8, 2), substr($f_mail_lastlog_min, 0, 4));
 				if ($time > 0) {
 					$criteria[] = "last_login > $time";
 				}
 			}
-			if (!empty($_POST['mail_lastlog_max'])) {
-				$f_mail_lastlog_max = trim($_POST['mail_lastlog_max']);
+			if (!empty($mail_lastlog_max)) {
+				$f_mail_lastlog_max = trim($mail_lastlog_max);
 				$time = mktime(0, 0, 0, substr($f_mail_lastlog_max, 5, 2), substr($f_mail_lastlog_max, 8, 2), substr($f_mail_lastlog_max, 0, 4));
 				if ($time > 0) {
 					$criteria[] = "last_login < $time";
 				}
 			}
-			if (!empty($_POST['mail_idle_more']) && is_numeric($_POST['mail_idle_more'])) {
-				$f_mail_idle_more = (int) ($_POST['mail_idle_more']);
+			if (!empty($mail_idle_more)) {
+				$f_mail_idle_more = (int) ($mail_idle_more);
 				$time = 60 * 60 * 24 * $f_mail_idle_more;
 				$time = time() - $time;
 				if ($time > 0) {
 					$criteria[] = "last_login < $time";
 				}
 			}
-			if (!empty($_POST['mail_idle_less']) && is_numeric($_POST['mail_idle_less'])) {
-				$f_mail_idle_less = (int) ($_POST['mail_idle_less']);
+			if (!empty($mail_idle_less)) {
+				$f_mail_idle_less = $mail_idle_less;
 				$time = 60 * 60 * 24 * $f_mail_idle_less;
 				$time = time() - $time;
 				if ($time > 0) {
@@ -127,25 +163,25 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 			}
 		}
 
-		if (!empty($_POST['mail_regd_min'])) {
-			$f_mail_regd_min = trim($_POST['mail_regd_min']);
+		if (!empty($mail_regd_min)) {
+			$f_mail_regd_min = trim($mail_regd_min);
 			$time = mktime(0, 0, 0, substr($f_mail_regd_min, 5, 2), substr($f_mail_regd_min, 8, 2), substr($f_mail_regd_min, 0, 4));
 			if ($time > 0) {
 				$criteria[] = "user_regdate > $time";
 			}
 		}
 
-		if (!empty($_POST['mail_regd_max'])) {
-			$f_mail_regd_max = trim($_POST['mail_regd_max']);
+		if (!empty($mail_regd_max)) {
+			$f_mail_regd_max = trim($mail_regd_max);
 			$time = mktime(0, 0, 0, substr($f_mail_regd_max, 5, 2), substr($f_mail_regd_max, 8, 2), substr($f_mail_regd_max, 0, 4));
 			if ($time > 0) {
 				$criteria[] = "user_regdate < $time";
 			}
 		}
 
-		if (!empty($criteria) || !empty($_POST['mail_to_group'])) {
+		if (!empty($criteria) || !empty($mail_to_group)) {
 			$criteria_object = new icms_db_criteria_Compo();
-			$criteria_object->setStart(@$_POST['mail_start']);
+			$criteria_object->setStart(@$mail_start);
 			$criteria_object->setLimit($limit);
 			foreach ($criteria as $c) {
 				list ($field, $op, $value) = explode(' ', $c);
@@ -154,7 +190,8 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 				$criteria_object->add($crit, 'AND');
 			}
 			$member_handler = icms::handler('icms_member');
-			$groups = empty($_POST['mail_to_group']) ? array() : array_map('intval', $_POST['mail_to_group']);
+			// may not need this since we're now using checkVarArray
+			// $groups = empty($mail_to_group) ? array() : array_map('intval', $mail_to_group);
 			$getusers = $member_handler->getUsersByGroupLink($groups, $criteria_object, TRUE);
 			$count_criteria = $member_handler->getUserCountByGroupLink($groups, $criteria_object);
 			foreach ($getusers as $getuser) {
@@ -165,8 +202,8 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 			}
 		}
 
-		if (!empty($_POST['mail_to_user'])) {
-			foreach ($_POST['mail_to_user'] as $to_user) {
+		if (!empty($mail_to_user)) {
+			foreach ($mail_to_user as $to_user) {
 				if (!in_array($to_user, $added_id)) {
 					$added[] = new icms_member_user_Object($to_user);
 					$added_id[] = $to_user;
@@ -183,14 +220,14 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 				$xoopsMailer->setToUsers($added[$i]);
 			}
 
-			$xoopsMailer->setFromName(icms_core_DataFilter::stripSlashesGPC($_POST['mail_fromname']));
-			$xoopsMailer->setFromEmail(icms_core_DataFilter::stripSlashesGPC($_POST['mail_fromemail']));
-			$xoopsMailer->setSubject(icms_core_DataFilter::stripSlashesGPC($_POST['mail_subject']));
-			$xoopsMailer->setBody(icms_core_DataFilter::stripSlashesGPC($_POST['mail_body']));
-			if (in_array("mail", $_POST['mail_send_to'])) {
+			$xoopsMailer->setFromName(icms_core_DataFilter::stripSlashesGPC($mail_fromname));
+			$xoopsMailer->setFromEmail(icms_core_DataFilter::stripSlashesGPC($mail_fromemail));
+			$xoopsMailer->setSubject(icms_core_DataFilter::stripSlashesGPC($mail_subject));
+			$xoopsMailer->setBody(icms_core_DataFilter::stripSlashesGPC($mail_body));
+			if (in_array("mail", $mail_send_to)) {
 				$xoopsMailer->useMail();
 			}
-			if (in_array("pm", $_POST['mail_send_to']) && empty($_POST['mail_inactive'])) {
+			if (in_array("pm", $mail_send_to) && empty($mail_inactive)) {
 				$xoopsMailer->usePM();
 			}
 
@@ -200,28 +237,28 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 
 			if ($count_criteria > $limit) {
 				$form = new icms_form_Theme(_AM_SENDMTOUSERS, "mailusers", "admin.php?fct=mailusers", 'post', TRUE);
-				if (!empty($_POST['mail_to_group'])) {
-					foreach ($_POST['mail_to_group'] as $mailgroup) {
+				if (!empty($mail_to_group)) {
+					foreach ($mail_to_group as $mailgroup) {
 						$group_hidden = new icms_form_elements_Hidden("mail_to_group[]", $mailgroup);
 						$form->addElement($group_hidden);
 					}
 				}
-				$inactive_hidden = new icms_form_elements_Hidden("mail_inactive", @$_POST['mail_inactive']);
-				$lastlog_min_hidden = new icms_form_elements_Hidden("mail_lastlog_min", icms_core_DataFilter::checkVar($_POST['mail_lastlog_min'], 'text'));
-				$lastlog_max_hidden = new icms_form_elements_Hidden("mail_lastlog_max", icms_core_DataFilter::checkVar($_POST['mail_lastlog_max'], 'text'));
-				$regd_min_hidden = new icms_form_elements_Hidden("mail_regd_min", icms_core_DataFilter::checkVar($_POST['mail_regd_min'], 'text'));
-				$regd_max_hidden = new icms_form_elements_Hidden("mail_regd_max", icms_core_DataFilter::checkVar($_POST['mail_regd_max'], 'text'));
-				$idle_more_hidden = new icms_form_elements_Hidden("mail_idle_more", icms_core_DataFilter::checkVar($_POST['mail_idle_more'], 'text'));
-				$idle_less_hidden = new icms_form_elements_Hidden("mail_idle_less", icms_core_DataFilter::checkVar($_POST['mail_idle_less'], 'text'));
-				$fname_hidden = new icms_form_elements_Hidden("mail_fromname", icms_core_DataFilter::checkVar($_POST['mail_fromname'], 'text'));
-				$femail_hidden = new icms_form_elements_Hidden("mail_fromemail", icms_core_DataFilter::checkVar($_POST['mail_fromemail'], 'text'));
-				$subject_hidden = new icms_form_elements_Hidden("mail_subject", icms_core_DataFilter::checkVar($_POST['mail_subject'], 'text'));
-				$body_hidden = new icms_form_elements_Hidden("mail_body", icms_core_DataFilter::checkVar($_POST['mail_body'], 'text'));
-				$start_hidden = new icms_form_elements_Hidden("mail_start", (int) $_POST['mail_start'] + $limit);
-				$mail_mailok_hidden = new icms_form_elements_Hidden("mail_mailok", icms_core_DataFilter::checkVar(@$_POST['mail_mailok']));
+				$inactive_hidden = new icms_form_elements_Hidden("mail_inactive", @$mail_inactive);
+				$lastlog_min_hidden = new icms_form_elements_Hidden("mail_lastlog_min", $mail_lastlog_min, 'text');
+				$lastlog_max_hidden = new icms_form_elements_Hidden("mail_lastlog_max", $mail_lastlog_max, 'text');
+				$regd_min_hidden = new icms_form_elements_Hidden("mail_regd_min", $mail_regd_min, 'text');
+				$regd_max_hidden = new icms_form_elements_Hidden("mail_regd_max", $mail_regd_max, 'text');
+				$idle_more_hidden = new icms_form_elements_Hidden("mail_idle_more", $mail_idle_more, 'text');
+				$idle_less_hidden = new icms_form_elements_Hidden("mail_idle_less", $mail_idle_less, 'text');
+				$fname_hidden = new icms_form_elements_Hidden("mail_fromname", $mail_fromname, 'text');
+				$femail_hidden = new icms_form_elements_Hidden("mail_fromemail", $mail_fromemail, 'text');
+				$subject_hidden = new icms_form_elements_Hidden("mail_subject", $mail_subject, 'text');
+				$body_hidden = new icms_form_elements_Hidden("mail_body", $mail_body, 'text');
+				$start_hidden = new icms_form_elements_Hidden("mail_start", $mail_start + $limit);
+				$mail_mailok_hidden = new icms_form_elements_Hidden("mail_mailok", $mail_mailok);
 				$op_hidden = new icms_form_elements_Hidden("op", "send");
 				$submit_button = new icms_form_elements_Button("", "mail_submit", _AM_SENDNEXT, "submit");
-				$sent_label = new icms_form_elements_Label(_AM_SENT, sprintf(_AM_SENTNUM, (int) $_POST['mail_start'] + 1, (int) $_POST['mail_start'] + $limit, $count_criteria + $added_count - $limit));
+				$sent_label = new icms_form_elements_Label(_AM_SENT, sprintf(_AM_SENTNUM, $mail_start + 1, $mail_start + $limit, $count_criteria + $added_count - $limit));
 				$form->addElement($sent_label);
 				$form->addElement($inactive_hidden);
 				$form->addElement($lastlog_min_hidden);
@@ -237,8 +274,8 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 				$form->addElement($op_hidden);
 				$form->addElement($start_hidden);
 				$form->addElement($mail_mailok_hidden);
-				if (isset($_POST['mail_send_to']) && is_array($_POST['mail_send_to'])) {
-					foreach ($_POST['mail_send_to'] as $v) {
+				if (isset($mail_send_to) && is_array($mail_send_to)) {
+					foreach ($mail_send_to as $v) {
 						$form->addElement(new icms_form_elements_Hidden("mail_send_to[]", $v));
 					}
 				} else {
@@ -256,4 +293,3 @@ if (!is_object(icms::$user) || !is_object($icmsModule) || !icms::$user->isAdmin(
 		icms_cp_footer();
 	}
 }
-
