11c20653f062d9e827a25bd0189bef3df9496956
David Janssens <david.j@impresscms.org>
2022-09-23 18:12:09 +0200

    Update FilesystemServiceProvider.php
    make it behave better when there are symbolic links in the file system : just skip them

--- core/Providers/FilesystemServiceProvider.php
+++ core/Providers/FilesystemServiceProvider.php
@@ -3,7 +3,7 @@
 namespace ImpressCMS\Core\Providers;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
-use League\Flysystem\Adapter\Local;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
 use League\Flysystem\Filesystem;
 use League\Flysystem\Local\LocalFilesystemAdapter;
 
@@ -16,60 +16,119 @@ class FilesystemServiceProvider extends AbstractServiceProvider
 	/**
 	 * @inheritdoc
 	 */
-	protected $provides = [
-		'filesystem.root',
-		'filesystem.cache',
-		'filesystem.modules',
-		'filesystem.uploads',
-		'filesystem.themes',
-		'filesystem.public',
-		'filesystem.compiled',
-		'filesystem.libraries',
-	];
-
-	/**
-	 * @inheritdoc
-	 */
 	public function register()
+		// TODO: make this more compact by using an iterator over the $provides array
 	{
 		$this->getContainer()->add('filesystem.root', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_ROOT_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.cache', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_CACHE_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.modules', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_MODULES_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.uploads', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_UPLOAD_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.themes', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_THEME_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.public', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_PUBLIC_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.compiled', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_COMPILE_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 		$this->getContainer()->add('filesystem.libraries', function () {
 			return new Filesystem(
-				new LocalFilesystemAdapter(ICMS_LIBRARIES_PATH)
+				new LocalFilesystemAdapter(ICMS_ROOT_PATH, PortableVisibilityConverter::fromArray([
+					'file' => [
+						'public' => 0640,
+						'private' => 0604,
+					],
+					'dir' => [
+						'public' => 0740,
+						'private' => 7604,
+					],
+				]),LOCK_EX, LocalFilesystemAdapter::SKIP_LINKS)
 			);
 		});
 	}
