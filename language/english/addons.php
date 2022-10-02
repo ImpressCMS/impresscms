14aceb754049bb3aece2bd4de4528b21d931a415
Raimondas Rimkevičius <github@mekdrop.name>
2022-10-03 01:26:45 +0300

    Added new addons translation namespace
    Moved some language strings from the system module to separate the namespace in the core. Related to ImpressCMS/system-module#12

---
+++ language/english/addons.php
@@ -0,0 +1,7 @@
+<?php
+
+// Using sometimes when installing/updating/uninstalling addon
+define('ADDONS_COPY_ASSETS_INFO', 'Updating assets...');
+define('ADDONS_COPY_ASSETS_DELETE_OLD', 'Deleting old public assets content...');
+define('ADDONS_COPY_ASSETS_COPYING', 'Copying \'%file%\'...');
+define('ADDONS_COPY_ASSETS_DELETING', 'Deleting assets...');
