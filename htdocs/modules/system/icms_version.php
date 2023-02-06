1d00b388db230bb0f12b7ffebfca1e5e3e629731
David Janssens <david.j@impresscms.org>
2023-02-06 23:20:36 +0100

    Update icms_version.php
    trailing comment block

--- htdocs/modules/system/icms_version.php
+++ htdocs/modules/system/icms_version.php
@@ -94,7 +94,6 @@ $modversion['autotasks'][] = array ('enabled' => TRUE,'name' => _MI_SYSTEM_REMOV
 /**
  * Manual
  */
- */
 $modversion['manual']['wiki'][] = "<a href='https://www.impresscms.org/modules/simplywiki/index.php?page=Administration" . _LANGCODE . "' target='_blank'>" . _LANGNAME . "</a>";
 
 /**
