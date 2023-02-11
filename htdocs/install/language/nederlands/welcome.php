01d523b944fb31e97c1469ec928b9d2e63627d12
David Janssens <david.j@impresscms.org>
2023-02-11 11:55:18 +0100

    Update welcome.php
    translation in Dutch, update minimum requirements

--- htdocs/install/language/nederlands/welcome.php
+++ htdocs/install/language/nederlands/welcome.php
@@ -2,29 +2,29 @@
 // $Id: welcome.php 12227 2013-07-19 08:07:21Z fiammy $
 $content .= '
 <p>
-	ImpressCMS is a Web publishing system written in PHP. It is an ideal tool for
-	developing small to large dynamic community websites, intra company portals, corporate portals, weblogs and much more.
+	ImpressCMS is een Website beheer systeem geschreven in PHP. Het is open source en gratis in gebruik. Het is de ideale tool om kleine tot middelgrote dynamische websites te ontwikkelen zoals bijvoorbeeld
+	community websites, intranets, bedrijfssites, blogs en veel meer.
 </p>
 <p>
-	ImpressCMS is released under the terms of the
-	<a href="http://www.gnu.org/copyleft/gpl.html" rel="external">GNU General Public License (GPL)</a>
-	and is free to use and modify.
-	It is free to redistribute as long as you abide by the distribution terms of the GPL.
+	ImpressCMS wordt ter beschikking gesteld onder de 
+	<a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#top" rel="external">GNU General Public License (GPL) v2</a>
+	en is vrij om te gebruiken en aan te passen.
+	Het is vrij om te herverdelen zolang de termen van de GPLv2 worden gevolgd.
 </p>
-<h3>Requirements</h3>
+<h3>Vereisten</h3>
 <ul>
-	<li>- WWW Server: <a href="http://www.apache.org/" rel="external">Apache</a>, NGinx, IIS, Roxen, etc</li>
-	<li>- Script: <a href="http://www.php.net/" rel="external">PHP</a> 5.5+ and 16mb minimum memory allocation - PHP7 not yet supported!</li>
-	<li>- Database: <a href="http://www.mysql.com/" rel="external">MySQL</a> 4.1.0 or higher, <a href="https://mariadb.org/" rel="external">MariaDB</a> 5.1 or higher</li>
+	<li>- Web Server: <a href="http://www.apache.org/" rel="external">Apache</a>, NGinx, IIS, Roxen, etc</li>
+	<li>- Script: <a href="http://www.php.net/" rel="external">PHP</a> 7.0+ en minstens 16MB beschikbaar geheugen.</li>
+	<li>- Database: <a href="http://www.mysql.com/" rel="external">MySQL</a> 4.1.0 of nieuwer, <a href="https://mariadb.org/" rel="external">MariaDB</a> 5.1 of nieuwer</li>
 </ul>
-<h3>Before you install</h3>
+<h3>Voorbereiding voor de installatie</h3>
 <ul>
-	<li>Setup the web server, PHP and database server properly.</li>
-	<li>Prepare a database for ImpressCMS. This can be an existing database as well as a newly created one.</li>
-	<li>Prepare a user account and grant this user access to the database (all rights).</li>
-	<li>Make the directories of uploads/, cache/, templates_c/, modules/ writable (chmod 777 or 755 - depending on servers).</li>
-	<li>Make the file mainfile.php writable (chmod 666 depending on server).</li>
-	<li>In your internet browser settings turn on the allowance of cookies and JavaScript.</li>
+	<li>Zorg voor een werkende instalatie van de web server, PHP en de database server.</li>
+	<li>Bereid een database voor die ImpressCMS kan gebruiker. Dit kan een bestaande of een nieuwe database zijn.</li>
+	<li>Geef op de database server een gebruiker de nodige rechten tot die database (alle rechten).</li>
+	<li>Geef schrijfrechten aan de webserver aan de mappen uploads/, cache/, templates_c/, modules/ (chmod 777 of 755 - afhankelijk van de server).</li>
+	<li>Geef schrijfrechten tot het bestand mainfile.php (chmod 666 afhankelijk van de server).</li>
+	<li>Zorg ervoor dat cookies en javascript toegelaten zijn in uw browser.</li>
 </ul>
 ';
 ?>
