e4ddd82c88a9534d940a0105e74e8dfd981f2ad2
Steve Kenow <skenow@impresscms.org>
2023-03-09 19:16:52 -0600

    PHP4 method name matched class name no longer valid

--- htdocs/editors/dhtmltextarea/dhtmltextarea.php
+++ htdocs/editors/dhtmltextarea/dhtmltextarea.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * FCKeditor adapter for XOOPS
+ * DHTML editor for XOOPS
  *
  * @copyright The XOOPS project http://www.xoops.org/
  * @license http://www.fsf.org/copyleft/gpl.html GNU public license
  * @author Taiwen Jiang (phppp or D.J.) <php_pp@hotmail.com>
  * @since 4.00
- * @version $Id: dhtmltextarea.php 1686 2008-04-19 14:33:00Z malanciault $
  * @package xoopseditor
  */
 if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
@@ -25,7 +24,7 @@ class FormDhtmlTextArea extends icms_form_elements_Dhtmltextarea {
 	 * @param array $configs Editor Options
 	 * @param binary $checkCompatible true - return false on failure
 	 */
-	function FormDhtmlTextArea($configs, $checkCompatible = false) {
+	function __construct($configs, $checkCompatible = false) {
 		if (!empty($configs)) {
 			foreach ($configs as $key => $val) {
 				${$key} = $val;
