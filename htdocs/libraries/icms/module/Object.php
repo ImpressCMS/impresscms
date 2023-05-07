7b9800b27f3e9b9201992f0c8942f09e8a11e00a
Steve Kenow <skenow@impresscms.org>
2023-05-07 18:24:26 -0500

    Replaced$icmsModule with icms::$module to fix fatal error

--- htdocs/libraries/icms/module/Object.php
+++ htdocs/libraries/icms/module/Object.php
@@ -1,59 +1,61 @@
 <?php
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
+// ------------------------------------------------------------------------ //
+// XOOPS - PHP Content Management System //
+// Copyright (c) 2000 XOOPS.org //
+// <http://www.xoops.org/> //
+// ------------------------------------------------------------------------ //
+// This program is free software; you can redistribute it and/or modify //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation; either version 2 of the License, or //
+// (at your option) any later version. //
+// //
+// You may not change or alter any portion of this comment or credits //
+// of supporting developers from this source code or any supporting //
+// source code which is considered copyrighted (c) material of the //
+// original comment or credit authors. //
+// //
+// This program is distributed in the hope that it will be useful, //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the //
+// GNU General Public License for more details. //
+// //
+// You should have received a copy of the GNU General Public License //
+// along with this program; if not, write to the Free Software //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+// ------------------------------------------------------------------------ //
+// Author: Kazumi Ono (AKA onokazu) //
 // URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
+// Project: The XOOPS Project //
 // ------------------------------------------------------------------------- //
 /**
  * Manage of modules
  *
-
- * @copyright	http://www.impresscms.org/ The ImpressCMS Project
- * @license		LICENSE.txt
- * @category	ICMS
- * @package		Module
- * @version		SVN: $Id: Object.php 12313 2013-09-15 21:14:35Z skenow $
+ *
+ * @copyright http://www.impresscms.org/ The ImpressCMS Project
+ * @license LICENSE.txt
+ * @category ICMS
+ * @package Module
+ * @version SVN: $Id: Object.php 12313 2013-09-15 21:14:35Z skenow $
  */
-
 defined('ICMS_ROOT_PATH') or die('ImpressCMS root path is not defined');
+
 /**
  * A Module
  *
- * @category	ICMS
- * @package		Module
- * @author		Kazumi Ono 	<onokazu@xoops.org>
- * @copyright	Copyright (c) 2000 XOOPS.org
- **/
+ * @category ICMS
+ * @package Module
+ * @author Kazumi Ono <onokazu@xoops.org>
+ * @copyright Copyright (c) 2000 XOOPS.org
+ */
 class icms_module_Object extends icms_core_Object {
 	/**
 	 * Module configuration
+	 *
 	 * @var array
 	 */
 	public $config = NULL;
 	/**
+	 *
 	 * @var string
 	 */
 	public $modinfo;
@@ -104,17 +106,17 @@ class icms_module_Object extends icms_core_Object {
 	 * Initializes the module/application.
 	 * This method is called during execution of icms::launchModule() to allow the module
 	 * to setup its global structures.
+	 *
 	 * @return void
-	 **/
-	public function launch() {
-	}
+	 */
+	public function launch() {}
 
 	/**
 	 * register class path with autoloader
 	 * notice: this function may not be used for the system module
 	 *
-	 * @param	bool	$isactive	if TRUE, the class path is only registered if the module is active
-	 *								if FALSE, the class path is only registered if the module is inactive
+	 * @param bool $isactive if TRUE, the class path is only registered if the module is active
+	 *        if FALSE, the class path is only registered if the module is inactive
 	 * @return void
 	 */
 	public function registerClassPath($isactive = NULL) {
@@ -129,8 +131,7 @@ class icms_module_Object extends icms_core_Object {
 
 		// register class path
 		if ($this->getVar("ipf")) {
-			$modname = ($this->getVar("modname") != "") ?
-				$this->getVar("modname") : $this->getVar("dirname");
+			$modname = ($this->getVar("modname") != "") ? $this->getVar("modname") : $this->getVar("dirname");
 			icms_Autoloader::register($class_path, "mod_" . $modname);
 		} else {
 			icms_Autoloader::register($class_path);
@@ -140,13 +141,15 @@ class icms_module_Object extends icms_core_Object {
 	/**
 	 * Load module info
 	 *
-	 * @param   string  $dirname    Directory Name
-	 * @param   boolean $verbose
-	 **/
+	 * @param string $dirname Directory Name
+	 * @param boolean $verbose
+	 */
 	public function loadInfoAsVar($dirname, $verbose = true) {
-		if (!isset($this->modinfo)) {$this->loadInfo($dirname, $verbose);}
+		if (!isset($this->modinfo)) {
+			$this->loadInfo($dirname, $verbose);
+		}
 		$this->setVar('name', $this->modinfo['name'], true);
-		$this->setVar('version', $this->modinfo['version'] , true);
+		$this->setVar('version', $this->modinfo['version'], true);
 		$this->setVar('dirname', $this->modinfo['dirname'], true);
 		$hasmain = (isset($this->modinfo['hasMain']) && $this->modinfo['hasMain'] == 1) ? 1 : 0;
 		$hasadmin = (isset($this->modinfo['hasAdmin']) && $this->modinfo['hasAdmin'] == 1) ? 1 : 0;
@@ -170,14 +173,18 @@ class icms_module_Object extends icms_core_Object {
 	/**
 	 * Get module info
 	 *
-	 * @param   string  	$name
-	 * @return  array|string	Array of module information.
-	 * If {@link $name} is set, returns a single module information item as string.
-	 **/
+	 * @param string $name
+	 * @return array|string Array of module information.
+	 *         If {@link $name} is set, returns a single module information item as string.
+	 */
 	public function &getInfo($name = null) {
-		if (!isset($this->modinfo)) {$this->loadInfo($this->getVar('dirname'));}
+		if (!isset($this->modinfo)) {
+			$this->loadInfo($this->getVar('dirname'));
+		}
 		if (isset($name)) {
-			if (isset($this->modinfo[$name])) {return $this->modinfo[$name];}
+			if (isset($this->modinfo[$name])) {
+				return $this->modinfo[$name];
+			}
 			$return = false;
 			return $return;
 		}
@@ -188,7 +195,7 @@ class icms_module_Object extends icms_core_Object {
 	 * Retreive the database version of this module
 	 *
 	 * @return int dbversion
-	 **/
+	 */
 	public function getDBVersion() {
 		$ret = $this->getVar('dbversion');
 		return $ret;
@@ -197,7 +204,7 @@ class icms_module_Object extends icms_core_Object {
 	/**
 	 * Get a link to the modules main page
 	 *
-	 * @return	string $ret or FALSE on fail
+	 * @return string $ret or FALSE on fail
 	 */
 	public function mainLink() {
 		if ($this->getVar('hasmain') == 1) {
@@ -210,7 +217,7 @@ class icms_module_Object extends icms_core_Object {
 	/**
 	 * Get links to the subpages
 	 *
-	 * @return	string $ret
+	 * @return string $ret
 	 */
 	public function subLink() {
 		$ret = array();
@@ -228,37 +235,43 @@ class icms_module_Object extends icms_core_Object {
 	public function loadAdminMenu() {
 		if ($this->getInfo('adminmenu') && $this->getInfo('adminmenu') != '' && file_exists(ICMS_ROOT_PATH . '/modules/' . $this->getVar('dirname') . '/' . $this->getInfo('adminmenu'))) {
 			include_once ICMS_ROOT_PATH . '/modules/' . $this->getVar('dirname') . '/' . $this->getInfo('adminmenu');
-			$this->adminmenu = & $adminmenu;
-			if (isset($headermenu)) {$this->adminheadermenu = & $headermenu;}
+			$this->adminmenu = &$adminmenu;
+			if (isset($headermenu)) {
+				$this->adminheadermenu = &$headermenu;
+			}
 		}
 	}
 
 	/**
 	 * Get the admin menu for the module
 	 *
-	 * @return	string $this->adminmenu
+	 * @return string $this->adminmenu
 	 */
 	public function &getAdminMenu() {
-		if (!isset($this->adminmenu)) {$this->loadAdminMenu();}
+		if (!isset($this->adminmenu)) {
+			$this->loadAdminMenu();
+		}
 		return $this->adminmenu;
 	}
 
 	/**
 	 * Get the admin header menu for the module
 	 *
-	 * @return	string $this->adminmenu
+	 * @return string $this->adminmenu
 	 */
 	public function &getAdminHeaderMenu() {
-		if (!isset($this->adminheadermenu)) {$this->loadAdminMenu();}
+		if (!isset($this->adminheadermenu)) {
+			$this->loadAdminMenu();
+		}
 		return $this->adminheadermenu;
 	}
 
 	/**
 	 * Load the module info for this module
 	 *
-	 * @param   string  $dirname    Module directory
-	 * @param   bool    $verbose    Give an error on fail?
-	 * @return  bool   TRUE if success, FALSE if fail.
+	 * @param string $dirname Module directory
+	 * @param bool $verbose Give an error on fail?
+	 * @return bool TRUE if success, FALSE if fail.
 	 */
 	public function loadInfo($dirname, $verbose = true) {
 		global $icmsConfig;
@@ -268,26 +281,30 @@ class icms_module_Object extends icms_core_Object {
 		} elseif (file_exists(ICMS_ROOT_PATH . '/modules/' . $dirname . '/xoops_version.php')) {
 			include ICMS_ROOT_PATH . '/modules/' . $dirname . '/xoops_version.php';
 		} else {
-			if (false != $verbose) {echo "Module File for $dirname Not Found!";}
+			if (false != $verbose) {
+				echo "Module File for $dirname Not Found!";
+			}
 			return false;
 		}
-		$this->modinfo =& $modversion;
+		$this->modinfo = &$modversion;
 		return true;
 	}
 
 	/**
 	 * Search contents within a module
 	 *
-	 * @param   string  $term
-	 * @param   string  $andor  'AND' or 'OR'
-	 * @param   integer $limit
-	 * @param   integer $offset
-	 * @param   integer $userid
-	 * @return  mixed   Search result or False if fail.
-	 **/
+	 * @param string $term
+	 * @param string $andor 'AND' or 'OR'
+	 * @param integer $limit
+	 * @param integer $offset
+	 * @param integer $userid
+	 * @return mixed Search result or False if fail.
+	 */
 	public function search($term = '', $andor = 'AND', $limit = 0, $offset = 0, $userid = 0) {
-		if ($this->getVar('hassearch') != 1) {return false;}
-		$search = & $this->getInfo('search');
+		if ($this->getVar('hassearch') != 1) {
+			return false;
+		}
+		$search = &$this->getInfo('search');
 		if ($this->getVar('hassearch') != 1 || !isset($search['file']) || !isset($search['func']) || $search['func'] == '' || $search['file'] == '') {
 			return false;
 		}
@@ -306,30 +323,26 @@ class icms_module_Object extends icms_core_Object {
 	/**
 	 * Displays the (good old) adminmenu
 	 *
-	 * @param int  $currentoption  The current option of the admin menu
-	 * @param string  $breadcrumb  The breadcrumb trail
-	 * @param bool  $submenus  Show the submenus!
-	 * @param int  $currentsub  The current submenu
+	 * @param int $currentoption The current option of the admin menu
+	 * @param string $breadcrumb The breadcrumb trail
+	 * @param bool $submenus Show the submenus!
+	 * @param int $currentsub The current submenu
 	 *
-	 * @return datatype  description
+	 * @return datatype description
 	 */
 	public function displayAdminMenu($currentoption = 0, $breadcrumb = '', $submenus = false, $currentsub = -1) {
-		global $icmsModule, $icmsConfig;
-		icms_loadLanguageFile($icmsModule->getVar('dirname'), 'modinfo');
-		icms_loadLanguageFile($icmsModule->getVar('dirname'), 'admin');
+		icms_loadLanguageFile(icms::$module->getVar('dirname'), 'modinfo');
+		icms_loadLanguageFile(icms::$module->getVar('dirname'), 'admin');
 		$tpl = new icms_view_Tpl();
-		$tpl->assign(
-			array(
-				'headermenu' => $this->getAdminHeaderMenu(),
-				'adminmenu' => $this->getAdminMenu(),
-				'current' => $currentoption,
-				'breadcrumb' => $breadcrumb,
-				'headermenucount' => count($this->getAdminHeaderMenu()),
-				'submenus' => $submenus,
-				'currentsub' => $currentsub,
-				'submenuscount' => count($submenus)
-			)
-		);
+		$tpl->assign(array(
+			'headermenu' => $this->getAdminHeaderMenu(),
+			'adminmenu' => $this->getAdminMenu(),
+			'current' => $currentoption,
+			'breadcrumb' => $breadcrumb,
+			'headermenucount' => count($this->getAdminHeaderMenu()),
+			'submenus' => $submenus,
+			'currentsub' => $currentsub,
+			'submenuscount' => count($submenus)));
 		$tpl->display('db:system_adm_modulemenu.html');
 	}
 
@@ -339,17 +352,21 @@ class icms_module_Object extends icms_core_Object {
 	 * @since ImpressCMS 1.2
 	 * @author Sina Asghari (aka stranger) <stranger@impresscms.org>
 	 *
-	 * @param string $msg	The Error Message
-	 * @param string $title	The Error Message title
-	 * @param	bool	$render	Whether to echo (render) or return the HTML string
+	 * @param string $msg The Error Message
+	 * @param string $title The Error Message title
+	 * @param bool $render Whether to echo (render) or return the HTML string
 	 *
 	 * @todo Make this work with templates ;)
 	 */
 	function setMessage($msg, $title = '', $render = false) {
 		$ret = '<div class="moduleMsg">';
-		if ($title != '') {$ret .= '<h4>' . $title . '</h4>';}
+		if ($title != '') {
+			$ret .= '<h4>' . $title . '</h4>';
+		}
 		if (is_array($msg)) {
-			foreach ($msg as $m) {$ret .= $m . '<br />';}
+			foreach ($msg as $m) {
+				$ret .= $m . '<br />';
+			}
 		} else {
 			$ret .= $msg;
 		}
