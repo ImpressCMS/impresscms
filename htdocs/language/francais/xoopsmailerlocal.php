bb45a7e5c34a981354416b90445512f452abf82f
Steve Kenow <skenow@impresscms.org>
2023-05-24 20:53:50 -0500

    Update PHPMailer to 6.8.0 for compatibility on PHP7 - PHP8.2

--- htdocs/language/francais/xoopsmailerlocal.php
+++ htdocs/language/francais/xoopsmailerlocal.php
@@ -1,28 +1,27 @@
 <?php
+
 // fonctions mail
-//traduction CPascalWeb
+// traduction CPascalWeb
 // Ne pas modifiez le nom de la classe
 class XoopsMailerLocal extends icms_messaging_EmailHandler {
 
-	function __construct(){
+	function __construct() {
 		parent::__construct();
 		// Il est pas n�cessaire de modifier le jeu de caract�res
-		$this->charSet = strtolower( _CHARSET );
-		//  indiquer le code de langue ainsi que le fichier existe: XOOPS_ROOT_PAT/class/mail/phpmailer/language/lang-["your-language-code"].php
-	    $this->multimailer->SetLanguage("fr");
+		$this->charSet = strtolower(_CHARSET);
+		// indiquer le code de langue ainsi que le fichier existe: ICMS_ROOT_PATH/libraries/phpmailer/language/phpmailer.lang-["your-language-code"].php
+		$this->multimailer->SetLanguage("fr");
 	}
-	
+
 	// Multi-langues l'encodage du nom
-	function encodeFromName($text)
-	{
+	function encodeFromName($text) {
 		// Activez la ligne suivante si n�cessaire
 		// $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
 		return $text;
 	}
 
 	// Multi-langues l'encodage Sujet
-	function encodeSubject($text)
-	{
+	function encodeSubject($text) {
 		// Activez la ligne suivante si n�cessaire
 		// $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
 		return $text;
