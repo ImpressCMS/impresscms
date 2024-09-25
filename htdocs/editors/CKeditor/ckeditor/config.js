95432c031085c00310762e1b1af3a4139a2783a9
David Janssens <fiammybe@gmail.com>
2024-09-25 14:52:55 +0200

    hack to remove the red nagging notice that this version if out of date.

--- htdocs/editors/CKeditor/ckeditor/config.js
+++ htdocs/editors/CKeditor/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -35,4 +35,6 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.versionCheck = false;
 };
