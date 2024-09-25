34e5ecda51fd11dc5d34e87dc130bad6fec1b774
David Janssens <david.j@impresscms.org>
2024-09-25 09:10:09 +0200

    version update to CKEditor 4.25 lts

--- htdocs/editors/CKeditor/icms_version.php
+++ htdocs/editors/CKeditor/icms_version.php
@@ -5,14 +5,14 @@
  * @copyright	The ImpressCMS Project http://www.impresscms.org/
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @package		core
- * @since		2.0 alpha 5
- * @author		Steve K <skenow@impresscms.org>
+ * @since		2.0 beta 2
+ * @author		fiammybe <david.j@impresscms.org>
  * @version		$Id$
  */
 
 $editorversion = array(
-		'name' => "CKEditor 4.5.3",
-		'version' => "4.5.3",
+		'name' => "CKEditor 4.25.0-lts",
+		'version' => "4.25.0",
 		'license' => "GPL see LICENSE",
 		'dirname' => "CKeditor",
 		'class' => "icmsFormCKEditor",
