3726c66e40361f71bf3dee59f68e481f9414f011
Steve Kenow <skenow@impresscms.org>
2024-11-24 16:41:55 -0600

    Updating to icms::$module for the MediaUploadHandler class

--- htdocs/libraries/icms/file/MediaUploadHandler.php
+++ htdocs/libraries/icms/file/MediaUploadHandler.php
@@ -514,9 +514,8 @@ class icms_file_MediaUploadHandler {
 	 * @return  bool
 	 */
 	public function checkMimeType() {
-		global $icmsModule;
 		$mimetypeHandler = icms_getModulehandler('mimetype', 'system');
-		$modulename = (isset($icmsModule) && is_object($icmsModule)) ? $icmsModule->getVar('dirname') : 'system';
+		$modulename = (isset(icms::$module) && is_object(icms::$module)) ? icms::$module->getVar('dirname') : 'system';
 		if (empty($this->mediaRealType) && empty($this->allowUnknownTypes)) {
 			self::setErrors(_ER_UP_UNKNOWNFILETYPEREJECTED);
 			return false;
