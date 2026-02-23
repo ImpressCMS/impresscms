ac290c773c989640c3cb072736d5363ab233356d
David Janssens <fiammybe@gmail.com>
2026-02-23 16:49:15 +0100

    Update Handler.php

--- htdocs/libraries/icms/messaging/Handler.php
+++ htdocs/libraries/icms/messaging/Handler.php
@@ -40,7 +40,7 @@
 
 defined("ICMS_ROOT_PATH") or die("ImpressCMS root path not defined");
 
-icms_loadLanguageFile('core', 'mail');
+icms_loadLanguageFile("core", "mail");
 
 /**
  * Class for sending messages.
@@ -51,7 +51,8 @@ icms_loadLanguageFile('core', 'mail');
  * @category	ICMS
  * @package		Messaging
  */
-class icms_messaging_Handler {
+class icms_messaging_Handler
+{
 	/**
 	 * reference to a {@link icms_messaging_EmailHandler}
 	 * @var		icms_messaging_EmailHandler
@@ -82,12 +83,6 @@ class icms_messaging_Handler {
 	// body of mail
 	private $body;
 
-	// whether to send as HTML email
-	private $isHtml;
-
-	// plain-text fallback body for HTML emails
-	private $altBody;
-
 	// error messages
 	private $errors;
 
@@ -110,98 +105,101 @@ class icms_messaging_Handler {
 	// line ending character
 	private $LE;
 
-	protected $charSet = 'utf-8';
+	protected $charSet = "utf-8";
 
-	protected $encoding = '8bit';
+	protected $encoding = "8bit";
 
-	public function __construct() {
-		icms_loadLanguageFile('core', 'xoopsmailerlocal');
-		if (class_exists('XoopsMailerLocal')) {
+	public function __construct()
+	{
+		icms_loadLanguageFile("core", "xoopsmailerlocal");
+		if (class_exists("XoopsMailerLocal")) {
 			$this->multimailer = new XoopsMailerLocal();
 		} else {
-			$this->multimailer = new icms_messaging_EmailHandler();
+			$this->multimailer = new icms_messaging_Handler();
 		}
 		$this->reset();
 	}
 
 	// reset all properties to default
-	public function reset() {
+	public function reset()
+	{
 		$this->fromEmail = "";
 		$this->fromName = "";
 		$this->fromUser = null;
-		$this->priority = '';
-		$this->toUsers = array();
-		$this->toEmails = array();
-		$this->headers = array();
+		$this->priority = "";
+		$this->toUsers = [];
+		$this->toEmails = [];
+		$this->headers = [];
 		$this->subject = "";
 		$this->body = "";
-		$this->isHtml = false;
-		$this->altBody = '';
-		$this->errors = array();
-		$this->success = array();
+		$this->errors = [];
+		$this->success = [];
 		$this->isMail = false;
 		$this->isPM = false;
-		$this->assignedTags = array();
+		$this->assignedTags = [];
 		$this->template = "";
 		$this->templatedir = "";
 		// Change below to \r\n if you have problem sending mail
-		$this->LE ="\n";
+		$this->LE = "\n";
 	}
 
-	public function setTemplateDir($value) {
+	public function setTemplateDir($value)
+	{
 		if (substr($value, -1, 1) != "/") {
 			$value .= "/";
 		}
 		$this->templatedir = $value;
 	}
 
-	public function setTemplate($value) {
+	public function setTemplate($value)
+	{
 		$this->template = $value;
 	}
 
-	public function setFromEmail($value) {
+	public function setFromEmail($value)
+	{
 		$this->fromEmail = trim($value);
 	}
 
-	public function setFromName($value) {
+	public function setFromName($value)
+	{
 		$this->fromName = trim($value);
 	}
 
-	public function setFromUser(&$user) {
+	public function setFromUser(&$user)
+	{
 		if (get_class($user) == "icms_member_user_Object") {
-			$this->fromUser =& $user;
+			$this->fromUser = &$user;
 		}
 	}
 
-	public function setPriority($value) {
+	public function setPriority($value)
+	{
 		$this->priority = trim($value);
 	}
 
-	public function setSubject($value) {
+	public function setSubject($value)
+	{
 		$this->subject = trim($value);
 	}
 
-	public function setBody($value) {
+	public function setBody($value)
+	{
 		$this->body = trim($value);
 	}
 
-	public function setAltBody($value) {
-		$this->altBody = trim($value);
-	}
-
-	public function useMail() {
+	public function useMail()
+	{
 		$this->isMail = true;
 	}
 
-	public function usePM() {
+	public function usePM()
+	{
 		$this->isPM = true;
 	}
 
-	public function useHtml($isHtml = true) {
-		$this->isHtml = (bool) $isHtml;
-	}
-
-	public function send($debug = false) {
+	public function send($debug = false)
+	{
 		global $icmsConfig;
 		if ($this->body == "" && $this->template == "") {
 			if ($debug) {
@@ -209,19 +207,15 @@ class icms_messaging_Handler {
 			}
 			return false;
 		} elseif ($this->template != "") {
-			$basedir = ($this->templatedir != "") ? $this->templatedir : (ICMS_ROOT_PATH . "/language/" . $icmsConfig['language'] . "/mail_template/");
-			// Auto-detect HTML mode: if an .html.tpl variant exists, use it and switch to HTML mode.
-			// This means callers never need to call useHtml() explicitly — just provide the template.
-			$htmlTemplate = preg_replace('/\.tpl$/i', '.html.tpl', $this->template);
-			$htmlPath = $basedir . $htmlTemplate;
-			if (is_readable($htmlPath)) {
-				$path = $htmlPath;
-				$this->isHtml = true;
-			} else {
-				$path = $basedir . $this->template;
-				$this->isHtml = false;
-			}
-			if (!($fd = @fopen($path, 'r'))) {
+			$path =
+				$this->templatedir != ""
+					? $this->templatedir . "" . $this->template
+					: ICMS_ROOT_PATH .
+						"/language/" .
+						$icmsConfig["language"] .
+						"/mail_template/" .
+						$this->template;
+			if (!($fd = @fopen($path, "r"))) {
 				if ($debug) {
 					$this->errors[] = _MAIL_FAILOPTPL;
 				}
@@ -231,7 +225,7 @@ class icms_messaging_Handler {
 		}
 
 		// for sending mail only
-		if ($this->isMail  || !empty($this->toEmails)) {
+		if ($this->isMail || !empty($this->toEmails)) {
 			if (!empty($this->priority)) {
 				$this->headers[] = "X-Priority: " . $this->priority;
 			}
@@ -247,9 +241,9 @@ class icms_messaging_Handler {
 
 		// add some standard tags (user-dependent tags are included later)
 		global $icmsConfig;
-		$this->assign ('X_ADMINMAIL', $icmsConfig['adminmail']);
-		$this->assign ('X_SITENAME', $icmsConfig['sitename']);
-		$this->assign ('X_SITEURL', ICMS_URL);
+		$this->assign("X_ADMINMAIL", $icmsConfig["adminmail"]);
+		$this->assign("X_SITENAME", $icmsConfig["sitename"]);
+		$this->assign("X_SITEURL", ICMS_URL);
 		// TODO: also X_ADMINNAME??
 		// TODO: X_SIGNATURE, X_DISCLAIMER ?? - these are probably best
 		//  done as includes if mail templates ever get this sophisticated
@@ -259,17 +253,20 @@ class icms_messaging_Handler {
 			$this->body = str_replace("{" . $k . "}", $v, $this->body);
 			$this->subject = str_replace("{" . $k . "}", $v, $this->subject);
 		}
-		// Plain-text newline normalization must be skipped for HTML emails
-		// because HTML line endings are controlled by tags, not literal newlines
-		if (!$this->isHtml) {
-			$this->body = str_replace("\r\n", "\n", $this->body);
-			$this->body = str_replace("\r", "\n", $this->body);
-			$this->body = str_replace("\n", $this->LE, $this->body);
-		}
+		$this->body = str_replace("\r\n", "\n", $this->body);
+		$this->body = str_replace("\r", "\n", $this->body);
+		$this->body = str_replace("\n", $this->LE, $this->body);
 
 		// send mail to specified mail addresses, if any
 		foreach ($this->toEmails as $mailaddr) {
-			if (!$this->sendMail($mailaddr, $this->subject, $this->body, $headers)) {
+			if (
+				!$this->sendMail(
+					$mailaddr,
+					$this->subject,
+					$this->body,
+					$headers,
+				)
+			) {
 				if ($debug) {
 					$this->errors[] = sprintf(_MAIL_SENDMAILNG, $mailaddr);
 				}
@@ -287,21 +284,50 @@ class icms_messaging_Handler {
 		// receives (potentially) a different message
 		foreach ($this->toUsers as $user) {
 			// set some user specific variables
-			$subject = str_replace("{X_UNAME}", $user->getVar("uname"), $this->subject);
-			$text = str_replace("{X_USERLOGINNAME}", $user->getVar("login_name"), $this->body);
+			$subject = str_replace(
+				"{X_UNAME}",
+				$user->getVar("uname"),
+				$this->subject,
+			);
+			$text = str_replace(
+				"{X_USERLOGINNAME}",
+				$user->getVar("login_name"),
+				$this->body,
+			);
 			$text = str_replace("{X_UID}", $user->getVar("uid"), $text);
 			$text = str_replace("{X_UEMAIL}", $user->getVar("email"), $text);
 			$text = str_replace("{X_UNAME}", $user->getVar("uname"), $text);
-			$text = str_replace("{X_UACTLINK}", ICMS_URL . "/user.php?op=actv&id=".$user->getVar("uid") . "&actkey=".$user->getVar('actkey'), $text);
+			$text = str_replace(
+				"{X_UACTLINK}",
+				ICMS_URL .
+					"/user.php?op=actv&id=" .
+					$user->getVar("uid") .
+					"&actkey=" .
+					$user->getVar("actkey"),
+				$text,
+			);
 			// send mail
 			if ($this->isMail) {
-				if (!$this->sendMail($user->getVar("email"), $subject, $text, $headers)) {
+				if (
+					!$this->sendMail(
+						$user->getVar("email"),
+						$subject,
+						$text,
+						$headers,
+					)
+				) {
 					if ($debug) {
-						$this->errors[] = sprintf(_MAIL_SENDMAILNG, $user->getVar("uname"));
+						$this->errors[] = sprintf(
+							_MAIL_SENDMAILNG,
+							$user->getVar("uname"),
+						);
 					}
 				} else {
 					if ($debug) {
-						$this->success[] = sprintf(_MAIL_MAILGOOD, $user->getVar("uname"));
+						$this->success[] = sprintf(
+							_MAIL_MAILGOOD,
+							$user->getVar("uname"),
+						);
 					}
 				}
 			}
@@ -309,11 +335,17 @@ class icms_messaging_Handler {
 			if ($this->isPM) {
 				if (!$this->sendPM($user->getVar("uid"), $subject, $text)) {
 					if ($debug) {
-						$this->errors[] = sprintf(_MAIL_SENDPMNG, $user->getVar("uname"));
+						$this->errors[] = sprintf(
+							_MAIL_SENDPMNG,
+							$user->getVar("uname"),
+						);
 					}
 				} else {
 					if ($debug) {
-						$this->success[] = sprintf(_MAIL_PMGOOD, $user->getVar("uname"));
+						$this->success[] = sprintf(
+							_MAIL_PMGOOD,
+							$user->getVar("uname"),
+						);
 					}
 				}
 			}
@@ -325,11 +357,17 @@ class icms_messaging_Handler {
 		return true;
 	}
 
-	public function sendPM($uid, $subject, $body) {
-		$pm_handler = icms::handler('icms_data_privmessage');
-		$pm =& $pm_handler->create();
+	public function sendPM($uid, $subject, $body)
+	{
+		$pm_handler = icms::handler("icms_data_privmessage");
+		$pm = &$pm_handler->create();
 		$pm->setVar("subject", $subject);
-		$pm->setVar('from_userid', !empty($this->fromUser) ? $this->fromUser->getVar('uid') : icms::$user->getVar('uid'));
+		$pm->setVar(
+			"from_userid",
+			!empty($this->fromUser)
+				? $this->fromUser->getVar("uid")
+				: icms::$user->getVar("uid"),
+		);
 		$pm->setVar("msg_text", $body);
 		$pm->setVar("to_userid", $uid);
 		if (!$pm_handler->insert($pm)) {
@@ -346,7 +384,8 @@ class icms_messaging_Handler {
 	 * @param	string
 	 * @return	boolean	FALSE on error.
 	 */
-	public function sendMail($email, $subject, $body, $headers) {
+	public function sendMail($email, $subject, $body, $headers)
+	{
 		$subject = $this->multimailer->encodeSubject($subject);
 		$this->multimailer->encodeBody($body);
 		$this->multimailer->ClearAllRecipients();
@@ -356,21 +395,13 @@ class icms_messaging_Handler {
 		$this->multimailer->CharSet = $this->charSet;
 		$this->multimailer->Encoding = $this->encoding;
 		if (!empty($this->fromName)) {
-			$this->multimailer->FromName = $this->multimailer->encodeFromName($this->fromName);
+			$this->multimailer->FromName = $this->multimailer->encodeFromName(
+				$this->fromName,
+			);
 		}
 		if (!empty($this->fromEmail)) {
-			$this->multimailer->Sender = $this->multimailer->From = $this->fromEmail;
-		}
-
-		// Set HTML or plain-text mode. Always call isHTML() explicitly so the
-		// ContentType is correctly reset between successive sends on the same
-		// multimailer instance (PHPMailer does not auto-reset it).
-		if ($this->isHtml) {
-			$this->multimailer->isHTML(true);
-			$this->multimailer->AltBody = $this->altBody;
-		} else {
-			$this->multimailer->isHTML(false);
-			$this->multimailer->AltBody = '';
+			$this->multimailer->Sender = $this->multimailer->From =
+				$this->fromEmail;
 		}
 
 		$this->multimailer->ClearCustomHeaders();
@@ -379,12 +410,13 @@ class icms_messaging_Handler {
 		}
 		if (!$this->multimailer->Send()) {
 			$this->errors[] = $this->multimailer->ErrorInfo;
-			return FALSE;
+			return false;
 		}
-		return TRUE;
+		return true;
 	}
 
-	public function getErrors($ashtml = true) {
+	public function getErrors($ashtml = true)
+	{
 		if (!$ashtml) {
 			return $this->errors;
 		} else {
@@ -400,7 +432,8 @@ class icms_messaging_Handler {
 		}
 	}
 
-	public function getSuccess($ashtml = true) {
+	public function getSuccess($ashtml = true)
+	{
 		if (!$ashtml) {
 			return $this->success;
 		} else {
@@ -414,7 +447,8 @@ class icms_messaging_Handler {
 		}
 	}
 
-	public function assign($tag, $value = null) {
+	public function assign($tag, $value = null)
+	{
 		if (is_array($tag)) {
 			foreach ($tag as $k => $v) {
 				$this->assign($k, $v);
@@ -430,13 +464,20 @@ class icms_messaging_Handler {
 		}
 	}
 
-	public function addHeaders($value) {
+	public function addHeaders($value)
+	{
 		$this->headers[] = trim($value) . $this->LE;
 	}
 
-	public function setToEmails($email) {
+	public function setToEmails($email)
+	{
 		if (!is_array($email)) {
-			if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+([\.][a-z0-9-]+)+$/i", $email)) {
+			if (
+				preg_match(
+					"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+([\.][a-z0-9-]+)+$/i",
+					$email,
+				)
+			) {
 				array_push($this->toEmails, $email);
 			}
 		} else {
@@ -446,7 +487,8 @@ class icms_messaging_Handler {
 		}
 	}
 
-	public function setToUsers(&$user) {
+	public function setToUsers(&$user)
+	{
 		if (!is_array($user)) {
 			if (get_class($user) == "icms_member_user_Object") {
 				array_push($this->toUsers, $user);
@@ -458,11 +500,17 @@ class icms_messaging_Handler {
 		}
 	}
 
-	public function setToGroups($group) {
+	public function setToGroups($group)
+	{
 		if (!is_array($group)) {
 			if (get_class($group) == "icms_member_group_Object") {
-				$member_handler = icms::handler('icms_member');
-				$this->setToUsers($member_handler->getUsersByGroup($group->getVar('groupid'), true));
+				$member_handler = icms::handler("icms_member");
+				$this->setToUsers(
+					$member_handler->getUsersByGroup(
+						$group->getVar("groupid"),
+						true,
+					),
+				);
 			}
 		} else {
 			foreach ($group as $g) {
@@ -470,5 +518,4 @@ class icms_messaging_Handler {
 			}
 		}
 	}
-
 }
