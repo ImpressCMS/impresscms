f77c7ced89169f98a82112b523bd3dea066bb17e
fiammybe <david.j@impresscms.org>
2026-02-25 23:04:12 +0100

    we really, really, really don't want to be cached

--- htdocs/keepalive.php
+++ htdocs/keepalive.php
@@ -14,6 +14,7 @@ if (!is_object(icms::$user) || icms::$user->isGuest()) {
 
 /* ----- 2  Send explicit no‑cache headers ------------- */
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header("Expires: 0");
 
