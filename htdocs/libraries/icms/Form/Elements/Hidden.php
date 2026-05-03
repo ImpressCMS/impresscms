908955f63d1878ea2abf56e48ef35eaae37455a7
fiammybe <david.j@impresscms.org>
2026-05-03 10:43:08 +0200

    Correct inherited string signature

--- htdocs/libraries/icms/Form/Elements/Hidden.php
+++ htdocs/libraries/icms/Form/Elements/Hidden.php
@@ -95,7 +95,7 @@ class icms_form_elements_Hidden extends icms_form_Element {
 	 *
 	 * @return	string	HTML
 	 */
-	public function render() {
+	public function render(): string {
 		if (is_array($this->getValue())) {
 			$ret = '';
 			foreach ($this->getValue() as $value){
